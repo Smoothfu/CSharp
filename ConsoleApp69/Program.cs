52ec53c45303d11fa4146fca2858d8191e9e060e
Fred1987 <2864326836@qq.com>
2017-10-13 20:38:24 +0800

    Type.GetType(specificType)

---
+++ ConsoleApp69/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = Type.GetType("System.String");
+            Console.WriteLine(type.Name);
+            Console.ReadLine();
+        }
+    }
+}
