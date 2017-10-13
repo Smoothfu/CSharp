d9e7d655e3d8ba86ed16ef998e3e1a0773ae2710
Fred1987 <2864326836@qq.com>
2017-10-13 20:57:12 +0800

    (DataTable)Activator.CreateInstance(type)

--- ConsoleApp69/Program.cs
+++ ConsoleApp69/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.Data;
 
 namespace ConsoleApp69
 {
@@ -12,8 +13,7 @@ namespace ConsoleApp69
         static void Main(string[] args)
         {
             Type type = Type.GetType("System.Data.DataTable,System.Data,Version=1.0.3300.0,  Culture=neutral,  PublicKeyToken=b77a5c561934e089");
-            Console.WriteLine(type.Name);
-            Console.ReadLine();
+            DataTable dt = (DataTable)Activator.CreateInstance(type);
         }
     }
 }
