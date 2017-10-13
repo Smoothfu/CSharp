b2dfcfa162f10e1ac51b2ea810ca671a101f60b5
Fred1987 <2864326836@qq.com>
2017-10-13 20:50:24 +0800

    System.Data.DataTable,System.Data,Version=1.0.3300.0,Culture=neutral,PublicKeyToken=b77a5c561934e089

--- ConsoleApp69/Program.cs
+++ ConsoleApp69/Program.cs
@@ -11,8 +11,7 @@ namespace ConsoleApp69
     {
         static void Main(string[] args)
         {
-            bool obj = false;
-            Type type = obj.GetType();
+            Type type = Type.GetType("System.Data.DataTable,System.Data,Version=1.0.3300.0,  Culture=neutral,  PublicKeyToken=b77a5c561934e089");
             Console.WriteLine(type.Name);
             Console.ReadLine();
         }
