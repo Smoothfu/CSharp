7d27c79d276caef1f600eb9058f677b30c26f06a
Fred1987 <2864326836@qq.com>
2017-10-15 22:57:39 +0800

    Type type.FullName,type.Module.Assembly.FullName,type.Assembly.FullName

---
+++ ConsoleApp72/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            Type type = obj.GetType();
+            Console.WriteLine(type.FullName);
+            Console.WriteLine(type.Module.Assembly.FullName);
+            Console.WriteLine(type.Module.FullyQualifiedName);
+            Console.WriteLine(type.Module.MetadataToken);
+            Console.WriteLine(type.Module.ModuleVersionId);
+            Console.WriteLine(type.Assembly.FullName);
+            Console.ReadLine();
+        }
+    }
+
+    public class MyClass
+    {
+        public void Add(int x,int y)
+        {
+            Console.WriteLine("{0}+{1}={2}", x, y, x + y);
+        }
+
+        public void Subtract(int x,int y)
+        {
+            Console.WriteLine("{0}-{1}={2}", x, y, x - y);
+        }
+    }
+}
