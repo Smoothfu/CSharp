25113c541c3857aecc591a5cc6b17f47bafc942b
Fred1987 <2864326836@qq.com>
2017-10-17 00:05:50 +0800

    public delegate void ShowNameHandler(string name);public static event ShowNameHandler ShowMsg

---
+++ ConsoleApp77/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp77
+{
+
+    public delegate void ShowNameHandler(string name);
+    class Program
+    {
+        public static event ShowNameHandler ShowMsg;       
+        static void Main(string[] args)
+        {
+            ShowMsg += Program_ShowMsg;
+            ShowMsg("Fred");
+            Console.ReadLine();
+        }
+
+        private static void Program_ShowMsg(string name)
+        {
+            Console.WriteLine("This name is " + name);
+        }
+    }
+}
