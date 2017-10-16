935cfd3f9a3e86174e34f1b1c5839a52a028c107
Fred1987 <2864326836@qq.com>
2017-10-16 19:31:01 +0800

    assembly.Modules.All(x=>{Console.WriteLine(x.FullyQualifiedName);Console.WriteLine(x.GetType().Name);return true;})

---
+++ ConsoleApp74/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Console.WriteLine(assembly.FullName);
+            assembly.Modules.All(x => { Console.WriteLine(x.FullyQualifiedName);Console.WriteLine(x.GetType().Name);return true;});
+            Console.ReadLine();
+        }
+    }
+}
