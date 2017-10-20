4fc4ee5b1150c800578ff28449ca42d75c5c8e96
Fred1987 <2864326836@qq.com>
2017-10-20 21:50:58 +0800

    ArrayList,Capacity,Count,Sort

---
+++ ConsoleApp79/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            Console.WriteLine("Adding some numbers：");
+            al.Add(45);
+            al.Add(78);
+            al.Add(33);
+            al.Add(56);
+            al.Add(12);
+            al.Add(23);
+            al.Add(9);
+
+            Console.WriteLine("Capacity :{0}", al.Capacity);
+            Console.WriteLine("Count:{0}\n", al.Count);
+
+            foreach(int i in al)
+            {
+                Console.Write(i+"\t");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("\nSorted Content: ");
+            al.Sort();
+
+            foreach(int i in al)
+            {
+                Console.Write(i+"\t");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
