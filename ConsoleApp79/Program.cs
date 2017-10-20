44301a23df7ec1b0e71389fe9df782251266fe8b
Fred1987 <2864326836@qq.com>
2017-10-20 22:13:18 +0800

    delegateTNumberChange<T>(Tn) NumberChange<int> nc1=new NumberChange<int>(AddNum)

--- ConsoleApp79/Program.cs
+++ ConsoleApp79/Program.cs
@@ -7,58 +7,38 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp79
 {
-    public class MyGenericArray<T>
-    {
-        private T[] array;
-        public MyGenericArray(int size)
-        {
-            array = new T[size + 1];
-        }
-
-        public T getItem(int index)
-        {
-            return array[index];
-        }
-
-        public void setItem(int index, T value)
-        {
-            array[index] = value;
-        }
-    }
+    delegate T NumberChange<T>(T n);
     class Program
     {
-        static void Swap<T>(ref T lhs,ref T rhs)
+        static int num = 10;
+        public static int AddNum(int p)
         {
-            T temp;
-            temp = lhs;
-            lhs = rhs;
-            rhs = temp;
+            num += p;
+            return num;
+        }
+
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+
+        public static int getNum()
+        {
+            return num;
         }
         static void Main(string[] args)
         {
-            int a, b;
-            char c, d;
-            a = 10;
-            b = 20;
-            c = 'I';
-            d = 'V';
+            //创建委托实例
+            NumberChange<int> nc1 = new NumberChange<int>(AddNum);
+            NumberChange<int> nc2 = new NumberChange<int>(MultNum);
 
-            //在交换之前显示值
-            Console.WriteLine("Int values before calling swap: ");
-            Console.WriteLine("a={0},b={1}", a, b);
-            Console.WriteLine("Char values before calling swap: ");
-            Console.WriteLine("c={0},d={1}", c, d);
+            //使用委托对象调用方法
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            nc2(5);
+            Console.WriteLine("Value of Num:{0}", getNum());
 
-            //调用 swap
-            Swap<int>(ref a, ref b);
-            Swap<char>(ref c, ref d);
-
-            //在交换之后显示值
-            Console.WriteLine("Int values after calling swap: ");
-            Console.WriteLine("a={0},b={1}", a, b);
-            Console.WriteLine("Char vlues after calling swap: ");
-            Console.WriteLine("c={0},d={1}", c, d);
-            Console.ReadLine();
             Console.ReadLine();
         }
     }
