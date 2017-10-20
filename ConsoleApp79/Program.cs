1ee445edf2faeaaccc2b22e7995a4727bfe0a6c6
Fred1987 <2864326836@qq.com>
2017-10-20 22:23:53 +0800

    Attribute [Obsolete(msg)]

--- ConsoleApp79/Program.cs
+++ ConsoleApp79/Program.cs
@@ -7,34 +7,23 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 
 namespace ConsoleApp79
-{
-    public class MyClass
-    {
-        [Conditional("DEBUG")]
-        public static void Message(string msg)
-        {
-            Console.WriteLine(msg);
-        }
-    }
+{     
      
     class Program
-    {
-        static void function1()
+    {      
+        [Obsolete("Don't use OldMethod,use NewMethod instead",true)]
+        static void OldMethod()
         {
-            MyClass.Message("In function 1.");
-            
+            Console.WriteLine("It is the old method");
         }
 
-        static void function2()
+        static void NewMethod()
         {
-            MyClass.Message("In function 2.");
+            Console.WriteLine("It is the new method");
         }
         static void Main(string[] args)
         {
-            MyClass.Message("In Main function.");
-            function1();
-
-            Console.ReadLine();
+            OldMethod();
         }
     }
 }
