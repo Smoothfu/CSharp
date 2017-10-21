ff84d8d459f4deb11dbc19f655e7042c82ffe72d
Fred1987 <2864326836@qq.com>
2017-10-21 11:26:33 +0800

    AttributeUsage(AttributeTargets.Class|AttributeTargets.Constructor|AttributeTargets.Field|AttributeTargets.Method|AttributeTargets.Property,AllowMultiple=true)] public class DebugInfo:System.Attribute Type type=typeof() type.GetCustomeAttributes(false)

--- ConsoleApp80/Program.cs
+++ ConsoleApp80/Program.cs
@@ -7,47 +7,124 @@ using System.Reflection;
 
 namespace ConsoleApp80
 {
-    [AttributeUsage(AttributeTargets.All)]
-    public class HelpAttribute:System.Attribute
+   //一个自定义特性BugFix被赋给类及其成员
+   [AttributeUsage(AttributeTargets.Class|AttributeTargets.Constructor|AttributeTargets.Field|AttributeTargets.Method|AttributeTargets.Property,AllowMultiple =true)]
+    public class DebugInfo : System.Attribute
     {
-        public readonly string Url;
+        private int bugNo;
+        private string developer;
+        private string lastReview;
+        public string message;
 
-        private string topic;
-        //topic 是一个命名(named)参数
-        public string Topic
+        public DebugInfo(int bg,string dev,string d)
+        {
+            this.bugNo = bg;
+            this.developer = dev;
+            this.lastReview = d;
+        }
+
+        public int BugNo
         {
             get
             {
-                return topic;
+                return bugNo;
+            }
+        }
+
+        public string Developer
+        {
+            get
+            {
+                return developer;
+            }
+        }
+
+        public string LastReview
+        {
+            get
+            {
+                return lastReview;
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return message;
             }
             set
             {
-                topic = value;
+                message = value;
             }
-        }
-
-        //url是一个定位参数
-        public HelpAttribute(string url)
-        {
-            this.Url = url;
         }
     }
 
-    [HelpAttribute("Information on the class MyClass")]
-    class MyClass
+    [DebugInfo(45,"Zara Ali","12/8/2012",message ="Return type mismatch")]
+    [DebugInfo(49,"Nuha Ali","10/10/2012",message ="Unused variable")]
+    class Rectangle
     {
+        //成员变量
+        protected double length;
+        protected double width;
+        public Rectangle(double l,double w)
+        {
+            length = l;
+            width = w;
+        }
 
+        [DebugInfo(55,"Zara Ali","19/10/2012",Message ="Return type mismatch")]
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        [DebugInfo(56,"Zara Ali","19/10/2012")]
+        public void Display()
+        {
+            Console.WriteLine("Length:{0}", length);
+            Console.WriteLine("Width:{0}", width);
+            Console.WriteLine("Area :{0}", GetArea());
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            MemberInfo mi = typeof(MyClass);
-            object[] attributes = mi.GetCustomAttributes(true);
-            for(int i=0;i<attributes.Length;i++)
+            Rectangle rect = new Rectangle(4.5, 7.5);
+            rect.Display();
+
+            Type type = typeof(Rectangle);
+            //遍历Rectangle类的特性
+            foreach(object attributes in type.GetCustomAttributes(false))
             {
-                Console.WriteLine(attributes[i]);
+                DebugInfo di = (DebugInfo)attributes;
+                if(null!=di)
+                {
+                    Console.WriteLine("Bug no:{0}", di.BugNo);
+                    Console.WriteLine("Developer:{0}", di.Developer);
+                    Console.WriteLine("Last Reviewed:{0}", di.LastReview);
+                    Console.WriteLine("Remarks:{0}", di.message);
+                }
             }
+
+            //遍历方法特性
+            foreach(MethodInfo mi in type.GetMethods())
+            {
+                foreach(Attribute at in mi.GetCustomAttributes(true))
+                {
+                    DebugInfo di = (DebugInfo)at;
+                    if (null != di)
+                    {
+                        Console.WriteLine("Bug no:{0},for Method:{1}", di.BugNo, mi.Name);
+                        Console.WriteLine("Developer: {0}", di.Developer);
+                        Console.WriteLine("Last Reviewed:{0}", di.LastReview);
+                        Console.WriteLine("Remarks:{0}", di.Message);
+                    }
+                }
+            }
+           
             Console.ReadLine();
         }
     }
