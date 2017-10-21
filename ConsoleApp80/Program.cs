3715a57eb84f40f71d6fe7e0be78bf18c7c7c159
Fred1987 <2864326836@qq.com>
2017-10-21 11:05:28 +0800

    [AttributeUsage(AttributeTargets.All)] class HelpAttribute:System.Attribute

---
+++ ConsoleApp80/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp80
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class HelpAttribute:System.Attribute
+    {
+        public readonly string Url;
+
+        private string topic;
+        //topic 是一个命名(named)参数
+        public string Topic
+        {
+            get
+            {
+                return topic;
+            }
+            set
+            {
+                topic = value;
+            }
+        }
+
+        //url是一个定位参数
+        public HelpAttribute(string url)
+        {
+            this.Url = url;
+        }
+    }
+
+    [HelpAttribute("Information on the class MyClass")]
+    class MyClass
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemberInfo mi = typeof(MyClass);
+            object[] attributes = mi.GetCustomAttributes(true);
+            for(int i=0;i<attributes.Length;i++)
+            {
+                Console.WriteLine(attributes[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
