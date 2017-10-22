971155faa55e068187bbe30a93286652e5ab47de
Fred1987 <2864326836@qq.com>
2017-10-22 22:05:26 +0800

    Thread lowThread=new Thread(LowPriorityMethod),lowThread.Priority=ThreadPriority.Lowest

---
+++ ConsoleApp96/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp96
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread lowThread = new Thread(LowPriorityMethod);
+            lowThread.Priority = ThreadPriority.Lowest;
+            lowThread.Start();
+
+            Thread highThread = new Thread(HighestPriorityMethod);
+            highThread.Priority = ThreadPriority.Highest;
+            highThread.Start();
+            Console.ReadLine();
+        }
+
+        static void LowPriorityMethod()
+        {
+            for(int i=0;i<100;i++)
+            {
+                Console.WriteLine("The thread with lowest priority!");
+            }            
+        }
+
+        static void HighestPriorityMethod()
+        {
+            for(int i=0;i<100;i++)
+            {
+                Console.WriteLine("The thread with highest priority");
+            }
+            
+        }
+    }
+}
