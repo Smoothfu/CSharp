12aa55dc305056077e5072d58aae0f1b9fcea346
Fred1987 <2864326836@qq.com>
2017-10-22 12:26:10 +0800

    IAsyncResult result=del.BeginInvoke(msg,null,null);result.AsyncWaitHandle.WaitOne(-1,false);

---
+++ ConsoleApp88/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp88
+{
+    class Program
+    {
+        public delegate void PrintDelegate(string msg);
+        static void Main(string[] args)
+        {
+            PrintDelegate del = Print;
+            Console.WriteLine("The main thread.");
+            IAsyncResult result = del.BeginInvoke("Hello World.", null, null);
+            Console.WriteLine("The main thread is continuing...");
+            result.AsyncWaitHandle.WaitOne(-1, false);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+
+        public static void Print(string msg)
+        {
+            Console.WriteLine("Begin to run the asynchronous thread: " + msg);
+        }
+    }
+}
