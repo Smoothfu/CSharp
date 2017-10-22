c65013cd8d88fc0370d9c5a6e811d4ea36a2c07c
Fred1987 <2864326836@qq.com>
2017-10-22 21:46:39 +0800

    Thread thread=new Thread(delegate(){Console.WriteLine();}); Thread thread2=new Thread(()=>Console.WriteLine())

---
+++ ConsoleApp94/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp94
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new a thread by annoymous delegate
+            Thread thread = new Thread(delegate ()
+              {
+                  Console.WriteLine("Create a new thread by annoymous delegate!");
+              });
+
+            thread.Start();
+
+            //new a thread by Lambda Expression
+            Thread thread2 = new Thread(() => Console.WriteLine("New a thread by Lambda Expression"));
+            thread2.Start();
+            Console.ReadLine();
+        }
+    }
+}
