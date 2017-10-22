c28d1b83051827b864267425a02240ff5b6d4464
Fred1987 <2864326836@qq.com>
2017-10-22 21:41:40 +0800

    Thread thread=new Thread(new ThreadStart(instance.method))

---
+++ ConsoleApp93/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp93
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new an instance of class ThreadTest
+            ThreadTest obj = new ThreadTest();
+
+            //Invoke the method in the instance
+            Thread thread = new Thread(new ThreadStart(obj.MyMethod));
+
+            //start the thread
+            thread.Start();
+            Console.ReadLine();
+            
+        }
+    }
+
+    class ThreadTest
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("This is a instance method!");
+        }
+    }
+}
