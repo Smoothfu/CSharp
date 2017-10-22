69bc7863df25ae12187fb7e7d8b62eb1700be38e
Fred1987 <2864326836@qq.com>
2017-10-22 21:57:39 +0800

    new ParameterizedThreadStart

---
+++ ConsoleApp95/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp95
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new a thread with parameter by parameterizedThreadStart
+            Thread thread = new Thread(new ParameterizedThreadStart(NewThread));
+
+            //given a value to the method
+            thread.Start("new ParameterizedThreadStart");
+            Console.ReadLine();
+        }
+
+
+        ///<summary>
+        ///new a method with parameter
+        /// </summary>
+        static void NewThread(object msg)
+        {
+            Console.WriteLine(msg.ToString());
+        }
+        
+    }
+}
