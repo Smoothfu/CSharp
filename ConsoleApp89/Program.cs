fb4239a5d32ab5868ad03deb17d132aa7cac0fb2
Fred1987 <2864326836@qq.com>
2017-10-22 13:16:10 +0800

    IAsyncResult result=del.BeginInvoke(msg,null,null);result.IsCompleted

---
+++ ConsoleApp89/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp89
+{
+    class Program
+    {
+        public delegate void PrintDelegate(string msg);
+        static void Main(string[] args)
+        {
+            PrintDelegate del = PrintInfo;
+            Console.WriteLine("The main thread :" + Thread.CurrentThread.ManagedThreadId);
+            IAsyncResult result = del.BeginInvoke("Hello world", null, null);
+            Console.WriteLine("The main thread: " + Thread.CurrentThread.ManagedThreadId + " , continuing executing....");
+            while (!result.IsCompleted)
+            {
+                Console.WriteLine(".");
+                Thread.Sleep(500);
+            }
+
+            del.EndInvoke(result);
+
+            Console.WriteLine("The main thread is " + Thread.CurrentThread.ManagedThreadId);
+            Console.ReadLine();
+        }
+
+        public static void PrintInfo(string msg)
+        {
+            Console.WriteLine("The current thread: " + Thread.CurrentThread.ManagedThreadId + "\t" + msg);
+        }
+    }
+}
