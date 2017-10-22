e142f82e30a67b2c79e2057ab00afc19c87def12
Fred1987 <2864326836@qq.com>
2017-10-22 21:36:35 +0800

    Thread newThread=new Thread(new ThreadStart(AnotherMethod))

---
+++ ConsoleApp92/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp92
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new a thread without parameter
+            Thread newThread = new Thread(new ThreadStart(AnotherMethod));
+
+            //invoke the Start to execute the thread
+            newThread.Start();
+            Console.ReadLine();
+        }
+
+        ///<summary>
+        ///new a method without parameters
+        /// </summary>
+        static void AnotherMethod()
+        {
+            Console.WriteLine("This method has no parameters！");
+        }
+    }
+}
