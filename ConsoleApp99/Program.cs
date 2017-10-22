afb61f192b8e4ac6486f705dea6033b11cb7e1d6
Fred1987 <2864326836@qq.com>
2017-10-22 23:15:22 +0800

    Thread lock(this)

---
+++ ConsoleApp99/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookShop book = new BookShop();
+            //create 2 thread to access the sale meanwhile
+            Thread thread1 = new Thread(new ThreadStart(book.Sale));
+            Thread thread2 = new Thread(new ThreadStart(book.Sale));
+
+            //start the thread
+            thread1.Start();
+            thread2.Start();
+            Console.ReadLine();
+        }
+    }
+
+    class BookShop
+    {
+        //the remaining book amount
+        public int num = 1;
+        public void Sale()
+        {
+            lock (this)
+            {
+                int temp = num;
+                if (temp > 0)
+                {
+                    Thread.Sleep(1000);
+                    num -= 1;
+                    Console.WriteLine("Saled one book,the remaining amout is {0}", num);
+                }
+                else
+                {
+                    Console.WriteLine("Empty");
+                }
+            }           
+        }
+    }
+}
