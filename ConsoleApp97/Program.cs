31e700ced99f526d970a7903e17dd7e1976efacf
Fred1987 <2864326836@qq.com>
2017-10-22 22:58:38 +0800

    thread.Name;int id=thread.ManagerThreadId;ThreadState state=thread.ThreadState;thread.Priority

---
+++ ConsoleApp97/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //get the running thread
+            Thread thread = Thread.CurrentThread;
+
+            //set the thread name
+            thread.Name = "Main Thread";
+
+            //get the unique name
+            int id = thread.ManagedThreadId;
+
+            //get the current status
+            ThreadState state = thread.ThreadState;
+
+            //get the priority of the current thread
+            ThreadPriority priority = thread.Priority;
+            string msg = string.Format("Thread ID:{0}\n" +
+                "Thread Name:{1}\n" +
+                "Thread State:{2}\n" +
+                "Thread Priority:{3}\n", id, thread.Name, state, priority);
+            Console.WriteLine(msg);
+            Console.ReadLine();
+        }
+    }
+}
