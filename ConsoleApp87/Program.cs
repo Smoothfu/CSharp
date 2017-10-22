33a8b0450b9cbbb3db78106c22b23a5f38f7fc48
Fred1987 <2864326836@qq.com>
2017-10-22 12:17:59 +0800

    IAsyncResult result=del.BeginInvoke(msg,null,null);del.BeginInvoke(result)

---
+++ ConsoleApp87/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp87
+{
+    class Program
+    {
+        public delegate void PrintDelegate(string msg);
+        public static void Print(string msg)
+        {
+            Console.WriteLine("Asynchronous method begin to execute: " + msg);
+            Thread.Sleep(500);
+        }
+        static void Main(string[] args)
+        {
+            PrintDelegate del = Print;
+            Console.WriteLine("The main thread.");
+
+            IAsyncResult result = del.BeginInvoke("Hello World", null, null);
+            Console.WriteLine("The main thread is continuing executing");
+            //When invoke the BeginInvoke as the asynchronously,if it did not complete,the EndInvoke will be pending until the method complete/
+            del.EndInvoke(result);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine(); 
+        }
+    }
+}
