af91dfc5d088ee038447660b686aa39d838344ca
Fred1987 <2864326836@qq.com>
2017-10-22 13:24:09 +0800

    del.BeginInvoke(msg,PrintComplete,del); (result.AsyncState as PrintDelegate).EndInvoke(result);

---
+++ ConsoleApp90/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp90
+{
+    class Program
+    {
+        public delegate void PrintDelegate(string msg);
+        static void Main(string[] args)
+        {
+
+            PrintDelegate del = PrintInfo;
+            Console.WriteLine("The main thread.");
+            del.BeginInvoke("Hello World", PrintComplete, del);
+            Console.WriteLine("The main thread is executing...");
+
+            Console.ReadLine();
+        }
+
+        public static void PrintInfo(string msg)
+        {
+            Console.WriteLine("The current thread: " + msg);
+        }
+
+        public static void PrintComplete(IAsyncResult result)
+        {
+            (result.AsyncState as PrintDelegate).EndInvoke(result);
+            Console.WriteLine("The current thread end." + result.AsyncState.ToString());
+        }
+    }
+}
