c0798b7973c4ea5a73ee409aecbf36dab63ca9c1
Fred1987 <2864326836@qq.com>
2017-10-26 01:17:40 +0800

    string.Compare(str1,str2,StringComparison.InvariantCultureIgnoreCase)

--- ConsoleApp101/Program.cs
+++ ConsoleApp101/Program.cs
@@ -15,16 +15,16 @@ namespace ConsoleApp101
             string str1 = "Hello World";
             string str2 = "Hello world";
 
-            if (str1.ToUpper() == str2.ToUpper())
-            {
-                Console.WriteLine("Equal");
-            }
-
-
-            //if ((string.Compare(str1, str2,StringComparison.InvariantCultureIgnoreCase)) == 0)
+            //if (str1.ToUpper() == str2.ToUpper())
             //{
             //    Console.WriteLine("Equal");
             //}
+
+
+            if ((string.Compare(str1, str2, StringComparison.InvariantCultureIgnoreCase)) == 0)
+            {
+                Console.WriteLine("Equal");
+            }
 
             var objSize = GC.GetTotalMemory(true) - currentBytes;
             Console.WriteLine(objSize.ToString());
