1f7235ae21de9778e985928ee18a825f0d4c60c3
Fred1987 <2864326836@qq.com>
2017-10-26 01:15:43 +0800

    str.ToUpper

---
+++ ConsoleApp101/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentBytes = GC.GetTotalMemory(true);
+            string str1 = "Hello World";
+            string str2 = "Hello world";
+
+            if (str1.ToUpper() == str2.ToUpper())
+            {
+                Console.WriteLine("Equal");
+            }
+
+
+            //if ((string.Compare(str1, str2,StringComparison.InvariantCultureIgnoreCase)) == 0)
+            //{
+            //    Console.WriteLine("Equal");
+            //}
+
+            var objSize = GC.GetTotalMemory(true) - currentBytes;
+            Console.WriteLine(objSize.ToString());
+            Console.ReadLine();
+        }
+    }
+}
