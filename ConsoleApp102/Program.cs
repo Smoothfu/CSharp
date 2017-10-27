0e8a49d8a4c2b5221710ea41935712a4da2324f7
Fred1987 <2864326836@qq.com>
2017-10-27 19:27:05 +0800

    Pause Console.WriteLine(pause); Console.ReadKey(true)

---
+++ ConsoleApp102/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(WriteY);
+            //Run WriteY on the new thread
+            thread.Start();
+            
+            while (true)
+            {
+                Console.WriteLine("X");
+            }
+
+            Pause();
+           
+            Console.ReadLine();
+        }
+        static void WriteY()
+        {
+            while (true)
+            {
+                Console.WriteLine("Y");
+            }
+           
+        }
+
+        static void Pause()
+        {
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadKey(true);
+        }
+    }
+}
