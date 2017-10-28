0b9b8f7c46d4b074d0583e5a05e8014640e60329
Fred1987 <2864326836@qq.com>
2017-10-28 11:48:14 +0800

    thread.Join(); the invoked thread blocks the main thread unitil the invoked thread completes

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        private static void ThreadExample()
+        {
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " i= " + i);
+            }
+            Console.WriteLine(Thread.CurrentThread.Name + " has finished");
+        }
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main Thread is here";
+
+            Thread newThread = new Thread(new ThreadStart(ThreadExample));
+            newThread.Name = "New Thread Name";
+
+            for(int j=0;j<20;j++)
+            {
+                if(j==10)
+                {
+                    newThread.Start();
+                    newThread.Join();
+                }
+                else
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " j= " + j);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
