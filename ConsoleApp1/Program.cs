910197e99ba50f39ddefd22d1e0e1352e86f755a
Fred1987 <2864326836@qq.com>
2017-10-28 10:44:18 +0800

    Thread thread=Thread.CurrentThread;thread.Name="MainThread";Console.WriteLine(thread.Name);

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = Thread.CurrentThread;
+            thread.Name = "MainThread";
+            Console.WriteLine("The current thread name is " + thread.Name);
+            Console.ReadKey();
+        }
+    }
+}
