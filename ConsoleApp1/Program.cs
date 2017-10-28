596f4c01e2626c319809ebf72e865449d2dee9eb
Fred1987 <2864326836@qq.com>
2017-10-28 11:42:17 +0800

    thread.Join()

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,6 +14,46 @@ namespace ConsoleApp1
             Thread thread = Thread.CurrentThread;
             thread.Name = "MainThread";
             Console.WriteLine("The current thread name is " + thread.Name);
+            Console.WriteLine(thread.ThreadState);
+            ////Abort
+            //thread.Abort();
+            //Console.WriteLine("Abort :" + thread.ThreadState);
+            //Console.WriteLine("IsAlive: " + thread.IsAlive);
+
+            //
+            thread.Join();
+            Console.WriteLine("Join :" + thread.ThreadState);
+            thread.Start();
+
+            //
+            thread.Resume();
+            Console.WriteLine("Resume: " + thread.ThreadState);
+
+            //
+            thread.Suspend();
+            Console.WriteLine("Suspend: " + thread.ThreadState);
+
+            //CurrentCulture
+            Console.WriteLine("CurrentCulture: " + thread.CurrentCulture);
+
+            //CurrentUICulture
+            Console.WriteLine("CurrentUICulture: " + thread.CurrentUICulture);
+
+            //Type
+            Console.WriteLine("Type: " + thread.GetType());
+
+            //Resume
+            thread.Resume();
+            Console.WriteLine("Resume: " + thread.ThreadState);
+
+            //Interrupt
+            thread.Interrupt();
+            Console.WriteLine("State: "+thread.ThreadState);
+
+            //Priority
+            Console.WriteLine("Priroity: " + thread.Priority);
+
+            
             Console.ReadKey();
         }
     }
