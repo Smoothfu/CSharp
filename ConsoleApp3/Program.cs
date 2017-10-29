4a600ecd3e2ee7fbb01663bb14e2cd5ee7155dc8
Fred1987 <2864326836@qq.com>
2017-10-29 17:55:32 +0800

    ThreadStart childRef=new ThreadStart(CallToChildThread); Thread childThread=new Thread(childRef);childThread.Start()

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,42 +9,47 @@ namespace ConsoleApp3
 {
     class Program
     {
-       static bool isContinuingDoing = true;
-        static void Main(string[] args)
+        public static void CallToChildThread()
         {
-            int x = 2435435, y = 2, z = 20;
-           
-
-            Thread thread = new Thread(() =>
+            try
             {
-                Add(x, y, z);
-                Subtract(x, y, z);
-            });
-            thread.Start();
+                Console.WriteLine("Child thread starts.");
 
-            thread.Join();
-
-            if (thread.ThreadState == ThreadState.Stopped)
-            {
-                isContinuingDoing = false;
+                //do some work,like counting to 10
+                for(int i=0;i<=10;i++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("Child Thread Completed!");
             }
 
-            while (isContinuingDoing)
+            catch(ThreadAbortException ex)
             {
-                Console.WriteLine("This is main thread " + DateTime.Now.ToString("yyyy-MM-dd:HHmmssfff"));
-            }            
+                Console.WriteLine("Thread Abort Execption");
+            }
 
+
+            finally
+            {
+                Console.WriteLine("Couldn't catch the thread Exception");
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart childRef = new ThreadStart(CallToChildThread);
+            Console.WriteLine("In main:Creating the Child thread");
+
+            Thread childThread = new Thread(childRef);
+            childThread.Start();
+
+            //Stop the main thread for some time
+            Thread.Sleep(2000);
+
+            //now abort the child
+            Console.WriteLine("In Main:Aborting the Child thread");
+            childThread.Abort();
             Console.ReadLine();
-        }
-
-        static void Add(int x,int y,int z)
-        {
-            Console.WriteLine("{0}+{1}+{2}={3}", x, y, z, x + y + z);
-        }
-
-        static void Subtract(int x,int y,int z)
-        {
-            Console.WriteLine("{0}-{1}-{2}={3}", x, y, z, x - y - z);
-        }
+        }         
     }
 }
