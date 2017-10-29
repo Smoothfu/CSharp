c2927510a00f413aec72ab6cce64eb1a5b65947b
Fred1987 <2864326836@qq.com>
2017-10-29 17:42:10 +0800

    Thread thread=new Thread(()=>{Add(x,y,z);Subtract(x,y,z);});

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,17 +9,42 @@ namespace ConsoleApp3
 {
     class Program
     {
-        public static void CallChildThread()
-        {
-            Console.WriteLine("Child thread starts!");
-        }
+       static bool isContinuingDoing = true;
         static void Main(string[] args)
         {
-            ThreadStart childRef = new ThreadStart(CallChildThread);
-            Console.WriteLine("In main:Creating the child thread.");
-            Thread childThread = new Thread(childRef);
-            childThread.Start();
-            Console.ReadKey();
+            int x = 2435435, y = 2, z = 20;
+           
+
+            Thread thread = new Thread(() =>
+            {
+                Add(x, y, z);
+                Subtract(x, y, z);
+            });
+            thread.Start();
+
+            thread.Join();
+
+            if (thread.ThreadState == ThreadState.Stopped)
+            {
+                isContinuingDoing = false;
+            }
+
+            while (isContinuingDoing)
+            {
+                Console.WriteLine("This is main thread " + DateTime.Now.ToString("yyyy-MM-dd:HHmmssfff"));
+            }            
+
+            Console.ReadLine();
+        }
+
+        static void Add(int x,int y,int z)
+        {
+            Console.WriteLine("{0}+{1}+{2}={3}", x, y, z, x + y + z);
+        }
+
+        static void Subtract(int x,int y,int z)
+        {
+            Console.WriteLine("{0}-{1}-{2}={3}", x, y, z, x - y - z);
         }
     }
 }
