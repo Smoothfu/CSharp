95fdc544b732084b2cc3a1cc3a1a6fc2e2baf2ea
Fred1987 <2864326836@qq.com>
2017-10-29 17:06:37 +0800

    Thread childThread=new Thread(new ThreadStart(ThreadMethod))

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public static void CallChildThread()
+        {
+            Console.WriteLine("Child thread starts!");
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart childRef = new ThreadStart(CallChildThread);
+            Console.WriteLine("In main:Creating the child thread.");
+            Thread childThread = new Thread(childRef);
+            childThread.Start();
+            Console.ReadKey();
+        }
+    }
+}
