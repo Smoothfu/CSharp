2250882f1c285421d5ed65e1922cfe30e6f7a37f
Fred1987 <2864326836@qq.com>
2017-11-03 19:22:04 +0800

    Thread thread=new Thread(()=>{Add(x,y);Subtract(x,y);});thread.Start();

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Add(int x,int y)
+        {
+            Console.WriteLine("{0}+{1}={2}", x, y, x + y);
+        }
+
+        static void Subtract(int x,int y)
+        {
+            Console.WriteLine("{0}-{1}={2}", x, y, x - y);
+        }
+        static void Main(string[] args)
+        {
+            int x = 10, y = 20;
+            Thread thred = new Thread(() =>
+              {
+
+                  Add(x, y);
+                  Subtract(x, y);
+              });
+            thred.Start();
+            Console.ReadLine();
+        }
+    }
+}
