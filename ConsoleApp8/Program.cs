96b881f520bdc50a39761f02dc0be84d1b289159
Fred1987 <2864326836@qq.com>
2017-11-06 22:13:02 +0800

    swap(ref int x,ref int y)

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 20;
+            Console.WriteLine("Before swap: x={0},y={1}", x, y);
+            Swap(ref x,ref  y);
+            Console.WriteLine("After swap: x={0},y={1}", x, y);
+            Console.ReadLine();
+        }
+
+        static void Swap(ref int x,ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
