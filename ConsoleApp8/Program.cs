e3ac6b2ec426d684fe1ff878312c72965a453375
Fred1987 <2864326836@qq.com>
2017-11-06 22:17:36 +0800

    static void Swap(ref T val1,ref T val2)

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -14,14 +14,19 @@ namespace ConsoleApp8
             Console.WriteLine("Before swap: x={0},y={1}", x, y);
             Swap(ref x,ref  y);
             Console.WriteLine("After swap: x={0},y={1}", x, y);
+
+            string str1 = "Fred", str2 = "ST";
+            Console.WriteLine("Before swap:str1={0},str2={1}", str1, str2);
+            Swap(ref str1, ref str2);
+            Console.WriteLine("After swap:str1={0},str2={1}", str1, str2);
             Console.ReadLine();
         }
 
-        static void Swap(ref int x,ref int y)
+        static void Swap<T>(ref T val1,ref T val2)
         {
-            int temp = x;
-            x = y;
-            y = temp;
+            T temp = val1;
+            val1 = val2;
+            val2 = temp;
         }
     }
 }
