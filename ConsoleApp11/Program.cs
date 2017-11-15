ebe3ffed3f1a19e2d981f867f9bc8dae244e4ff2
Fred1987 <2864326836@qq.com>
2017-11-15 23:08:02 +0800

    Base b=d as Base;if(b!=null)

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Base
+    {
+        public override string ToString()
+        {
+            return "Base";
+        }
+    }
+
+    class Derived : Base
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Derived d = new Derived();
+            Base b = d as Base;
+            if(b!=null)
+            {
+                Console.WriteLine(b.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
