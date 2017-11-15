991de76f448030faef1fb2e7b3cea34752f6eef0
Fred1987 <2864326836@qq.com>
2017-11-15 21:46:28 +0800

    [Conditional("DEBUG")]

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp10
+{
+    public class MyClass
+    {
+        [Conditional("DEBUG")]
+        public static void Message(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class Program
+    {
+        static void function1()
+        {
+            MyClass.Message("In Function 1.");
+            function2();
+        }
+
+        static void function2()
+        {
+            MyClass.Message("In Function 2.");
+        }
+        static void Main(string[] args)
+        {
+            MyClass.Message("In Main function");
+            function1();
+            Console.ReadLine();
+        }
+    }
+}
