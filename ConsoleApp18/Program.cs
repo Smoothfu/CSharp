2bce0b6422ba524d37a3a95ef34712c181a51fee
Fred1987 <2864326836@qq.com>
2017-11-21 22:03:57 +0800

    typeof(MathClass).GetMethods(),GetProperties(),GetFields()

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -12,14 +12,55 @@ namespace ConsoleApp18
         static void Main(string[] args)
         {
             Type type = typeof(MathClass);
-            MathClass.GetMethodOfType(type);
+            MathClass.ListProps(type);
             Console.ReadLine();
-
         }
     }
 
-    public class MathClass
+    public class MathClass 
     {
+
+        public int num = 10000;
+        public string thisString = "This string";
+        public bool IsTrue = true;
+        public char c = 'a';
+
+        private int x;
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+
+        private int y;
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+
+
+        public int Z { get; set; }
+
+        public string shortName { get; set; }
+         
+        public MathClass(int x,int y)
+        {
+            X = x;
+            Y = y;
+        }
         public void AddMethod(int x,int y)
         {
             Console.WriteLine("{0}+{1}={2}", x, y, x + y);
@@ -43,6 +84,30 @@ namespace ConsoleApp18
             {
                 Console.WriteLine("Name: " + name);
             }
+        }
+
+        public static void ListFields(Type type)
+        {
+            Console.WriteLine("*****Fields*****");
+            var fieldNames = from f in type.GetFields() select f.Name;
+
+            foreach(var name in fieldNames)
+            {
+                Console.WriteLine("Field Name: " + name);
+            }
+        }
+
+        //Display property names of type.
+        public static void ListProps(Type type)
+        {
+            Console.WriteLine("****Properties****");
+            var propNames = from p in type.GetProperties() select p.Name;
+
+            foreach(var name in propNames)
+            {
+                Console.WriteLine("Name: " + name);
+            }
+
         }
     }
 }
