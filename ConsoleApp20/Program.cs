1ea111d238121eccaa117524b22408da10664add
Fred1987 <2864326836@qq.com>
2017-11-22 22:46:50 +0800

    AppDomain.CurrentDomain.BaseDirectory; var allFiles=Directory.GetFiles(filePath,.dll) foreach(var file in allFiles)

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void DisplayTypesInAsm( )
+        {
+            string filePath = AppDomain.CurrentDomain.BaseDirectory;
+            var allFiles = Directory.GetFiles(filePath,"*.dll");
+            foreach(var file in allFiles)
+            {
+                var asm = Assembly.LoadFrom(file);
+                Type[] types = asm.GetTypes();
+                foreach(var t in types)
+                {
+                    Console.WriteLine(t.FullName);
+                }
+            }
+            
+            Console.WriteLine("\n");
+        }
+        static void Main(string[] args)
+        {
+            DisplayTypesInAsm();
+            Console.ReadLine();            
+        }
+    }
+}
