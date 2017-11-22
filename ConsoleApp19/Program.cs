192f9a36211982af7af3c7e8deccdfe29be26dd0
Fred1987 <2864326836@qq.com>
2017-11-22 21:16:52 +0800

    type.IsAbstract,type.IsSealed,type.IsGenericTypeDefinition,type.IsClass

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Type type = typeof(MathClass);
+            MathClass.ListVariousStatus(type);
+            Console.ReadLine();
+        }
+    }
+
+    public class MathClass
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Z = 1000;
+
+        public MathClass()
+        {
+
+        }
+
+        public MathClass(int x,int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Add(int x,int y)
+        {
+            Console.WriteLine("{0}+{1}={2}", x, y, x + y);
+        }
+
+        public void Subtract(int x,int y)
+        {
+            Console.WriteLine("{0}-{1}={2}", x, y, x - y);
+        }
+
+        public static void ListVariousStatus(Type type)
+        {
+            Console.WriteLine("*****Various Statistics*****");
+            Console.WriteLine("Is type abstract? {0}", type.IsAbstract);
+            Console.WriteLine("Is type sealed?{0}", type.IsSealed);
+            Console.WriteLine("Is type generic?{0}", type.IsGenericTypeDefinition);
+            Console.WriteLine("Is type a class type?{0}", type.IsClass);
+        }
+    }
+}
