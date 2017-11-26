c31869142fc0aefd90c7bca4ae4d452730cecd6b
Fred1987 <2864326836@qq.com>
2017-11-26 18:26:56 +0800

    Dynamic vartype="string"

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic varType = "type";
+            Console.WriteLine(varType.GetType());
+            varType = new List<string>();
+            Console.WriteLine(varType.GetType());
+            Console.ReadLine();
+        }
+    }
+}
