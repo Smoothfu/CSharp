81f1017c47b9814a9ce0d581b3972ba2d41a1221
Fred1987 <2864326836@qq.com>
2017-11-29 22:06:59 +0800

    Process[] proList=Process.GetProcesses()

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] proList = Process.GetProcesses();
+            foreach(var pl in proList)
+            { 
+                Console.WriteLine(pl.Handle);
+                Console.WriteLine(pl.Id);
+                Console.WriteLine(pl.MachineName); 
+                Console.WriteLine(pl.SessionId); 
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
