839f1cb8a5a5a6ac2d677a77362085b6dbf34664
Fred1987 <2864326836@qq.com>
2017-11-29 22:20:51 +0800

    from proc in Process.GetProcesses() orderby proc.Id select proc

--- ConsoleApp28/Program.cs
+++ ConsoleApp28/Program.cs
@@ -11,16 +11,20 @@ namespace ConsoleApp28
     {
         static void Main(string[] args)
         {
-            Process[] proList = Process.GetProcesses();
-            foreach(var pl in proList)
-            { 
-                Console.WriteLine(pl.Handle);
-                Console.WriteLine(pl.Id);
-                Console.WriteLine(pl.MachineName); 
-                Console.WriteLine(pl.SessionId); 
+            //Get all the processes on the local machine,ordered by PID.
+            var runningProcs = from proc in Process.GetProcesses(".")
+                               orderby proc.Id
+                               select proc;
+
+            //Print out PID and name of each process.
+
+            foreach(var p in runningProcs)
+            {
+                string info = string.Format("PID:{0} \t Name: {1}", p.Id, p.ProcessName);
+                Console.WriteLine(info);
             }
 
-            Console.ReadLine();
+             Console.ReadLine();
         }
     }
 }
