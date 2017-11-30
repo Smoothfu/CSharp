cd88b83342b9d9f23d19e18fca167dd8d2e581d4
Fred1987 <2864326836@qq.com>
2017-11-30 20:51:04 +0800

    Process.GetProcessById(7716)

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        //If there is no process with the PID of 7716,a runtime exception will be shown.
+        static void Main(string[] args)
+        {
+            Process proc = null;
+            try
+            {
+                proc = Process.GetProcessById(7716);
+                Console.WriteLine(proc.ProcessName);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
