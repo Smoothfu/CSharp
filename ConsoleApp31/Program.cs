7182ee888d0983319bb990485d7224db0a2c0edf
Fred1987 <2864326836@qq.com>
2017-12-01 23:22:18 +0800

    fibonacciNumbers.Add(prev+prev2)

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = new List<int> { 1, 1 };
+            var prev = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var prev2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            fibonacciNumbers.Add(prev + prev2);
+
+            foreach(var item in fibonacciNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
