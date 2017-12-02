253cb5955fa07969c1edf73ad34b9088191f7473
Fred1987 <2864326836@qq.com>
2017-12-02 23:19:03 +0800

    Process.GetProcesses();Process.GetProcesses(".") var newProcs=runningAllProcs.Where(x=>!runningProcs.Any(y=>y.Id==x.Id))

--- ConsoleApp32/Program.cs
+++ ConsoleApp32/Program.cs
@@ -8,22 +8,42 @@ namespace ConsoleApp32
         static void Main(string[] args)
         {
             //Get all the processes on the local machine,ordered by PID
-            var runningProcs = from proc in Process.GetProcesses(".")
+            var runningAllProcs = from proc in Process.GetProcesses()
                                orderby proc.ProcessName select proc;
+            Console.WriteLine("Process.GetProcess()'s process count is {0}\n", runningAllProcs.Count());
+            var runningProcs = from proc in Process.GetProcesses(".") orderby proc.ProcessName select proc;
+            Console.WriteLine("Process.GetProcess(\".\")'s process count is {0}\n", runningProcs.Count());
+            var newProcs = runningAllProcs.Where(x => !runningProcs.Any(y => y.Id == x.Id));
 
-            //Print out PID and name of each process
-
-            if(runningProcs==null ||!runningProcs.Any())
+           if(newProcs==null||!runningProcs.Any())
             {
                 return;
             }
 
-            Console.WriteLine("There are {0} process running now\n", runningProcs.Count());
+            Console.WriteLine("The difference between GetProcess(\".\") and GetProcess()：\n");
+
             foreach(var proc in runningProcs)
             {
-                string info = string.Format("->PID: {0}\tName :{1}", proc.Id, proc.ProcessName);
-                Console.WriteLine(info);
+                Console.WriteLine(string.Format("The process by Process.GetProcess(\".\") and its Id is:{0} and Process Name is :{1}", proc.Id, proc.ProcessName));
             }
+
+
+             
+           
+
+            //Print out PID and name of each process
+
+            //if (runningProcs==null ||!runningProcs.Any())
+            //{
+            //    return;
+            //}
+
+            //Console.WriteLine("There are {0} process running now\n", runningProcs.Count());
+            //foreach(var proc in runningProcs)
+            //{
+            //    string info = string.Format("->PID: {0}\tName :{1}", proc.Id, proc.ProcessName);
+            //    Console.WriteLine(info);
+            //}
             Console.ReadLine();
         }
     }
