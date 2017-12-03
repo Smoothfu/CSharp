085e604630419fd16e09b4aaec3edf149941992d
Fred1987 <2864326836@qq.com>
2017-12-03 09:29:51 +0800

    Process[] procArr=Process.GetProcess(".");

---
+++ ConsoleApp36/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The currently running process infomation");
+            PrintOutProcs();
+            Console.ReadLine();
+        }
+
+        static void PrintOutProcs()
+        {
+            Process[] procArr = Process.GetProcesses(".");
+            if(procArr == null)
+            {
+                return;
+            }
+
+            foreach(Process proc in procArr)
+            {
+                string info = string.Format("The proc id:{0} \tMachineName:{1}", proc.Id, proc.MachineName);
+                Console.WriteLine(info);
+            }
+        }
+    }
+}
