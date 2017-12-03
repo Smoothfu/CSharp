d2519c3b87efb8a37775ff740d930f9cefe90a15
Fred1987 <2864326836@qq.com>
2017-12-03 10:29:04 +0800

    procArr=(from proc in procArr orderby proc.ProcessName select proc).ToArray(); foreach(Process proc in procArr.OrderBy(x=>x.Threads.Count)

--- ConsoleApp36/Program.cs
+++ ConsoleApp36/Program.cs
@@ -1,29 +1,46 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace ConsoleApp36
 {
     class Program
     {
+        static int num = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("The currently running process infomation");
+            Console.WriteLine("The threads of the  currently running process infomation");
             PrintOutProcs();
+            Console.WriteLine("Currently there are totally {0} threads in process", num);
             Console.ReadLine();
         }
 
         static void PrintOutProcs()
         {
-            Process[] procArr = Process.GetProcesses(".");
+            var procArr = Process.GetProcesses(".");
             if(procArr == null)
             {
                 return;
             }
 
-            foreach(Process proc in procArr)
-            {
-                string info = string.Format("The proc id:{0} \tMachineName:{1}", proc.Id, proc.MachineName);
-                Console.WriteLine(info);
+            procArr = (from proc in procArr orderby proc.ProcessName select proc).ToArray();
+            foreach(Process proc in procArr.OrderBy(x=>x.Threads.Count))
+            {                
+                ProcessThreadCollection ptc = proc.Threads;
+                Console.WriteLine("There are {0} {1} in the {2}", ptc.Count, ptc.Count > 1 ? "threads" : "thread", proc.ProcessName);
+
+                foreach (ProcessThread pt in ptc)
+                {
+                    if (pt.Id == 0)
+                    {
+                        continue;
+                    }
+                    string info = string.Format("->Thread ID:{0}",
+                        pt.Id);
+                    Console.WriteLine(info);                   
+                    num++;
+                }
+                Console.WriteLine("\n");
             }
         }
     }
