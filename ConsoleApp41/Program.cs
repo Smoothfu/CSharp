a31d89b6ea973260e5b66fe84172c8e2f11dd3d7
Fred1987 <2864326836@qq.com>
2017-12-04 22:07:42 +0800

    AppDomain defaultAD=AppDomain.CurrentDomain

---
+++ ConsoleApp41/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayDADStats();
+            Console.ReadLine();
+        }
+
+        private static void DisplayDADStats()
+        {
+            //Get access to the AppDomain for the current thread.
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+
+            //Print out various stats about this domain.
+            Console.WriteLine("Name of this domain: {0}", defaultAD.FriendlyName);
+
+            Console.WriteLine("ID of domain in this process: {0}", defaultAD.Id);
+            Console.WriteLine("Is this the default domain? :{0}", defaultAD.IsDefaultAppDomain());
+            Console.WriteLine("Base directory of this domain: {0}", defaultAD.BaseDirectory);
+        }
+    }
+}
