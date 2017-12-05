7fb9d1f2afa49d5ed48e57924ba5584f4f63eb39
Fred1987 <2864326836@qq.com>
2017-12-05 22:04:18 +0800

    Process.GetCurrentProcess()

---
+++ ConsoleApp43/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListProcessAndNames();
+            Console.ReadLine();
+        }
+
+        static void ListProcessAndNames()
+        {
+            var currentProc = Process.GetCurrentProcess();
+
+            Console.WriteLine("{0},{1}",currentProc.MainModule, currentProc.ProcessName);
+        }
+    }
+}
