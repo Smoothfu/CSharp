495791bf7d8966bfabf79822dacdf5f1cef9a2b9
Fred1987 <2864326836@qq.com>
2017-12-05 22:20:16 +0800

    var runningProcs=from proc in Process.GetProcess(".") orderby proc.ProcessName select proc

--- ConsoleApp43/Program.cs
+++ ConsoleApp43/Program.cs
@@ -11,23 +11,33 @@ namespace ConsoleApp43
     {
         static void Main(string[] args)
         {
-            ListProcessAndNames();
+            ListAllRunningProcesses();
             Console.ReadLine();
         }
 
         static void ListProcessAndNames()
         {
-            var procs = Process.GetProcesses();
-            if(procs!=null && procs.Count()>0)
+            Process.GetProcesses().ToList().ForEach(x =>
             {
+                Console.WriteLine("ProcessName:{0},Id:{1}", x.ProcessName, x.Id);
+            });  
+        }
 
-                Console.WriteLine("There are {0} processes!\n\n\n", procs.Count());
+        static void ListAllRunningProcesses()
+        {
+            //Get al the processes on the local machine ordered by PID.
+            var runningProcs = from proc in Process.GetProcesses(".")
+                               orderby proc.ProcessName
+                               select proc;
+            Console.WriteLine("There are totally {0} processes\n\n\n\n", runningProcs.Count());
+            //Print out PID and name of each process.
+            foreach(var proc in runningProcs)
+            {
+                string info = string.Format("->PID: {0}\tName:{1}", proc.Id, proc.ProcessName);
+                Console.WriteLine(info);
+            }
+            Console.WriteLine("*************************************************88");
 
-                procs.ToList().ForEach(x =>
-                {
-                    Console.WriteLine("ProcessName:{0},Id:{1}",x.ProcessName, x.Id);
-                });
-            } 
         }
     }
 }
