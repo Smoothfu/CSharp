333216470c01947166f47cb687585a7bc9ca9ec4
Fred1987 <2864326836@qq.com>
2017-12-06 23:00:56 +0800

    AppDomain defaultAD=AppDomain.CurrentDomain; Assembly[] loadedAssemblies=defaultAD.GetAssemblies()

---
+++ ConsoleApp48/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ConsoleApp48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListAllAssembliesInAppDomain();
+            Console.ReadLine();
+        }
+        
+        static void ListAllAssembliesInAppDomain()
+        {
+            //Get access to the AppDomain for the current thread.
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+
+            //Now get all loaded assemblies in the default AppDomain.
+            Assembly[] loadedAssemblies = defaultAD.GetAssemblies();
+            Console.WriteLine("*****************Here are the assemblies loaded in{0}**************\n",defaultAD.FriendlyName);
+
+            foreach(Assembly asm in loadedAssemblies)
+            {
+                Console.WriteLine("->Name: {0}\t    Version:{1}\t", asm.GetName().Name, asm.GetName().Version);
+            }
+
+        }
+    }
+}
