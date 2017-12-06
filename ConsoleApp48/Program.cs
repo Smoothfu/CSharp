f358d3d3050e1cf48d9d963ebdc3c1b96c7ff3dc
Fred1987 <2864326836@qq.com>
2017-12-06 23:58:54 +0800

    newAD.DomainUnload+=(o,s)=>{Console.WriteLine("")

--- ConsoleApp48/Program.cs
+++ ConsoleApp48/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Reflection;
+using System.IO;
 
 namespace ConsoleApp48
 {
@@ -12,7 +13,7 @@ namespace ConsoleApp48
     {
         static void Main(string[] args)
         {
-            ListAllAssembliesInAppDomainOrder();
+            MakeNewAppDomain();
             Console.ReadLine();
         }
         
@@ -49,6 +50,55 @@ namespace ConsoleApp48
             {
                 string info = string.Format("{0,-30}{1,-100}{2,10}", asm.GetName().Name, asm.FullName,asm.GetName().Version);
                 Console.WriteLine(info);
+            }
+        }
+
+        private static void InitDAD()
+        {
+            //This logic will print out the name of ant assembly.
+            //loaded into the application domain,after it has been created.
+
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+            defaultAD.AssemblyLoad += (o, s) =>
+            {
+                Console.WriteLine("{0} has been loaded!", s.LoadedAssembly.GetName().Name);
+            };
+        }
+
+        private static void MakeNewAppDomain()
+        {
+            //Make a new AppDomain in the current process and list loaded assemblies.
+            AppDomain newAD = AppDomain.CreateDomain("SecondAppDomain");
+
+            newAD.DomainUnload += (o, s) =>
+            {
+                Console.WriteLine("The second AppDomain has been unloaded!");
+            };
+            try
+            {
+                //Now load ConsoleApp47.exe into this new domain.
+                newAD.Load("ConsoleApp47");
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            ListAllAssembliesInAppDomain(newAD);
+        }
+
+        static void ListAllAssembliesInAppDomain(AppDomain ad)
+        {
+            //Now get all loaded assemblies in the default AppDomain.
+            var loadedAssemblies=from asm in ad.GetAssemblies()
+                                 orderby asm.GetName().Name
+                                 select asm;
+
+            Console.WriteLine("*****Here are the assemblies loaded in {0}*****\n", ad.FriendlyName);
+
+            foreach(var a in loadedAssemblies)
+            {
+                Console.WriteLine("->Name:{0,-30}Version:{1,-10}", a.GetName().Name, a.GetName().Version);
             }
         }
     }
