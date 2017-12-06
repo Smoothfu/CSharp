b938868f317ed5afa289660bd481e889a7ed5a39
Fred1987 <2864326836@qq.com>
2017-12-06 20:09:26 +0800

    AppDomain defaultAD=AppDomain.CurrentDomain;var loadedAssemblies=from asm in defaultAD.GetAssemblies() orderby asm.GetName().Name select asm

---
+++ ConsoleApp46/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListAllAssembliesInAppDomain();
+            Console.ReadLine();
+        }
+
+        static void ListAllAssembliesInAppDomain()
+        {
+            //Get access to the AppDomain for the current thread.
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+
+            //Now get all loaded assemblies in the default AppDomain.
+            var loadedAssemblies = from asm in defaultAD.GetAssemblies()
+                                   orderby asm.GetName().Name
+                                   select asm;
+
+            Console.WriteLine("***********Here are the assemblies loaded in {0} **************\n", defaultAD.FriendlyName);
+            foreach(var asm in loadedAssemblies)
+            {
+                Console.WriteLine("->Name:{0}", asm.GetName().Name);
+                Console.WriteLine("->Version:{0}\n", asm.GetName().Version);
+            }
+        }
+    }
+}
