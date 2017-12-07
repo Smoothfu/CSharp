03ee4f42a1067a6a9b8b3ef28a4ccff9b29aed01
Fred1987 <2864326836@qq.com>
2017-12-07 23:20:42 +0800

    Type type=typeof(Program)type.AssemblyQualifiedNameFullNameName

---
+++ ConsoleApp49/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(Program);
+            Console.WriteLine(type.AssemblyQualifiedName);
+            Console.WriteLine(type.FullName);
+            Console.WriteLine(type.Name);
+            Console.ReadLine();
+        }
+    }
+}
