b2b2e4395483682f6dbaf8063ce94bdb88047501
Fred1987 <2864326836@qq.com>
2017-12-07 23:37:32 +0800

    MethodInfo [] mis=type.GetMethods();foreach(MethodInfo mi in mis)

--- ConsoleApp49/Program.cs
+++ ConsoleApp49/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +11,44 @@ namespace ConsoleApp49
     {
         static void Main(string[] args)
         {
-            Type type = typeof(Program);
-            Console.WriteLine(type.AssemblyQualifiedName);
-            Console.WriteLine(type.FullName);
-            Console.WriteLine(type.Name);
+            Type type = typeof(MathClass);
+            ListMethods(type);
             Console.ReadLine();
+        }
+
+        //Display method names of type.
+        static void ListMethods(Type t)
+        {
+            Console.WriteLine("*****Methods*****");
+            MethodInfo[] mis = t.GetMethods();
+
+            foreach(MethodInfo mi in mis)
+            {
+                Console.WriteLine(mi.Name);
+            }
+        }
+    }
+
+    public class MathClass
+    {
+        public void Add(int x,int y)
+        {
+            Console.WriteLine("{0}+{1}={2}", x, y, x + y);
+        }
+
+        public void Subtract(int x,int y)
+        {
+            Console.WriteLine("{0}-{1}={2}", x, y, x - y);
+        }
+
+        public void Multiply(int x,int y)
+        {
+            Console.WriteLine("{0}*{1}={2}", x, y, x * y);
+        }
+
+        public void Divide(int x,int y)
+        {
+            Console.WriteLine("{0}/{1}={2}", x, y, x / y);
         }
     }
 }
