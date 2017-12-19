8e0529da59d6017b0c6fae8411944f28706cda66
Fred1987 <2864326836@qq.com>
2017-12-19 23:36:08 +0800

    public delegate void CanEngineHandler(string msgForCaller) private CarEngineHandler listOfHandlers

--- ConsoleApp59/Program.cs
+++ ConsoleApp59/Program.cs
@@ -10,17 +10,31 @@ namespace ConsoleApp59
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*****Simple Delegate Example*****\n");
+            Console.WriteLine("*****Delegates as event enablers*****\n");
 
-            //Create a BinaryOp delegate object that 'points to ' SimpleMath.Add().
+            //First,make a car object.
 
-            BinaryOp b = new BinaryOp(SimpleMath.Add);
+            Car c1 = new Car("Mercedes Benz", 100, 10);
 
-            //Invoke Add() method indirectly using delegate object.
-            Console.WriteLine("10+10 is {0}\n\n\n\n", b.Invoke(10, 10));
+            //Now,tell the car which method to call when it wants to send us messages.
+            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
 
-            DisplayDelegateInfo(b);
+
+            //Speed up(this will trigger the events).
+            Console.WriteLine("*****Speeding up *****");
+            for(int i=0;i<10;i++)
+            {
+                c1.Accelerate(20);
+            }
+
             Console.ReadLine();
+        }
+
+        private static void OnCarEngineEvent(string msgForCaller)
+        {
+            Console.WriteLine("\n*****Message From Car Object*****");
+            Console.WriteLine("=>{0}", msgForCaller);
+            Console.WriteLine("*************************************************\n");
         }
 
         static void DisplayDelegateInfo(Delegate delObj)
@@ -57,5 +71,76 @@ namespace ConsoleApp59
         {
             return a * a;
         }
+    }
+
+    public class Car
+    {
+        //internal state data.
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+
+        //Is the car alive or dead?
+        private bool carIsDead;
+
+        //class constructors.
+        public Car()
+        {
+
+        }
+
+        public Car(string name,int maxSp,int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        //1) Define a delegate type.
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        //2)Define a member variable of this delegate.
+        private CarEngineHandler listOfHandlers;
+
+        //3)Add registration function for the caller.
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers = methodToCall;
+        }
+
+        //4)Implement the Accelerate() method to invoke the delegate's invocation list under the correct circumstances.
+
+        public void Accelerate(int delta)
+        {
+            //If this car is "dead",send dead message.
+            if(carIsDead)
+            {
+                if(listOfHandlers!=null)
+                {
+                    listOfHandlers("Sorry,this car is dead...");
+                }
+            }
+
+            else
+            {
+                CurrentSpeed += delta;
+
+                //Is this car "almost dead"?
+                if(10==(MaxSpeed-CurrentSpeed) && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful buddy!Gonna blow!");
+                }
+
+                if(CurrentSpeed>=MaxSpeed)
+                {
+                    carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("CurrentSpeed={0}", CurrentSpeed);
+                }
+            }
+        }
+
     }
 }
