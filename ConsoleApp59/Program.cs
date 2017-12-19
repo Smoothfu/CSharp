658ffc23e983552982ec3d4abe8a11a1b54a5504
Fred1987 <2864326836@qq.com>
2017-12-19 22:08:21 +0800

    foreach(Delegate del in delObj.GetInvocationList());del.Method;del.Target

--- ConsoleApp59/Program.cs
+++ ConsoleApp59/Program.cs
@@ -17,8 +17,20 @@ namespace ConsoleApp59
             BinaryOp b = new BinaryOp(SimpleMath.Add);
 
             //Invoke Add() method indirectly using delegate object.
-            Console.WriteLine("10+10 is {0}", b.Invoke(10, 10));
+            Console.WriteLine("10+10 is {0}\n\n\n\n", b.Invoke(10, 10));
+
+            DisplayDelegateInfo(b);
             Console.ReadLine();
+        }
+
+        static void DisplayDelegateInfo(Delegate delObj)
+        {
+            //Print the names of each member in the delegate's invocation list.
+            foreach(Delegate del in delObj.GetInvocationList())
+            {
+                Console.WriteLine("Method Name:{0}", del.Method);
+                Console.WriteLine("Type Name:{0}", del.Target);
+            }
         }
     }
 
@@ -39,6 +51,11 @@ namespace ConsoleApp59
         public static int Subtract(int x,int y)
         {
             return x - y;
+        }
+
+        public static int SquareNumber(int a)
+        {
+            return a * a;
         }
     }
 }
