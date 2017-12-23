236c2e67c1937b69e0b33824d580809b23f002e1
Fred1987 <2864326836@qq.com>
2017-12-23 10:40:26 +0800

    MyGenericDelegate<string> strTarget=new MyGenericDelegate<string>(StringTarget)

---
+++ ConsoleApp63/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp63
+{
+    //This generic delegate can represent any method returning void and taking a single parameter of type T.
+    public delegate void MyGenericDelegate<T>(T arg);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Generic Delegates*****\n");
+
+            //Register targets
+
+            MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(StringTarget);
+            strTarget("This is a new begining!");
+
+            MyGenericDelegate<int> intTarget = new MyGenericDelegate<int>(IntTarget);
+            intTarget(10000000);
+            Console.ReadLine();
+        }
+
+        static void StringTarget(string arg)
+        {
+            Console.WriteLine("arg in uppercase is :{0}\n\n", arg.ToUpperInvariant());
+        }
+
+        static void IntTarget(int arg)
+        {
+            Console.WriteLine("++arg is :{0}", ++arg);
+        }
+    }
+}
