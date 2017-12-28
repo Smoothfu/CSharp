4d77005189f03c00b79d268dfe80185648532764
Fred1987 <2864326836@qq.com>
2017-12-28 22:39:06 +0800

    If you would rather not force the caller to wrap the invocation within an unsafe context,you could update main() with the unsafe keyword.In this case ,the following code would compile

--- ConsoleApp66/Program.cs
+++ ConsoleApp66/Program.cs
@@ -8,18 +8,23 @@ namespace ConsoleApp66
 {
     class Program
     {
-        static void Main(string[] args)
+        unsafe static void Main(string[] args)
         {
-            //Make an anonymous type that is composed of another.
-            var purchaseItem = new
+            unsafe
             {
-                TimeBought = DateTime.Now,
-                ItemBought = new { Color = "Black", Make = "Mercedes Benz", CurrentSpeed = 55 },
-                Price = 34.000
-            };
+                int myInt = 10;
 
+                //Ok,because we are in an unsafe context.
+                SquareIntPointer(&myInt);
+                Console.WriteLine("myInt: {0}\n", myInt);
+            }
 
-            ReflectOverAnonymousType(purchaseItem);
+            int myInt2 = 5;
+
+            //Compiler error! Must be in unsafe context!
+            SquareIntPointer(&myInt2);
+            Console.WriteLine("myInt: {0}\n", myInt2);
+
             Console.ReadLine();
         }
 
@@ -31,5 +36,31 @@ namespace ConsoleApp66
             Console.WriteLine("obj.GetHashCode()=={0}\n", obj.GetHashCode());
             Console.WriteLine();
         }
+
+        unsafe static void SquareIntPointer(int* myIntPointer)
+        {
+            //Square the value just for a test.
+            *myIntPointer *= *myIntPointer;
+        }
+    }
+
+    //This entire structure is "unsafe" and can be used only in an unsafe context.
+    unsafe struct Node
+    {
+        public int Value;
+        public Node* Left;
+        public Node* Right;
+    }
+
+    //The struct is safe,but the Node2* members are not. Technically,you may access "Value" from outside an 
+    //unsafe context,but not "Left" and "Right"
+    public struct Node2
+    {
+        public int Value;
+
+        //Thes can be accessed only in an unsafe context!
+        public unsafe Node2* Left;
+        public unsafe Node2* Right;
+
     }
 }
