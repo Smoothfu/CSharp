82c7f2acae5b7219d6b65d7deec35cc69300e0c1
Fred1987 <2864326836@qq.com>
2017-12-29 23:41:01 +0800

    Implicit typing of local variables,you learned about the var keyword of C#.This keyword allows you to define a local variable without explicitly specifying the underlying data type.The variable,however,is strongly typed,as the compiler will determine the correct data type based on the initial assignment.Recall the following code example from Chapter 3

---
+++ ConsoleApp67/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeclareImplicitVars();
+            Console.ReadLine();
+        }
+
+        static void DeclareImplicitVars()
+        {
+            //Implicitly typed local variables.
+            var myInt = 0;
+            var myBool = true;
+            var myString = "Time,marches on...";
+
+            //print out the underlying type.
+            Console.WriteLine("myInt is a :{0}\n", myInt.GetType().Name);
+            Console.WriteLine("myBool is a :{0}\n", myBool.GetType().Name);
+            Console.WriteLine("myString is a :{0}\n", myString.GetType().Name);
+
+        }
+
+    }
+}
