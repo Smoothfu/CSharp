0203e6c624cc9cf18c6ad7a04da80da3c533133c
Fred1987 <2864326836@qq.com>
2017-12-30 18:34:30 +0800

    Object and Collection initialization syntax.Chapter 5 explored the role of object initialization syntax,which allows you to create a class or structure variable and to set any number of its public properties in one fell swoop.The end result is a compact yet still easy on the eyes syntax that can be used to get your objects ready for use.Also recall from chapter 9,the C# language allows you to use a similar to initialize collections of objects.Consider the following code snippet,which uses collection initialization syntax to fill a List<T> of Rectangle objects,each of which maintains two Point objects to represent an (x,y) position

--- ConsoleApp68/Program.cs
+++ ConsoleApp68/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,20 @@ namespace ConsoleApp68
     {
         static void Main(string[] args)
         {
-            DeclareImplicitVars();
+            List<Rectangle> myListOfRects = new List<Rectangle>
+            {
+                new Rectangle{TopLeft=new Point{X=10,Y=10},
+                BottomRight=new Point{X=200,Y=200}},
+                new Rectangle{TopLeft=new Point{X=2,Y=2},
+                BottomRight=new Point{X=100,Y=100}},
+                new Rectangle{TopLeft=new Point{X=5,Y=5},
+                BottomRight=new Point{X=90,Y=75}}
+            };
+
+            foreach(var rect in myListOfRects)
+            {
+                Console.WriteLine(rect + "\n");
+            }
             Console.ReadLine();
         }
 
@@ -25,6 +39,35 @@ namespace ConsoleApp68
             Console.WriteLine("myInt is a:{0}\n", myInt.GetType().Name);
             Console.WriteLine("myBool is a: {0}\n", myBool.GetType().Name);
             Console.WriteLine("myString is a:{0}\n", myString.GetType().Name);
+        }
+    }
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("X:{0},Y:{0}", X, Y);
+        }
+    }
+
+
+    public class Rectangle : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public Point TopLeft { get; set; }
+
+        public Point BottomRight { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("TopLeft:{0},BottomRight:{1}\n", TopLeft, BottomRight);
         }
     }
 }
