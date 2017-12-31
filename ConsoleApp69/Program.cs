ef150552929278114f0d31852b2b33d8dd98ae8b
Fred1987 <2864326836@qq.com>
2017-12-31 16:45:58 +0800

    Object and collection initialization syntax Chapter 5 explored the role of object initialization syntax, which allows you to create a class or structure variable and to set any number of its public properties in one fell swoop,The end result is a compact yet still easy on the eyes syntax that can be used to get your  objects for use. Alos recall from Chapter 9,the C# language allows you to use a similar syntax to initialize collections of objects. Consider the following code snippet,which uses collection initialization syntax to fill a List<T> of Rectangle objects,each of which maintains two Point objects to represent an (x,y) position

--- ConsoleApp69/Program.cs
+++ ConsoleApp69/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,29 @@ namespace ConsoleApp69
     {
         static void Main(string[] args)
         {
-            DeclareImplicitVars();
+            List<Rectangle> myListRectangles = new List<Rectangle>
+            {
+                new Rectangle
+                {
+                    LeftTop=new Point{X=10,Y=10},
+                    RightBottom=new Point{X=200,Y=200}
+                },
+                new Rectangle
+                {
+                    LeftTop=new Point{X=2,Y=2},
+                    RightBottom=new Point{X=100,Y=100}
+                },
+                new Rectangle
+                {
+                    LeftTop=new Point{X=5,Y=5},
+                    RightBottom=new Point{X=90,Y=75}
+                }
+            };
+
+            foreach(var rect in myListRectangles)
+            {
+                Console.WriteLine(rect + "\n");
+            }
             Console.ReadLine();
         }
 
@@ -25,6 +48,33 @@ namespace ConsoleApp69
             Console.WriteLine("myInt is a: {0}\n", myInt.GetType().Name);
             Console.WriteLine("myBool is a :{0}\n", myInt.GetType().Name);
             Console.WriteLine("myString is a:{0}\n", myString.GetType().Name);           
+        }
+    }
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("X:{0},Y:{0}", X, Y);
+        }
+    }
+
+    public class Rectangle:IEnumerable
+    {
+        public Point LeftTop { get; set; }
+        public Point RightBottom { get; set; }
+
+        public IEnumerator GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("LeftTop: {0},RightBottom:{1}\n", LeftTop, RightBottom);
         }
     }
 }
