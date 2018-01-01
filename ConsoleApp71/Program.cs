bb711d87ab201754f1fc28a95948039a71782bf5
Fred1987 <2864326836@qq.com>
2018-01-01 19:02:00 +0800

    Classes,Objects,and references.To frame the topics covered in this chapter,it is important to further clarify the distinction between classes,objects,and reference variables.Recall that a class is nothing more than a blueprint that describes how an instance of this type will look and feel in memory.Classes,of course,are defined within a code file which in C# takes a .cs extension by convension.Consider the following simple car class defined within a new C# Console Application project named SimpleGC.Understanding Object Lifetime.At this point in the book,you have learned a great deal about how to build custom class type using C#.Now you will see how the CLR managers allocated class instances aka objects via garbage collection.C Sharp programmers never direcly deallocate a managed object from memory recall there is no delete keyword in the C# language.Rather,.net objects are allocated to a region of memory termed the managed heap, where they will be automatically destoryed by the garbage collector sometimes in the future

---
+++ ConsoleApp71/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****GC Basics*****\n");
+
+            //Create a new Car object on the managed heap.We are returned a reference to this object.
+
+            Car refToMyCar = new Car("BMW", 50);
+
+            //The C# dot operator(.) is used to invoke members on the object using our reference variable.
+            Console.WriteLine(refToMyCar.ToString());
+            Console.ReadLine();
+        }
+    }
+
+    //Car.cs
+    public class Car
+    {
+        public int CurrentSpeed { get; set;}
+        public string PetName { get; set;}
+
+        public Car()
+        {
+
+        }
+
+        public Car(string name,int speed)
+        {
+            PetName = name;
+            CurrentSpeed = speed;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} is going {1} MPH", PetName, CurrentSpeed);
+        }
+
+    }
+}
