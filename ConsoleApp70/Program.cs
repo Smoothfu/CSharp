86ff9bdcbf9147a0e04f37c88b55718df631a0c3
Fred1987 <2864326836@qq.com>
2018-01-01 16:46:22 +0800

    Linq concat remove duplicates.When you call the Concat() extension method,you could very well end up with redundant entries in the fetched result,which could be exactly what you want in some cases. However,in other cases,you might want to remove duplicate entries in your data. To do so,simply call the Distinct() extension method,as show here.Concat Distinct()

---
+++ ConsoleApp70/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayConcatNoDups();
+            Console.ReadLine();
+        }
+
+        static void DisplayConcatNoDups()
+        {
+            List<string> myCars = new List<string> { "Yugo", "Aztec", "BMW" };
+            List<string> yourCars = new List<string> { "BMW", "Saab", "Aztec" };
+
+            var carContact = (from c in myCars select c).Concat(from c2 in yourCars select c2);
+
+            //Prints:
+            foreach(string str in carContact.Distinct())
+            {
+                Console.WriteLine(str + "\n");
+            }
+        }
+    }
+}
