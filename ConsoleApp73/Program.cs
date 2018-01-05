9f538329958dd868ff77032c4b24642c491c8667
Fred1987 <2864326836@qq.com>
2018-01-05 23:11:48 +0800

    [SecuritySafeCritical] public static long GetTotalMemory(bool forceFullCollection)  retrieves the number of bytes currently thought to be allocated.A parameter indicates whether this method can wait a short interval before returning,to allow the systemm to collect garbage and finalize objects.forcefullCollection true to indicate this method can wait for garbage collection to occur before returning;otherwise false.returns a number that us the best avaiblable approximation of the number of bytes currently allocated in managed memory. public static int MaxGeneration Gets the maximum number of generations that the system currently supports.returns a value that ranges from zero to the maximum number of supported generations.[SecuritySafeCritical] public static int GetNeneration(object obj).Returns the current generation number of the specified object.the object the generation information is retrieved for.returns the current generations number of obj.public static int GetGeneration(object obj)

---
+++ ConsoleApp73/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Fun with System.GC*****\n");
+
+            //Print out estimated number of bytes on heap.
+            Console.WriteLine("Estimated bytes on head:{0}\n", GC.GetTotalMemory(true));
+
+            //MaxGeneration is zero based,so add 1 for display purposes.
+            Console.WriteLine("This OS has {0} object generations.\n", (GC.MaxGeneration + 1));
+
+            Car refToMyCar = new Car("BMW", 50);
+            Console.WriteLine(refToMyCar.ToString());
+
+
+            //Print out generations of refToMyCar object.
+            Console.WriteLine("Generation of refToMyCar is :{0}\n", GC.GetGeneration(refToMyCar));
+            Console.ReadLine();
+       
+        }
+    }
+
+    public class Car
+    {
+        public string CarName { get; set; }
+        public int CarSpeed { get; set; }
+
+        public Car(string carName,int carSpeed)
+        {
+            CarName = carName;
+            CarSpeed = carSpeed;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}'s speed is {1}\n", CarName, CarSpeed);
+        }
+    }
+}
