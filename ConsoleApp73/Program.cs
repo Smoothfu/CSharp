147074513ca121d8b3acf753a25a6ac1d1b18c66
Fred1987 <2864326836@qq.com>
2018-01-05 23:59:15 +0800

    GarbageCollection GC.Collect() promotes surviving generations.To illustrate, assume that your Main() method has been updated as follows.GC.GetTotalMemory(false),GC.MaxGeneration+1,GC.GetGeneration(refToMyCar),GC.Collect(0,GCCollectionMode.Forced),GC.WaitForPendingFinalizers(),GC.GetGeneration(refToMyCar),GC.CollectionCount(0)

--- ConsoleApp73/Program.cs
+++ ConsoleApp73/Program.cs
@@ -10,26 +10,51 @@ namespace ConsoleApp73
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*****Fun with System.GC*****\n");
+            Console.WriteLine("Fun with System.GC*****\n");
 
             //Print out estimated number of bytes on heap.
-            Console.WriteLine("Estimated bytes on head:{0}\n", GC.GetTotalMemory(true));
+            Console.WriteLine("Estimated bytes on heap:{0}\n", GC.GetTotalMemory(false));
 
-            //MaxGeneration is zero based,so add 1 for display purposes.
+            //MaxGeneration is zero based.
             Console.WriteLine("This OS has {0} object generations.\n", (GC.MaxGeneration + 1));
 
             Car refToMyCar = new Car("BMW", 50);
-
-            //Force a garbage collection and wait for each object to be finialized.
-            GC.Collect(0,GCCollectionMode.Default);
-            GC.WaitForPendingFinalizers();
-
             Console.WriteLine(refToMyCar.ToString());
 
-            //Print out generations of refToMyCar object.
+            //Print out  generation of refToMyCar.
+            Console.WriteLine("\nGeneration of refToMyCar is :{0}\n", GC.GetGeneration(refToMyCar));
+
+            //Make a ton of objects for testing purposes.
+            object[] tonsOfObjects = new object[50000];
+            for(int i=0;i<50000;i++)
+            {
+                tonsOfObjects[i] = new object();
+            }
+
+            //Collect only gen 0 objects.
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+
+            //Print out generation of refToMyCar.
             Console.WriteLine("Generation of refToMyCar is :{0}\n", GC.GetGeneration(refToMyCar));
-            Console.ReadLine();
-       
+
+            //See if tonsOfObjects[9000] is still alive.
+            if(tonsOfObjects[9000]!=null)
+            {
+                Console.WriteLine("Generation of tonsOfObjects[9000] is:{0}\n", GC.GetGeneration(tonsOfObjects[9000]));
+            }
+
+            else
+            {
+                Console.WriteLine("tonsOfObjects[9000] is no longer alive.");
+            }
+
+            //Print out how many times a generation has been swept.
+
+            Console.WriteLine("\nGen 0 has been swept {0} times\n", GC.CollectionCount(0));
+            Console.WriteLine("Gen 1 has been swept {0} times\n", GC.CollectionCount(1));
+            Console.WriteLine("Gen 2 has been swept {0} times\n", GC.CollectionCount(2));
+            Console.ReadLine();       
         }
     }
 
