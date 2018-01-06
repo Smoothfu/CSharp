292fd267031c3bd0e93bcc008a335f851fdfa10c
Fred1987 <2864326836@qq.com>
2018-01-06 18:53:46 +0800

    GC.GetTotalMemory(false), GC.MaxGeneration+1,GC.GetGeneration(refToMyCar),GC.Collect(0,GCCollectionMode.Forced),GC.WaitForPendingFinalizers(),

--- ConsoleApp74/Program.cs
+++ ConsoleApp74/Program.cs
@@ -10,36 +10,56 @@ namespace ConsoleApp74
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("*****Fun with System.GC*****\n");
 
-            //Print out estimated number of bytes on heap.
+            //Print out the estimated number of bytes on heap.
             Console.WriteLine("Estimated bytes on heap:{0}\n", GC.GetTotalMemory(false));
 
-            //MaxGeneration is zero based,so add 1 for display purposes.
+            //MaxGeneration is zero based.
             Console.WriteLine("This OS has {0} object generations.\n", (GC.MaxGeneration + 1));
 
             Car refToMyCar = new Car("BENZ", 50);
-            Console.WriteLine(refToMyCar.ToString());
+            Console.WriteLine(refToMyCar);
 
-            //Print out generation of refToMyCar object.
+            //Print out generation of refToMyCar.
+            Console.WriteLine("\nGeneration of refToMyCar is :{0}\n", GC.GetGeneration(refToMyCar));
+
+            //Make a ton of objects for testing purposes.
+            object[] tonsOfObjects = new object[50000];
+
+            for(int i=0;i<50000;i++)
+            {
+                tonsOfObjects[i] = new object();
+            }
+
+            //Collect only gen 0 objects.
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+
+            //Print out generation of the reToMyCar.
             Console.WriteLine("Generation of refToMyCar is :{0}\n", GC.GetGeneration(refToMyCar));
 
-            //Only investigate generation 0 objects.
-            GC.Collect(0);
-            GC.WaitForPendingFinalizers();
-            Console.WriteLine("Allocated memory: {0}\n", GC.GetTotalMemory(false));
+
+            //See if tonsOfObjects[9000] is still alive.
+            if(tonsOfObjects[9000]!=null)
+            {
+                Console.WriteLine("Generation of tonsOfObjects[9000] is :{0}\n", GC.GetGeneration(tonsOfObjects[9000]));
+            }
+            else
+            {
+                Console.WriteLine("tonsOfObjects[9000] is no longer alive.\n");
+            }
+
+            //Print out how many times a generation has been swept.
+
+            Console.WriteLine("\nGen 0 has been swept {0} times\n", GC.CollectionCount(0));
+
+            Console.WriteLine("Gen 2 has been swept {0} times.\n", GC.CollectionCount(2));
+
+
+            Console.WriteLine("Estimated bytes on heap:{0}\n", GC.GetTotalMemory(false));
+
             Console.ReadLine();
-
-        /***
-         * public enum GCCollectionMode{
-         * default,//forced is the current default.
-         * Forced,//Tells the runtime to collect immediately!
-         Optimized, //Allows the runtime to determine whether the current time is optimal to reclaim objects
-
-         * 
-         * ***/
-
         }
     }
 
