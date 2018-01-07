0e4561a7205cdfb559dc72b19918ffbc9b6bc2fa
Fred1987 <2864326836@qq.com>
2018-01-07 16:27:02 +0800

    Resolving Name Clashes with Aliases. The C Sharp using keyword also lets you create an alias for a types fully qualified name.When you do so,you define a token that is substituted for the types full name at compile time.Defining aliases provides a second way to resolve name clashes.

--- ConsoleApp76/Program.cs
+++ ConsoleApp76/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using My3DShapes;
+
+//Resolve the ambiguity using a custom alias.
+using The3DHexagon = My3DShapes.Hexagon;
 
 namespace ConsoleApp76
 {
@@ -17,6 +21,9 @@ namespace ConsoleApp76
             My3DShapes.Circle c3 = new My3DShapes.Circle();
             My3DShapes.Hexagon h3 = new My3DShapes.Hexagon();
             My3DShapes.Square s3 = new My3DShapes.Square();
+
+            //This is really creating a My3DShapes.Hexagon class.
+            The3DHexagon h32 = new The3DHexagon();
             Console.ReadLine();
         }
     }     
