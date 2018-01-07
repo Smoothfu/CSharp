287f16d4cbc80a90ccb735f8822a0a967759b6df
Fred1987 <2864326836@qq.com>
2018-01-07 16:20:54 +0800

    However,fully qualified names can be helpful and sometimes necessary to avoid potential name clashes when using multiple namespace that contain identically named types.Assume you have a new namesapce termed My3DShapes,which defines the following thres classes,capable of rendering a shape in stunning 3D. If you updated the Program class as shown next,you are issued a number of comiple-time error,because both namespace define identically named classes.The ambiguity can be resoloved using the types fully qualified name like so

--- ConsoleApp76/Program.cs
+++ ConsoleApp76/Program.cs
@@ -13,8 +13,43 @@ namespace ConsoleApp76
             ConsoleApp76.Hexagon h = new Hexagon();
             ConsoleApp76.Circle c = new Circle();
             ConsoleApp76.Square s = new Square();
+
+            My3DShapes.Circle c3 = new My3DShapes.Circle();
+            My3DShapes.Hexagon h3 = new My3DShapes.Hexagon();
+            My3DShapes.Square s3 = new My3DShapes.Square();
             Console.ReadLine();
         }
-    }
-     
+    }     
 }
+
+//Another shape-centric namespace.
+namespace My3DShapes
+{
+    //3D Circle class
+    public class Circle
+    {
+        //3D Circle class.
+        public Circle()
+        {
+            Console.WriteLine("This is the constructor of the 3D Circle class!\n");
+        }
+    }
+
+    //3D Hexagon class.
+    public class Hexagon
+    {
+        public Hexagon()
+        {
+            Console.WriteLine("The construtor of the 3D Hexagon class!\n");
+        }
+    }
+
+
+    //3D Square class.
+    public class Square
+    {
+
+    }
+
+}
+
