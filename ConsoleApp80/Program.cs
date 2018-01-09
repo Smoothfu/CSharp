bdcd16f4c1fa9216f493d64aeda85e6e7fb8bad5
Fred1987 <2864326836@qq.com>
2018-01-09 22:37:24 +0800

    The Type.GetType() method has been overloaded to allow you to specify two boolean parameters,one of which controls whether an exceptions should be thrown if the type cannot be found, and the other of which establishes the case sensitivity of the string. To illustrate,ponder the following.In the previous example,notice that the string you are passing into GetType() makes no mention of the assembly containing the type. In this case,the assumption is that the type is defined within  the currently exeuting assembly.However,when you want to obtain metadata for a type within an external private assembly,the string parameter is formated using the types fully qualified name,followed by a comma,followed by the friendly name of the assembly containing the type.As well,do know that string passed into Type.GetType() may specify a plus token + to denote a nested type.Assume you want to obtain type information for an enumeration nested within a class named .To do so,you would write the following

--- ConsoleApp80/Program.cs
+++ ConsoleApp80/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CarLibrary;
 using System.Configuration;
+using System.Reflection;
 
 namespace ConsoleApp80
 {
@@ -12,22 +13,17 @@ namespace ConsoleApp80
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*****Reading <appSettings> Data*****\n");
-
-            //Get our custom data from the *.config file.
-            AppSettingsReader ar = new AppSettingsReader();
-
-            int numberOfTimes = (int)ar.GetValue("RepeatCount", typeof(int));
-            string textColor = (string)ar.GetValue("TextColor", typeof(string));
-
-            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), textColor);
-
-            //Now print a message correctly.
-            for(int i=0;i<numberOfTimes;i++)
-            {
-                Console.WriteLine("Wonderful!Floomberg!\n");
-            }
-
+            Type t = typeof(SuperCar);
+            Console.WriteLine("Assembly: {0}\n", t.Assembly);
+            Console.WriteLine("BaseType: {0}\n", t.BaseType);
+            Console.WriteLine("MemberType: {0}\n", t.MemberType);
+            Console.WriteLine("Module: {0}\n", t.Module);
+            Console.WriteLine("NameSpace: {0}\n", t.Namespace);
+            Console.WriteLine("ReflectedType:{0}\n", t.ReflectedType);
+            Console.WriteLine("ToString():{0}\n", t.ToString());
+            Console.WriteLine("Name: {0}\n",t.Name);
+            Console.WriteLine("FullName: {0}\n",t.FullName);
+            Console.WriteLine("GUID: \n",t.GUID);             
             Console.ReadLine();
         }
     }
