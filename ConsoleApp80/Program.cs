dfb382a09339ee8b41bab0a973b66c4b3b71bb5f
Fred1987 <2864326836@qq.com>
2018-01-09 23:15:10 +0800

    Reflecting on Methods. The program class will be updated to define a number of static methods,each of which takes a single System.Type parameter and return void. First you have ListMethods(),which (as you might guess)  prints the name of each method defined by the incoming type.Notice how Type.GetMethods() returns an array of System.Refection.MethodInfo objects,which you can enumerated over using a standard foreach loop, as follows

--- ConsoleApp80/Program.cs
+++ ConsoleApp80/Program.cs
@@ -13,18 +13,80 @@ namespace ConsoleApp80
     {
         static void Main(string[] args)
         {
-            Type t = typeof(SuperCar);
-            Console.WriteLine("Assembly: {0}\n", t.Assembly);
-            Console.WriteLine("BaseType: {0}\n", t.BaseType);
-            Console.WriteLine("MemberType: {0}\n", t.MemberType);
-            Console.WriteLine("Module: {0}\n", t.Module);
-            Console.WriteLine("NameSpace: {0}\n", t.Namespace);
-            Console.WriteLine("ReflectedType:{0}\n", t.ReflectedType);
-            Console.WriteLine("ToString():{0}\n", t.ToString());
-            Console.WriteLine("Name: {0}\n",t.Name);
-            Console.WriteLine("FullName: {0}\n",t.FullName);
-            Console.WriteLine("GUID: \n",t.GUID);             
+            Type type = Type.GetType("ConsoleApp80.MathClass");
+            ListMethods(type);
+
             Console.ReadLine();
         }
+
+        //Display method names of type.
+        static void ListMethods(Type type)
+        {
+            Console.WriteLine("*****Methods information*****\n");
+            MethodInfo[] mis = type.GetMethods();
+            foreach(MethodInfo mi in mis)
+            {
+                Console.WriteLine("Name: {0}\n", mi.Name);
+                Console.WriteLine("Module: {0}\n", mi.Module);
+                Console.WriteLine("MemberType: {0}\n", mi.MemberType);
+                Console.WriteLine("\n\n");
+            }
+        }
+    }
+
+    public class MathClass
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Sum = 0;
+
+        public MathClass()
+        {
+
+        }
+
+        public MathClass(int x,int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Add(int x,int y)
+        {
+            Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+        }
+
+        public void Subtract(int x,int y)
+        {
+            Console.WriteLine("{0}-{1}={2}\n", x, y, x - y);
+        }
+
+        public int Multiply(int x,int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x,int y)
+        {
+            if(y==0)
+            {
+                throw new DivideByZeroException();
+            }
+            return x / y;
+        }
+
+        public virtual void RemainMethod(int x,int y)
+        {
+            if(y==0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            Console.WriteLine("{0}%{1}={2}\n", x, y, x % y);
+        }
+
+        public delegate void MathDel(int x, int y);
+        public event MathDel MathEvent;
     }
 }
