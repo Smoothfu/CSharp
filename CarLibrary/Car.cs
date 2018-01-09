58ac5deeb2c834cbb874b9645e5bc5876dfea1c7
Fred1987 <2864326836@qq.com>
2018-01-09 21:37:21 +0800

    Understanding the <codeBase> Element.Application configuration files can also specify code bases.The <codeBase> element can be used to instruct the CLR to probe for dependent assemblies located at arbitrary locations. If the value assigned to a <CodeBase> element is located on a remote machine,the assembly will be downloaded on demand to specific directory in the GAC termed the download cache.Given what you have learned about deploying assemblies to the GAC,it should make sense that assemblies loaded from a codeBase element will be assigned a strong name

---
+++ CarLibrary/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace CarLibrary
+{
+    public  class Car
+    {
+        public string CarName { get; set; }
+        public double CurrentSpeed { get; set; }
+        public double MaxSpeed { get; set; }
+        public Car(string carName,double curSpeed)
+        {
+            CarName = carName;
+            CurrentSpeed = curSpeed;
+        }
+
+        public Car(string carName,double curSpeed,double maxSpeed):this(carName,curSpeed)
+        {
+            MaxSpeed = maxSpeed;
+        }
+
+        public Car()
+        {
+
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}'s speed is :{1}\n", CarName, CurrentSpeed);
+        }
+
+        public virtual void Turbust()
+        {
+            Console.WriteLine("The car is turbust!\n");
+        }
+    }
+
+    public class SuperCar : Car
+    {
+        public SuperCar(string carName, double curSpeed, double maxSpeed) : base(carName, curSpeed, maxSpeed)
+        {
+        }
+
+        public override void Turbust()
+        {
+            Console.WriteLine("{0}'s speed is too fast!\n", CarName);
+        }
+    }
+
+    public class MiniVan : Car
+    {
+        public MiniVan(string carName, double curSpeed, double maxSpeed) : base(carName, curSpeed, maxSpeed)
+        {
+        }
+
+        public override void Turbust()
+        {
+            Console.WriteLine("{0}'s speed is too low!\n", CarName);
+        }
+    }
+}
---
+++ ConsoleApp80/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace ConsoleApp80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperCar sc = new SuperCar("BENZ",50.0,100.0);
+            Console.WriteLine(sc);
+            sc.Turbust();
+
+            MiniVan mv = new MiniVan("MiniCooper",25.0,50.0);
+            Console.WriteLine(mv);
+            mv.Turbust();
+
+            Console.ReadLine();
+        }
+    }
+}
