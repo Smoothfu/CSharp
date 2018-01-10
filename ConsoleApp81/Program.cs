f5f2c2e43d08ac00f2fa5fb64d78d6093ae9ddc5
Fred1987 <2864326836@qq.com>
2018-01-10 21:05:15 +0800

    In both cases,notice how the myShapes namespace acts as the conceptual container of these classes.When another namespace wants to use types in a separate namespace,you use the using keyword,just as you would when using namespaces of the .net base class libraries.

---
+++ ConsoleApp81/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp81
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hexagon h = new Hexagon();
+            Circle c = new Circle();
+            Square s = new Square();
+            Console.ReadLine();
+        }
+    }
+
+    //Circle class
+    public class Circle
+    {
+        /*Interesting members...*/
+        public Circle()
+        {
+            Console.WriteLine("This is Circle!\n");
+        }
+    }
+
+    //Hexagon class
+    public class Hexagon
+    {
+        /* More interesting members...*/
+        public Hexagon()
+        {
+            Console.WriteLine("This is Hexagon!\n");
+        }        
+     }
+
+    //Square class
+    public class Square
+    {
+        /*Even more interesting members...*/
+
+        public Square()
+        {
+            Console.WriteLine("This is Square!\n");
+        }
+    }
+}
