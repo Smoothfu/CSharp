1440b778d071d301547286cb88b03b27fe6fa9e9
Fred1987 <2864326836@qq.com>
2018-01-11 21:19:13 +0800

    Building a C# Client Application. Because each ofo the CarLibrary types has been declared using the public keyword,other .NET applications are able to use them as well. Recall that you may also define types using the C# internal keyword in fact this is the default C# access mode. Internal types can be used only by the assembly in which they are defined.External clients can neither see nor create types marked with the internal keyword.To use your librarys functionality,create a new C# console Application project named

---
+++ CarLibrary/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarLibrary
+{
+
+    //Represents the state of the engine.
+    public enum EngineState
+    {
+        engineAlive,engineDead
+    }
+
+    //The abstract base class in the hierarchy.
+
+    public abstract class Car
+    {
+        public string CarName { get; set; }
+        public double CurrentSpeed { get; set; }
+        public double MaxSpeed { get; set; }
+
+        protected EngineState engineState = EngineState.engineAlive;
+        public EngineState EngineState
+        {
+            get
+            {
+                return engineState;
+            }
+        }
+
+        public abstract void TurboBoost();
+
+        public Car()
+        {
+
+        }
+
+        public Car(string carName,double maxSp,double currSp)
+        {
+            CarName = carName;
+            MaxSpeed = maxSp;
+            CurrentSpeed = currSp;
+        }
+    }
+
+    public class SportsCar : Car
+    {
+        public SportsCar()
+        {
+
+        }
+
+        public SportsCar(string carName, double maxSp, double currSp) : base(carName, maxSp, currSp)
+        {
+
+
+        }
+        public override void TurboBoost()
+        {
+            MessageBox.Show("Ramming Speed!", "Faster is forbidden!");
+        }
+    }
+
+    public class MiniVan : Car
+    {
+        public MiniVan()
+        {
+
+        }
+
+        public MiniVan(string carName,double maxSp,double currSp):base(carName,maxSp,currSp)
+        {
+
+        }
+        public override void TurboBoost()
+        {
+            //MiniVans have poor turbo capabilities!
+            engineState = EngineState.engineDead;
+            MessageBox.Show("Eek!", "You have a good rest!");
+        }
+    }
+
+    public class Class1
+    {
+    }
+}
--- ConsoleApp81/Program.cs
+++ ConsoleApp81/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CarLibrary;
 
 namespace ConsoleApp81
 {
@@ -10,75 +11,22 @@ namespace ConsoleApp81
     {
         static void Main(string[] args)
         {
-            Hexagon h = new Hexagon();
-            Circle c = new Circle();
-            Square s = new Square();
+            Console.WriteLine("*****C# CarLibrary Client App*****\n");
 
-            My3DShapes.Circle c3 = new My3DShapes.Circle();
-            My3DShapes.Hexagon h3 = new My3DShapes.Hexagon();
-            My3DShapes.Square s3 = new My3DShapes.Square();
+            //Make a sport car.
+            SportsCar sc = new SportsCar("BENZ", 100, 50);
+            sc.TurboBoost();
+
+            //Make a minivan.
+            MiniVan mv = new MiniVan();
+            mv.TurboBoost();
+
+            Console.WriteLine("Done! Press any key to terminate!");
+
             Console.ReadLine();
         }
     }
-
-    //Circle class
-    public class Circle
-    {
-        /*Interesting members...*/
-        public Circle()
-        {
-            Console.WriteLine("This is Circle!\n");
-        }
-    }
-
-    //Hexagon class
-    public class Hexagon
-    {
-        /* More interesting members...*/
-        public Hexagon()
-        {
-            Console.WriteLine("This is Hexagon!\n");
-        }        
-     }
-
-    //Square class
-    public class Square
-    {
-        /*Even more interesting members...*/
-
-        public Square()
-        {
-            Console.WriteLine("This is Square!\n");
-        }
-    }
 }
 
-namespace My3DShapes
-{
-    //3D Circle class.
-    public class Circle
-    {
-        public Circle()
-        {
-            Console.WriteLine("This is the 3D Circle!\n");
-        }
-
-    }
-    public class Hexagon
-    {
-        public Hexagon()
-        {
-            Console.WriteLine("This is the 3D Hexagon!\n");
-        }
-    }
-
-    public class Square
-    {
-        public Square()
-        {
-            Console.WriteLine("This is the 3D Square!\n");
-        }
-    }
-}
 
 
