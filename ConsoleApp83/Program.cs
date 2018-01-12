e4879bc7bda9c4a0c71ee4ae5fa48590ffcd6bfc
Fred1987 <2864326836@qq.com>
2018-01-13 00:28:46 +0800

    MultiThreaded,Parallel,and Async Programming.The .net Platform provides a number of ways for you to build software that can perform complex operations on unique paths of execution,with far fewer pain points

---
+++ ConsoleApp83/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp83
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtractExecutingThread();
+            Console.ReadLine();
+        }
+
+        static void ExtractExecutingThread()
+        {
+            //Get the method currently executing this method.
+            Thread currentThread = Thread.CurrentThread;
+            Console.WriteLine("ManagedThreadId: {0}\n",currentThread.ManagedThreadId);
+        }
+    }
+}
