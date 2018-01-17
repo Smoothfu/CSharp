cb66e96d91a7b72c3acc987393609f7dafe1049f
Fred1987 <2864326836@qq.com>
2018-01-17 22:27:32 +0800

    A single thread may also be moved into a particular context at any given time,and it may be relocated within a new context at the whim of the CLR.When you want to obtain the current context a thread happens to be executing in,use the static Thread.CurrentContext property which returns a System.Runtime.Remoting.Contexts.Context object

---
+++ ConsoleApp85/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.Remoting.Contexts;
+
+namespace ConsoleApp85
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtractCurrentThreadContext();
+            Console.ReadLine();
+        }
+
+        static void ExtractCurrentThreadContext()
+        {
+            //Obtain the context under which the curret thread is operating.
+            Context ctx = Thread.CurrentContext;
+            Console.WriteLine("ContextID: {0}\nctx:{1}",ctx.ContextID,ctx.ToString());
+        }
+    }
+}
