d22ce7d565d9023b0ec2795288798905516fa71c
Fred1987 <2864326836@qq.com>
2018-01-17 18:27:43 +0800

    The System.Threading namespace was released with .NET 1.0 and offers one approach to build multithreaded applications.The thread class is perhaps the core type,as it represents a given thread.If you want to programmatically obtain a reference to the thread currently executing a given member,simply call the static Thread.CurrentThread property

---
+++ ConsoleApp84/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp84
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtractExecutingThread();
+            Console.ReadLine();
+        }
+
+        static void ExtractExecutingThread()
+        {
+            //Get the thread currently executing this method.
+            Thread currThread = Thread.CurrentThread;
+            Console.WriteLine("ManagedThreadId: {0}\n",currThread.ManagedThreadId);
+            Console.WriteLine("IsAlive:{0}\nIsBackground:{1}\nIsThreadPoolThread:{2}\nCurrentCulture:{3}\n"+
+                "CurrentUICulture:{4}\nName:{5}\nPriority:{6}\nThreadState:{7}\n",currThread.IsAlive,
+                currThread.IsBackground, currThread.IsThreadPoolThread, 
+                currThread.CurrentCulture.ToString(), currThread.CurrentUICulture.ToString(),
+                currThread.Name,currThread.Priority, currThread.ThreadState);
+        }
+    }
+}
