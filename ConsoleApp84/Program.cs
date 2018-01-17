c45267dedfb6ff18b348ad7c57f8ad56f484323f
Fred1987 <2864326836@qq.com>
2018-01-17 18:49:32 +0800

    A single thread may also be moved into a particular context at any given time,and it may be relocated within a new context at the whim of the CLR.When you want to obatin the current context a thread happens to be executing in,use the static Thread.CurrentContext property which returns a System.Runtime.Remoting.Contexts.Context object

--- ConsoleApp84/Program.cs
+++ ConsoleApp84/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp84
     {
         static void Main(string[] args)
         {
-            ExtractExecutingThread();
+            ExtractAppDomainHostingThread();
             Console.ReadLine();
         }
 
@@ -25,6 +25,20 @@ namespace ConsoleApp84
                 currThread.IsBackground, currThread.IsThreadPoolThread, 
                 currThread.CurrentCulture.ToString(), currThread.CurrentUICulture.ToString(),
                 currThread.Name,currThread.Priority, currThread.ThreadState);
+        }
+
+        static void ExtractAppDomainHostingThread()
+        {
+            //obtain the AppDomain hosting the current thread.
+            AppDomain ad = Thread.GetDomain();
+            string str = string.Format("ApplicationIdentity:{0}\nBaseDirectory:{1}\nDynamicDirectory:{2}\n" +
+            "FriendlyName:{3}\nId:{4}\nIsDefaultAppDomain:{5}\nIsFinalizingForUnload:{6}\nIsFullyTrusted:{7}\n" +
+            "IsHomogenous:{8}\nPermissionSet:{9}\nRelativeSearchPath:{10}",
+            ad.ApplicationIdentity, ad.BaseDirectory, ad.DynamicDirectory,
+            ad.FriendlyName, ad.Id, ad.IsDefaultAppDomain(), ad.IsFinalizingForUnload(),
+            ad.IsFullyTrusted, ad.IsHomogenous,
+            ad.PermissionSet, ad.RelativeSearchPath);
+            Console.WriteLine(str);
         }
     }
 }
