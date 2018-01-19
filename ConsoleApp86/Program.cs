b21ce06398ea8ed2828401828561da9d1ceae723
Fred1987 <2864326836@qq.com>
2018-01-19 21:02:03 +0800

    Recall that the generated Invoke() method is used to invoke the methods maintained by a delegate object in a synchronous manner.Therefore,the calling thread such as primary thread of the application is forced to wait until the delegate invocation completes.Also recall that in C# the Invoke() method does not need to be directly called in code but can be triggered indirectly, under the hood,when applying normal method in invocation syntax.Consider the following Console Application program SyncDelegateReview which invokes the static Add() method in a synchronous aka blocking manner be sure to import the System.Threading namesapce into your C# code file,as you will be calling the Thread.Sleep() method

---
+++ ConsoleApp86/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp86
+{
+    public delegate int BinaryOp(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Synchnorous Delegate Review*****\n");
+
+            //Print out the ID of the executing thread.
+            Console.WriteLine("Main() invoked on thread {0}\n", Thread.CurrentThread.ManagedThreadId);
+
+            //Invoke Add() in a synchronous manner.
+            BinaryOp bo = new BinaryOp(Add);
+            int answer = bo(10, 10);
+
+            //These lines will not execute until the Add() method has completed.
+            Console.WriteLine("Doing more work in Main()!\n");
+            Console.WriteLine("10+10 is {0}\n", answer);
+            Console.ReadLine();
+        }
+
+        static int Add(int x,int y)
+        {
+            //Print out the ID of the executing thread.
+            Console.WriteLine("Add() invoked on thread {0}.\n", Thread.CurrentThread.ManagedThreadId);
+
+            //Pause to sumulate a lengthy operation.
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
