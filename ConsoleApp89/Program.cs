6c62baf82257d43b36ae13d21e88ec7a8a58faf5
Fred1987 <2864326836@qq.com>
2018-01-22 23:01:05 +0800

    To begin,set a reference to the System.Windows.Forms.dll assembly and import the System.Windows.Forms namespace and display a message within Main() using MessageBox.Show() you will see the point of doing so after you run the program.Here is the complete implementation of Main()

--- ConsoleApp89/Program.cs
+++ ConsoleApp89/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace ConsoleApp89
 {
@@ -12,8 +13,42 @@ namespace ConsoleApp89
         //Be sure to import the System.Threading namespace.
         static void Main(string[] args)
         {
+
+            Console.WriteLine("*****The Amazing Thread App!*****\n");
+            Console.WriteLine("Do you want [1] or [2] threads?\n");
+
+            string threadCount = Console.ReadLine();
+
+            //Name the current thread.
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "Primary";
+
+            //Display Thread info.
+            Console.WriteLine("-> {0} is executing Main()!\n", Thread.CurrentThread.Name);
+
+            //Make worker class.
             Printer printer = new Printer();
-            printer.PrintNumbers();
+
+            switch(threadCount)
+            {
+                case "2":
+                    //Now make the thread.
+                    Thread backgroundThread = new Thread(new ThreadStart(printer.PrintNumbers));
+                    backgroundThread.Name = "Secondary";
+                    backgroundThread.Start();
+                    break;
+
+                case "1":
+                    printer.PrintNumbers();
+                    break;
+
+                default:
+                    Console.WriteLine("I do not know what you want...you get 1 thread.\n");
+                    goto case "1";
+            }
+
+            //Do some additional work.
+            MessageBox.Show("I am busy!", "Work on main thread...");
             Console.ReadLine();
         }
     }
@@ -30,7 +65,7 @@ namespace ConsoleApp89
 
             for(int i=0;i<10;i++)
             {
-                Console.WriteLine("{0}\t", i);
+                Console.Write("{0}\t", i);
                 Thread.Sleep(2000);
             }
             Console.WriteLine("\n\n\n");
