ba4311ab19eebf4b2b318f711a8d9158245a2d99
Fred1987 <2864326836@qq.com>
2018-01-23 23:19:06 +0800

    The AutoResetEvent class,in these first few examples,you have used a few crude ways to inform the primary thread to wait until the secondary thread has completed.During your examination of asynchronous delegates,you used a simple bool variable as a toggle.however,this is not a recommended solution,as both threads can access the same point of data,and this can lead to data corruption.A safer but still undesirable alternative is to call Thread.Sleep() for a fixed amout of time. The problem here is that you do not want to wait longer than necessary.One Simple,and thread-safe,in the thread that needs to wait such  as a Main() method,create an insatnce of this class and pass in false to the constructor to signify you have not yet been notified.Then,at the point at which you are willing to wait, call the WaitOne() method.Here is the update to the program class,which will do this very thing using a static-level AutoResetEvent member variable

---
+++ ConsoleApp88/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp88
+{
+    class Program
+    {
+        private static AutoResetEvent waitHandle = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Adding with Thread objects*****\n");
+            Console.WriteLine("ID of thread in Main():{0}\n", Thread.CurrentThread.ManagedThreadId);
+            AddParams ap = new AddParams(10, 10);
+
+            Thread t = new Thread(new ParameterizedThreadStart(Add));
+            t.Start(ap);
+
+
+            //Wait here untile you are notified.
+            waitHandle.WaitOne();
+            Console.WriteLine("Other thread is done!\n");
+            Console.ReadLine();
+        }
+
+        static void Add(object data)
+        {
+            if(data is AddParams)
+            {
+                Console.WriteLine("Id of thread in Add(): {0}\n", Thread.CurrentThread.ManagedThreadId);
+                AddParams ap = (AddParams)data;
+                Console.WriteLine("{0}+{1}={2}\n", ap.X, ap.Y, ap.X + ap.Y);
+
+                //Tell other thread we are done.
+                waitHandle.Set();
+            }
+        }
+    }
+
+    public class AddParams
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public AddParams(int x,int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
