a9467a08a470873ae442d2e60cb5f4b5ac90196c
Fred1987 <2864326836@qq.com>
2018-01-25 00:52:16 +0800

    ThreadPool.SexMaxThreads(int workerThreads,int completionPortThreads) Sets the number of requests to the thread pool that can be active concurrently.All requests above that number remain queued until thread pool threads become available.workerThreads,the maximum number of worker threads in the thread pool;CompletionPortThreads.The maximum number of asynchronous I/O threads in the thread pool

--- ConsoleApp90/Program.cs
+++ ConsoleApp90/Program.cs
@@ -5,27 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Runtime.Remoting.Contexts;
+using System.Windows.Forms;
 
 namespace ConsoleApp90
 {
     class Program
     {
+
+        //总收入
+        public static float money = 0;
+        private static object lockObj = new object();
         static void Main(string[] args)
         {
-            Console.WriteLine("*****Fun with the CLR Thread Pool*****\n");
-            Console.WriteLine("Main thread started. ThreadId={0}\n", Thread.CurrentThread.ManagedThreadId);
-
-            Printer p = new Printer();
-            WaitCallback workItem = new WaitCallback(PrintTheNumbers);
-
-
-            //Queue the method ten  times.
-            for(int i=0;i<10;i++)
+            //最多5个线程
+            ThreadPool.SetMaxThreads(5, 5);
+            lock(lockObj)
             {
-                ThreadPool.QueueUserWorkItem(workItem, p);
+                for (int i = 0; i < 60; i++)
+                {
+                    ThreadPool.QueueUserWorkItem(CalculateSalary, 5000);
+                }
             }
-            Console.WriteLine("All Tasks queued!\n");
+            
+            Thread.Sleep(1000);
+
+            MessageBox.Show(money.ToString());
             Console.ReadLine();
+        }
+
+        private static void CalculateSalary(object mny)
+        {
+            money += int.Parse(mny.ToString());
+            Console.WriteLine("The money parameter is :{0}\n", money.ToString());
         }
 
         static void PrintTheNumbers(object state)
