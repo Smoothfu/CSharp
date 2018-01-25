7180a88cbbfde808fe71edc4fe2a5bed1a70032e
Fred1987 <2864326836@qq.com>
2018-01-25 20:41:10 +0800

    [SecuritySafeCritical] public static bool QueueUserWorkItem(WaitCallback callBack). Queues a method for execution.The method executes when a thread pool thread becomes available.parameters,callback.A System.Threading.WaitCallback that represents the method to be executed.returns true if the method is successfully queued;System.NotSupportedException is thrown if the work item could not be queued.Exceptions.System.ArgumentNullException callback is null.System.NotSupportedException. The common language runtime CLR is hosted,and the host does not support this action

---
+++ ConsoleApp91/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp91
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mainThread = Thread.CurrentThread;
+            Console.WriteLine("MainThread ManagedThreadId :{0}\n", mainThread.ManagedThreadId);
+
+            //Queue the task.
+            ThreadPool.QueueUserWorkItem(ThreadProc);
+            Console.WriteLine("Main thread does some work,then sleeps.\n");
+            Thread.Sleep(2000);
+            Console.WriteLine("Main thread exists.\n");
+            Console.ReadLine();
+        }
+
+        //This method procedure performs the task.
+        static void ThreadProc(object stateInfo)
+        {
+            //No state object was passed to QueueUserWorkItem,so stateinfo is null.
+            Thread thread = Thread.CurrentThread;
+            Console.WriteLine("ThreadProc ManagerThreadId :{0}\n", thread.ManagedThreadId);
+            Console.WriteLine("Hello from the thread pool.\n");
+        }
+    }
+}
