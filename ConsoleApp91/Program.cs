6c7a4001f9776580070e59f6ad257e474f1a98fa
Fred1987 <2864326836@qq.com>
2018-01-25 21:34:53 +0800

    [SecuritySafeCritical] public static bool QueueUserWorkItem(WaitCallback callback).Queues a method for execution.The method executes when a thread pool thread becomes available.callback,a system.Threading.WaitCallback that represents the method to be executed.true if the method is successfully queued.System.NotSupportedException is thrown if the work item could  not been queued

--- ConsoleApp91/Program.cs
+++ ConsoleApp91/Program.cs
@@ -12,45 +12,16 @@ namespace ConsoleApp91
     {
         static void Main(string[] args)
         {
-            Action<object> action = (object obj) =>
-              {
-                  Console.WriteLine("Task={0},obj={1},Thread={2}\n", Task.CurrentId, obj, Thread.CurrentThread.ManagedThreadId);
-              };
+            //Queue the task.
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc));
 
-            //Create a task but do not start it.
-            Task t1 = new Task(action, "alpha Task t1 = new Task(action");
+            Console.WriteLine("Main thread does some work,then sleeps!\n");
 
-            //Constructor a started task.
-            Task t2 = Task.Factory.StartNew(action, "betaTask.Factory.StartNew");
+            //If you comment out the Sleep,the main thread exists before the thread pool task runs,The thread pool uses background threads
+            //which do not keep the application running.
+            //Thread.Sleep(1000);
 
-            //Block the main thread to demonstrate that t2 is executing.
-            t2.Wait();
-
-            //Launch t1
-            t1.Start();
-            Console.WriteLine("t1 has been launched.(Main Thread={0})\n", Thread.CurrentThread.ManagedThreadId);
-
-            //wait for the task to finish.
-            t1.Wait();
-
-            //Construct a started task using Task.Run.
-            string taskData = "delta";
-            Task t3 = Task.Run(() =>
-            {
-                Console.WriteLine("Task={0},obj={1},Thread={2}\n", Task.CurrentId, taskData, Thread.CurrentThread.ManagedThreadId);
-            });
-
-            //wait for the task to finish
-            t3.Wait();
-
-            //Construct an unstarted task.
-            Task t4 = new Task(action, "gamma");
-            //Run it synchronously.
-            t4.RunSynchronously();
-
-            //Although the task was run synchronously,it is a good practice to wait for it in the event 
-            // exceptions were thrown by the task.
-            t4.Wait();
+            Console.WriteLine("This is the end of the Main thread!\n");
             Console.ReadLine();
         }
 
