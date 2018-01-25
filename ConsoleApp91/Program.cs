4fcda2dd36d59b288443c3e74b2a52418d0440fb
Fred1987 <2864326836@qq.com>
2018-01-25 21:21:29 +0800

    public delegate void Action<int T>(T obj); Encapsulates a method that has a single parameter and does not return a value to browse the .NET Framework source code for this type,see the reference source.Parameters.obj,The parameter of the method that this delegate encapsulatesType parameter The type of the parameter of the method that this delegate encapsulates

--- ConsoleApp91/Program.cs
+++ ConsoleApp91/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleApp91
 {
@@ -11,14 +12,45 @@ namespace ConsoleApp91
     {
         static void Main(string[] args)
         {
-            Thread mainThread = Thread.CurrentThread;
-            Console.WriteLine("MainThread ManagedThreadId :{0}\n", mainThread.ManagedThreadId);
+            Action<object> action = (object obj) =>
+              {
+                  Console.WriteLine("Task={0},obj={1},Thread={2}\n", Task.CurrentId, obj, Thread.CurrentThread.ManagedThreadId);
+              };
 
-            //Queue the task.
-            ThreadPool.QueueUserWorkItem(ThreadProc);
-            Console.WriteLine("Main thread does some work,then sleeps.\n");
-            Thread.Sleep(2000);
-            Console.WriteLine("Main thread exists.\n");
+            //Create a task but do not start it.
+            Task t1 = new Task(action, "alpha");
+
+            //Constructor a started task.
+            Task t2 = Task.Factory.StartNew(action, "beta");
+
+            //Block the main thread to demonstrate that t2 is executing.
+            t2.Wait();
+
+            //Launch t1
+            t1.Start();
+            Console.WriteLine("t1 has been launched.(Main Thread={0})\n", Thread.CurrentThread.ManagedThreadId);
+
+            //wait for the task to finish.
+            t1.Wait();
+
+            //Construct a started task using Task.Run.
+            string taskData = "delta";
+            Task t3 = Task.Run(() =>
+            {
+                Console.WriteLine("Task={0},obj={1},Thread={2}\n", Task.CurrentId, taskData, Thread.CurrentThread.ManagedThreadId);
+            });
+
+            //wait for the task to finish
+            t3.Wait();
+
+            //Construct an unstarted task.
+            Task t4 = new Task(action, "gamma");
+            //Run it synchronously.
+            t4.RunSynchronously();
+
+            //Although the task was run synchronously,it is a good practice to wait for it in the event 
+            // exceptions were thrown by the task.
+            t4.Wait();
             Console.ReadLine();
         }
 
