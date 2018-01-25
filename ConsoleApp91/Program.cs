41cebecd3a78f4746324d1c79708af7e1ca26fef
Fred1987 <2864326836@qq.com>
2018-01-25 22:45:05 +0800

    Task.Run(Action action),Queues the specified work to run on the thread pool and returns a System.Threading.Tasks.Task object that represents that work.action,The work to execute asynchronously.Returns:A task that represents the work queued to execute in the ThreadPool

--- ConsoleApp91/Program.cs
+++ ConsoleApp91/Program.cs
@@ -12,71 +12,21 @@ namespace ConsoleApp91
     {
         static void Main(string[] args)
         {
-            //The main thread uses AutoResetEvent to signal the registered wait handle,which executes the callback method.
-            AutoResetEvent ev = new AutoResetEvent(false);
-
-            TaskInfo ti = new TaskInfo();
-            ti.otherInfo = "First task";
-
-            //The TaskInfo for the task includes the registered wait handle returned by RegisterWaitForSingleObject.This allows
-            //the wait to be terminated when the object has been singaled once.
-
-            ti.handle = ThreadPool.RegisterWaitForSingleObject(ev,
-                new WaitOrTimerCallback(WaitProc), ti, 1000, false);
-
-            //The main thread waits three seconds,to demonstrate the time-outs on the queued thread.and then signals.
-            Thread.Sleep(3100);
-            Console.WriteLine("Main thread signals.\n");
-            ev.Set();
-
-            //The main thread sleeps,which should give the callback method time to execute. If you comment out this line,
-            //the program usually ends before the ThreadPool thread can execute.
-            Thread.Sleep(1000);
-
-            //If you start a thread yourself,you can wait for it to end by calling Thread.Join.This option is not 
-            //available with thread pool threads.
-
-            Console.ReadLine();
-             
-
-            Console.ReadLine();
-        }
-
-        public static void WaitProc(object state,bool timedout)
-        {
-            //The state object must be cast to the correct type,because the signature of the WaitOrTimerCallback
-            //delegate specifies type object.
-            TaskInfo ti = (TaskInfo)state;
-            string cause = "TIMED OUT";
-            if(!timedout)
+            Task t = Task.Run(() =>
             {
-                cause = "SIGNALED";
-                //If the callback method executes because the WaitHandle is signaled,stop future execution
-                //of the callback method by unregistering the WaitHandle.
-                if(ti.handle!=null)
+                //Just loop.
+                int ctr = 0;
+                for(ctr=0;ctr<=10000000;ctr++)
                 {
-                    ti.handle.Unregister(null);
+
                 }
-            }
+                Console.WriteLine("Finished {0} loop iterations!\n", ctr);
+
+            });
+            t.Wait();
+            
+            Console.ReadLine();
         }
-
-        //This method procedure performs the task.
-        static void ThreadProc(object stateInfo)
-        {
-            //No state object was passed to QueueUserWorkItem,so stateinfo is null.
-            Thread thread = Thread.CurrentThread;
-            Console.WriteLine("ThreadProc ManagerThreadId :{0} \n", thread.ManagedThreadId);
-            Console.WriteLine("Hello from the thread pool.\n");
-        }
-    }
-
-    //TaskInfo contains data that will be passed to the callback method.
-    public class TaskInfo
-    {
-        public RegisteredWaitHandle handle = null;
-        public string otherInfo = "default";
-    }
-
-    
-     
+         
+    }    
 }
