f8455edb4f8565a79b138b8dc71a1e4250018365
Fred1987 <2864326836@qq.com>
2018-01-25 23:30:14 +0800

    TypeForwardedFrom public delegate void Action() encapsulates a method taht has no parameters and does not return a value

---
+++ ConsoleApp92/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp92
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Name instance = new Name("NameInstance");
+            Action action = instance.DisplayToWindow;
+            action();
+            Console.ReadLine();
+        }
+    }
+
+    public class Name
+    {
+        private string instanceName;
+        public Name(string name)
+        {
+            this.instanceName = name;
+        }
+
+        private void DisplayToConsole()
+        {
+            Console.WriteLine(instanceName);
+        }
+
+        public void DisplayToWindow()
+        {
+            MessageBox.Show(instanceName);
+        }
+    }
+}
