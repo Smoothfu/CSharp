bd9ca71f1e62dd3c9b52ea2b6663b7ee5b70f322
Fred1987 <2864326836@qq.com>
2018-01-26 21:51:09 +0800

    [ComVisible(true)]public delegate void TimerCallback(object state). Represents the method that handles calls from a System.Threading.Timer.state,An object containing application-specific information relevant to the method invoked by this delegate

---
+++ ConsoleApp93/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp93
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Working with Timer type*****\n");
+
+            //Create the delegate for the Timer type.
+            TimerCallback timeCB = new TimerCallback(PrintTime);
+
+
+            //Establish timer settings.
+            Timer timer = new Timer(
+                timeCB,     //The TimerCallback delegate object.
+                null,       //Any info to pass into the called method null for no info.
+                0,          //Amount of time to wait before starting in milliseconds.
+                1000        //Interval of time between calls in milliseconds
+                );
+
+            Console.WriteLine("Hit key to terminate...\n");
+            Console.ReadLine();
+        }
+
+        static void PrintTime(object state)
+        {
+            Console.WriteLine("Time is :{0}\n", DateTime.Now.ToLongTimeString());
+        }
+    }
+}
