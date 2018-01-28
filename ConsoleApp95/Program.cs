9b5599d5a4b8adc16fc188aa550816836a86dd7b
Fred1987 <2864326836@qq.com>
2018-01-29 01:21:01 +0800

    You can also supply a cancellation token by calling the Wait(CancellationToken) and Wait(Int32,CancellationToken) methods.If the tokens IsCancellationRequested property is true,the wait is cancelled.if it becomes true while the Wait method terminates.In some cases.you may want to wait for the first of a series of executing tasks to complete but do not care which task it is

--- ConsoleApp95/Program.cs
+++ ConsoleApp95/Program.cs
@@ -51,25 +51,48 @@ namespace ConsoleApp95
             //}
 
             //Wait on a single task with a timeout specified.
-            Task taskA = Task.Run(() => Thread.Sleep(2000));
+            //Task taskA = Task.Run(() => Thread.Sleep(2000));
+
+            //try
+            //{
+            //    //Wait for 1 second.
+            //    Console.WriteLine("Sleep over!\n");
+            //    taskA.Wait(1000);
+            //    bool completed = taskA.IsCompleted;
+            //    Console.WriteLine("Task A completed:{0} Status:{1}\n", completed, taskA.Status);
+
+            //    if(!completed)
+            //    {
+            //        Console.WriteLine("Timed out before task A completed.\n");
+            //    }
+            //}
+
+            //catch(AggregateException ex)
+            //{
+            //    Console.WriteLine("Exception in taskA.\n");
+            //}
+
+            var tasks = new Task[3];
+            var rnd = new Random();
+            for(int ctr=0;ctr<=2;ctr++)
+            {
+                tasks[ctr] = Task.Run(() => Thread.Sleep(rnd.Next(500, 3000)));
+            }
 
             try
             {
-                //Wait for 1 second.
-                Console.WriteLine("Sleep over!\n");
-                taskA.Wait(1000);
-                bool completed = taskA.IsCompleted;
-                Console.WriteLine("Task A completed:{0} Status:{1}\n", completed, taskA.Status);
-
-                if(!completed)
+                int index = Task.WaitAny(tasks);
+                Console.WriteLine("Task #{0} completed first.\n", tasks[index].Id);
+                Console.WriteLine("Status of all tasks: ");
+                foreach(var t in tasks)
                 {
-                    Console.WriteLine("Timed out before task A completed.\n");
+                    Console.WriteLine("Task #{0}:{1}\n", t.Id, t.Status);
                 }
             }
 
             catch(AggregateException ex)
             {
-                Console.WriteLine("Exception in taskA.\n");
+                Console.WriteLine("An exception occured.\n");
             }
             Console.ReadLine();
         }
