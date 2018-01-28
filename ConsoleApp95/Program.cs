43c615b09e42345cea26c22cf7d233dba6cb4f1e
Fred1987 <2864326836@qq.com>
2018-01-29 00:58:23 +0800

    Waiting for one or more tasks to complete.Because tasks typically run asynchronously on a thread pool thread,the thread that creates and starts the task continus execution as soon as the task has been instantiated.In some cases,when calling thread is the main application thread, the app may terminate before any the task actually being execution.In others,your application s logic may require that the calling thread continue execution only when one or more tasks has completed execution.You can synchronize the execution of the calling thread and the asynchronous tasks it launches ny calling a Wait method to wait for one or more tasks to complete.

--- ConsoleApp95/Program.cs
+++ ConsoleApp95/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApp95
 {
@@ -22,18 +23,32 @@ namespace ConsoleApp95
             //});
             //t.Wait();
 
-            Task t = Task.Factory.StartNew(() =>
+            //Task t = Task.Factory.StartNew(() =>
+            //{
+            //    //Just loop.
+            //    int ctr = 0;
+            //    for(ctr=0;ctr<=10000000;ctr++)
+            //    {
+
+            //    }
+            //    Console.WriteLine("Finished {0} loop iterations.\n", ctr);
+            //});
+
+            //t.Wait();
+
+            //Wait on a single task with no timeout specified.
+            Task taskA = Task.Run(() => Thread.Sleep(2000));
+            Console.WriteLine("taskA status: {0}\n", taskA.Status);
+
+            try
             {
-                //Just loop.
-                int ctr = 0;
-                for(ctr=0;ctr<=10000000;ctr++)
-                {
-
-                }
-                Console.WriteLine("Finished {0} loop iterations.\n", ctr);
-            });
-
-            t.Wait();
+                taskA.Wait();
+                Console.WriteLine("taskA Status: {0}\n", taskA.Status);
+            }
+            catch(AggregateException ex)
+            {
+                Console.WriteLine("Exception in Task A.\n");
+            }
             Console.ReadLine();
         }
     }
