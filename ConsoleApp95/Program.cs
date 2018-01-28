2a86f37bd10bb431614cc43e09f42851fb45520d
Fred1987 <2864326836@qq.com>
2018-01-29 01:07:41 +0800

    You can also contidionally wait for a task to complete.The Wait(int32) and Wait(TimeSpan) methods block the calling thread until the task finishes or a timeout interval elapses,whichever comes first.Since the following example launches a task that sleeps for two seconds but defines a one-second timeout value.the calling thread blocks until the timeout expires and before the task has completed execution

--- ConsoleApp95/Program.cs
+++ ConsoleApp95/Program.cs
@@ -37,17 +37,39 @@ namespace ConsoleApp95
             //t.Wait();
 
             //Wait on a single task with no timeout specified.
+            //Task taskA = Task.Run(() => Thread.Sleep(2000));
+            //Console.WriteLine("taskA status: {0}\n", taskA.Status);
+
+            //try
+            //{
+            //    taskA.Wait();
+            //    Console.WriteLine("taskA Status: {0}\n", taskA.Status);
+            //}
+            //catch(AggregateException ex)
+            //{
+            //    Console.WriteLine("Exception in Task A.\n");
+            //}
+
+            //Wait on a single task with a timeout specified.
             Task taskA = Task.Run(() => Thread.Sleep(2000));
-            Console.WriteLine("taskA status: {0}\n", taskA.Status);
 
             try
             {
-                taskA.Wait();
-                Console.WriteLine("taskA Status: {0}\n", taskA.Status);
+                //Wait for 1 second.
+                Console.WriteLine("Sleep over!\n");
+                taskA.Wait(1000);
+                bool completed = taskA.IsCompleted;
+                Console.WriteLine("Task A completed:{0} Status:{1}\n", completed, taskA.Status);
+
+                if(!completed)
+                {
+                    Console.WriteLine("Timed out before task A completed.\n");
+                }
             }
+
             catch(AggregateException ex)
             {
-                Console.WriteLine("Exception in Task A.\n");
+                Console.WriteLine("Exception in taskA.\n");
             }
             Console.ReadLine();
         }
