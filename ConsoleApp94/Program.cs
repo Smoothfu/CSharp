836dfead6703c1f174623156695bc7c5a4d9da12
Fred1987 <2864326836@qq.com>
2018-01-29 00:01:20 +0800

    When you use the Action<T> delegate,you do not have to explicitly define a delegate that encapsulates a method with a single parameter.For example,the following code explicitly declares a delegate named DisplayMessage assigns a reference to either the WriteLine method or the ShowWindowMessage method to its delegate instance

---
+++ ConsoleApp94/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp94
+{
+    delegate void DisplayMessageDel(string msg);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMessageDel msgDel;
+            if(Environment.GetCommandLineArgs().Length>0)
+            {
+                msgDel = ShowWindowsMessage;
+            }
+            else
+            {
+                msgDel = Console.WriteLine;
+            }
+
+            msgDel("This is the delegate method");
+            Console.ReadLine();
+        }
+
+        private static void ShowWindowsMessage(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+    }
+}
