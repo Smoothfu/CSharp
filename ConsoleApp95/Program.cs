5ddee76055611582779c49d2cd8e69190c395f06
Fred1987 <2864326836@qq.com>
2018-01-29 00:43:23 +0800

    Creating and executing a task.Task instances may be created in a variety of ways.The most common approach which is available starting the .NET Framework 4.5 is to call the static Run method.The Run method provides a simple way to start a task using default values and without requiring additional parameters.The following example uses the Run(Action) method to start a task that loops and then displays the number of loop iterations

---
+++ ConsoleApp95/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp95
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = Task.Run(() => {
+                //Just loop.
+                int ctr = 0;
+                for (ctr=0;ctr<=100000000;ctr++)
+                {
+                     
+                }
+
+                Console.WriteLine("Finished {0} loop iteration.\n", ctr);
+            });
+            t.Wait();
+            Console.ReadLine();
+        }
+    }
+}
