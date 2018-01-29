92f723e4992e28b24f39c250f848c193d16eb2c4
Fred1987 <2864326836@qq.com>
2018-01-29 23:51:11 +0800

    You can use this delegte to pass a method as a parameter without explicitly declaring a custom delegate.The encapsulated method must correspond to the method signature that is defined by this delegate.This means that the encapsulated method must have no paramters and no return value.In C#,the method must return void.Typically,such a method is used to perform an operation

--- ConsoleApp96/Program.cs
+++ ConsoleApp96/Program.cs
@@ -3,43 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ConsoleApp96
 {
+    public delegate void ShowValue();
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> namesList = new List<string>();
-            namesList.Add("Fred");
-            namesList.Add("Floomberg");
-            namesList.Add("Michael Bloomberg");
-            namesList.Add("Bill Gates");
-            namesList.Add("Jeff Bezos");
-            namesList.Add("Elon Musk");
-            namesList.Add("Larry Ellison");
-            namesList.Add("Larry Page");
-            namesList.Add("Pony Ma");
-            namesList.Add("Jack Ma");
-
-          
-            //Display the contents of the list using the PrintNames method.
-            namesList.ForEach(PrintNames);
-
-            Console.WriteLine("\n\n\n");
-            //The following demonstrates the anonymous method feature of C# to 
-            //display the contents of the list to the console.
-            namesList.ForEach(delegate (string name)
-            {
-                Console.WriteLine(name);
-            });
-
+            Name obj = new Name("Fred");
+            ShowValue showMethod = obj.DisplayToWindow;
+            showMethod();
             Console.ReadLine();
         }
 
         private static void PrintNames(string str)
         {
             Console.WriteLine(str);
+        }
+    }
+
+    public class Name
+    {
+        private string instanceName;
+
+        public Name(string name)
+        {
+            this.instanceName = name;
+        }
+
+        public void DisplayToConsole()
+        {
+            Console.WriteLine(this.instanceName);
+        }
+        public void DisplayToWindow()
+        {
+            MessageBox.Show(this.instanceName);
         }
     }
 }
