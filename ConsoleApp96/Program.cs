9e6f67096d44089adc11137db44a6f9bd3f79c9f
Fred1987 <2864326836@qq.com>
2018-01-29 23:35:28 +0800

    The following example demonstrates the use of the Action<T> delegate to print the contents of a List<T> object.In this example,the PrintNames method is used to display the contents of the list to the console.In addition,the C# example also demonstrates the use of anonymous methods to display the contents to the console.Note that the example does not explicitly declare an Action<T> variable.Instead,it passes a reference to a method that takes a single parameter and that does not return a value to the List<T>.ForEach method,whose single parameter is an Action<T> delegate.Similarly,in the C# example,an Action<T> delegate is not explicitly instantiated because the signature of the anonymous method matches the signature of the Action<T> delegate that is expected by the List<T>.ForEach method

---
+++ ConsoleApp96/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp96
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namesList = new List<string>();
+            namesList.Add("Fred");
+            namesList.Add("Floomberg");
+            namesList.Add("Michael Bloomberg");
+            namesList.Add("Bill Gates");
+            namesList.Add("Jeff Bezos");
+            namesList.Add("Elon Musk");
+            namesList.Add("Larry Ellison");
+            namesList.Add("Larry Page");
+            namesList.Add("Pony Ma");
+            namesList.Add("Jack Ma");
+
+          
+            //Display the contents of the list using the PrintNames method.
+            namesList.ForEach(PrintNames);
+
+            Console.WriteLine("\n\n\n");
+            //The following demonstrates the anonymous method feature of C# to 
+            //display the contents of the list to the console.
+            namesList.ForEach(delegate (string name)
+            {
+                Console.WriteLine(name);
+            });
+
+            Console.ReadLine();
+        }
+
+        private static void PrintNames(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
