ab0a4c26d8f0f3708f1afb74ac0d1cfd5e3d989b
Fred1987 <2864326836@qq.com>
2018-02-05 06:29:37 +0800

    The Main Thread.In C#,the System.Threading.Thread class is used for working with threads.It allows creating and accessing individual threads in a multithreaded application.The first thread to be executed in a process is called the main thread. When a C# program starts execution,the main thread is automatically created.The threads created using the Thread class are called the child threads of the main thread.You can access a thread using the CurrentThread property of the Thread class.

---
+++ ConsoleApp100/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread th = Thread.CurrentThread;
+            th.Name = "Main Thread";
+            Console.WriteLine("This is {0}\n", th.Name);
+            Console.ReadLine();
+        }
+    }
+}
