71f002f0dcc86ed855e9bb2b0a3006670a42a2c6
Fred1987 <2864326836@qq.com>
2018-02-05 06:49:57 +0800

    Managing Threads.The Thread class provides various methods for managing threads.The following example demonstrates the use of the Sleep() method for making a thread pause for a specific period of time

--- ConsoleApp100/Program.cs
+++ ConsoleApp100/Program.cs
@@ -12,15 +12,29 @@ namespace ConsoleApp100
         static void Main(string[] args)
         {
             ThreadStart childRef = new ThreadStart(CallToChildThread);
-            Console.WriteLine("In Main:Creating the child thread!\n");
+            Console.WriteLine("In Main:Creating the Child thread.!\n");
+
             Thread childThread = new Thread(childRef);
+
             childThread.Start();
             Console.ReadLine();
         }
 
+        //public static void CallToChildThread()
+        //{
+        //    Console.WriteLine("Child thread starts!\n");
+        //}
+
         public static void CallToChildThread()
         {
             Console.WriteLine("Child thread starts!\n");
+
+            //the thread is paused for 5000 milliseconds.
+            int sleepFor = 5000;
+
+            Console.WriteLine("Child Thread Paused for {0} seconds.\n", sleepFor / 1000);
+            Thread.Sleep(sleepFor);
+            Console.WriteLine("Child thread resumes!\n");
         }
     }
 }
