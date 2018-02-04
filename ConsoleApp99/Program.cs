9eafc614dedf457bb1b744e982881803e04e268b
Fred1987 <2864326836@qq.com>
2018-02-04 20:00:34 +0800

    Recall that the generated Invoke() method is used to invoke the methods maintained by a delegate object in a synchronous manner.Therefore,the calling thread such as the primary thread of the application is forced to wait until the delegate invocation completes.Also recall that in C# the Invoke() method does not need to be directly called in code but can be triggered indirectly.under the hood,when applying normal method invocation synctax.Consider the following ConsoleApplication program which invokes the static Add() method in a synchronous manner.be sure to import the System.Threading namespace into your C# code file as you will be calling the Thread.Sleep() method

--- ConsoleApp99/Program.cs
+++ ConsoleApp99/Program.cs
@@ -8,11 +8,26 @@ using System.Runtime.Remoting.Contexts;
 
 namespace ConsoleApp99
 {
+    delegate int BinaryOp(int x, int y);
     class Program
     {
+        
         static void Main(string[] args)
         {
-            ExtractCurrentThreadContext();
+            Console.WriteLine("*****Synch Delegate Review*****\n");
+
+            //Print out the ID of the executing thread.
+            Console.WriteLine("Main() invoked on thread {0}.\n", Thread.CurrentThread.ManagedThreadId);
+
+            //Invoke Add() in a synchronous manner.
+            BinaryOp bo = new BinaryOp(Add);
+
+            //Could also write bo.Invoke(10,10);
+            int answer = bo(10, 10);
+
+            //These lines will not  execute until the Add() method has completed.
+            Console.WriteLine("Doing more work in Main()!\n");
+            Console.WriteLine("10+10 is {0}\n", answer);
             Console.ReadLine();
         }
 
@@ -55,6 +70,16 @@ namespace ConsoleApp99
             Console.WriteLine("ContextID:{0}\n", ctx.ContextID);
             Console.WriteLine("ContextProperties:{0}\n", ctx.ContextProperties);
            
+        }
+
+        static int Add(int x,int y)
+        {
+            //Print out the ID of the executing thread.
+            Console.WriteLine("Add() invoked on thread {0}\n", Thread.CurrentThread.ManagedThreadId);
+
+            //Pause to simulate a lengthy operation.
+            Thread.Sleep(5000);
+            return x +y;
         }
     }
 }
