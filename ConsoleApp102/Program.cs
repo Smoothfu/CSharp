a15e8b5f064c706f7dbe40a0269ff3b71ca71923
Fred1987 <2864326836@qq.com>
2018-02-06 21:21:42 +0800

    Thread thread=new Thread(new ThreadStart(new Action(()=>{Add(x,y);})));

---
+++ ConsoleApp102/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1000, y = 1000;
+            Thread thread = new Thread(new ThreadStart(new Action(() => {
+                Add(x, y);
+            })));
+
+            thread.Start();
+            Console.ReadLine();
+        }
+
+        static void Add(int x,int y)
+        {
+            for(int i=0;i<10000;i++)
+            {
+                Console.WriteLine("{0}+{1}={2}", x, y, x + y);
+                x = x + 10000;
+                y = y + 10000;
+            }
+            
+        }
+    }
+}
---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!\n");
+            Console.WriteLine("10+10={0}\n", 10 + 10);
+            Console.ReadLine();
+        }
+    }
+}
