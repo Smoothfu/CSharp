beb142a34c1fd1ae6f64613384adc97d34d8c8f2
Fred1987 <2864326836@qq.com>
2018-02-07 07:49:03 +0800

    Invoking a Method Asynchronously.To instruct the BinaryOp delegate to invoke Add() asynchronously,you will modify the logic in the previous project feel free to addcode to the existing project;however,in your lab downloads,you will find a new ConsoleApplication project named AsyncDelegate.

---
+++ ConsoleApp103/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp103
+{
+    public delegate int BinaryDel(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Async Delegate Invocation*****\n");
+
+            //Print out the ID of the executing thread.
+            Console.WriteLine("Main() invoked on thread {0}.\n", Thread.CurrentThread.ManagedThreadId);
+
+            //Invoke Add() on a secondary thread.
+            BinaryDel del = new BinaryDel(Add);
+            IAsyncResult asyncResult = del.BeginInvoke(10, 10, null, null);
+
+            //Do other work on primary thread...
+            Console.WriteLine("Doing more work in Main()!\n");
+
+            //Obtain the result of the Add() method when ready.
+            int answer = del.EndInvoke(asyncResult);
+
+            Console.WriteLine("10+10={0}\n", answer);
+
+            Console.WriteLine("This is the end of the Main() thread!\n");
+
+            Console.ReadLine();
+        }
+
+        static int Add(int x,int y)
+        {
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
---
+++ ConsoleApp103/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp103")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp103")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6518b05b-c811-47db-8b7f-befc35a356a4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
