f299a402c49948b74815700fed4bdddac8cc9ab0
Fred1987 <2864326836@qq.com>
2018-02-06 21:43:59 +0800

    .NET delegate is essentially a type-safe,object-oriented,function pointer.When you define a .NET delegate type,the C# compiler responds by building a sealed class that derives from System.MulticastDelegate which in turn derives from System.Delegate.These base classes provide every delegate with the ability to maintain a list of method addresses,all of which may be invoked at a later time.

--- ConsoleApp102/Program.cs
+++ ConsoleApp102/Program.cs
@@ -7,28 +7,37 @@ using System.Threading;
 
 namespace ConsoleApp102
 {
+    public delegate int BinaryOp(int x, int y);
     class Program
     {
         static void Main(string[] args)
         {
-            int x = 1000, y = 1000;
-            Thread thread = new Thread(new ThreadStart(new Action(() => {
-                Add(x, y);
-            })));
 
-            thread.Start();
+            Console.WriteLine("*****Synch Delegate Review*****\n");
+
+            //Print out the ID of the executing thread.
+            Console.WriteLine("Main() invoked on thread {0}.\n", Thread.CurrentThread.ManagedThreadId);
+
+            //Invoke Add() in a synchronous manner.
+            BinaryOp bo = new BinaryOp(Add);
+
+            //Could also write bo.Invoke(10,10);
+            int answer = bo(10, 10);
+
+            //These lines will not execute until the Add() method has completed.
+            Console.WriteLine("Doing more work in Main()!\n");
+            Console.WriteLine("10+10={0}\n", answer);
             Console.ReadLine();
         }
 
-        static void Add(int x,int y)
+        static int Add(int x,int y)
         {
-            for(int i=0;i<10000;i++)
-            {
-                Console.WriteLine("{0}+{1}={2}", x, y, x + y);
-                x = x + 10000;
-                y = y + 10000;
-            }
-            
+            //Print out the ID of the executing thread.
+            Console.WriteLine("Add() invoked on thread {0}.\n", Thread.CurrentThread.ManagedThreadId);
+
+            //Pause to simulate a lengthy operation.
+            Thread.Sleep(5000);
+            return x + y;
         }
     }
 }
