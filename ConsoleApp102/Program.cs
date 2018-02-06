618b9d084704afd030805ce1843c08f9067f6a04
Fred1987 <2864326836@qq.com>
2018-02-06 21:46:48 +0800

    public delegate void SubtractDel(int x,int y);SubtractDel del=new SubtractDel(SubtractMethod);del(x,y)

--- ConsoleApp102/Program.cs
+++ ConsoleApp102/Program.cs
@@ -8,25 +8,14 @@ using System.Threading;
 namespace ConsoleApp102
 {
     public delegate int BinaryOp(int x, int y);
+    public delegate void SubtractDel(int x, int y);
     class Program
     {
         static void Main(string[] args)
         {
+            SubtractDel del = new SubtractDel(Subtract);
+            del(10, 20);
 
-            Console.WriteLine("*****Synch Delegate Review*****\n");
-
-            //Print out the ID of the executing thread.
-            Console.WriteLine("Main() invoked on thread {0}.\n", Thread.CurrentThread.ManagedThreadId);
-
-            //Invoke Add() in a synchronous manner.
-            BinaryOp bo = new BinaryOp(Add);
-
-            //Could also write bo.Invoke(10,10);
-            int answer = bo(10, 10);
-
-            //These lines will not execute until the Add() method has completed.
-            Console.WriteLine("Doing more work in Main()!\n");
-            Console.WriteLine("10+10={0}\n", answer);
             Console.ReadLine();
         }
 
@@ -38,6 +27,11 @@ namespace ConsoleApp102
             //Pause to simulate a lengthy operation.
             Thread.Sleep(5000);
             return x + y;
+        }
+
+        static void Subtract(int x,int y)
+        {
+            Console.WriteLine("{0}-{1}={2}\n", x, y, x - y);
         }
     }
 }
