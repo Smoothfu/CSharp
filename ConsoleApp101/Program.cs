827906015e0efca06f8145d91e70eb0f24ea0a21
Fred1987 <2864326836@qq.com>
2018-02-06 08:17:44 +0800

    ThreadStart,Represents the method that executes on a System.Threading.Thread

---
+++ ConsoleApp101/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1000, y = 1000;
+            Thread thread = new Thread(new ThreadStart(new Action(() =>
+            {
+                Add(x, y);
+            })));
+
+            thread.Start();
+            Console.ReadLine();
+        }
+
+        static void Add(int x,int y)
+        {
+            for(int i=0;i<1000;i++)
+            {
+                Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+                Thread.Sleep(100);
+                x = x + 10000;
+                y = y + 10000;
+            }
+        }
+    }
+}
