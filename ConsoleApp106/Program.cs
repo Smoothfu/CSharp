f9e73d57dc19531aec53cd3361aae54ce5edb833
Fred1987 <2864326836@qq.com>
2018-02-09 07:33:10 +0800

    Thread thread=new Thread(new ThreadStart(new Action(()=>{Add(x,y);}))); thread.Start(); public struct TimeSpan,Represents a time interval To browse the .NET Framework source code for this type,see the reference type. public TimeSpan(int days,int hours,int minutes,int seconds,int milliseconds);Initialize a new instance of the System.TimeSpan structure to a specified number of days,hours,minutes,seconds,and milliseconds.

--- ConsoleApp106/Program.cs
+++ ConsoleApp106/Program.cs
@@ -13,6 +13,13 @@ namespace ConsoleApp106
         {
             Console.WriteLine("*****Primary Thread stats*****\n");
 
+            int x = 10000, y = 10000;
+            Thread thread = new Thread(new ThreadStart(new Action(() =>
+              {
+                  Add(x, y);
+              })));
+            thread.Start();
+            Console.WriteLine("The thread state: {0}\n", thread.ThreadState);
             //Obtain and name the current thread.
             Thread primaryThread = Thread.CurrentThread;
             primaryThread.Name = "ThePrimaryThread";
@@ -29,8 +36,21 @@ namespace ConsoleApp106
             Console.WriteLine("Priority Level:{0}\n", primaryThread.Priority);
 
             Console.WriteLine("Thread State:{0}\n", primaryThread.ThreadState);
-            Console.ReadLine();
 
+            Console.WriteLine("This is the end of the main thread!\n");
+            Console.ReadLine();
+        }
+
+        static void Add(int x,int y)
+        {
+            TimeSpan ts = new TimeSpan(0, 0,0, 0,10);
+            for(int i=0;i<1000000;i++)
+            {
+                Console.WriteLine("{0}+{1}={2}.Now is  {3}\n", x, y, x + y, DateTime.Now.ToString("yyyy-MM-dd:HH-mm-ss:fff"));
+                x = x + 1000;
+                y = y + 1000;
+                Thread.Sleep(ts);
+            }
         }
     }
 }
