180be34f8acb28de776dfea1b0d24eb2c3758812
Fred1987 <2864326836@qq.com>
2018-02-09 07:11:41 +0800

    Obtaining Statistics about the Current Thread of Execution.Thread.GetDomain().FriendlyName,Thread.CurrentContext.ContextID,thread.ThreadState,ThreadState,specifies the execution states of a System.Threading.Thread.Running=0,The Thread has been started,it is not blocked and there is no pending System.Thread.ThreadAbortException.StopRequested=1,The thread is being requested to stop.This is for internal use only.StopRequested=1.SuspendRequested=2,The thread is being requested to suspend.Background=4,The thread is being executed as a background thread,as opposed to a foreground thread.The state is controlled by setting the System.Threading.Thread.IsBackground property.Unstarted=8,The System.Threading.Thread.Start method has not been invoked on the thread.Stopped=16,The thread has stopped.WaitSleepJoin=32.The thread is blocked.This could be the result of calling System.Threading.Thread.Sleep(System.Int32) or System.Threading.Thread.Join,of requesting a lock-for example,by calling System.Threading.Monitor.Enter (System.Object) or System.Threading.Monitor.Wait(System.Object,System.Int32,System.Boolean) or of waitting on a thread synchronization object such as System.Threading.ManualReset.Suspended=64,The thread has been suspened.AbortedRequested=128.The System.Threading.Thread.Abort(System.Object) method has been invoked on the thread,but the thread has not yet received the pending System.Threading.ThreadAbortException that will attempt to terminate it.Aborted=256.The thread state include System.Threading.ThreadState.AbortRequested and the thread is now dead,but its state has not yet changed to System.Threading.ThreadState.Stopped

---
+++ ConsoleApp106/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Primary Thread stats*****\n");
+
+            //Obtain and name the current thread.
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "ThePrimaryThread";
+
+            //Show details of hosting AppDomain/Context.
+            Console.WriteLine("Name of current AppDomain:{0}\n", Thread.GetDomain().FriendlyName);
+            Console.WriteLine("ID of current Context:{0}\n", Thread.CurrentContext.ContextID);
+
+            //Print out some stats about this thread.
+            Console.WriteLine("Thread Name:{0}\n", primaryThread.Name);
+
+            Console.WriteLine("Has thread started?:{0}\n", primaryThread.IsAlive);
+
+            Console.WriteLine("Priority Level:{0}\n", primaryThread.Priority);
+
+            Console.WriteLine("Thread State:{0}\n", primaryThread.ThreadState);
+            Console.ReadLine();
+
+        }
+    }
+}
