b36d8dcfdf5790a58c3d5784b9242408bc9da73c
Fred1987 <2864326836@qq.com>
2018-02-08 22:09:16 +0800

    The role of the AsyncResult class.Currently, the AddComplete() method is not printing the actual result of the operation adding two numbers.The reason is that the target of the AsyncCallback delegate AddComplete() in this example.does not have access to the original BinaryOp delegate created in the scope of Main() and,therefore,you can not call EndInvoke() from within AddComplete()! While you could simply declare the BinaryOp variable as a static member variable in the class to allow both methods to access the same object,a more elegant solution is to use the incoming IAsyncResult parameter.The incoming IAsyncResult parameter passed into the target of the AsyncCallback delegate is actually an instance of the AsyncResult class note the lack of an I prefix defined in the System.Runtime.Remoting.Message namespace.The AsyncDelegate property returns a reference to the original asynchronous delegate that was created elsewhere.Therefore,if you want to obatin a reference to the BinaryOp delegate object allocated within Main(),simply cast the System.Object returned by the AsyncDelegate property into type BinaryOp.At this point,you can trigger EndInvoke() as expected

---
+++ ConsoleApp105/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+
+namespace ConsoleApp105
+{
+    public delegate int BinaryOp(int x, int y);
+    class Program
+    {
+        private static bool isDone;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****The beginning of the main thread!*****\n");
+            BinaryOp bo = new BinaryOp(Add);
+            IAsyncResult asyncResult = bo.BeginInvoke(10, 10, AddComplete, null);
+
+            while(!isDone)
+            {
+                Console.WriteLine("Now is :{0}\n", DateTime.Now.ToString("yyyy-MM-dd:HH-mm-ss:fff"));
+            }
+
+            Console.WriteLine("The end of the main thread!\n");
+            Console.ReadLine();
+        }
+
+        static void AddComplete(IAsyncResult asyncResult)
+        {
+            Console.WriteLine("AddComplete() invoked on thread {0}\n",Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Your addition is complete\n");
+
+            //Now get the result;
+
+            AsyncResult ar = (AsyncResult)asyncResult;
+            BinaryOp bo = (BinaryOp)ar.AsyncDelegate;
+            Console.WriteLine("10+10 is {0}\n", bo.EndInvoke(asyncResult));
+            isDone = true;
+        }
+
+        private static int Add(int x,int y)
+        {
+            return x + y;
+        }
+    }
+}
