8d3c1a61a168af8f45fe0d1156bc2eb7a07b23aa
Fred1987 <2864326836@qq.com>
2018-02-08 08:24:56 +0800

    public delegate void AsyncCallback(IAsyncResult asyncResult).Represents a method to be called when a corresponding asynchronous operation completes.The result of the asynchronous operation.IAsyncResult asyncResult=bo.BeginInvoke(x,y,AsyncCallback asyncCallback,object obj)

---
+++ ConsoleApp104/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp104
+{
+    public delegate int BinaryOp(int x, int y);
+    class Program
+    {
+        private static bool isDone = false;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****AsyncCallbackDelegate Example*****\n");
+            Console.WriteLine("Main() invoked on thread {0}\n", Thread.CurrentThread.ManagedThreadId);
+
+            BinaryOp bo = new BinaryOp(Add);
+            IAsyncResult asyncResult = bo.BeginInvoke(10, 10, new AsyncCallback(AddComplete), null);
+
+            //Assume other work is performed here...
+            while(!isDone)
+            {
+                Console.WriteLine("Working,and now is ：{0}\n",DateTime.Now.ToString("yyyy-MM-dd:hh-mm-sss:fff"));
+            }
+
+            Console.ReadLine();
+        }
+
+        static int Add(int x,int y)
+        {
+            Console.WriteLine("Add() invoked on thread {0}\n", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(5000);
+            return x + y;
+        }
+
+        static void AddComplete(IAsyncResult asyncResult)
+        {
+            Console.WriteLine("AddComplete() invoked on thread {0}\n", Thread.CurrentThread.ManagedThreadId);
+            isDone = true;
+        }
+    }
+}
