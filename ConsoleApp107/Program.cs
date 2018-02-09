e30eef384d59b59506d093f1d13eb0912f01c700
Fred1987 <2864326836@qq.com>
2018-02-09 08:32:56 +0800

    Thread thread=new Thread(methodName); thread.Start(paramName)

---
+++ ConsoleApp107/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp107
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start a thread that calls a parameterized static method.
+            Thread newThread = new Thread(DoWork);
+            newThread.Start("This is embeded data in thread.");
+
+            //Start a thread that calls a parameterized instance method.
+
+            Program obj = new Program();
+            newThread = new Thread(obj.DoMoreWork);
+            newThread.Start("This is not a static method parameter!");
+            Console.ReadLine();
+        }
+
+        public static void DoWork(object data)
+        {
+            Console.WriteLine("Static thread procedure,data ='{0}'.", data);
+        }
+
+        public void DoMoreWork(object data)
+        {
+            Console.WriteLine("Instance thread procedure.data='{0}'.", data);
+        }
+    }
+}
