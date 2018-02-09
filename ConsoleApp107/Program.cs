3cab6ef49b9a38357fe7c383f147cd811f783374
Fred1987 <2864326836@qq.com>
2018-02-09 08:51:32 +0800

    Working with the ThreadStart Delegate.To illustrate the process of building a multithreaded application as wel as to demonstrate the usefulness of doing so,assume you have a Consoel Application project that allows the end user to choose whether the application will perform its duties using the single primary thread or split its workload using two separate threads of execution.Assuming you have imported the System.Threading namespace,your first step is to define a method to perform the work of the possible secondary thread.To keep focused on the mechanics of building multithreaded programs,this method will simply print out a sequence of numbers to the console window,pausing for approximately two seconds with each pass.

--- ConsoleApp107/Program.cs
+++ ConsoleApp107/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace ConsoleApp107
 {
@@ -11,15 +12,42 @@ namespace ConsoleApp107
     {
         static void Main(string[] args)
         {
-            //Start a thread that calls a parameterized static method.
-            Thread newThread = new Thread(DoWork);
-            newThread.Start("This is embeded data in thread.");
+            Console.WriteLine("*****The Amazing Thread App*****\n");
+            Console.WriteLine("Do you want [1] or [2] threads?\n");
 
-            //Start a thread that calls a parameterized instance method.
+            string threadCount = Console.ReadLine();
 
-            Program obj = new Program();
-            newThread = new Thread(obj.DoMoreWork);
-            newThread.Start("This is not a static method parameter!");
+            //Name the current thread.
+
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "Primary";
+
+            //Display Thread info.
+            Console.WriteLine("->{0} is executing Main()\n", Thread.CurrentThread.Name);
+
+
+            //Make worker class.
+            Printer p = new Printer();
+
+            switch(threadCount)
+            {
+                case "2":
+                    //Now make the thread.
+                    Thread backgroundThread = new Thread(new ThreadStart(p.PrintNumbers));
+                    backgroundThread.Name = "Secondary";
+                    backgroundThread.Start();
+                    break;
+
+                case "1":
+                    p.PrintNumbers();
+                    break;
+                default:
+                    Console.WriteLine("I don't now what you want... you get 1 thread!\n");
+                    goto case "1";
+            }
+
+            //Do some additional work.
+            MessageBox.Show("I'm busy!", "Work on main thread...");
             Console.ReadLine();
         }
 
@@ -31,6 +59,24 @@ namespace ConsoleApp107
         public void DoMoreWork(object data)
         {
             Console.WriteLine("Instance thread procedure.data='{0}'.", data);
+        }
+    }
+
+    public class Printer
+    {
+        public void PrintNumbers()
+        {
+            //Display Thread info.
+            Console.WriteLine("->{0} is executing PrintNumbers()!\n", Thread.CurrentThread.Name);
+
+            //Print out numbers
+            Console.WriteLine("You numbers:\n");
+            for(int i=0;i<10;i++)
+            {
+                Console.Write("{0}\n", i);
+                Thread.Sleep(10);
+            }
+            Console.WriteLine();
         }
     }
 }
