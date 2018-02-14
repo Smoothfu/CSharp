f900b46849faa9aeccae601f0d55d8db1606c001
Fred1987 <2864326836@qq.com>
2018-02-14 21:45:36 +0800

    Creates and starts a System.Threading.Tasks.Task.action.The action delegate to execute asynchronously.An object containing data to be used by the action delegate/Returns the started System.Threading.Tasks.Task

--- ConsoleApp108/Program.cs
+++ ConsoleApp108/Program.cs
@@ -11,17 +11,25 @@ namespace ConsoleApp108
     {
         static void Main(string[] args)
         {
-            //Create the task object by using an Action(of object) to pass in the loop counter.This produces an unexpected result.
+            //Create the task object by using an Action(of object) to pass in custom data to the Task constructor
+            //This is useful when you need to capture outer variables from within a loop.
 
             Task[] taskArray = new Task[10];
+
             for(int i=0;i<taskArray.Length;i++)
             {
                 taskArray[i] = Task.Factory.StartNew((object obj) =>
                   {
-                      var data = new CustomData() { Name = i.ToString(), CreationTime = DateTime.Now.Ticks };
+                      CustomData data = obj as CustomData;
+                      if (data == null)
+                      {
+                          return;
+                      }
+
                       data.ThreadNum = Thread.CurrentThread.ManagedThreadId;
                       Console.WriteLine("Task #{0} created at {1} on thread #{2}.\n", data.Name, data.CreationTime, data.ThreadNum);
-                  },i);
+
+                  }, new CustomData() { Name = i.ToString(), CreationTime = DateTime.Now.Ticks });
             }
 
             Task.WaitAll(taskArray);
