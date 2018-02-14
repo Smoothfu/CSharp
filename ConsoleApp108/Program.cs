f9be2c03c8ab822ffbb8c16deac28cc4f3baba00
Fred1987 <2864326836@qq.com>
2018-02-14 22:25:52 +0800

    public sealed class TargetFrameworkAttribute:Attribute Initializes an instance of the System.Runtime.Versioning.TargetFrameworkAttribute  class by specifying the .NET Framework version against which an assembly was built,frameworkName The version of the .NET Framework against which the assembly was built.

--- ConsoleApp108/Program.cs
+++ ConsoleApp108/Program.cs
@@ -4,46 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Runtime.Versioning;
+using System.Globalization;
 
+[assembly: TargetFramework(".NETFramework,Version=v4.6")]
 namespace ConsoleApp108
 {
+   
     class Program
     {
         static void Main(string[] args)
         {
-            //Create the task object by using an Action(of object) to pass in custom data to the Task constructor
-            //This is useful when you need to capture outer variables from within a loop.
 
-            Task[] taskArray = new Task[10];
+            decimal[] values = { 163025412.32m, 18905365.59m };
+            string formatString = "C2";
 
-            for(int i=0;i<taskArray.Length;i++)
+            Func<string> formatDelegate = () =>
             {
-                taskArray[i] = Task.Factory.StartNew((object obj) =>
-                  {
-                      CustomData data = obj as CustomData;
-                      if (data == null)
-                      {
-                          return;
-                      }
+                string output = string.Format("Formatting using the {0} culture on thread {1}.\n",
+                    CultureInfo.CurrentCulture.Name, Thread.CurrentThread.ManagedThreadId);
 
-                      data.ThreadNum = Thread.CurrentThread.ManagedThreadId;
-                      Console.WriteLine("Task #{0} created at {1} on thread #{2}.\n", data.Name, data.CreationTime, data.ThreadNum);
-
-                  }, new CustomData() { Name = i.ToString(), CreationTime = DateTime.Now.Ticks });
-            }
-
-            Task.WaitAll(taskArray);
-
-            Console.WriteLine("\n\n\n");
-
-            foreach(var task in taskArray)
-            {
-                var data = task.AsyncState as CustomData;
-                if(data!=null)
+                foreach (var val in values)
                 {
-                    Console.WriteLine("Task #{0} created at {1},ran on thread #{2}.\n", data.Name, data.CreationTime, data.ThreadNum);
+                    output += string.Format("{0}  ", val.ToString(formatString));
                 }
+
+                output += Environment.NewLine;
+                return output;
+            };
+
+            Console.WriteLine("The example is running on thread {0}\n", Thread.CurrentThread.ManagedThreadId);
+
+            //Make the current culture different from the system culture.
+            Console.WriteLine("The current culture is {0}\n", CultureInfo.CurrentCulture.Name);
+
+            if(CultureInfo.CurrentCulture.Name=="fr-FR")
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
             }
+            else
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("fr-FR");
+            }
+
+            Console.WriteLine("Changed the current culture to {0}.\n", CultureInfo.CurrentCulture.Name);
+
+            //Execute the delegate synchronously.
+
+            Console.WriteLine("Executing the delegate synchronously;");
+            Console.WriteLine(formatDelegate());
+
+            //Call an async delegate to format the values using one format string.
+            Console.WriteLine("Executing a task asynchronouly:");
+            var t1 = Task.Run(formatDelegate);
+            Console.WriteLine(t1.Result);
+
+            Console.WriteLine("Executing a task synchronously:");
+
+            var t2 = new Task<string>(formatDelegate);
+            t2.RunSynchronously();
+            Console.WriteLine(t2.Result);
+
             Console.ReadLine();
         }
 
