b00a698d99f365a511b03fe2fdfe2930ad4a16b9
Fred1987 <2864326836@qq.com>
2018-02-20 20:19:34 +0800

    Passing an argument by reference.When used in a methods parameter list,the ref keyword indicates that an argument is passed by reference,not by value.The effect of passing by reference is that any change to the argument in the called method is reflected in the calling method.For example,if the caller passes a local variable expression or an array element access expression,and the called method replcaces the object to which the ref parameter refers,then the callers local variable or the array element now refers to the new object when the method returns.Do not confuse the concept of passing by reference with the concept of reference types.The two concepts are not the same. A method parameter can be modified by ref regardless of whether it is a value type or a reference type.There is no boxing of a value type when it is passed by reference

---
+++ ConsoleApp112/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp112
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1000000000;
+            Add10000000(ref i);
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+
+        static void Add10000000(ref int i)
+        {
+            i = i + 10000000;
+        }
+    }
+}
