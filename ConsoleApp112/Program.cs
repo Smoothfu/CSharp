6387376be596345d935b5fc502214be6eae6508e
Fred1987 <2864326836@qq.com>
2018-02-20 21:23:59 +0800

    out parameter modifier.The out keyword causes arguments to be passed by reference.It is like the ref keyword,except that ref requires that the variable be initialized before it is passed.To use an out parameter,both the method definition and the calling method must explicitly use the out keyword

--- ConsoleApp112/Program.cs
+++ ConsoleApp112/Program.cs
@@ -10,55 +10,15 @@ namespace ConsoleApp112
     {
         static void Main(string[] args)
         {
-            var bc = new BookCollection();
-            bc.ListBooks();
-            Console.WriteLine("\n\n\n");
-
-            ref var book = ref bc.GetBookByTitle("Data Structure");
-
-            if(book!=null)
-            {
-                book = new Book { Title = "Algorithm", Author = "Alexander" };
-            }
-
-            bc.ListBooks();
+            int value;
+            Method(out value);
+            Console.WriteLine(value);
             Console.ReadLine();
-        }       
-    }      
-
-    public class Book
-    {
-        public string Author;
-        public string Title;
-    }
-
-    public class BookCollection
-    {
-        private Book[] books = {new Book { Title="Dive into C#",Author="Fred"},
-        new Book{Title="Data Structure",Author="Floomberg"}};
-
-        private Book noBook = null;
-
-        public ref Book GetBookByTitle(string title)
+        }     
+        
+        static void Method(out int i)
         {
-            for(int ctr=0;ctr<books.Length;ctr++)
-            {
-                if(title==books[ctr].Title)
-                {
-                    return ref books[ctr];
-                }
-            }
-
-            return ref noBook;
+            i = 44;
         }
-
-        public void ListBooks()
-        {
-            foreach(var book in books)
-            {
-                Console.WriteLine($"{book.Title},by {book.Author}");
-            }
-            Console.WriteLine();
-        }
-    }
+    }         
 }
