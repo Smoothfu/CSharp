2355c0e5f08a17f9cfa2bed0d7b4578bc64b87ca
Fred1987 <2864326836@qq.com>
2018-02-20 21:31:49 +0800

    Declaring out arguments.Declaring a method with out arguments is useful when you want a method to return multiple values.The following example uses out to return three variables with a single method call.Note that the third argument is assigned to null.this enables methods to return values optionally;The Try pattern involves returning a bool indicate whether an operation succeeded and failed,and returning the value produced by the operation in an out argument.A number of parsing methods,such as DateTime.TryParse method ,use this pattern

--- ConsoleApp112/Program.cs
+++ ConsoleApp112/Program.cs
@@ -11,14 +11,20 @@ namespace ConsoleApp112
         static void Main(string[] args)
         {
             int value;
-            Method(out value);
-            Console.WriteLine(value);
+            string str1, str2;
+            Method(out value, out str1, out str2);
+            Console.WriteLine("Value ={0}\n", value);
+            Console.WriteLine("str1={0}\n", str1);
+            Console.WriteLine("str2={0}\n", str2);
             Console.ReadLine();
         }     
         
-        static void Method(out int i)
+       
+        static void Method(out int i,out string s1,out string s2)
         {
             i = 44;
+            s1 = "I've been returned";
+            s2 = null;
         }
     }         
 }
