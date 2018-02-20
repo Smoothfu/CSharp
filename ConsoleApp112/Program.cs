23cf777e109fc088435ead4153b948bbc5d2e254
Fred1987 <2864326836@qq.com>
2018-02-20 20:49:12 +0800

    Passing an argument by reference. The previous example pass value types by reference.you can also use the ref keyword to pass reference types by reference.Passing a reference type by reference enables the called method to replace the object to which the reference parameter refers in the caller.The storage location of the object is passed to the method as the value of the reference parameter.If you change the value in this storage location of the parameter to point to a new object,you also change the storage location to which the caller refers.The following example passes an instance of a reference type as a ref parameter

--- ConsoleApp112/Program.cs
+++ ConsoleApp112/Program.cs
@@ -10,15 +10,40 @@ namespace ConsoleApp112
     {
         static void Main(string[] args)
         {
-            int i = 1000000000;
-            Add10000000(ref i);
-            Console.WriteLine(i);
+            //Declare an instance of Product and display its initial values.
+            Product item = new Product("Fasteners", 63434);
+            Console.WriteLine("Original values in Main.Name:{0},ID:{1}\n", item.ItemName, item.ItemID);
+
+            //Pass the product instance to ChangeByReference.
+            ChangeByReference(ref item);
+            Console.WriteLine("Back in Main.Name:{0},ID:{1}\n", item.ItemName, item.ItemID);
             Console.ReadLine();
         }
 
         static void Add10000000(ref int i)
         {
             i = i + 10000000;
+        }
+
+        static void ChangeByReference(ref Product itemRef)
+        {
+            //Change the address that is stored in the itemRef parameter.
+            itemRef = new Product("Stapler", 999999);
+
+            //you can change the value of one of the properties of itemRef.The change happens 
+            //to item in Main as well.
+            itemRef.ItemID = 234923243;
+        }
+    }
+
+    class Product
+    {
+        public string ItemName { get; set; }
+        public int ItemID { get; set; }
+        public Product(string name,int newId)
+        {
+            ItemName = name;
+            ItemID = newId;
         }
     }
 }
