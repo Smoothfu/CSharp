7461b5da8bbe88ea09a07724e0ba5be3b787f83e
Fred1987 <2864326836@qq.com>
2018-02-28 22:06:23 +0800

    Waiting for one or more tasks to complete.Because tasks typically run asynchronously on a thread pool thread,the thread that creates and starts the task continues execution as soon as tne tasks has been instantiated

---
+++ ConsoleApp118/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp118
+{
+    class Program
+    {
+        static Random rand = new Random();
+        static void Main(string[] args)
+        {
+            //Wait on a single task with no timeout specified.
+            Task taskA = Task.Run(() => Thread.Sleep(2000));
+
+            Console.WriteLine("taskA Status:{0} \n", taskA.Status);
+
+            try
+            {
+                taskA.Wait();
+                Console.WriteLine("taskA Status:{0}\n", taskA.Status);
+            }
+
+            catch(AggregateException)
+            {
+                Console.WriteLine("Exception in taskA!\n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
