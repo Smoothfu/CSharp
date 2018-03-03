fa14c277d2eaf8cffe9604477261bd885b227c21
Fred1987 <2864326836@qq.com>
2018-03-03 09:46:26 +0800

    public Task StartNew(Action action) ,Creates and starts a task.Parameters.action.The action delegate to execute asynchronously.Returns.The Started work.

--- ConsoleApp121/Program.cs
+++ ConsoleApp121/Program.cs
@@ -13,14 +13,14 @@ namespace ConsoleApp121
         {
             Thread.CurrentThread.Name = "Main";
 
-            //Define and run the task.
-            Task taskA = Task.Run(() =>
+            //Better:Create and start the task in one operation.
+            Task taskA = Task.Factory.StartNew(() =>
             {
                 Console.WriteLine("Hello from taskA!\n");
             });
 
-            //output a message from the calling thread.
-            Console.WriteLine("Hello from thread '{0}'\n", Thread.CurrentThread.Name);
+            //Output a message from the calling thread.
+            Console.WriteLine("Hello from thread '{0}'.\n", Thread.CurrentThread.Name);
             taskA.Wait();
             Console.ReadLine();
         } 
