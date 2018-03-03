bc0902eed8aef7966ca234de5014c4c6230ca32b
Fred1987 <2864326836@qq.com>
2018-03-03 09:34:59 +0800

    When you create a task,you give it a user delegate that encapsulates the code that the task will execute.The delegate can be expressed as a named delegate,an anonoymous method,or a lambda expression.Lambda expressions can contain a call to a named method,as shown in the following example.Note that the example includes a call to the Task.Wait method to ensure that the task completes execution before the console mode application ends.

---
+++ ConsoleApp121/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+
+            //Create a task and supply a user delegate by using a lambda expression.
+            Task taskA = new Task(() => Console.WriteLine("Hello from taskA!\n"));
+
+            //start the task.
+            taskA.Start();
+
+            //output a message from the calling thread.
+            Console.WriteLine("Hello from thread '{0}'\n", Thread.CurrentThread.Name);
+            taskA.Wait();
+            Console.ReadLine();
+        } 
+    }
+}
