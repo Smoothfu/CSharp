55d5393dd83c4284bd7eb7c7745319a9aa0dc6d9
Fred1987 <2864326836@qq.com>
2018-03-04 14:17:45 +0800

    The Process/AppDomain/Context/Thread relationship. A thread was defined as a path of execution within an executable application. While many .net applications can live happy and productive single-threaded lives,an assemblys primary thread spawned by the CLR when Main() executes may create secondary threads of execution at any time to perform additional units of work.By creating addtional threads,you can build more responsive but not necessarily faster executing on single-core machines applications

---
+++ ConsoleApp122/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp122
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtractExecutingThread();
+            Console.ReadLine();
+        }
+
+        static void ExtractExecutingThread()
+        {
+            //Get the thread currently executing this method.
+
+            Thread currentThread = Thread.CurrentThread;
+            Console.WriteLine("Id: {0}\n", currentThread.ManagedThreadId);
+            Console.WriteLine("CurrentCulture :{0}\n",currentThread.CurrentCulture);
+            Console.WriteLine("CurrentUICulture:{0}\n", currentThread.CurrentUICulture);
+            Console.WriteLine("IsAlive:{0}\n",currentThread.IsAlive);
+            Console.WriteLine("IsBackground:{0}\n",currentThread.IsBackground);
+            Console.WriteLine("IsThreadPoolThread: {0}\n",currentThread.IsThreadPoolThread);
+            Console.WriteLine("Name: {0}\n",currentThread.Name);
+            Console.WriteLine("Priority:{0}\n",currentThread.Priority);
+            Console.WriteLine("ThreadState:{0}\n", currentThread.ThreadState);
+        }
+    }
+}
