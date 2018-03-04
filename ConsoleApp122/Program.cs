f468d4f460e02fc09b53ef586b9b2913e869d8c7
Fred1987 <2864326836@qq.com>
2018-03-04 16:43:03 +0800

    Passing and receiving custom state data. The final aspect of asynchronous delegates you need to address is the final argument to the BeginInvoke() method which has been null up to this point.This parameter allows you to pass additional state information to the callback method from the primary thread.Because this argument is prototyped as a System.Object,you can pass in any type of data whatsoever;as long as the callback method knows what to expect.Assume for the sake of demonstration that the primary thread wants to pass in a custom text message to the AddComplete() method

--- ConsoleApp122/Program.cs
+++ ConsoleApp122/Program.cs
@@ -22,7 +22,7 @@ namespace ConsoleApp122
             Console.WriteLine("Main() invoked on thread {0}\n", Thread.CurrentThread.ManagedThreadId);
 
             BinaryOp bo = new BinaryOp(Add);
-            IAsyncResult iftAR = bo.BeginInvoke(10, 10, new AsyncCallback(AddComplete), null);
+            IAsyncResult iftAR = bo.BeginInvoke(10, 10, new AsyncCallback(AddComplete), "Main() thanks you for adding these numbers.!\n");
 
             //Assume other work is performed here...
             while(!isDone)
@@ -53,6 +53,11 @@ namespace ConsoleApp122
             AsyncResult ar = (AsyncResult)itfAR;
             BinaryOp bo = (BinaryOp)ar.AsyncDelegate;
             Console.WriteLine("10+10 is {0}.\n", bo.EndInvoke(itfAR));
+
+            //Retrieve the informational object and cast it to string.
+            string msg = (string)itfAR.AsyncState;
+            Console.WriteLine(msg);
+
             isDone = true;
         }
 
