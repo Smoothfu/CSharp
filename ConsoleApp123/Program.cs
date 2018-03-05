f0c59ccc23703a99c48500be1349355d82886e18
Fred1987 <2864326836@qq.com>
2018-03-05 20:07:45 +0800

    The issue of concurrency. When you build multithreaded applicayions,your program needs to ensure that any piece of shared data is protected against the possibility of numeros;

---
+++ ConsoleApp123/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Synchronizing Threads*****\n");
+            Printer p = new Printer();
+
+            //Make 10 threads that are all pointing to the same method on the same object.
+            Thread[] threads = new Thread[10];
+            for(int i=0;i<10;i++)
+            {
+                threads[i] = new Thread(new ThreadStart(p.PrintNumbers));
+                threads[i].Name = string.Format("Worker thread #{0}", i);
+            }
+
+            //Now start each one.
+            foreach(Thread t in threads)
+            {
+                t.Start();
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class Printer
+    {
+        public void PrintNumbers()
+        {
+            for(int i=0;i<10;i++)
+            {
+                //put thread to sleep for a random amount of time.
+                Random rnd = new Random();
+                Thread.Sleep(1000 * rnd.Next(5));
+                Console.Write(i+"\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
