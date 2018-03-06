5b6e788d1b9c2fb33d23e4e78193356d63cd4ddd
Fred1987 <2864326836@qq.com>
2018-03-06 22:50:37 +0800

    Synchronization using the System.Threading.Interlocked type.

--- ConsoleApp123/Program.cs
+++ ConsoleApp123/Program.cs
@@ -9,25 +9,67 @@ namespace ConsoleApp123
 {
     class Program
     {
+
+        //0 for false, 1 for true.
+        private static int usingResource = 0;
+        private const int numThreadIterations = 5;
+        private const int numThreads = 10;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("*****Synchronizing Threads*****\n");
-            Printer p = new Printer();
+            Thread myThread;
+            Random rnd = new Random();
 
-            //Make 10 threads that are all pointing to the same method on the same object.
-            Thread[] threads = new Thread[10];
-            for(int i=0;i<10;i++)
+            for(int i=0;i<numThreads;i++)
             {
-                threads[i] = new Thread(new ThreadStart(p.PrintNumbers));
-                threads[i].Name = string.Format("Worker thread #{0}", i);
-            }
+                myThread = new Thread(new ThreadStart(MyThreadProc));
+                myThread.Name = string.Format("Thread {0}\n", i + 1);
 
-            //Now start each one.
-            foreach(Thread t in threads)
-            {
-                t.Start();
+                //wait a random amount of time before starting next thread.
+                Thread.Sleep(rnd.Next(0, 1000));
+                myThread.Start();
+
+
             }
+            
             Console.ReadLine();
+        }
+
+        private static void MyThreadProc()
+        {
+            for(int i=0;i<numThreadIterations;i++)
+            {
+                UseResource();
+
+                //wait 1 second before next attempt.
+                Thread.Sleep(1000);
+            }
+        }
+
+        //A simple method that denies reentrancy.
+        static bool UseResource()
+        {
+            //0 indicates that the method is not in use.
+            if (0 == Interlocked.Exchange(ref usingResource, 1))
+            {
+                Console.WriteLine("{0} acquired the lock!\n", Thread.CurrentThread.Name);
+
+                //Code to access a resource that is not thread safe would go here.
+
+                //simulate some work.
+                Thread.Sleep(500);
+                Console.WriteLine("{0} existing lock", Thread.CurrentThread.Name);
+
+                //Release the lock
+                Interlocked.Exchange(ref usingResource, 0);
+                return true;            
+            }
+
+            else
+            {
+                Console.WriteLine(" {0} was denied the lock.\n", Thread.CurrentThread.Name);
+                return false;
+            }
         }
     }
 
