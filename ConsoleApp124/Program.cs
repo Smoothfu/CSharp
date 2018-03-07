5c458e70e759b2d56edc40c809fd2026eaca0b81
Fred1987 <2864326836@qq.com>
2018-03-07 08:34:37 +0800

    Synchronization using the System.Threading.Monitor type.The C# lock statement is really just a shorthand notation for working with the System.Threading.Monitor class,once processed by the C# compiler,a lock scope actually resolves to the following which you can verify using ildasm.exe. Monitor.Enter(threadLock); try finally Monitor.Exit(threadLock);First,notice that the Monitor.Enter(0 method is the ultimate recipient of the thread token you specified as the argument to the lock keyword.Next,all code within a lock scope is wrapped within a try block.The corresponding finally clauses ensures that the thread token is released via the Monitor.Exit() method,regardless of any possible runtime exception.If you were to modify the MultiThreadPrinting program to make direct use of the Monitor type,you would find the output is identical

---
+++ ConsoleApp124/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printerObj = new Printer();
+            printerObj.PrintNumbers();
+            Console.ReadLine();
+        }
+    }
+
+    public class Printer
+    {
+        object threadLock = new object();
+        public void PrintNumbers()
+        {
+            Monitor.Enter(threadLock);
+
+            try
+            {
+                //Display Thread info.
+                Console.WriteLine("-> {0} is executing PrintNumbers()!\n", Thread.CurrentThread.Name);
+
+                //Print out numbers.
+                Console.Write("Your numbers:\n");
+                for(int i=0;i<10;i++)
+                {
+                    Console.Write(i + "\t");
+                }
+                Console.WriteLine();
+            }
+            finally
+            {
+                Monitor.Exit(threadLock);
+            }
+        }
+    }
+}
