d2a4cc962072130170731f8cb9e9b467432ada80
Fred1987 <2864326836@qq.com>
2018-03-08 22:06:23 +0800

    Thread mainThread=Thread.CurrentThread;mainThread.ManagerdThreadId; Thread newThread=new Thread(()=>{ AddMethod()};); newThread.Start();

---
+++ ConsoleApp127/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp127
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mainThread = Thread.CurrentThread;
+            Console.WriteLine("The main thread ManagedThreadId:{0}\n", mainThread.ManagedThreadId);
+
+            Thread newThread = new Thread(() =>
+              {                  
+                  AddMethod(10000, 10000000, 1000000000);
+              });
+            newThread.Start();
+            Console.ReadLine();
+        }
+
+        static void AddMethod(int x,int y,int z)
+        {
+            Thread addThread = Thread.CurrentThread;
+            Console.WriteLine("The addThread's ManagedThreadId :{0}\n", addThread.ManagedThreadId);
+            Console.WriteLine("{0}+{1}+{2}={3}\n", x, y, z, x + y + z);
+            Console.WriteLine();
+        }
+    }
+}
