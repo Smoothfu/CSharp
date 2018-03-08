95760dda06f45c2a3f62e42b9e605d3489a55a64
Fred1987 <2864326836@qq.com>
2018-03-08 19:27:44 +0800

    Thread th=Thread.CurrentThread;th.Name=assignName;

--- ConsoleApp126/Program.cs
+++ ConsoleApp126/Program.cs
@@ -11,21 +11,10 @@ namespace ConsoleApp126
     {
         static void Main(string[] args)
         {
-            Person p = new Person(1, "Fred");
-            Thread pThread = new Thread(new ThreadStart(p.DescribePerson));
-            Console.WriteLine(pThread.ThreadState + "\n");
-            pThread.Start();
-            Console.WriteLine(pThread.ThreadState+"\n");
+            Thread th = Thread.CurrentThread;
+            th.Name = "MainThread";
 
-            while(!(pThread.ThreadState==ThreadState.Stopped))
-            {
-                Console.WriteLine("Now is :{0}\n", DateTime.Now.ToString("yyyy-MM-dd:HH-mm-ss:fff"));
-            }
-
-            if(pThread.ThreadState==ThreadState.Stopped)
-            {
-                Console.WriteLine("Now The thread finished,it's {0}\n", DateTime.Now.ToString("yyyy-MM-dd:HH-mm-ss:fff"));
-            }
+            Console.WriteLine("This is {0}\n", th.Name);
             Console.ReadLine();
         }
 
