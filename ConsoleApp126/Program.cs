31e903be96ac0c61805bd80df9a06f975854a824
Fred1987 <2864326836@qq.com>
2018-03-08 19:05:40 +0800

    Thread thread=new Thread(()=>{ AddMethod(x,y,z);});

---
+++ ConsoleApp126/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100, y = 10000, z = 10000000;
+            Thread thread = new Thread(() =>
+              {
+                  AddMethod(x, y, z);
+              });
+            thread.Start();
+            Console.ReadLine();
+        }
+
+        static void AddMethod(int x,int y,int z)
+        {
+            Thread addThread = Thread.CurrentThread;
+            Console.WriteLine("Thread Id:{0}\n", addThread.ManagedThreadId);
+
+            Console.WriteLine("{0}+{1}+{2}={3}\n", x, y, z, x + y + z);
+        }
+    }
+}
