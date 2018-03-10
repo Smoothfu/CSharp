f18a233e3c99463e33b46fa254b598db2ea362b4
Fred1987 <2864326836@qq.com>
2018-03-10 22:50:57 +0800

    public static ParallelQuery<TSource>AsParallel<TSource>(this IEnumerable<TSource> source); Enables parallelization of a query. An System.Collection.IEnumerable`1 to convert to a System.Linq.ParallelQuery`1.TSource:The type of elements of source.Returns.The source as a System.Linq.ParallelQuery`1 to bind to ParallelEnumerable extension methods

--- WpfApp11/MainWindow.xaml.cs
+++ WpfApp11/MainWindow.xaml.cs
@@ -46,7 +46,7 @@ namespace WpfApp11
             int[] source = Enumerable.Range(1, 100000000).ToArray();
 
             //Find the numbers where num%3==0 is true,returned descending order.
-            int[] modThreeIsZero = (from num in source where num % 3 == 0 orderby num descending select num).ToArray();
+            int[] modThreeIsZero = (from num in source.AsParallel() where num % 3 == 0 orderby num descending select num).ToArray();
 
             MessageBox.Show(string.Format("Found {0} numbers that match query!\n", modThreeIsZero.Count()));
 
