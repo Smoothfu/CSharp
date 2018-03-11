d4b1294602db4842074765768411ad91232ff17e
Fred1987 <2864326836@qq.com>
2018-03-11 16:46:13 +0800

    Parallel LINQ Queries PLINQ.To wrap up your look at the TPL,be aware that there is another way you can incorporate parallel tasks into your .net applications.If you choose,you can use a set of extension methods,which allow you to construct a LINQ query that will perform its workload in parallel.Fittingly,LINQ queries that are designed to run in parallel are termed PLINQ queries

---
+++ WpfApp12/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void processBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Start a new "task" to process the ints.
+            Task.Factory.StartNew(() =>
+            {
+                ProcessIntData();
+            });
+
+        }
+
+        private void ProcessIntData()
+        {
+            //Get a very large array of integers.
+            int[] source = Enumerable.Range(0, 100000000).ToArray();
+
+            //Find the numbers where num%3==0 is true,returned in descending order.
+            int[] modThreeIsZero = (from num in source where num % 3 == 0 orderby num descending select num).ToArray();
+
+            MessageBox.Show(string.Format("Found {0} numbers that match query!\n", modThreeIsZero.Count()));
+        }
+    }
+}
