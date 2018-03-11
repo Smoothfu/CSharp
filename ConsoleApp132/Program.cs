860258fc7bb84de88178424d92ffb05814abe4e9
Fred1987 <2864326836@qq.com>
2018-03-11 22:40:53 +0800

    Retrofiting the AddWithThreads example using Async/Await. Much earlier in this chapter,you built an example named AddWithThreads using the original threading API of the .NET platform.System.Threading.Now let us retrofit this example to use the C# async and await keywords to showcase just how much cleaner the application logic can become

---
+++ ConsoleApp132/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp132
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddAsync();
+            Console.ReadLine();
+        }
+
+        private static async Task AddAsync()
+        {
+            Console.WriteLine("*****Adding with Thread objects*****");
+            Console.WriteLine("ID of thread in Main():{0} \n", Thread.CurrentThread.ManagedThreadId);
+
+            AddParam ap = new AddParam(10, 10);
+            await Sum(ap);
+
+            Console.WriteLine("Other thread is done!");
+        }
+        static async Task Sum(object data)
+        {
+            await Task.Run(() =>
+            {
+                if (data is AddParam)
+                {
+                    Console.WriteLine("ID of thread in Add():{0}\n", Thread.CurrentThread.ManagedThreadId);
+                    AddParam ap = (AddParam)data;
+                    Console.WriteLine("{0}+{1} is {2}\n", ap.num1, ap.num2, ap.num1 + ap.num2);
+                }
+            });
+        }
+    }
+
+    public class AddParam
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public AddParam(int a,int b)
+        {
+            num1 = a;
+            num2 = b;
+        }
+    }
+}
