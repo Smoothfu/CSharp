f9f33bdcd8122bf1c478c837d0d4dd7d5096b3be
Fred1987 <2864326836@qq.com>
2018-03-12 20:47:51 +0800

    public static Task Run(Action action,CancellationToken cancellationToken); Queues the specified work to run on the thread pool and returns a System.Threading.Tasks.Task object that represents that work.A cancellation token allows the work to be cancelled

--- ConsoleApp133/Program.cs
+++ ConsoleApp133/Program.cs
@@ -12,27 +12,86 @@ namespace ConsoleApp133
         static Random rnd = new Random();
         static void Main(string[] args)
         {
-            var tasks = new Task[10]; 
+            //Create a cancellation token and cancel it.
+            var source1 = new CancellationTokenSource();
+            var token1 = source1.Token;
+            source1.Cancel();
 
-            for(int ctr=0;ctr<10;ctr++)
+            //Create a cancellation token for later cancellation.
+
+            var source2 = new CancellationTokenSource();
+            var token2 = source2.Token;
+
+            //create a series of tasks that will complete,be cancelled,timeout,or thrown an exception.
+            Task[] tasks = new Task[12];
+            for(int i=0;i<12;i++)
             {
-                tasks[ctr] = Task.Run(() => Thread.Sleep(rnd.Next(500, 3000)));
+                switch(i%4)
+                {
+                    //Task should run to completion.
+                    case 0:
+                        tasks[i] = Task.Run(() => Thread.Sleep(2000));
+                        break;
+
+                    //Task should be set to canceled state.
+                    case 1:
+                        tasks[i] = Task.Run(() => Thread.Sleep(2000),token1);
+                        break;
+
+                    case 2:
+                        //Task should throw an exception.
+                        tasks[i] = Task.Run(() =>
+                        {
+                            throw new NotSupportedException();
+                        });
+                        break;
+
+                    case 3:
+                        //Task should examine cancellation token.
+                        tasks[i] = Task.Run(() =>
+                        {
+                            Thread.Sleep(2000);
+                            if (token2.IsCancellationRequested)
+                            {
+                                token2.ThrowIfCancellationRequested();
+
+                            }
+                            Thread.Sleep(500);
+                        }, token2);
+                        break; 
+
+                }
             }
+
+            Thread.Sleep(250);
+            source2.Cancel();
 
             try
             {
                 Task.WaitAll(tasks);
             }
-
             catch(AggregateException ae)
             {
                 Console.WriteLine("One or more exceptions occured:\n");
-                foreach(var ex in ae.Flatten().InnerExceptions)
+                foreach(var ex in ae.InnerExceptions)
                 {
-                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("{0}:{1}\n", ex.GetType().Name, ex.Message);
                 }
             }
- 
+
+            Console.WriteLine("\nStatus of tasks:\n");
+            foreach(var t in tasks)
+            {
+                Console.WriteLine("Task #{0}:{1}\n", t.Id, t.Status);
+                if(t.Exception!=null)
+                {
+                    foreach(var ex in t.Exception.InnerExceptions)
+                    {
+                        Console.WriteLine("{0}:{1}\n", ex.GetType().Name, ex.Message);
+                    }
+                }
+            }
+
             Console.ReadLine();
         }
 
