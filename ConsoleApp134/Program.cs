1562c6cd32198bed25397bac4c3db86aafe0b327
Fred1987 <2864326836@qq.com>
2018-03-12 22:54:51 +0800

    Use the Task.Run methods to create and start a task in one operation.To manage the task,the Run methods use the default task scheduler,regardless of which task scheduler is associated with the current thread.The Run methods are the prefered way to create and start tasks when more control over the creation and scheduling of the task is not needed

--- ConsoleApp134/Program.cs
+++ ConsoleApp134/Program.cs
@@ -13,12 +13,8 @@ namespace ConsoleApp134
         {
             Thread.CurrentThread.Name = "Main";
 
-            //Create a task and supply a user delegate by using a lambda expression.
-            Task taskA = new Task(() => Console.WriteLine("Hello from taskA!\n"));
-
-            Console.WriteLine("Status: {0},\n", taskA.Status);
-            //Start the task.
-            taskA.Start();
+            //Define and run the task.
+            Task taskA = Task.Run(() => Console.WriteLine("Hello from taskA!\n"));
             Console.WriteLine("Status: {0},\n", taskA.Status);
             //Output a message from the calling thread.
             Console.WriteLine("Hello from thread '{0}.\n", Thread.CurrentThread.Name);
