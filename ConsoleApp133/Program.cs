dd67ffdf380911234466c159960e2674ff6b974d
Fred1987 <2864326836@qq.com>
2018-03-12 19:32:32 +0800

    public Task(Action action);Initializes a new System.Threading.Tasks.Task with the specified action. action:The delegate that represents the code to execute in the task.

---
+++ ConsoleApp133/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp133
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1000000, y = 23207890;
+            Task task = new Task(() =>
+              {
+                  Add(x, y);
+
+              });
+
+            Console.WriteLine("Task Status:{0}\n", task.Status);
+            task.Start();
+            Console.WriteLine("Task Status:{0}\n", task.Status);
+            task.Wait();
+            Console.WriteLine("Task Status:{0}\n", task.Status);
+            Console.ReadLine();
+        }
+
+        static void Add(int x,int y)
+        {
+            Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+        }
+    }
+}
