ef56d7a5173ed3327f7901f63d8e63387cdae0d1
Fred1987 <2864326836@qq.com>
2018-03-12 23:20:31 +0800

    [DebuggerDisplayDebuggerTypeProxy]public class Task<TResult>:Task. Represents an asynchronous operation that can return a value

--- ConsoleApp134/Program.cs
+++ ConsoleApp134/Program.cs
@@ -11,17 +11,67 @@ namespace ConsoleApp134
     {
         static void Main(string[] args)
         {
-            Thread.CurrentThread.Name = "Main";
+            //Return a value type with a lambda expression.
+            Task<int> task1 = Task<int>.Factory.StartNew(() => 1);
 
-            //Better:Create and start the task in one operation,
-            Task taskA = Task.Factory.StartNew(() => Console.WriteLine("Hello from taskA!\n"));
+            int i = task1.Result;
 
-            Console.WriteLine("Status: {0},\n", taskA.Status);
-            //Output a message from the calling thread.
-            Console.WriteLine("Hello from thread '{0}.\n", Thread.CurrentThread.Name);
-            taskA.Wait();
-            Console.WriteLine("Status: {0},\n",taskA.Status);
+            Console.WriteLine("task1.Result：{0}\n",i);
+            //Return a named reference type with a multi-line statement lambda.
+            Task<Test> task2 = Task<Test>.Factory.StartNew(() =>
+            {
+                string s = ".NET";
+                double d = 4.0;
+                return new Test { Name = s, Number = d };
+            });
+
+            Console.WriteLine("\n\n\ntask2.Result:\n");
+            Test test = task2.Result;
+            Console.WriteLine(test);
+
+
+            //Return an array produced by a PLINQ query.
+            Task<string[]> task3 = Task<string[]>.Factory.StartNew(() =>
+            {
+
+                string path = @"C:\Users\Fred\Pictures\";
+                string[] files = System.IO.Directory.GetFiles(path);
+
+                var result = (from file in files.AsParallel()
+                              let info = new System.IO.FileInfo(file)
+                              where info.Extension == ".jpg"
+                              select file).ToArray();
+                return result;
+            });
+
+            Console.WriteLine("\n\n\n task3 result:\n");
+            foreach(var name in task3.Result)
+            {
+                Console.WriteLine(name);
+            }
             Console.ReadLine();
+        }
+
+        private static Double DoComputation(Double start)
+        {
+            Double sum = 0;
+            for(var value=start;value<=start+10;value+=10.0)
+            {
+                sum += value;
+            }
+            return sum;
+        }
+    }
+
+    class Test
+    {
+        public string Name { get; set; }
+        public double Number { get; set; }
+
+
+        public override string ToString()
+        {
+            return string.Format("Name:{0},Number:{1}\n", Name, Number);
         }
     }
 }
