c97ebfb4b1d9b895355c4f21c4776d06c99e0d18
Fred1987 <2864326836@qq.com>
2018-03-18 11:30:41 +0800

    Use the dot(.) notation if you want to obtain access to the current working directory.the directory of the executing applicaation.

---
+++ ConsoleApp137/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bind to the current working directory.
+            DirectoryInfo dir = new DirectoryInfo(".");
+            Console.WriteLine(dir.FullName);
+            Console.ReadLine();
+        }
+    }
+}
