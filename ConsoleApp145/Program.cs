528c81c7721d8d56ef77b8c81d75916e60c024e4
Fred1987 <2864326836@qq.com>
2018-03-26 19:22:52 +0800

    Thread newThread=new Thread(()=>{ Add(x,y,z);});

---
+++ ConsoleApp145/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100, y = 200, z = 300;
+            Thread addThread = new Thread(() =>
+              {
+                  Add(x, y, z);
+              });
+            addThread.Start();
+            Console.ReadLine();
+        }
+
+        static void Add(int x,int y,int z)
+        {
+            Console.WriteLine("{0}+{1}+{2}={3}\n", x, y, z, x + y + z);
+        }
+    }
+}
