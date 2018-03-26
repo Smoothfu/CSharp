b792c1e64f3c3c7dd7ad2ae3f7607487baa0d78d
Fred1987 <2864326836@qq.com>
2018-03-26 21:59:17 +0800

    public static void ForEach<T>(T[] array,Action<T> action); Performs the specified action on each element of the specified array

--- ConsoleApp145/Program.cs
+++ ConsoleApp145/Program.cs
@@ -11,16 +11,29 @@ namespace ConsoleApp145
     {
         static void Main(string[] args)
         {
-            //Create Task,defer starting it,continue with another task.
-            Task<int> task = new Task<int>(x => Sum((int)x), 1000);
-            task.Start();
+            Task<int[]> parent = new Task<int[]>(() =>
+              {
+                //Create an array for the results
 
-            //Notice the use of the Result property.
-            Task cwt = task.ContinueWith(x => Console.WriteLine("The sum is :" + x.Result));
-            
-            //for the testing only.
+                var results = new int[3];
+
+                //This tasks create and star 3 child tasks.
+                new Task(() => results[0] = Sum(1000), TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => results[1] = Sum(2000), TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => results[2] = Sum(3000), TaskCreationOptions.AttachedToParent).Start();
+
+                //Returns a reference to the array  even though the elements may not be initialized yet
+                return results;
+              });
+
+            //When the parent and its children have run to completion,display the results.
+            var cwt = parent.ContinueWith(x => Array.ForEach(x.Result, Console.WriteLine));
+
+            //Start the parent Task so it can start its children.
+            parent.Start();
+
+            //For testing purposes.
             cwt.Wait();
-            
             Console.ReadLine();
         } 
 
