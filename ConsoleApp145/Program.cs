46bcafaecdac80a83cbd16392718e742b4e88c4e
Fred1987 <2864326836@qq.com>
2018-03-26 20:21:54 +0800

    Use an Action delegate and named method. Task task1=new Task(new Action(PrintMessage)); Use an anonymous delegate. Task task2=new Task(delegate {PrintMessage();});

--- ConsoleApp145/Program.cs
+++ ConsoleApp145/Program.cs
@@ -11,18 +11,38 @@ namespace ConsoleApp145
     {
         static void Main(string[] args)
         {
-            int x = 100, y = 200, z = 300;
-            Task addTask = new Task(new Action(() =>
+            //Use an Action delegate and named method
+            Task task1 = new Task(new Action(PrintMessage));
+
+            //Uase an anonymous delegate.
+            Task task2 = new Task(delegate { PrintMessage(); });
+
+            //Use a lambda expression and a named method
+            Task task3 = new Task(() => PrintMessage());
+
+            //Use a Lambda expression and an anonymous method;.
+            Task task4 = new Task(() =>
               {
-                  Add(x, y, z);
-              }));
-            addTask.RunSynchronously();
+                  PrintMessage();
+              });
+
+            task1.Start();
+            task2.Start();
+            task3.Start();
+            task4.Start();
+            Console.WriteLine("Main method complete.Press <Enter> to finish.");
+            
             Console.ReadLine();
         }
 
         static void Add(int x,int y,int z)
         {
             Console.WriteLine("{0}+{1}+{2}={3}\n", x, y, z, x + y + z);
+        }
+
+        private static void PrintMessage()
+        {
+            Console.WriteLine("The world is fair!\n");
         }
     }
 }
