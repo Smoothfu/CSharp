cac2ad902e955332cb49b629e7a9cf85b4cd2398
Fred1987 <2864326836@qq.com>
2018-03-27 22:43:18 +0800

    public BinaryWriter(Stream output);Initializes a new instance of the System.IO.BinaryWriter class based on the specified stream and using UTF-8 encoding

--- ConsoleApp146/Program.cs
+++ ConsoleApp146/Program.cs
@@ -12,25 +12,29 @@ namespace ConsoleApp146
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("******Fun with StringWriter/StringReader******\n");
+            Console.WriteLine("******Fun with Binary Writers/Readers******\n");
 
-            //Create a StringWriter and emit character data to memory.
-            using (StringWriter stringWriter = new StringWriter())
+            //Open a binary writer for a file.
+            FileInfo fi = new FileInfo("BinFile.dat");
+            using (BinaryWriter bw = new BinaryWriter(fi.OpenWrite()))
             {
-                stringWriter.WriteLine("The world is wonderful and fair,and everything depend on myself.\n");
-                //Get a copy of the contents stored in a string and dump to console.
-                Console.WriteLine("Contents of StringWriter:\n{0}", stringWriter);
+                //Print out the type of BaseStream.
+                //System.IO.FileStream in this case.
+                Console.WriteLine("Base stream is :{0}\n", bw.BaseStream);
 
-                //Read data from the StringBuilder
-                using (StringReader stringReader = new StringReader(stringWriter.ToString()))
-                {
-                    string input = null;
-                    while((input=stringReader.ReadLine())!=null)
-                    {
-                        Console.WriteLine(input);
-                    }
-                }                    
+                //Create some data to save in the file.
+                double aDouble = 1234.67;
+                int anInt = 34567;
+                string aString = "A,B,C";
+
+                //Write the data.
+                bw.Write(aDouble);
+                bw.Write(anInt);
+                bw.Write(aString);
             }
+
+            Console.WriteLine("Done!");
+
 
             Console.ReadLine();
         }
