83480a6c4ab950aca706df48b1ba56c42421f4b6
Fred1987 <2864326836@qq.com>
2018-03-27 22:27:03 +0800

    public StringReader(string str); Initializes a new instance of the System.IO.StringReader class that reads from the specified string

--- ConsoleApp146/Program.cs
+++ ConsoleApp146/Program.cs
@@ -17,16 +17,19 @@ namespace ConsoleApp146
             //Create a StringWriter and emit character data to memory.
             using (StringWriter stringWriter = new StringWriter())
             {
-                stringWriter.WriteLine("Don't forget Mother's Day this year...\n");
+                stringWriter.WriteLine("The world is wonderful and fair,and everything depend on myself.\n");
                 //Get a copy of the contents stored in a string and dump to console.
                 Console.WriteLine("Contents of StringWriter:\n{0}", stringWriter);
 
-                //Get the internal StringBuilder.
-                StringBuilder stringBuilder = stringWriter.GetStringBuilder();
-                stringBuilder.Insert(0, "Hey!!!");
-                Console.WriteLine("->{0}\n", stringBuilder.ToString());
-                stringBuilder.Remove(0, "Hey!!".Length);
-                Console.WriteLine("->{0}\n", stringBuilder.ToString());
+                //Read data from the StringBuilder
+                using (StringReader stringReader = new StringReader(stringWriter.ToString()))
+                {
+                    string input = null;
+                    while((input=stringReader.ReadLine())!=null)
+                    {
+                        Console.WriteLine(input);
+                    }
+                }                    
             }
 
             Console.ReadLine();
