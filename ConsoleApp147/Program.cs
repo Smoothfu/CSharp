c2af7771893c79159212d02518e05221c8d7c211
Fred1987 <2864326836@qq.com>
2018-03-28 21:35:17 +0800

    public FileSystemWatcher() Initializes a new instance of the system.IO.FileSystemWatcher class. public delegate void FileSystemEventHandler(object sender,FileSystemEventArgs e);

--- ConsoleApp147/Program.cs
+++ ConsoleApp147/Program.cs
@@ -11,36 +11,58 @@ namespace ConsoleApp147
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("******Fun with Binary Writers/Readers******\n");
+            Console.WriteLine("******The Amazing File Watcher App******\n");
 
-            //Open a binary writer for a file.
-            FileInfo fi = new FileInfo("BinaryFile.dat");
-            using (BinaryWriter bw = new BinaryWriter(fi.OpenWrite()))
+            //Establish the path to the directory to watch.
+            FileSystemWatcher watcher = new FileSystemWatcher();
+
+            try
             {
-                //Print out the type of BaseStream.
-                Console.WriteLine("Base stream is:{0}\n", bw.BaseStream);
-
-                //Create some data to save in the file.
-                double aDouble = 1234.67;
-                int anInt = 34567;
-                string aString = "A,B,C";
-
-                //Write the data.
-                bw.Write(aDouble);
-                bw.Write(anInt);
-                bw.Write(aString);
+                watcher.Path = @"C:\Users\Fred\Downloads\金庸作品集世纪新修版【TXT】";
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return; 
             }
 
+            //Set up the things to be on the lookout for.
+            watcher.NotifyFilter = NotifyFilters.LastAccess
+                | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
 
-            //Read the binary data from the stream.
-            using (BinaryReader br = new BinaryReader(fi.OpenRead()))
+            //Only watch text files
+
+            watcher.Filter = "*.txt";
+
+            //Add event handlers.
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+
+            //Begin watching the directory.
+            watcher.EnableRaisingEvents = true;
+
+            //Wait for the user to quit the program.
+            Console.WriteLine(@"Press 'q' to quit app.\n");
+            while(Console.Read()!='q')
             {
-                Console.WriteLine(br.ReadDouble());
-                Console.WriteLine(br.ReadInt32());
-                Console.WriteLine(br.ReadString());
+                ;
             }
-                Console.WriteLine("Done!\n");
             Console.ReadLine();
+        }
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            //Specify what is done when a file is renamed.
+            Console.WriteLine("File:{0} renamed to {1}\n", e.OldFullPath, e.FullPath);
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            //Specify what is done when a file is changed,created,or deleted.
+            Console.WriteLine("File:{0} {1}!", e.FullPath, e.ChangeType);
         }
     }
 }
