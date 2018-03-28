467dc6bc23bbf771d876299a6dd2a753bea7f3b3
Fred1987 <2864326836@qq.com>
2018-03-28 20:34:50 +0800

    public FileStream OpenWrite(); Creates a write-only System.IO.FileStream. public BinaryWriter(Stream output); Initializes a new instance of the System.IO.BinaryWriter class based on the specified stream and using UTF-8 encoding

---
+++ ConsoleApp147/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp147
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******Fun with Binary Writers/Readers******\n");
+
+            //Open a binary writer for a file.
+            FileInfo fi = new FileInfo("BinaryFile.dat");
+            using (BinaryWriter bw = new BinaryWriter(fi.OpenWrite()))
+            {
+                //Print out the type of BaseStream.
+                Console.WriteLine("Base stream is:{0}\n", bw.BaseStream);
+
+                //Create some data to save in the file.
+                double aDouble = 1234.67;
+                int anInt = 34567;
+                string aString = "A,B,C";
+
+                //Write the data.
+                bw.Write(aDouble);
+                bw.Write(anInt);
+                bw.Write(aString);
+            }
+
+            Console.WriteLine("Done!\n");
+            Console.ReadLine();
+        }
+    }
+}
