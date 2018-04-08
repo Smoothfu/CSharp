7e7f87dad3a47dadbe8631cb29ef22391ad41c03
Fred1987 <2864326836@qq.com>
2018-04-08 17:22:38 +0800

    public DbDataReader ExecuteReader(); Executes the System.Data.Common.DbCommand.CommandText against the System.Data.Common.DbCommand.Connection and returns an System.Data.Common.DbDataReader.Returns  A System.Data.Common.DbDataReader object. public virtual DbCommand CreateCommand(); Returns a new instance of the providers class that implement the System.Data.Common.DbCommand class.Returns a new instance of the System.Data.Common.DbCommand.

--- ConsoleApp152/Program.cs
+++ ConsoleApp152/Program.cs
@@ -23,10 +23,54 @@ namespace ConsoleApp152
     {
         static void Main(string[] args)
         {
-            //Get the factory for the SQL data provider.
-           DbProviderFactory sqlFactory = DbProviderFactories.GetFactory("System.Data.SqlClient");
-            WriteLine(sqlFactory.GetType().Name);
-           ReadLine();
+            WriteLine("*****Fun with Data Provider Factories*****\n");
+
+            //Get connection string/provider from *.config.
+            string dataProvider = ConfigurationManager.AppSettings["provider"];
+            string connectionString = ConfigurationManager.AppSettings["connectionString"];
+
+            //Get the factory provider.
+            DbProviderFactory factory = DbProviderFactories.GetFactory(dataProvider);
+            
+            //Now get the connection object.
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                if(connection==null)
+                {
+                    ShowError("Command");
+                    return;
+                }
+
+                WriteLine($"Your connection object is a:{connection.GetType().Name}");
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                //Make command object.
+                DbCommand command = factory.CreateCommand();
+                if(command==null)
+                {
+                    ShowError("Command");
+                    return;
+                }
+
+                WriteLine($"Your command object is a :{command.GetType().Name}");
+                command.Connection = connection;
+                command.CommandText = "select * from mt";
+
+                //Print out data with data reader.
+                using (DbDataReader dataReader = command.ExecuteReader())
+                {
+                    WriteLine($"Your data reader object is a:{dataProvider.GetType().Name}");
+
+                    WriteLine("\n*****Current mt*****");
+                    while(dataReader.Read())
+                    {
+                        WriteLine($"->Car #{dataReader["CarId"]} is a {dataReader["Make"]}.");
+                    }
+                }
+            }
+
+            ReadLine();
         }
 
         //This method returns a specific connection object based on the value 
@@ -49,6 +93,12 @@ namespace ConsoleApp152
             }
 
             return connection;
+        }
+
+        private static void ShowError(string objectName)
+        {
+            WriteLine($"There was an issue creating the {objectName}");
+            ReadLine();
         }
     }
 }
--- ConsoleApp152/dbo.Table_2.sql
+++ ConsoleApp152/dbo.Table_2.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Orders]
 (
-	[OrderId] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[OrderId] INT  IDENTITY(1,1) not null, 
     [CustId] INT NOT NULL, 
     [CarId] INT NOT NULL, 
+	primary key clustered ([OrderId] asc),
     CONSTRAINT [FK_Orders_MT] FOREIGN KEY ([CarId]) REFERENCES [mt]([CarId]), 
-    CONSTRAINT [FK_Orders_Customers] FOREIGN KEY ([CustId]) REFERENCES [Customers]([CustId]),
-	
+    CONSTRAINT [FK_Orders_Customers] FOREIGN KEY ([CustId]) REFERENCES [Customers]([CustId]),	
 )
