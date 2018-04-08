0f91d5df6fb39de43aedb390f0339c15727eb140
Fred1987 <2864326836@qq.com>
2018-04-08 15:48:33 +0800

    Server explorer. PRIMARY KEY CLUSTERED Update DataBase

---
+++ ConsoleApp152/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[mt] 
+(
+	[CarId] INT IDENTITY(1,1) not null, 
+    [Make] NVARCHAR(50) NULL, 
+    [Color] NVARCHAR(50) NULL, 
+    [PetName] NVARCHAR(50) NULL,
+	PRIMARY KEY CLUSTERED ([CarID] ASC)
+)
