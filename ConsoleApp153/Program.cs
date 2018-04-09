83370fcbc24b6f9c3dcc3dc014f3199139ee9a9d
Fred1987 <2864326836@qq.com>
2018-04-09 23:51:05 +0800

    public Task ContinueWith(Action<Task> continuationAction); Creates a continuation that executes asynchronously when the target System.Threading.Tasks.Task completes

---
+++ ConsoleApp153/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp153
+{
+    class Program
+    {
+        static DateTime dt = DateTime.Now;
+        static DateTime dtNew = dt.AddSeconds(60);
+        static int x = 1000, y = 1000;
+       
+        static void Main(string[] args)
+        {
+            Task.Run(() =>
+            {
+                //simulate processing.
+                while (DateTime.Now.Subtract(dtNew).Seconds < 0)
+                {
+                    AddMethod(ref x, ref y);
+                }
+            }).ContinueWith(x => System.Windows.Forms.MessageBox.Show("Done!"));          
+            
+
+            Console.ReadLine();            
+        }
+
+        static void AddMethod(ref int x,ref int y)
+        {
+           Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+            x += 1000;
+            y += 1000;
+        }
+    }   
+}
