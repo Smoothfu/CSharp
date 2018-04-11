5da24e63789a8238d2e4d0aa9eba9ca24df8fb82
Fred1987 <2864326836@qq.com>
2018-04-11 23:57:25 +0800

    public static Task<TResult> Run<TResult>(Func<TResult> function); Summary:Queues the specified work to run on the thread pool and returns a Sysyem.Threading.Tasks.Task`1 object that represents that work.function:The work to execute asynchronously. TResult The return type of the task.Returns A task object that represents the work queued to execute in the thread pool

--- ConsoleApp154/Program.cs
+++ ConsoleApp154/Program.cs
@@ -13,17 +13,22 @@ namespace ConsoleApp154
         static int x = 1000, y = 10000;
         static DateTime dt = DateTime.Now;
         static DateTime dtNew = dt.AddSeconds(30);
+        static int count = 0;
 
         static void Main(string[] args)
         {
-            Task.Run(() =>
+            var task=Task.Run<int>(() =>
             {
                 while (DateTime.Now.Subtract(dtNew).Seconds < 0)
                 {
                     AddMethod(ref x, ref y);
+                    count++;
                 }
+                return count;
             });
-            
+
+
+            Console.WriteLine("Result: {0}\n", task.Result);
             Console.ReadLine();
         }
 
