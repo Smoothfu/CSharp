726f03568d2c7547a13fcd86ac10b6263395e697
Fred1987 <2864326836@qq.com>
2018-04-11 23:40:28 +0800

    static void AddMethod(ref int x,ref int y)

---
+++ ConsoleApp154/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleApp154
+{
+    class Program
+    {
+        static int x = 1000, y = 10000;
+        static DateTime dt = DateTime.Now;
+        static DateTime dtNew = dt.AddSeconds(10);
+
+        static void Main(string[] args)
+        {
+            while(DateTime.Now.Subtract(dtNew).Seconds<0)
+            {
+                AddMethod(ref x, ref y);
+            }
+            Console.ReadLine();
+        }
+
+        static void AddMethod(ref int x,ref int y)
+        {
+            Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+            x = x + 1000;
+            y = y + 1000;
+        }
+    }
+}
