b64dbcbcf9b00b1fec873b82a53a26ef74626393
Fred1987 <2864326836@qq.com>
2018-04-18 20:29:17 +0800

    [ComVisible(false)] public delegate void ParameterizedThreadStart(object obj); Represents the method that executes on a System.Threading.Thread. public void Start(object parameter); Causes the operating system to change the state of the current instance to System.Threading.ThreadState.Running and optionally supplies an object containing data to be used by the method the thread executes

---
+++ ConsoleApp156/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Adding with Thread objects*****\n");
+            Console.WriteLine("ID of thread in Main():{0}\n", Thread.CurrentThread.ManagedThreadId);
+
+            //Make an AddParams object to pass to the secondary thread.
+            AddParams ap = new AddParams(100000000, 100000000);
+            Thread thread = new Thread(new ParameterizedThreadStart(Add));
+            thread.Start(ap);
+            Console.ReadLine();
+        }
+
+        static void Add(object data)
+        {
+            var addObj = data as AddParams;
+            if(addObj!=null)
+            {
+                Console.WriteLine("ID of thread in Add():{0}\n", Thread.CurrentThread.ManagedThreadId);
+
+                Console.WriteLine("{0}+{1}={2}\n", addObj.a, addObj.b, addObj.a + addObj.b);
+            }
+        }
+    }
+
+    class AddParams
+    {
+        public int a, b;
+        public AddParams(int numb1,int numb2)
+        {
+            a = numb1;
+            b = numb2;
+        }
+    }
+}
