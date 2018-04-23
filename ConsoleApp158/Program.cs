0130b46e5b9ccf12304b4dc1e24e0ff6fb493c93
Fred1987 <2864326836@qq.com>
2018-04-23 21:58:32 +0800

    Task.Factory.StartNew(()=>{ ProcessIntData();});

---
+++ ConsoleApp158/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp158
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start a new "task" to process the ints.
+            Task.Factory.StartNew(() =>
+            {
+                ProcessIntData();
+            });
+
+            Console.ReadLine();
+        }
+
+        static void ProcessIntData()
+        {
+            //Get a very large array of integers.
+            int[] source = Enumerable.Range(1, 100000000).ToArray();
+
+            //Find the numbers where num%3==0 is true,returned in descending order.
+            int[] modThreeIsZero = (from num in source where num % 3 == 0 orderby num descending select num).ToArray();
+            MessageBox.Show(string.Format("Found {0} numbers that match query!\n", modThreeIsZero.Count()));
+            
+        }
+    }
+}
