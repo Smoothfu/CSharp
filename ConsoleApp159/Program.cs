aa40a80fa01921b09f5e45914b7644a8348aae40
Fred1987 <2864326836@qq.com>
2018-04-24 22:48:09 +0800

    DirectoryInfo dir=new DirectoryInfo(@"D: \C\ConsoleApp159\ConsoleApp159New

--- ConsoleApp159/Program.cs
+++ ConsoleApp159/Program.cs
@@ -14,13 +14,26 @@ namespace ConsoleApp159
     {
      static void Main(string[] args)
         {
-            //Bind to the current working directory.
-            DirectoryInfo dir = new DirectoryInfo(@"d\c\ConsoleApp159New\NewFile");
-            dir.Create();
-            Console.WriteLine(dir.FullName);
+            Console.WriteLine("*****Fun with DirectoryInfo*****\n");
+            ShowWindowDirectoryInfo();
             Console.ReadLine();
         }
 
+        static void ShowWindowDirectoryInfo()
+        {
+            //Dump directory information.
+            DirectoryInfo dir = new DirectoryInfo(@"D:\C\ConsoleApp159\ConsoleApp159New");
+            dir.Create();
+            Console.WriteLine("*****Directory Info*****\n");
+            Console.WriteLine("FullName:{0}\n", dir.FullName);
+            Console.WriteLine("Name:{0}\n", dir.Name);
+            Console.WriteLine("Parent:{0}\n", dir.Parent);
+            Console.WriteLine("Creation:{0}\n", dir.CreationTime);
+            Console.WriteLine("Attributes:{0}\n", dir.Attributes);
+            Console.WriteLine("Root:{0}\n", dir.Root);
+            Console.WriteLine("*************************************************************\n");
+
+        }
         static async Task<int> AccessTheWebAsync()
         {
             //You need to add a reference to System.Net.Http to declare client.
