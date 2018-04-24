407c56d8e9bb3e2a59185db34a4b7b158c828e4b
Fred1987 <2864326836@qq.com>
2018-04-24 21:55:50 +0800

    public Task<strig> GetStringAsync(string requireUri); Send a GET request to the specified Uri and return the response body as string in an asynchronous operation

---
+++ ConsoleApp159/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace ConsoleApp159
+{
+    class Program
+    {
+     static void Main(string[] args)
+        {
+           Task<int> str = AccessTheWebAsync();
+            Console.WriteLine(str.Result);
+            Console.ReadLine();
+        }
+
+        static async Task<int> AccessTheWebAsync()
+        {
+            //You need to add a reference to System.Net.Http to declare client.
+            HttpClient client = new HttpClient();
+
+            //GetStringAsync returns a Task<string> that means that when you await the task you'll get a string 
+            Task<string> getStringTask = client.GetStringAsync("http://msdn.microsoft.com");
+
+            //You can do work here that does not rely on the string from GetStringAsync .
+             
+
+            string urlContents = await getStringTask;
+            return urlContents.Length;
+        }
+    }
+}
