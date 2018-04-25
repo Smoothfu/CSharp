64468d74edcc8d6071e7511a1e3168d5e23ba7c0
Fred1987 <2864326836@qq.com>
2018-04-25 21:13:19 +0800

    DirectoryInfo dir=new DirectoryInfo("."); dir.FullName,dir.CreationTime,dir.LastAccessTime,dir.LastWriteTimeUtc,dir.Name,dir.Root,dir.ToString()

---
+++ ConsoleApp160/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(".");
+            Console.WriteLine(dir.FullName);
+            Console.WriteLine(dir.CreationTime);
+            Console.WriteLine(dir.LastAccessTime);
+            Console.WriteLine(dir.LastWriteTimeUtc);
+            Console.WriteLine(dir.Name);
+            Console.WriteLine(dir.Root);
+            Console.WriteLine(dir.ToString());
+            Console.ReadLine();
+        }
+    }
+}
