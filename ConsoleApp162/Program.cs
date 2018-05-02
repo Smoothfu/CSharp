cf4161d7d2096dd90d26727a5256d4708775fa79
Fred1987 <2864326836@qq.com>
2018-05-02 15:55:05 +0800

    public FileStream Create(); Creates a file.;

---
+++ ConsoleApp162/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace ConsoleApp162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            string filePath = path + "\\NewText.txt";
+            //Make a new file on the file
+            FileInfo fi = new FileInfo(filePath);
+            FileStream fs = fi.Create();
+
+            //Use the FileStream object...
+            //Close down file stream.
+            fs.Close();
+            Console.ReadLine();
+
+        }
+    }
+}
