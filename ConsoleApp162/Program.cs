d8f9464f0e9755ae9c171b94bd6add780d5945cc
Fred1987 <2864326836@qq.com>
2018-05-02 16:20:00 +0800

    [SecuritySafeCritical] public static void BlockCopy(Array src,int srcOffset,Array dst,int dstOffset,int count); Copies a specified number of bytes from a source arrary starting at a particular offset to a destination array starting at a particular offset

--- ConsoleApp162/Program.cs
+++ ConsoleApp162/Program.cs
@@ -19,10 +19,21 @@ namespace ConsoleApp162
             //Defining a using scope for file I/O type is ideal.
             FileInfo fi = new FileInfo(filePath);
 
+            string str = "This world is fair and everything depend on myself!\n";
+            char[] allBytes = str.ToCharArray();
+
             using (FileStream fs = fi.Create())
             {
                 //Use the FileStream object...
+                fs.Write(GetBytes(str),0,str.Length*2);                
             }
+        }
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
         }
     }
 }
