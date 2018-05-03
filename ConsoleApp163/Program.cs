9679e4c0deb38f0e301fb1a514adae8e59ef98af
Fred1987 <2864326836@qq.com>
2018-05-03 20:28:04 +0800

    public FileStream OpenRead(); Creates a read-only System.IO.FileStream. public FileStream OpenWrite(); Creates a write-only System.IO.FileSystem

---
+++ ConsoleApp163/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp163
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get a FileStream object with read-only permissions.
+            FileInfo fi = new FileInfo("NewFile.dat");
+            using (FileStream readOnlyStream = fi.OpenRead())
+            {
+
+            }
+
+            //Now get a FileStream object with write-only permissions.
+            FileInfo fi2 = new FileInfo("NewFile2.dat");
+            using (FileStream writeOnlyStream = fi2.OpenWrite())
+            {
+
+            }
+
+        }
+    }
+}
