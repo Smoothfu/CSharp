0be172e34c8a8f219ea105290f4aca358ce9c75c
Fred1987 <2864326836@qq.com>
2018-05-08 23:27:12 +0800

    Working with the ParameterizedThreadStart delegate.Recall that the ThreadStart delegate can point only to methods that return void and take no arguments.While this might fit the bill in some cases, if you want to pass data to the method executing on the secondary thread,you will need to use the ParameterizedThreadStart delegate type.To illustrate,lets recreate the logic of the AsyncCallbackDelegate project created earlier in this chapter,this time using the ParameterizedThreadStart delegate type

---
+++ ConsoleApp165/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Adding with Thread objects*****\n");
+            Console.WriteLine("ID of thread in Main():{0}\n", Thread.CurrentThread.ManagedThreadId);
+
+            //Make an AddParams object to pass to the secondary thread.
+            AddParams ap = new AddParams(10, 10);
+            Thread thread = new Thread(new ParameterizedThreadStart(AddMethod));
+            thread.Start(ap);
+            Console.ReadLine();
+        }
+
+        static void AddMethod(object data)
+        {
+            var objAddParams = data as AddParams;
+            if (objAddParams != null)
+            {
+                Console.WriteLine("ID of thread in Add():{0}\n", Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine("{0}+{1}={2}\n", objAddParams.a, objAddParams.b, objAddParams.a + objAddParams.b);
+            }
+        }
+    }
+
+    class AddParams
+    {
+        public int a, b;
+
+        public AddParams(int numb1,int numb2)
+        {
+            a = numb1;
+            b = numb2;
+        }
+    }
+}
