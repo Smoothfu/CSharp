2baac6fc8c33b4df0e2cbe5a836c455e0a844835
Fred1987 <2864326836@qq.com>
2018-05-09 22:02:39 +0800

    [SecuritySafeCritical]public Timer(TimeCallback callback,object state,int dueTime,int period); Initializes a new instance of the Timer class,using a 32-bit signed integer to specify the time interval. [ComVisible(true)] public delegate void TimerCallback(object state); Represents the method that handles calls from a System.Threading.Timer

--- ConsoleApp166/Program.cs
+++ ConsoleApp166/Program.cs
@@ -13,59 +13,23 @@ namespace ConsoleApp166
         private static AutoResetEvent waitHandler = new AutoResetEvent(false);
         static void Main(string[] args)
         {
-            Console.WriteLine("*****Synchronizing Threads*****\n");
+            Console.WriteLine("*****Working with Timer type*****\n");
 
-            Printer p = new Printer();
+            //Create the delegate for the Timer type.
+            TimerCallback timeCB = new TimerCallback(PrintTime);
 
-            //Make 10 threads that are all pointing to the same method on the same object.
-            Thread[] allThreads = new Thread[10];
-
-            for(int i=0;i<10;i++)
-            {
-                allThreads[i] = new Thread(new ThreadStart(p.PrintNumbers));
-                allThreads[i].Name = string.Format("Worker thread #{0}\n", i);
-            }
-
-            //Now start each one.
-            foreach(Thread thread in allThreads)
-            {
-                thread.Start();
-            }
+            //Establish timer settings.
+            Timer timer = new Timer(timeCB, null, 0, 1000);
+            Console.WriteLine("Hit key to termimate...\n");
+           
             Console.ReadLine();
-        }        
-    }
-
-    class AddParams
-    {
-        public int a, b;
-        public AddParams(int num1,int num2)
+        }   
+        
+        static void PrintTime(Object state)
         {
-            a = num1;
-            b = num2;
+            Console.WriteLine("Time is {0}\n", DateTime.Now.ToString());
         }
     }
-
-    //All methods of Printer are now thread safe!
-    [Synchronization]
-    public class Printer:ContextBoundObject
-    {
-
-        //Lock token.
-        private object threadLock = new object();
-        public void PrintNumbers()
-        {
-            
-                //Display Thread info.
-                Console.WriteLine("The PrintNumbers thread id is {0}\n", Thread.CurrentThread.ManagedThreadId);
-                for (int i = 0; i < 10; i++)
-                {
-                    //Put thread to sleep for a random amount of time.
-                    Random rnd = new Random();
-                    Thread.Sleep(rnd.Next(5));
-                    Console.Write("{0}\t", i);
-                }
-                Console.WriteLine("\n\n\n\n\n");
-            
-        }
-    }
+     
+    
 }
