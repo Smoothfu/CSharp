ee661dd577611d1606168df1d1d0ef576d1694ea
Fred1987 <2864326836@qq.com>
2018-05-12 17:00:32 +0800

    DbConnection connection =factory.CreateConnection();var sqlConn=connection as SqlConnection;sqlConn.Serversion;sqlConn.AccessToken;sqlConn.ClientConnectionId;sqlConn.ConnectionString;sqlConn.ConnectionTimeout;sqlConn.Container;sqlConn.Credential;sqlConn.Database;sqlConn.DataSource;sqlConn.FireInfoMessageEventOnUserErrors;sqlConn.GetHashCode();sqlConn.PacketSize;sqlConn.ServerVersion;sqlConn.Site;sqlConn.State;sqlConn.StatisticsEnabled;sqlConn.WorkstationId

--- ConsoleApp168/Program.cs
+++ ConsoleApp168/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data.Common;
 using static System.Console;
 using System.Configuration;
+using System.Data.SqlClient;
 
 namespace ConsoleApp168
 {
@@ -34,6 +35,29 @@ namespace ConsoleApp168
                 WriteLine($"Your connection object is {{0}}\n",connection.GetType().Name);
                 connection.ConnectionString = connectionString;
                 connection.Open();
+
+                var sqlConn = connection as SqlConnection;
+                if (sqlConn != null)
+                {
+                    //Print out which version of SQL Server is used.
+                    WriteLine($"ServerVersion is {sqlConn.ServerVersion}");
+                    WriteLine($"AccessToken is {sqlConn.AccessToken}");
+                    WriteLine($"ClientConnectionId is {sqlConn.ClientConnectionId}");
+                    WriteLine($"ConnectionString is {sqlConn.ConnectionString}");                 
+                    WriteLine($"ConnectionTimeout is {sqlConn.ConnectionTimeout}");
+                    WriteLine($"Container is {sqlConn.Container}");
+                    WriteLine($"Credential is {sqlConn.Credential}");
+                    WriteLine($"Database is {sqlConn.Database}");
+                    WriteLine($"DataSource is {sqlConn.DataSource}");
+                    WriteLine($"FireInfoMessageEventOnUserErrors is {sqlConn.FireInfoMessageEventOnUserErrors}");
+                    WriteLine($"GetHashCode() is {sqlConn.GetHashCode()}");
+                    WriteLine($"PacketSize is {sqlConn.PacketSize}");
+                    WriteLine($"ServerVersion is {sqlConn.ServerVersion}");
+                    WriteLine($"Site is {sqlConn.Site}");
+                    WriteLine($"State is {sqlConn.State}");
+                    WriteLine($"StatisticsEnabled is {sqlConn.StatisticsEnabled}");
+                    WriteLine($"WorkstationId is {sqlConn.WorkstationId}");                    
+                }               
 
                 //Make command object.
                 DbCommand dbCommand = factory.CreateCommand();
