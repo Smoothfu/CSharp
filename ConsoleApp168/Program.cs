471429e30de52d4a61f5299ac3dd4be33fd64a5e
Fred1987 <2864326836@qq.com>
2018-05-12 15:43:29 +0800

    public static DbProviderFactory GetFactory(string providerInvariantName); Returns an instance of a System.Data.Common.DbProviderFactory

---
+++ ConsoleApp168/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+
+namespace ConsoleApp168
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get the factory for the SQL data provider.
+            DbProviderFactory sqlFactory = DbProviderFactories.GetFactory("System.Data.SqlClient");
+            Console.WriteLine(sqlFactory.CanCreateDataSourceEnumerator);
+            Console.WriteLine(sqlFactory.ToString());
+            Console.ReadLine();
+        }
+    }
+}
