6b6cb1c2579f902962c8759c138e8f7a9429391f
Fred1987 <2864326836@qq.com>
2018-05-12 14:22:50 +0800

    set identity_insert tablename on

--- ConsoleApp167/SQLQuery4.sql
+++ ConsoleApp167/SQLQuery4.sql
@@ -1,22 +1,31 @@
-﻿CREATE table [dbo].[Customers]
-([CustId] int identity(1,1) not null,
-[FirstName] nvarchar (50) null,
-[LastName] nvarchar(50) null,
-primary key clustered ([CustId] ASC)
-)
+﻿ create table [dbo].[Orders]
+ (
+ [OrderId] int not null primary key identity,
+ [CustId] int not null,
+ [CarId] int not null
+ );
 
-insert into dbo.Customers
-(FirstName,lastName)
-values
-('Fred','Fu'),
-('Floomberg','Fu'),
-('Michael','Bloomberg'),
-('Bill','Gates'),
-('Jeff','Bezos'),
-('Elon','Musk'),
-('Larry','Ellison'),
-('Larry','Page'),
-('Sergey','Brin'),
-('Mark','Zuckerberg')
+ use AutoLol
 
-select * from Customers
+ select * from sysobjects where xtype='u'
+
+ select column_name from INFORMATION_SCHEMA.COLUMNS where table_name='orders' order by COLUMN_NAME
+
+ insert into dbo.Orders
+ (CustId,CarId)
+ values
+ (1,1),
+ (1,2),
+ (2,1),
+ (2,2),
+ (3,1),
+ (3,2),
+ (4,1),
+ (4,2),
+ (5,3),
+ (5,4),
+ (6,2),
+ (6,3)
+
+
+ select * from dbo.Orders
