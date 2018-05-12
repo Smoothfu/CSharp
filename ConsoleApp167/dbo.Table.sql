9e7d1d89fe6cdfa0640923e6f6976740a244933a
Fred1987 <2864326836@qq.com>
2018-05-12 12:57:53 +0800

    CREATE TABLE [dbo].[Inventory] ([CarId] INT IDENTITY(1,1) NOT NULL, [Make] NVARCHAR(50) null,[Color] NVARCHAR(50) null,[PetName] NVARCHAR(50) null,[Comment] NVARCHAR(300000) null, primary key clustered ([CarId]ASC));

---
+++ ConsoleApp167/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[CarId] INT IDENTITY(1,1) NOT NULL,
+	[Make] NVARCHAR(50) null,
+	[Color] NVARCHAR(50) null,
+	[PetName] NVARCHAR(50) null,
+	[Comment] NVARCHAR(3000) null,
+	primary key clustered ([CarId] ASC)
+);
