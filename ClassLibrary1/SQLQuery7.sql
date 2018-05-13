c5435eff82cff86d19594b2592a25ee9b2031d1e
Fred1987 <2864326836@qq.com>
2018-05-13 13:55:18 +0800

    Create table [dbo].[CreditRisks] ( CustId int identity(1,1) not null,FirstName nvarchar(50) null,LastName nvarchar(50) null, primary key clustered ([custed] ASC));

--- ClassLibrary1/SQLQuery7.sql
+++ ClassLibrary1/SQLQuery7.sql
@@ -3,3 +3,10 @@ Delete from iventory where CarId='7'
 
 alter table Orders
 drop CONSTRAINT FK__Orders__CarId__173876EA
+
+CREATE TABLE [dbo].[CreditRisks] (
+    [CustId]    INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (50) NULL,
+    [LastName]  NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([CustId] ASC)
+);
