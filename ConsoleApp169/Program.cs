2da5f4b8d2c33e1017fc8b1b62455dbe3e4443fa
Fred1987 <2864326836@qq.com>
2018-05-13 17:45:56 +0800

    Represents an open connection to a data source,and is implemented by .NET Framework data providers that access relational databases. public interface IDbConnection:IDisposable

---
+++ ConsoleApp169/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+using System.Data.OleDb;
+
+
+namespace ConsoleApp169
+{
+    //A list of possible providers.
+    enum DataProvider
+    {
+        SqlServer,OleDb,Odbc,None
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("*****Very Simple Connection Factory*****\n");
+
+            //Get a specific connection.
+            IDbConnection conn = GetConnection(DataProvider.SqlServer);
+            WriteLine($"Your connection is a {conn.GetType().Name}");
+
+            //Open,use and close connection...
+            ReadLine();
+        }
+
+        //This method returns a specific connection object based on the value of a DataProvider enum.
+        static IDbConnection GetConnection(DataProvider dataProvider)
+        {
+            IDbConnection connection = null;
+            switch(dataProvider)
+            {
+                case DataProvider.SqlServer:
+                    connection = new SqlConnection();
+                    break;
+
+                case DataProvider.OleDb:
+                    connection = new OleDbConnection();
+                    break;
+
+                case DataProvider.Odbc:
+                    connection = new OdbcConnection();
+                    break;
+            }
+            return connection;
+        }
+    }
+}
