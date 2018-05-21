a0ef47f68c83f1bfc6ef0330592c03782b8ba1b7
Fred1987 <2864326836@qq.com>
2018-05-21 19:43:09 +0800

    var temp=new DataTable("Temp"); temp.Columns.Add(new DataColumn("TempColumn",typeof(int)); var row=temp.NewRow();

---
+++ ConsoleApp172/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using static System.Console;
+
+
+namespace ConsoleApp172
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManipulateDataRowState();
+            Console.ReadLine();
+        }
+
+        private static void ManipulateDataRowState()
+        {
+            //Create a temp DataTable for testing.
+            var temp = new DataTable("Temp");
+            temp.Columns.Add(new DataColumn("TempColumn", typeof(int)));
+
+            //RowState=Detached.
+            var row = temp.NewRow();
+            WriteLine($"After calling NewRow():{row.RowState}");
+
+            //RowState=Added.
+            temp.Rows.Add(row);
+            WriteLine($"After calling Rows.Add():{row.RowState}");
+
+            //RowState=Added.
+            row["TempColumn"] = 10;
+            WriteLine($"After first assignment:{row.RowState}");
+
+            //RowState=Unchanged.
+            temp.AcceptChanges();
+            WriteLine($"After calling AcceptChanges:{row.RowState}");
+
+            //RowState=Modified.
+            row["TempColumn"] = 11;
+            WriteLine($"After first assignment:{row.RowState}");
+
+            //RowState=Deleted.
+            temp.Rows[0].Delete();
+            WriteLine($"After calling Delete:{row.RowState}");
+        }
+    }
+}
