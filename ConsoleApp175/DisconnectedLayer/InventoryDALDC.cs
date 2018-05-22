098297822cd9676a6f6268ed63343d19228c44db
Fred1987 <2864326836@qq.com>
2018-05-22 19:34:14 +0800

    public int Update(DataTable dataTable); Updates the values in the database by executing the respective INSERT,UPDATE,or DELETE statements for each inserted,updated,or deleted row in the specified System.Data.DataTable

---
+++ ConsoleApp175/DisconnectedLayer/InventoryDALDC.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace ConsoleApp175.DisconnectedLayer
+{
+    public class InventoryDALDC
+    {
+        //Field data.
+        private string _connectionString;
+        private SqlDataAdapter _adapter = null;
+
+        public InventoryDALDC(string connectionString)
+        {
+            _connectionString = connectionString;
+
+            //Configure the SqlDataAdapter.
+            ConfigureAdapter(out _adapter);
+        }
+
+        private void ConfigureAdapter(out SqlDataAdapter adapter)
+        {
+            //Create the adapter and set up the SelectCommand.
+            adapter = new SqlDataAdapter("Select * from inventory", _connectionString);
+
+
+            //Obtain the remaining command objects dynamically at runtime.
+            //using the SqlCommandBuilder.
+            var builder = new SqlCommandBuilder(adapter);
+        }
+
+        public DataTable GetAllInventory()
+        {
+            DataTable dt = new DataTable("Inventory");
+            _adapter.Fill(dt);
+            return dt;
+        }
+
+        public void UpdateInventory(DataTable modifiedTable)
+        {
+            _adapter.Update(modifiedTable);
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsoleApp175.DisconnectedLayer;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        InventoryDALDC _dal = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string conStr = @"Data Source=FRED\SQLEXPRESS;Initial Catalog=AutoLot; Integrated Security=SSPI;Pooling=False";
+
+            //Creat our data access object.
+            _dal = new InventoryDALDC(conStr);
+
+            //Fill up your grid.
+            dataGridView1.DataSource = _dal.GetAllInventory();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Get modified data from the grid.
+            DataTable changedDT = (DataTable)dataGridView1.DataSource;
+
+            try
+            {
+                //Commit our changes.
+                _dal.UpdateInventory(changedDT);
+                dataGridView1.DataSource = _dal.GetAllInventory();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
