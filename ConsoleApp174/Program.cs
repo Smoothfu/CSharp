fe8055e5e1b9ea7b510c30d6ef6de76f2ed6605e
Fred1987 <2864326836@qq.com>
2018-05-22 14:03:36 +0800

    foreach(DataTable dt in ds.Tables)

---
+++ ConsoleApp174/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using static System.Console;
+using System.Collections;
+
+namespace ConsoleApp174
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = ConfigurationManager.AppSettings["conString"].ToString();
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+
+            if(!(conn.State==ConnectionState.Open))
+            {
+                return;
+            }
+
+            string selectSQL = "select * from AdventureWorks2014.Person.vStateProvinceCountryRegion;select * from AdventureWorks2014.Sales.vStoreWithContacts;select * from AdventureWorks2014.Person.vAdditionalContactInfo";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(selectSQL, conn);
+            DataSet ds = new DataSet();
+            sqlDataAdapter.Fill(ds);
+
+            if(ds.Tables[0].Rows.Count>0)
+            {
+                PrintDataSet(ds);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void PrintDataSet(DataSet ds)
+        {
+            //Print out the DataSet name and any extended properties.
+            WriteLine($"DataSet is named: {ds.DataSetName}");
+            
+            foreach(DictionaryEntry de in ds.ExtendedProperties)
+            {
+                WriteLine($"Key={de.Key},Value={de.Value}");
+            }
+
+            WriteLine();
+
+            //Print out each table using rows and columns.
+
+            foreach(DataTable dt in ds.Tables)
+            {
+                WriteLine($"=>{dt.TableName} Table:");
+
+                //Print out the column names.
+                for(var curCol=0;curCol<dt.Columns.Count;curCol++)
+                {
+                    Write("{0,-20}",$"{dt.Columns[curCol].ColumnName}\t");
+                }
+
+                WriteLine("\n-----------------------------------------------");
+
+                //Print the DataTable.
+                for(var curRow=0;curRow<dt.Rows.Count;curRow++)
+                {
+                    for(var curCol = 0; curCol < dt.Columns.Count; curCol++)
+                    {
+                        Write("{0,-20}",$"{dt.Rows[curRow][curCol]}\t");
+                    }
+                    WriteLine();
+                }
+
+                Console.WriteLine("\n\n\n\n\n\n\n");
+            }
+        }
+
+    }
+}
