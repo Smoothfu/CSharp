bfa756b10e31594928ed3e369059708137baabba
Fred1987 <2864326836@qq.com>
2018-05-23 12:04:07 +0800

    public int Update(DataSet dataset,string srcTable); updates the values in the databases by executing the respective INSERT,UPDATE or DELETE statements for each inserted,updated,or deleted row in System.Data.DataSet with the specified  System.Data.DataTable name

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -31,7 +31,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -39,21 +45,22 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(23, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.Size = new System.Drawing.Size(59, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Inventory";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(25, 44);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(586, 138);
             this.dataGridView1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 226);
+            this.button1.Location = new System.Drawing.Point(1052, 453);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(177, 23);
             this.button1.TabIndex = 2;
@@ -61,17 +68,59 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(25, 216);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(586, 107);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(25, 364);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 23;
+            this.dataGridView3.Size = new System.Drawing.Size(586, 112);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Customers";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 346);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Orders";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1241, 488);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +131,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,36 +8,85 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ClassLibrary1;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        //Form wide DataSet.
+        private DataSet _autoLotDs = new DataSet("AutoLot");
+
+        //Make use of command builders to simplify data adapter configuration.
+        private SqlCommandBuilder _sqlCbInventory;
+        private SqlCommandBuilder _sqlCbCustomers;
+        private SqlCommandBuilder _sqlCbOrders;
+
+        //Our data adapters for each table.
+        private SqlDataAdapter _invTableAdapter;
+        private SqlDataAdapter _custTableAdapter;
+        private SqlDataAdapter _ordersTableAdapter;
+
+        //Form wide connection string.
+        private String _connectionString;
         InventoryDALDC _dal = null;
         public Form1()
         {
             InitializeComponent();
 
-            string connectionString = @"Data Source=FRED\SQLEXPRESS;Initial Catalog=AutoLot;Integrated Security=SSPI;Pooling=False";
+            //Get connection string.
+            _connectionString = ConfigurationManager.ConnectionStrings["AutoLotSqlProvider"].ConnectionString;
 
-            //Create our data access object.
-            _dal = new InventoryDALDC(connectionString);
+            //Create adapters.
+            _invTableAdapter = new SqlDataAdapter("select * from inventory", _connectionString);
+            _custTableAdapter = new SqlDataAdapter("select * from Customers", _connectionString);
+            _ordersTableAdapter = new SqlDataAdapter("select * from orders", _connectionString);
 
-            //Fill up our grid!
-            dataGridView1.DataSource = _dal.GetAllInventory();
+            //Autogenerate commands/
+            _sqlCbInventory = new SqlCommandBuilder(_invTableAdapter);
+            _sqlCbOrders = new SqlCommandBuilder(_ordersTableAdapter);
+            _sqlCbCustomers = new SqlCommandBuilder(_custTableAdapter);
+
+            //Fill tables in DataSet.
+            _invTableAdapter.Fill(_autoLotDs, "Inventory");
+            _custTableAdapter.Fill(_autoLotDs, "Customers");
+            _ordersTableAdapter.Fill(_autoLotDs, "Orders");
+
+            //Build relations between tables.
+            BuildTableRelationShip();
+
+            //Bind to grids.
+            dataGridView1.DataSource = _autoLotDs.Tables["Inventory"];
+            dataGridView2.DataSource = _autoLotDs.Tables["Customers"];
+            dataGridView3.DataSource = _autoLotDs.Tables["Orders"];
+        }
+
+        private void BuildTableRelationShip()
+        {
+            //Create CustomerOrder data relation object.
+            DataRelation dr = new DataRelation("CustomerOrder", _autoLotDs.Tables["Customers"].Columns["CustID"],
+                _autoLotDs.Tables["Orders"].Columns["CustId"]);
+            _autoLotDs.Relations.Add(dr);
+
+            //Create InventoryOrder data relation object.
+            dr = new DataRelation("InventoryOrder",
+                _autoLotDs.Tables["Inventory"].Columns["CarID"],
+                _autoLotDs.Tables["Orders"].Columns["CarID"]);
+
+            _autoLotDs.Relations.Add(dr);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Get modified data from the grid.
-            DataTable changedDT = (DataTable)dataGridView1.DataSource;
-
             try
             {
-                //Commit our changes.
-                _dal.UpdateInventory(changedDT);
-                dataGridView1.DataSource = _dal.GetAllInventory();
+                _invTableAdapter.Update(_autoLotDs, "Inventory");
+                _custTableAdapter.Update(_autoLotDs, "Customers");
+                _ordersTableAdapter.Update(_autoLotDs, "Orders");
+
             }
+
             catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
