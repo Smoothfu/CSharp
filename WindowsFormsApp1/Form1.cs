571879d59ff532dedd7c17723271154d650de5b8
Fred1987 <2864326836@qq.com>
2018-05-23 11:35:56 +0800

    public int Fill(DataTable dataTable); Adds or references rows in a specified range in the System.Data.DataSet to match those in the data source using the System.Data.DataTable name. public int Update(DataTable dataTable); Updates the values in the database by executing the respective INSERT,UPDATE or DELETE statements for each inserted,updated,or deleted row in the specified System.Data.DataTable

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        InventoryDALDC _dal = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string connectionString = @"Data Source=FRED\SQLEXPRESS;Initial Catalog=AutoLot;Integrated Security=SSPI;Pooling=False";
+
+            //Create our data access object.
+            _dal = new InventoryDALDC(connectionString);
+
+            //Fill up our grid!
+            dataGridView1.DataSource = _dal.GetAllInventory();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Get modified data from the grid.
+            DataTable changedDT = (DataTable)dataGridView1.DataSource;
+
+            try
+            {
+                //Commit our changes.
+                _dal.UpdateInventory(changedDT);
+                dataGridView1.DataSource = _dal.GetAllInventory();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
