1a560b916a6b7ef4149569abdf00badd845262ce
Fred1987 <2864326836@qq.com>
2018-05-24 16:23:02 +0800

    public class DbContext:IDisposable,IObjectContextAdapter A DbContext instance represents a combination of the unit of work and repository patterns such that it can be used to query from a database and group together changes that will then be written back to the store as a unit.DbContext is conceptually similar to ObjectContext.

---
+++ ConsoleApp178/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+
+namespace ConsoleApp178
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class AutoLotEntities: DbContext
+    {
+        public AutoLotEntities() base("name=AutoLotConnection")
+        {
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+           
+        }
+    }
+}
