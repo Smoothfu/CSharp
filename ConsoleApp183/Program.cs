1bb7d1cbbe70c47e546210dc1acaee95fad4fc8c
Fred1987 <2864326836@qq.com>
2018-05-27 20:50:02 +0800

    public Message[] GetAllMessages(); Returns all the messages that are in the queue

---
+++ ConsoleApp183/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace ConsoleApp183
+{
+    class Program
+    {
+        static string queuePath = ".\\Private$\\myQueue";
+        static void Main(string[] args)
+        {
+            if(!MessageQueue.Exists(queuePath))
+            {
+                MessageQueue.Create(queuePath);
+            }
+            SendPublic();
+            Console.ReadLine();
+        }
+
+        //References public queues.
+        static void SendPublic()
+        {
+            MessageQueue myQueue = new MessageQueue(queuePath);
+            for(int i=0;i<100;i++)
+            {
+                myQueue.Send("Public queue by path name. Now is " + DateTime.Now.ToString("yyyyMMdd-HHmmssfff"));
+            }
+            
+            myQueue.Formatter = new XmlMessageFormatter(new Type[] {typeof(string)});
+
+            Message[] allMessages = myQueue.GetAllMessages();
+
+            if(allMessages!=null && allMessages.Any())
+            {
+                foreach(Message msg in allMessages)
+                {
+                    Console.WriteLine(msg.Body.ToString());
+                }
+            }
+            
+            return;
+        }
+    }
+}
