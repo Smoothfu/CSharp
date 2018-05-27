116d5503e6d4220160b3d50f3b4eb7c2b98b26b4
Fred1987 <2864326836@qq.com>
2018-05-27 19:30:24 +0800

    public void Send(object obj,MessageQueueTransactionType transactionType); Sends an object to the queue referenced  by this System.Messaging.MessageQueue. public enum MessageQueueTransactionType specifies the type of a message Queueing transaction. None=0,Operation will not be transactional.Automatic=1,A transaction type used for Microsoft Transaction Server MTS or COM+ 1.0 Services.If there is already an MTS transaction context,it will be used when sending or receiving the message. Single=3,A transaction type used for single internal transactions

---
+++ ConsoleApp182/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace ConsoleApp182
+{
+    class Program
+    {
+        static string queuePath=".\\Private$\\MQ";        
+        
+        static MessageQueue msgQueue = new MessageQueue(queuePath);
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+
+            if(!MessageQueue.Exists(queuePath))
+            {
+                MessageQueue.Create(queuePath);
+            }
+
+            obj.SendQueue();
+            obj.ReceiveQueue();
+            Console.ReadLine();
+        }
+
+        void SendQueue()
+        {
+            Person person = new Person
+            {
+                PId = 1,
+                PName = "Fred"
+            };
+
+            msgQueue.Send(person, MessageQueueTransactionType.Automatic);
+        }
+
+        void ReceiveQueue()
+        {
+            Message msg = msgQueue.Receive();
+            Person p = msg.Body as Person;
+            if (p != null)
+            {
+                Console.WriteLine("PId:{0},PName:{1}\n", p.PId, p.PName);
+            }
+        }
+    }
+
+    public class Person
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+    }
+}
