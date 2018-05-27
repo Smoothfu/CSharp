1c2eb19807c8e8e32a843bdc769bbcd294cb3040
Fred1987 <2864326836@qq.com>
2018-05-27 16:59:56 +0800

    public XmlMessageFormatter(Type[] targetTypes); Initializes a new instance of the System.Messaging.XmlMessageFormatter class,setting target types passed in as an array of object types.An array of type System.Type that specifies the set of possible types that will be deserialized by the formatter from the message provided

--- ConsoleApp180/Program.cs
+++ ConsoleApp180/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp180
 {
     class Program
     {
+        public static List<Person> PersonList { get; set; }
         static void Main(string[] args)
         {
             Program obj = new Program();
@@ -19,6 +20,14 @@ namespace ConsoleApp180
         //Creates private queues and sens a message.
         public void CreatePrivateQueues()
         {
+
+            PersonList = new List<Person>
+            {
+                new Person(1, "Fred"),
+                new Person(2, "Bill Gates"),
+                new Person(3, "Michael Bloomberg")
+            };
+
             string mqPath = ".\\Private$\\MyQueue";
             if(!MessageQueue.Exists(mqPath))
             {
@@ -27,30 +36,43 @@ namespace ConsoleApp180
 
             //Open queue.
             MessageQueue queue = new MessageQueue(mqPath);
+            queue.Purge();
 
             //Create message
             Message msg = new Message();
-            Person p = new Person(1,"Fred");
 
-            msg.Body = p;
+            msg.Body = PersonList;
 
-            //msg.Formatter = new XmlMessageFormatter(new Type[] { typeof(Person) });
+            msg.Formatter = new XmlMessageFormatter(new Type[] { typeof(List<Person>) });
 
             //put message into queue
             queue.Send(msg);
 
             Message receiveMsg = new Message();
             receiveMsg = queue.Receive();
-            receiveMsg.Formatter = new XmlMessageFormatter(new Type[] { typeof(Person) });
-            string queueMsg = receiveMsg.Body.ToString();
-            Console.WriteLine(queueMsg);
-        }        
+            receiveMsg.Formatter = new XmlMessageFormatter(new Type[] { typeof(List<Person>) });
+            
+            var bodyResult=receiveMsg.Body as List<Person>;
+            if (bodyResult != null && bodyResult.Any())
+            {
+                bodyResult.ForEach(x =>
+                {
+                    Console.WriteLine(x.ToString());
+                });
+            }
+           
+            
+        }
+
+        
     }
 
     public class Person
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+       
         public Person(int id,string name)
         {
             Id = id;
@@ -58,12 +80,12 @@ namespace ConsoleApp180
         }
         public Person()
         {
-
+            ToString();
         }
 
         public override string ToString()
         {
-            return string.Format("Name:{0}\nId:{1}\n", Name, Id);
+            return string.Format("Id:{0},Name:{1}\n", Id, Name);
         }
 
     }
