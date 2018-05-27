dc691b963bf42cf124d501454d46b82705c6b065
Fred1987 <2864326836@qq.com>
2018-05-27 13:13:32 +0800

    public static bool Exists(string path); Determines whether a Message Queuing queue exists at the specified path. Returns: true if a queue with the specified path exists;otherwise,false

---
+++ ConsoleApp180/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace ConsoleApp180
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.CreatePrivateQueues();
+            Console.ReadLine();
+        }
+
+        //Creates private queues and sens a message.
+        public void CreatePrivateQueues()
+        {
+            string mqPath = ".\\Private$\\MyQueue";
+            if(!MessageQueue.Exists(mqPath))
+            {
+                MessageQueue.Create(mqPath);
+            }
+            MessageQueue mq = new MessageQueue(mqPath);
+            mq.Send("Private queue by path name!");
+
+            Message msg = mq.Receive();
+            string msgString = msg.Body.ToString();
+            Console.WriteLine(msgString);         
+        }        
+    }
+}
