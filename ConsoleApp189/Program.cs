038ac993dfcfa223a4f1f5903af26ddc08c8ae8a
Fred1987 <2864326836@qq.com>
2018-05-28 23:52:14 +0800

    params [] arr parameters can be nulll

---
+++ ConsoleApp189/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp189
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = paramsDemo();
+            Console.WriteLine(arr.Length);
+
+            foreach(int a in arr)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+        }
+
+        public static int[] paramsDemo(params int[] arr)
+        {
+            return arr;
+        }
+    }
+}
