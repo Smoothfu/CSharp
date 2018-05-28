3de37e94398a1c9c0ac0dc3422aed0ee4aead0ca
Fred1987 <2864326836@qq.com>
2018-05-28 17:39:24 +0800

    hangeRedis StackExchange.Redis;public sealed class ConnectionMultiplexer:IConnectionMultiplexer,IDisposable. Represents an inter-related group of connections to redis servers. public static ConnectionMultiplexer Connect(string configuration,TextWriter log=null); Create a new ConnectionMultiplexer instance.publicIDatabase GetDatabase(int db=-1,object asyncState=null); obtain an interactive connection to a database inside redis.bool StringSet(RedisKey key,RedisValue value,TimeSpan?expiry=null,When  when=When.Always,CommandFlags flags=CommandFlags.None); Set key to hold the string vlaue,if key already holds a value,it is overwritten,regardless of its type.ConnectionMultiplexer was designed for code sharing by the whole application,is not necessary to create a new instance every time you need a simple operation,Creating a new instance every time we need cache,the performance may be affected.If we are use Redis cache from azure,there is a limit connection so if we create a new connection each time we need the cache,this limit can be exceeded.

---
+++ ConsoleApp184/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Commands;
+using ServiceStack.Redis;
+using ServiceStack.Text;
+using ServiceStack.Common; 
+using ServiceStack;
+using StackExchange.Redis;
+
+namespace ConsoleApp184
+{
+    class Program
+    {
+        static int devicesCount = 100000;
+        static void Main(string[] args)
+        {
+            var obj = new Program();
+            Console.WriteLine("Saving random data in cache!\n");
+            obj.SaveBigData();
+
+            Console.WriteLine("Reading data from cache!\n");
+            obj.ReadData();
+
+            Console.ReadLine();
+        }
+
+        public void ReadData()
+        {
+            var cache = RedisConnectorHelper.Connection.GetDatabase();
+            
+            for(int i=0;i<devicesCount;i++)
+            {
+                var value = cache.StringGet($"Device_Status:{i}");
+                Console.WriteLine($"Value={value}");
+            }
+        }
+
+        public void SaveBigData()
+        {
+            
+            var rnd = new Random();
+            var cache = RedisConnectorHelper.Connection.GetDatabase();
+            for(int i=0;i<devicesCount;i++)
+            {
+                var value = rnd.Next(0, 10000000);
+                cache.StringSet($"Device_Status:{i}", value);
+            }
+        }
+    }
+
+    public class RedisConnectorHelper
+    {
+        static RedisConnectorHelper()
+        {
+            RedisConnectorHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+              {
+                  return ConnectionMultiplexer.Connect("localhost");
+              });
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
