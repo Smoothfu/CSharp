d31d8a0575a6a9125cc9f5991783dca9c27eb57e
Fred1987 <2864326836@qq.com>
2018-05-28 20:23:53 +0800

    using System.ServiceModel;[AttributeUsage(AttributeTargets.Class|AttributeTargets.Interface,Inherited=false,AllowMultiple=false)] public sealed class ServiceContractAttribute:Attribute.Indicate that an interface or a class defines a service contract in a Windows Communication Foundation (WCF) application. public ServiceContractAttribute(); INitializes a new instance of the System.ServiceModel.ServiceContractAttribute class

---
+++ ConsoleApp186/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp186;
+using System.ServiceModel;
+
+namespace ConsoleApp186
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(ConsoleApp186.Service1)))
+            {
+                host.Open();
+                Console.WriteLine("The server has started!\n");
+                
+                Console.ReadLine();
+                host.Close();
+            }
+        }
+    }
+}
---
+++ ConsoleApp186/Service1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ConsoleApp186
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {       
+
+        public string DoWork(int value)
+        {
+            int squareValue = value * value;
+            return string.Format("The {0} of square value is {1}", value, squareValue);
+        }
+
+        public string Add(int x,int y)
+        {
+            return string.Format("{0}+{1}={2}", x, y, x + y);
+        }
+    }
+}
