e3c457fb9de2a8f981c0400fbf34569e9862306a
Fred1987 <2864326836@qq.com>
2018-05-28 18:50:30 +0800

    ADO.NET.public SqlDataAdapter(string selectCommandText,SqlConnection selectConnection); Initializes a new instance of the System.Data.SqlClient.SqlDataAdapter class with a System.Data.SqlClient.SqlDataAdapter.SelectCommand and a System.Data.SqlClient.SqlConnection object.selectCommandText:A System.String that is a Transact-SQL SELECT statement or stored procedure to be used by the SystemDataSqlClient.SqlDataAdapter.SelectCommand property of the System.Data.SqlClient.SqlDataAdapter.selectConnection:A System.Data.SqlClient.SqlConnection that represents the connection.If your connection string does not use Integrated Security=true,you can use System.Data.SqlClient.SqlCredential to pass the user ID and password more securely than by specifying the user ID and password as text in the connection string

---
+++ ConsoleApp185/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ConsoleApp185
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = ConfigurationManager.AppSettings["connString"].ToString();
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            
+            if(conn.State==ConnectionState.Open)
+            {
+                string selectSQL = "select * from AdventureWorks2014.sales.vStoreWithContacts;select * from AdventureWorks2014.Production.ProductCostHistory;select * from AdventureWorks2014.Sales.vStoreWithDemographics";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(selectSQL, conn);
+                DataSet ds = new DataSet();
+                sqlDataAdapter.Fill(ds);
+
+                for(int i=0;i<ds.Tables.Count;i++)
+                {                   
+                    if(ds.Tables[i].Rows.Count>0)
+                    {
+                        Console.WriteLine("\n\nThis is the table{0}\n\n", i + 1);
+                        for(int c=0;c<ds.Tables[i].Columns.Count;c++)
+                        {
+                            string columnName = ds.Tables[i].Columns[c].ColumnName;
+                            Console.Write(string.Format("{0,-20}", columnName));
+                        }
+                        Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                        for (int j=0;j<ds.Tables[i].Rows.Count;j++)
+                        {
+                            for(int k=0;k<ds.Tables[i].Columns.Count;k++)
+                            {
+                                Console.Write(string.Format("{0,-20}", ds.Tables[i].Rows[j][k].ToString()));
+                            }
+                            Console.WriteLine();
+                        }
+
+                        Console.WriteLine("\n\n\n\n\n");
+                    }
+                }
+            }
+
+            conn.Close();
+            Console.ReadLine();
+        }
+    }
+}
