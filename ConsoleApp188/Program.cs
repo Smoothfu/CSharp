40328ffc0ec9511a661331fe85b5e60b6522f65b
Fred1987 <2864326836@qq.com>
2018-05-28 21:35:06 +0800

    WcfServiceLibrary1.Service1Startedhttp://localhost:8733/Design_Time_Addresses/WcfServiceLibrary1/Service1/mex. [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface,Inherited=false,AllowMultiple=false)] public sealed class ServiceContractAttribute:Attribute public ServiceContractAttribute(); Initializes a new instance of the System.ServiceModel.ServiceContractAttribute class

---
+++ ConsoleApp188/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary1;
+
+namespace ConsoleApp188
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WcfServiceLibrary1.Service1 obj = new Service1();
+            string strResult = obj.GetData(10000000);
+            Console.WriteLine(strResult);
+            Console.WriteLine();
+            string addStringResult = obj.AddMethod(1000000, 100000);
+            Console.WriteLine(addStringResult);
+            Console.ReadLine();
+        }
+    }
+}
