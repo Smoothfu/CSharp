32e764ad8061417ec1a68deee7a5d61394c48742
Fred1987 <2864326836@qq.com>
2018-05-29 20:44:25 +0800

    Service host for Windows Communication Foundation services. WcfServiceLibrary3.Service1Startedhttp://localhost:8733/Design_Time_Addresses/WcfServiceLibrary3/Service1/mex

---
+++ ConsoleApp191/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace ConsoleApp191
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            string strResult=client.AddMethod(1000000, 23462456);
+            Console.WriteLine(strResult);
+            Console.ReadLine();
+        }
+    }
+}
