1d73774e0bee712acc00f95d89bc63f030ccedf6
Fred1987 <2864326836@qq.com>
2018-05-29 20:11:07 +0800

    public partial class ServiceClient:System.ServiceModel.ClientBase<ConosoleApp190.ServiceReference1.IService1> public ServiceClient(){} [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IService1/addMethod",ReplyAction="http://tempuri.org/IService1/addMethodResponse")]

---
+++ ConsoleApp190/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp190
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            string result = client.addMethod(100, 200);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
