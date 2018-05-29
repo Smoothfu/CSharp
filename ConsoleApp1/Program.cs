1a5009f4460df50d8d893010abc65125ab3e8047
Fred1987 <2864326836@qq.com>
2018-05-29 20:35:43 +0800

    WCF Service Host,status:Started;MetaData Addresshttp://localhost:8733/Design_Time_Addresses/WcfServiceLibrary2/Service1/mex

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary2;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WcfServiceLibrary2.Service1 obj = new Service1();
+            string strResult = obj.AddMethod(10000, 132324887);
+            Console.WriteLine(strResult);
+            Console.ReadLine();
+        }
+    }
+}
