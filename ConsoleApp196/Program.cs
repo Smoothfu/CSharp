7069e175f290e1b5f11f8c588ba21c36e0e93436
Fred1987 <2864326836@qq.com>
2018-05-30 20:15:18 +0800

    [ComVisible(true)] Specifies the apartment state of a System.Threading.Thread.  STA=0,The System.Threading.Thread will create and enter a single-threaded apartment.MTA=1,The System.Threading.Thread will create and enter a multithreadeed apartment. Unknown=2,The System.Threading.Thread.ApartmentState property has not been set

---
+++ ConsoleApp196/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp196
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtractExecutingThread();
+            Console.ReadLine();
+        }
+
+        static void ExtractExecutingThread()
+        {
+            //Get the thread currently executing this method.
+            Thread ct = Thread.CurrentThread;
+            Console.WriteLine(ct.ManagedThreadId);
+            Console.WriteLine(ct.ApartmentState);
+            Console.WriteLine(ct.CurrentCulture);
+            Console.WriteLine(ct.CurrentUICulture);
+            Console.WriteLine(ct.IsAlive);
+            Console.WriteLine(ct.IsBackground);
+            Console.WriteLine(ct.IsThreadPoolThread);
+            Console.WriteLine(ct.Name);
+            Console.WriteLine(ct.Priority);
+            Console.WriteLine(ct.ThreadState);             
+        }
+    }
+}
