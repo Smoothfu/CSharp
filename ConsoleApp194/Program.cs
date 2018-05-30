d11a08865e1f091baa141ad8f84131a7231a4bba
Fred1987 <2864326836@qq.com>
2018-05-30 15:57:33 +0800

    [System.Diagnostics.DebuggerStepThroughAttribute()] [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel","4.0.0.0 ")] public partial class ServiceClient:System.ServiceModel.ClientBase<ConsoleApp194.ServiceReference1.IService1>,ConsoleApp194.ServiceReference1.IService1

---
+++ ConsoleApp194/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp194
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            string strResult=client.AddMethod(10000000, 100000000);
+            Console.WriteLine(strResult);
+            Console.ReadLine();
+        }
+    }
+}
