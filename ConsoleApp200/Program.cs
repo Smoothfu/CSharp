9fd3331d705aebd2162d4c676723004012c4e5dc
Fred1987 <2864326836@qq.com>
2018-06-01 21:03:26 +0800

    [ComVisible(true)] public delegate void ParameterizedThreadStart(object obj);  Represents the method that executes on a System.Threading.Thread. obj:An object that contains data for the thread procedure

--- ConsoleApp200/Program.cs
+++ ConsoleApp200/Program.cs
@@ -12,19 +12,42 @@ namespace ConsoleApp200
         static int x = 36245645, y = 3453454;
         static void Main(string[] args)
         {
-            Thread newThread = new Thread(() =>
-              {
-                  AddMethod(x,y);
-              });
-            newThread.Start();
-            
-            Console.ReadLine();
+            Person p = new Person(1, "Floomberg");
+            ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart(p.OutputPerson);
+            Thread newThread = new Thread(parameterizedThreadStart);
+            newThread.Start(p);
+            Console.WriteLine("State {0}\n", newThread.ThreadState);
 
+            Console.ReadLine();
         }
 
         static void AddMethod(int x,int y)
         {
             Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+        }
+    }
+
+    public class Person
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public Person(int pId,string pName)
+        {
+            PId = pId;
+            PName = pName;
+        }
+
+        public void OutputPerson(object obj)
+        {
+            var p = obj as Person;
+            if(p!=null)
+            {
+                Console.WriteLine("PId:{0},PName:{1}\n", p.PId, p.PName);
+            }            
+        }
+        public override string ToString()
+        {
+            return string.Format("PId:{0},PName:{1}\n", PId, PName);
         }
     }
 }
