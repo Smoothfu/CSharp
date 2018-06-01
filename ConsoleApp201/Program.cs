c80be02e2689f7341b8816091091476d083e5dff
Fred1987 <2864326836@qq.com>
2018-06-01 21:58:38 +0800

    public class DataSet:MarshalByValueComponent,IListSource,IXmlSerializable,ISupportInitializeNotification,ISupportInitialize,ISerializeable. Represents an in-memory cache of data

--- ConsoleApp201/Program.cs
+++ ConsoleApp201/Program.cs
@@ -17,7 +17,43 @@ namespace ConsoleApp201
             SqlConnection conn = new SqlConnection(connString);
             conn.Open();
             Console.WriteLine(conn.State);
-            Console.WriteLine(1);
+
+            
+            if(conn.State==ConnectionState.Open)
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "spGetProductByPIDRange";
+                    cmd.Parameters.Add(new SqlParameter("@MinPID", 1));
+                    cmd.Parameters.Add(new SqlParameter("@MaxPID", 10));
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+                    DataSet ds = new DataSet();
+                    sqlDataAdapter.SelectCommand = cmd;
+                    sqlDataAdapter.Fill(ds);
+
+                    if(ds!=null && ds.Tables!=null && ds.Tables[0].Rows.Count>0)
+                    {
+                        //Column names
+                        for(int i=0;i<ds.Tables[0].Columns.Count;i++)
+                        {
+                            Console.Write("{0,-30}", ds.Tables[0].Columns[i].ColumnName);
+                        }
+
+                        Console.WriteLine();
+                        
+                        for(int i=0;i<ds.Tables[0].Rows.Count;i++)
+                        {
+                            for(int j=0;j<ds.Tables[0].Columns.Count;j++)
+                            {
+                                Console.Write("{0,-30}", ds.Tables[0].Rows[i][j].ToString());
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
             Console.ReadLine();
         }
     }
