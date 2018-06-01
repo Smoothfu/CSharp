af39430b39b8e058e49acd055f374e2d91e58840
Fred1987 <2864326836@qq.com>
2018-06-01 19:57:14 +0800

    Thread newThread=new Thread(()=>{ obj.AddMethod(x,y);}); newThread.Start(); newThread.ThreadState;

---
+++ ConsoleApp200/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Thread newThread = new Thread(() =>
+            {
+                obj.AddMethod(234523452, 345245624);
+            });
+            newThread.Start();
+            Console.WriteLine(newThread.ThreadState);
+            Console.ReadLine();
+
+        }
+
+        private void AddMethod(int x,int y)
+        {
+            Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+        }
+    }
+}
