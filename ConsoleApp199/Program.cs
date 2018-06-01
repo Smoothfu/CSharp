53d44bf5105b89b3335e05b29d289397290c9377
Fred1987 <2864326836@qq.com>
2018-06-01 19:14:36 +0800

    [ComVisible(true)] public interface IDisposable void Dispose(); Performs application-defined tasks associated with freeing,releasing,or resettting unmanaged resource.Provides a mechanism for releasing unmanaged resources.To browse the .NET Framework source code for this type

--- ConsoleApp199/Program.cs
+++ ConsoleApp199/Program.cs
@@ -13,53 +13,52 @@ namespace ConsoleApp199
     {
         static void Main(string[] args)
         {
-            string connString = ConfigurationManager.AppSettings["connString"].ToString();
+            string connString = @"Server=FRED\SQLEXPRESS;Database=AdventureWorks2014;Integrated Security=SSPI;";
             SqlConnection conn = new SqlConnection(connString);
             conn.Open();
 
-            using (SqlCommand cmd = new SqlCommand())
+            Console.WriteLine("Now the SqlConnection's state is {0}.\n\n", conn.State);
+
+            if (conn.State == ConnectionState.Open)
             {
-                cmd.Connection = conn;
-                cmd.CommandText = "spGetStoreById";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add(new SqlParameter("@bEID", 292));
-                
-                cmd.Parameters[0].Value = 292;
-                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
-                sqlDataAdapter.SelectCommand = cmd;
-                DataSet ds = new DataSet();
-                sqlDataAdapter.Fill(ds);
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "spRetriveStores";
+                    SqlDataAdapter sda = new SqlDataAdapter();
+                    DataSet ds = new DataSet();
+                    sda.SelectCommand = cmd;
+                    sda.Fill(ds);
 
-                if(ds!=null && ds.Tables.Count>0 && ds.Tables[0].Rows.Count>0)
-                {                    
-                    Console.Write("{0,-10}", ds.Tables[0].Columns[0].ColumnName);
-                    Console.Write("{0,-40}", ds.Tables[0].Columns[1].ColumnName);
-                    Console.Write("{0,-10}", ds.Tables[0].Columns[2].ColumnName);
-                    Console.Write("{0,-40}", ds.Tables[0].Columns[3].ColumnName);
-                    Console.Write("{0,-30}", ds.Tables[0].Columns[4].ColumnName);                     
-
-                    Console.WriteLine("\n----------------------------------------------------------------------------------------------------------------------------");
-
-                    for(int i=0;i<ds.Tables[0].Rows.Count;i++)
+                    if (ds.Tables != null && ds.Tables[0] != null && ds.Tables[0].Rows.Count > 0)
                     {
-                        Console.Write("{0,-10}", ds.Tables[0].Rows[i][0].ToString());
-                        Console.Write("{0,-40}", ds.Tables[0].Rows[i][1].ToString());
-                        Console.Write("{0,-10}", ds.Tables[0].Rows[i][2].ToString());
-                        Console.Write("{0,-40}", ds.Tables[0].Rows[i][3].ToString());
-                        Console.Write("{0,-30}", ds.Tables[0].Rows[i][4].ToString());
+                        Console.WriteLine("\nThere are {0} rows data totally!\n", ds.Tables[0].Rows.Count);
+                        Console.Write("{0,-10}", ds.Tables[0].Columns[0].ColumnName);
+                        Console.Write("{0,-40}", ds.Tables[0].Columns[1].ColumnName);
+                        Console.Write("{0,-10}", ds.Tables[0].Columns[2].ColumnName);
+                        Console.Write("{0,-40}", ds.Tables[0].Columns[3].ColumnName);
+                        Console.Write("{0,-30}", ds.Tables[0].Columns[4].ColumnName);
+
+                        Console.WriteLine("\n------------------------------------------------------------------------------------------------------------------------------------------\n");
+
+                        for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                        {
+                            Console.Write("{0,-10}", ds.Tables[0].Rows[i][0].ToString());
+                            Console.Write("{0,-40}", ds.Tables[0].Rows[i][1].ToString());
+                            Console.Write("{0,-10}", ds.Tables[0].Rows[i][2].ToString());
+                            Console.Write("{0,-40}", ds.Tables[0].Rows[i][3].ToString());
+                            Console.Write("{0,-30}\n", ds.Tables[0].Rows[i][4].ToString());
+                        }
                     }
                 }
             }
 
-            Console.WriteLine("\n\n\n");
-
             conn.Close();
-            Console.WriteLine("The conn state after Dispose();\n");
-            Console.WriteLine(conn.State);
-            conn.Dispose();
-            Console.WriteLine("The conn state after Dispose();\n");
-            Console.WriteLine(conn.State);
+            Console.WriteLine("\nAfter conn.Close(),the conn.State is:{0}\n", conn.State);
 
+            conn.Dispose();
+            Console.WriteLine("\nAfter conn.Dispose(),the conn.State is:{0}\n", conn.State);
             Console.ReadLine();
         }
     }
