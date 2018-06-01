2070c512031dd9800b1f325f917124272ae7ca11
Fred1987 <2864326836@qq.com>
2018-06-01 21:46:49 +0800

    <configuration> <connectionStrings> <add name="sqlConnString" providerName="System.Data.SqlClient" connectionString="Server=FRED\SQLEXPRESS;Database=mydb;Integrated Security=true;"/>

---
+++ ConsoleApp201/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp201
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = ConfigurationManager.ConnectionStrings["sqlConnString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            Console.WriteLine(conn.State);
+            Console.WriteLine(1);
+            Console.ReadLine();
+        }
+    }
+}
