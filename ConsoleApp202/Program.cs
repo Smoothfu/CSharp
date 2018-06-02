906fc77fe2d4d9b17b5a91b47a30518b3f2af6fe
Fred1987 <2864326836@qq.com>
2018-06-02 18:28:41 +0800

    [ComVisible(true)] public delegate void ThreadStart(); Repressents the method that executes on a System.Threading.Thread

---
+++ ConsoleApp202/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Threading;
+
+
+namespace ConsoleApp202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personObj = new Person(1,"Floomberg");
+            ThreadStart ts = new ThreadStart(personObj.GetPersonInfo);
+            Thread thread = new Thread(ts);
+            thread.Start();
+            Console.ReadLine();
+        }        
+    }
+
+    public class Person
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public Person(int pId,string pName)
+        {
+            PId = pId;
+            PName = pName;
+        }
+
+        public void GetPersonInfo()
+        {
+            Console.WriteLine("PId:{0},PName:{1}\n", PId, PName);
+        }
+        public override string ToString()
+        {
+            return string.Format("PId:{0},PName:{1}\n", PId, PName);
+        }
+    }
+}
