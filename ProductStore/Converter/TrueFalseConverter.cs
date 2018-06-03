97eb9b24ca2c65e9981b91dcc0d2fa9066376aef
Fred1987 <2864326836@qq.com>
2018-06-03 19:33:18 +0800

    xmlns:converter="clr-namespace:ProductStore.Converter"

---
+++ ProductStore/Converter/TrueFalseConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductStore.CommonClass;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace ProductStore.Converter
+{
+    public class TrueFalseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value==null)
+            {
+                return TrueFalseEnum.Unknow;
+            }
+
+            bool IsTrue;
+            if(Boolean.TryParse(value.ToString(),out IsTrue))
+            {
+                if(IsTrue)
+                {
+                    return TrueFalseEnum.True;
+                }
+                else
+                {
+                    return TrueFalseEnum.False;
+                }
+            }
+            return TrueFalseEnum.Unknow;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
