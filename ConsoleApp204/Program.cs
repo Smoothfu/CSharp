4068abc2e713f5e9b6c4048b522b292d3ed76fe8
Fred1987 <2864326836@qq.com>
2018-06-03 12:13:46 +0800

    Add ServiceReference http://localhost:7288/Service1.svc

---
+++ ConsoleApp204/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp204
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // http://localhost:8117/Service1.svc
+            //http://localhost:7288/Service1.svc
+            string connString = @"Server=FRED\SQLEXPRESS;Database=AdventureWorks2014;Integrated Security=SSPI;";
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            string connectionStatus = client.SqlConnectionStatus(connString);
+            Console.WriteLine(connectionStatus);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WcfService5/WcfService5/Service1.svc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace WcfService5
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        static string connString = @"Server=FRED\SQLEXPRESS;Database=AdventureWorks2014;Integrated Security=SSPI;";
+        public int AddMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string SqlConnectionStatus(string conString)
+        {
+            SqlConnection conn = new SqlConnection(conString);
+            conn.Open();
+            return conn.State.ToString();
+        }
+    }
+}
