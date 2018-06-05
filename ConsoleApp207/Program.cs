889eb00a60eb921d39314df4fc76c55336ced928
Fred1987 <2864326836@qq.com>
2018-06-05 23:29:30 +0800

    public static Point operator+(Point x,Point y);

---
+++ ConsoleApp207/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace ConsoleApp207
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point X = new Point(100, 200);
+            Point Y = new Point(300, 400);
+            Point Z = X + Y;
+            Console.WriteLine(Z.ToString());
+            Console.ReadLine();
+        }
+    }
+
+    public class Point
+    {
+        public int XCoord { get; set; }
+        public int YCoord { get; set; }
+        public Point(int xCoord,int yCoord)
+        {
+            XCoord = xCoord;
+            YCoord = yCoord;
+        }
+
+       public static  Point operator +(Point X,Point Y)
+        {
+            int zXCoord = X.XCoord + Y.YCoord;
+            int zYCoord = X.YCoord + Y.YCoord;
+
+            Point Z = new Point(zXCoord, zYCoord);
+            return Z;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("X:{0},Y:{1}\n", XCoord, YCoord);
+        }
+    }
+}
