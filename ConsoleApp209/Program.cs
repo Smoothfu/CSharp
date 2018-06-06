aed899fff5143f73cf4e43240ca394a6b5ade61b
Fred1987 <2864326836@qq.com>
2018-06-06 23:27:05 +0800

    sqlDataAdpater.SelectCommand=cmd;

---
+++ ConsoleApp209/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = @"Server=FRED\SQLEXPRESS;Database=AdventureWorks2014;Integrated Security=SSPI;";
+            SqlConnection conn = new SqlConnection(connString);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            conn.Open();
+
+            if(conn.State==ConnectionState.Open)
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "spGetProductInventory";
+                    sqlDataAdapter.SelectCommand = cmd;
+                    sqlDataAdapter.Fill(ds);
+
+                    if(ds.Tables[0].Rows.Count>0)
+                    {
+                        for(int i=0;i<ds.Tables[0].Rows.Count;i++)
+                        {
+                            for(int j=0;j<ds.Tables[0].Columns.Count;j++)
+                            {
+                                Console.Write("{0,-10} {1,-30}",ds.Tables[0].Columns[j].ColumnName ,ds.Tables[0].Rows[i][j].ToString()+"\t");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+
+            }
+
+            Console.ReadLine();
+           
+        }
+    }
+}
