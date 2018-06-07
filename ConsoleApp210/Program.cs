485f631141c1d4ce84edbd1dc79f33cdf566734e
Fred1987 <2864326836@qq.com>
2018-06-07 11:42:21 +0800

    public class Socket:IDisposable Implements the Berkeyley sockets interface. public Socket(AddressFamily addressFamily,SocketType socketType,ProtocolType protocolTYpe); Initializes a new instance of the System.Net.Sockets.Socket class using the specified address family,socket type and protocal. addressFamily:One of the System.Net.Sockets.AddressFamily values.socketType:One of the System.Net.Sockets.SocketType values. protocolType:One of the System.Net.Sockets.ProtocolType values

--- ConsoleApp210/Program.cs
+++ ConsoleApp210/Program.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Messaging;
-
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
 
 namespace ConsoleApp210
 {
@@ -12,8 +14,21 @@ namespace ConsoleApp210
     {
         static void Main(string[] args)
         {
-            SendMessage();
-            ReceiveMessage();
+            string host;
+            int port = 80;
+
+            if(args.Length==0)
+            {
+                //If no server name is passed as argument to this program,use the current host name as the default
+                host = Dns.GetHostName();
+            }
+            else
+            {
+                host = args[0];
+            }
+
+            string result = SocketSendReceive(host, port);
+            Console.WriteLine(result);
             Console.ReadLine();
         }
 
@@ -57,6 +72,69 @@ namespace ConsoleApp210
                 Console.WriteLine(ex.Message);
                 System.Windows.Forms.MessageBox.Show(ex.Message);
             }
+        }
+
+        private static Socket ConnectSocket(string server,int port)
+        {
+            Socket s = null;
+            IPHostEntry hostEntry = null;
+
+            //Get host related information.
+            hostEntry = Dns.GetHostEntry(server);
+
+            //Loop through the AddressList to obtain the supported AddressFamily.This is to avoid 
+            //an exception that occurs when the host IP Address is not compatible with the address family
+            //typical in the IPv6 case.
+            foreach(IPAddress address in hostEntry.AddressList)
+            {
+                IPEndPoint ipe = new IPEndPoint(address, port);
+                Socket tempSocket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                tempSocket.Connect(ipe);
+
+                if(tempSocket.Connected)
+                {
+                    s = tempSocket;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return s;
+        }
+
+        //This method requests the home page content for the specified server.
+        private static string SocketSendReceive(string server,int port)
+        {
+            string request = "GET/HTTP/1.1\r\nHost: " + server + "\r\nConnection:Close\r\n\r";
+            Byte[] bytesSend = Encoding.ASCII.GetBytes(request);
+            Byte[] bytesReceived = new Byte[256];
+
+            //Create a socket connection with the specified server and port.
+            Socket st = ConnectSocket(server, port);
+
+            if(st==null)
+            {
+                return ("Connection failed");
+            }
+
+            //Send request to the server.
+            st.Send(bytesSend, bytesSend.Length, 0);
+
+            //Receive the server home page content.
+            int bytes = 0;
+            string page = "Default HTML page on " + server + ":\r\n";
+
+            //The following will block until the page is  transmitted.
+            do
+            {
+                bytes = st.Receive(bytesReceived, bytesReceived.Length, 0);
+                page = page + Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+            }
+            while (bytes > 0);
+            return page;
         }
 
     }
