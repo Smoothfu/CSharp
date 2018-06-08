f110bfa345372f41c17c3ba526a1c0b385e01c42
Fred1987 <2864326836@qq.com>
2018-06-08 21:57:00 +0800

    Customize EventArgs. public event EventHandler<PersonArgs> PersonMaxAgeReached. protected virtual void OnPersonMaxAgeReached(object sender,PersonArgs e);

--- ConsoleApp215/Program.cs
+++ ConsoleApp215/Program.cs
@@ -10,23 +10,35 @@ namespace ConsoleApp215
     {
         static void Main(string[] args)
         {
-            Counter c = new Counter(10);
-            c.ThresholdReached += C_ThresholdReached;
-
-            Console.WriteLine("Press 'a' key to increase total!");
+            Person p = new Person(10);
+            p.PersonMaxAgeReached += P_PersonMaxAgeReached;
             do
             {
-                c.Add(1);
-            } while (c.total < 11);
+                p.AddAge(1);
+            } while (p.PAge < 101);
+
+            //Counter c = new Counter(10);
+            //c.ThresholdReached += C_ThresholdReached;
+
+            //Console.WriteLine("Press 'a' key to increase total!");
+            //do
+            //{
+            //    c.Add(1);
+            //} while (c.total < 11);
 
             Console.ReadLine();
         }
 
-        private static void C_ThresholdReached(object sender, ThreadsholdReachedEventArgs e)
+        private static void P_PersonMaxAgeReached(object sender, PersonArgs e)
         {
-            Console.WriteLine("Threshold:{0},TimeReached:{1}\n", e.Threshold, e.TimeReached);
-           
+            Console.WriteLine("MaxAge:{0},NowTime:{1}\n", e.MaxAge, e.NowTime);
         }
+
+        //private static void C_ThresholdReached(object sender, ThreadsholdReachedEventArgs e)
+        //{
+        //    Console.WriteLine("Threshold:{0},TimeReached:{1}\n", e.Threshold, e.TimeReached);
+           
+        //}
     }
 
     class Counter
@@ -56,11 +68,7 @@ namespace ConsoleApp215
 
         protected virtual void OnThresholdReached(ThreadsholdReachedEventArgs e)
         {
-            EventHandler<ThreadsholdReachedEventArgs> handler = ThresholdReached;
-            if(handler!=null)
-            {
-                handler(this, e);
-            }
+            ThresholdReached(this, e);
         }
     }
 
@@ -68,5 +76,41 @@ namespace ConsoleApp215
     {
         public int Threshold { get; set; }
         public DateTime TimeReached { get; set; }
+    }
+
+    public class Person
+    {
+        public event EventHandler<PersonArgs> PersonMaxAgeReached;
+        public int PAge { get; set; }
+        public int MAge = 100;
+
+        public Person(int pAge)
+        {
+            PAge = pAge;
+        }
+
+        protected virtual void OnPersonMaxAgeReached(object sender,PersonArgs e)
+        {
+            PersonMaxAgeReached(this, e);
+        }
+
+        public void AddAge(int i)
+        {
+            PAge += 10 * i;
+            if(PAge>MAge)
+            {
+                PersonArgs pa = new PersonArgs();
+                pa.MaxAge = MAge;
+                pa.NowTime = DateTime.Now;
+                OnPersonMaxAgeReached(this,pa);
+            }
+        }
+
+    }
+
+    public class PersonArgs:EventArgs
+    {
+        public int MaxAge { get; set; }
+        public DateTime NowTime { get; set; }
     }
 }
