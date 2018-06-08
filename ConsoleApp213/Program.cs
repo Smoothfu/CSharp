b577486afa43eabad5b682fa8554c0478cecbc4a
Fred1987 <2864326836@qq.com>
2018-06-08 19:01:03 +0800

    public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource,bool> predicate); Determines whether all elements of a sequence satisfy a condition.source:An System.Collections.Generic.IEnumerable`1 that contains the elements the predicate to.predicate:A function to test each element for a condition. Returns true if elvery element of the source passes the test in teh specified predicate or if the sequence is empty,otherwise false. public static IEnumerable<TSource> Distinct<TSource>(this IEnumerable<TSource> source);Returns distinct elements from a sequence by using the default equality comparer to compare values.The sequence to remove duplicate elements from.Returns an System.Collections.Generic.IEnumerable`1 that contains distinct elements from the source sequence

---
+++ ConsoleApp213/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp213
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 43, 43, 34, 4565, 355, 4654526, 5426456, 2456234 };
+            Console.WriteLine("There are {0} raw data in arr", arr.Length);
+            arr.All(x =>
+            {
+                Console.WriteLine(x); return true;
+            });
+
+            Console.WriteLine();
+            var distinctValues = arr.Distinct();
+            Console.WriteLine("\nThere are {0} distinct data in arr", distinctValues.Count());
+            distinctValues.ToList().ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+            Console.ReadLine();
+        }
+    }
+}
