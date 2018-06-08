d9b8e55a9922eb323c5ee2eb2877b139d65fe858
Fred1987 <2864326836@qq.com>
2018-06-08 20:55:42 +0800

    [ComVisible(true)] public class EventArgs  Represents the base class for classes that contain event data.and provides a value to use for events that do not include event data

--- WpfApp17/MainWindow.xaml.cs
+++ WpfApp17/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace WpfApp17
 {
@@ -24,9 +27,52 @@ namespace WpfApp17
         public MainWindow()
         {
             InitializeComponent();
+            btn.Content = "This button";
+            this.DataContext = this;
+            dg.ItemsSource = InitDataGrid();
+            InitDataGrid();
             myEvent += MainWindow_myEvent;
         }
 
+
+        static List<ProdInv> InitDataGrid()
+        {
+            string connSource = @"Server=FRED\SQLEXPRESS;Database=AdventureWorks2014;Integrated Security=SSPI;";
+            SqlConnection conn = new SqlConnection(connSource);
+            conn.Open();
+
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "spGetProductInventory";
+                SqlDataAdapter sda = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                sda.SelectCommand = cmd;
+                sda.Fill(ds);
+
+                if(ds.Tables[0].Rows.Count>0)
+                {
+                    for(int i=0;i<ds.Tables[0].Rows.Count;i++)
+                    {
+                        prodList.Add(new ProdInv()
+                        {
+                            PId = (int)ds.Tables[0].Rows[i][0],
+                            LId = (short)ds.Tables[0].Rows[i][1],
+                            Shelf = (string)ds.Tables[0].Rows[i][2],
+                            Bin = (byte)ds.Tables[0].Rows[i][3],
+                            Quantity = (short)ds.Tables[0].Rows[i][4],
+                            RowGuid=(Guid)ds.Tables[0].Rows[i][5],
+                            MD=(DateTime)ds.Tables[0].Rows[i][6]
+                        });
+                    }
+                }
+
+                return prodList;    
+            }
+            return null;
+
+        }
         private void MainWindow_myEvent(object sender, EventArgs e)
         {
             System.Windows.Forms.MessageBox.Show(e.ToString());
@@ -36,5 +82,12 @@ namespace WpfApp17
         {
             System.Windows.Forms.MessageBox.Show(e.Source.ToString());
         }
+
+        private void dg_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            MessageBox.Show(e.Row.Item.ToString());
+        }
+
+        public  static List<ProdInv> prodList = new List<ProdInv>();
     }
 }
---
+++ WpfApp17/ProdInv.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp17
+{
+    public class ProdInv
+    {
+        //0,int;
+        //1,shortint,
+        //2,string,
+        //3.byte,
+        //4,short,
+        //5,System.Guid,
+
+        //6,System.DateTime
+        public int PId{get;set;}
+        public short LId { get; set; }
+        public string Shelf { get; set; }
+        public byte Bin { get; set; }
+        public short Quantity { get; set; }
+        public Guid RowGuid { get; set; }
+        public DateTime MD { get; set; }
+
+
+        public override string ToString()
+        {
+            return string.Format("PId:{0},LId:{1},Shelf:{2},Bin:{3},Quantity:{4},RowGuid:{5},MD:{6}\n",
+                PId, LId, Shelf, Bin, Quantity, RowGuid, MD);
+        }
+    }
+}
