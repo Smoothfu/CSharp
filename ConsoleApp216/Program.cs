e6181379650d92f15bfd359f13d89e482088a79e
Fred1987 <2864326836@qq.com>
2018-06-09 15:15:40 +0800

    Thread thread=new Thread(()=>{});

---
+++ ConsoleApp216/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace ConsoleApp216
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Thread thread = new Thread(() =>
+              {
+                  Add(100, 2000000);
+              });
+            Console.WriteLine(thread.ManagedThreadId);
+            Console.ReadLine();
+        }
+
+        static void Add(int x,int y)
+        {
+            Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+        }
+
+    }
+}
