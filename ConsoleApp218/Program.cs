3e5163d1aa3c700627cd5f76450ed62931010a87
Fred1987 <2864326836@qq.com>
2018-06-10 13:31:32 +0800

    static Task<int> GetIntAsync(int x,int y) { return Task.Run(()=>{ return x+y;});}  static async void AddMethod() { int result=await GetIntAsync(x,y);

---
+++ ConsoleApp218/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddMethod();
+            Console.ReadLine();
+        }
+
+        static async void AddMethod()
+        {
+            int result = await GetIntAsync(243345234, 3452345);
+            Console.WriteLine(result);
+        }
+        static Task<int> GetIntAsync(int x,int y)
+        {
+            return Task.Run(() => {
+                return x + y;
+            });
+                
+        }
+    }
+}
