bdf0ebca1916e93ad7468479c9b273746ac3f4c6
Fred1987 <2864326836@qq.com>
2018-06-10 14:34:01 +0800

    the async and await keywords can simplify the process of invoking methods on a secondary thread of execution.While you have worked through only a few examples of what can be done with this aspect of the C# language,you are in a good position for further exploration

---
+++ ConsoleApp219/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp219
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddAsync();
+            Console.ReadLine();
+        }
+
+        static async Task Sum(object obj)
+        {
+            await Task.Run(() =>
+            {
+                var objAP = obj as AddParams;
+                if (objAP != null)
+                {
+                    Console.WriteLine("ID of thread in Add():{0}\n", Thread.CurrentThread.ManagedThreadId);
+
+                    Console.WriteLine("{0}+{1}={2}\n", objAP.PA, objAP.PB, objAP.PA + objAP.PB);
+                }
+            });
+        }
+
+        private static async Task AddAsync()
+        {
+            Console.WriteLine("******Adding with Thread objects******");
+            Console.WriteLine("ID of thread in Main():{0}\n", Thread.CurrentThread.ManagedThreadId);
+
+            AddParams ap = new AddParams(1000000, 463674567);
+            await Sum(ap);
+            Console.WriteLine("Other thread is done!");
+        }
+    }
+
+    public class AddParams
+    {
+        public int PA { get; set; }
+        public int PB { get; set; }
+
+        public AddParams(int pA,int pB)
+        {
+            PA = pA;
+            PB = pB;
+        }
+    }
+}
