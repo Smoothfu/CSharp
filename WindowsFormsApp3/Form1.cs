53a3e924d5a50477d9c63bf3214a31dab1740cd9
Fred1987 <2864326836@qq.com>
2018-06-10 10:33:39 +0800

    [ComVisible(true)] [DefaultMember("Chars")] public sealed class StringBuiler:ISerializable. Represents a mutable string of characters.This class cannot be inherited. public StringBuilder(string value);  Initializes a new instance of the System.Text.StringBuilder class using the specified string

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        string theEBook = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Download
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            wc.DownloadStringCompleted += Wc_DownloadStringCompleted;
+            //The project Gutenberg EBook of a Tale of two cities,by charles Dickens.
+            wc.DownloadStringAsync(new Uri("http://www.gutenberg.org/files/98/98-8.txt"));
+        }
+
+        private void Wc_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            theEBook = e.Result;
+            textBox1.Text = theEBook;
+        }
+
+        //Cancel
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Get the words from the e-book
+            string[] words = theEBook.Split(new char[] {' ','\u000A',',','.',';',':','-','?','/' },StringSplitOptions.RemoveEmptyEntries);
+
+            //Now find the ten most common words.
+            string[] tenMostCommon = FindTenMostCommon(words);
+
+            //Get the longest word.
+            string longestWord = FindLongestWord(words);
+
+            //Now that all tasks are complete,build a string to show all stats in a message box.
+            StringBuilder bookStats = new StringBuilder("Ten Most Common Words are:\n");
+            foreach(string str in tenMostCommon)
+            {
+                bookStats.AppendLine(str);
+            }
+
+            bookStats.AppendFormat("Longest word is :{0}\n", longestWord);
+            bookStats.AppendLine();
+            MessageBox.Show(bookStats.ToString(), "Book Inof");
+        }
+
+        private string[] FindTenMostCommon(string[] words)
+        {
+            if(words==null || words.Length<1)
+            {
+                return null;
+            }
+
+            var frequencyOrder = from word in words
+                                 where word.Length > 6
+                                 group word by word into g
+                                 orderby g.Count() descending
+                                 select g.Key;
+            string []commonWords = (frequencyOrder.Take(10)).ToArray();
+
+            return commonWords;
+        }
+
+        private string FindLongestWord(string[] words)
+        {
+            return (from w in words orderby w.Length descending select w).FirstOrDefault();
+        }
+    }
+}
