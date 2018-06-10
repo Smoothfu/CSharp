5bcb8e0cefc5d0725199deea812dc91979fcaec7
Fred1987 <2864326836@qq.com>
2018-06-10 16:19:00 +0800

    public static string[] GetFiles(string path,string searchPattern,SearchOption searchOption);  Returns the names of files including their paths that match the specified search pattern in the specified directory,using a value to determine whether to search subdirectories. path:The relative or absolute path to the directory to search.This string is not case-sensitive.searchPattern,The search string to match against the names of files in path.This parameter can contain a combination of  valid literal path and wildcard characters but does not support regular expressions, searchOption:One of the enumearation values that specifies whether the search operation should include all subdirectories or only the current directory. Returns:An array of the full names including paths for the files in the specified directory that match the specified search pattern and option,or an empty array if no files are found;

---
+++ ConsoleApp220/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp220
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\C\ConsoleApp219";
+            List<string> allFiles = GetAllFiles(path);
+
+            if (allFiles != null && allFiles.Any())
+            {
+                Console.WriteLine("There are {0} files in the {1}\n\n\n", allFiles.Count(), path);
+                allFiles.All(x =>
+                {
+                    Console.WriteLine(x);
+                    return true;
+                });
+               
+            }
+            Console.ReadLine();
+        }
+
+        public static List<string> GetAllFiles(string dir)
+        {
+            return Directory.GetFiles(dir, "*", SearchOption.AllDirectories).ToList();
+        }
+         
+    }
+}
