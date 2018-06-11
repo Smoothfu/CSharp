319871a654de7dfc91621c54fe0126b6178e3b7d
Fred1987 <2864326836@qq.com>
2018-06-11 11:36:21 +0800

    [ComVisible(true)] [Flags] public enum FileAttributes. Provides attributes for files and directories.ReadOnly=1,The file is read-only;Hidden=2,The file is hidden,and thus is not included in an ordinary directory listing.System=4,The file is a System file,that is,the file is part of the operating system or is used exclusively by the operating system.Directory=16,the file is a directory.Archive=32,the file is a candidate for backup or removal.Device=64,Reserved for future use.Normal=128,The file is a standard file that has no special attributes.This attribute is valid only if it is used alone.Temporary=256,The file is temporary.A temporary file contains data that is needed while an application is executing but is not needed after the application is finished.File Systems try to keep all the data in memory for quicker access rather than flushing the data back to mass storage.A temporary file should be deleted by the application as soon as it is no longer needed.SparseFile=512,The file is a sparse file.Sparse files are typically large files whose data consists of mostly zeros. ReparsePoint=1024,The file contains a reparse point,which is a block of user-defined data assoiciated with a file or a directory.Compressed=2048,The file is compressed.Offline=4096,the file is offline,the data of the file is not immediately available.NotContentIndexed=8192.The file will not be indexed by the operating systems context indexing service.Encrypted=16384,The file or directory is encrypted,for a file,this means that all data in the file is encrypted.For a directory,this means that encryption is the default for newly created files and directories.InterityStream=32768,The file or directory includes data integrity support.When this value is applied to a file,all data streams in the file have integrity support.When this value is applied to a directory,all new files and subdirectories within that directory by default,include integrity support.NoScrubData=131072 The file or directory is excluded from the data integrity scan.When this vlaue is applied to a directory,by default,all new files and subdirectories within that directory are excluded from the data integrity

--- ConsoleApp221/Program.cs
+++ ConsoleApp221/Program.cs
@@ -19,17 +19,8 @@ namespace ConsoleApp221
                 Console.WriteLine("There are totally {0} files in the specified path!\n\n\n",fis.Count());
                 fis.All(x =>
                 {
-                    Console.WriteLine(x.Attributes);
-                    Console.WriteLine(x.CreationTime);
-                    Console.WriteLine(x.Directory);
-                    Console.WriteLine(x.DirectoryName);
-                    Console.WriteLine(x.FullName);
-                    Console.WriteLine(x.LastAccessTime);
-                    Console.WriteLine(x.LastWriteTime);
-                    Console.WriteLine(x.Length);
-                    Console.WriteLine(x.Name);
-                    Console.WriteLine(x.ToString());
-                    Console.WriteLine("\n\n\n");
+                    Console.WriteLine("Attributes:{0,-30}\t FullName:{1}",x.Attributes,x.FullName);                    
+                    Console.WriteLine("");
                     return true;
                 });
             }
