e41e6997efdb4875eda0e9248e13906163486ef2
Fred1987 <2864326836@qq.com>
2018-06-11 10:49:40 +0800

    string[] allFiles=Directory.GetFiles("@D:\c\ConsoleApp221","*",SearchOption.AllDirectories);

---
+++ ConsoleApp221/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string[] allFiles=Directory.GetFiles(@"D:\C\ConsoleApp220","*",SearchOption.AllDirectories);
+          if(allFiles!=null && allFiles.Any())
+            {
+                Console.WriteLine("There are {0} totally files in the path\n\n\n", allFiles.Count());
+                allFiles.All(x =>
+                {
+                    Console.WriteLine(x);
+                    return true;
+                });
+            }
+            Console.ReadLine();
+        }
+    }
+}
