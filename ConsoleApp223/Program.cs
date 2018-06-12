e9a737c4b134a3a83d2518555b7d63350819960e
Fred1987 <2864326836@qq.com>
2018-06-12 19:58:17 +0800

    The event model in the .NET Framework is based on having an event delegate that connects an event with its handler.To raise an event,two elements are needed.A delegate that identifies the method that provides the response to the event.Optionally,a class that holsd the event data,if the event provides data

---
+++ ConsoleApp223/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp223
+{
+    public delegate void JudgeHandler(AdultPerson p,PersonEventArgs e);
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            AdultPerson p = new AdultPerson(31, 1);
+            p.JudgeEvent += P_JudgeEvent1;
+            p.JudgeAdultPerson(p);
+             
+            Console.ReadLine();
+        }
+
+        private static void P_JudgeEvent1(AdultPerson p, PersonEventArgs e)
+        {
+            Console.WriteLine("This is an adult person,and its age is {0} and now is {1}", e.PAge,e.Dt);
+        }
+        
+    }
+
+    public class AdultPerson
+    {
+        public event JudgeHandler JudgeEvent;
+        public int PAge { get; set; }
+        public int PId { get; set; }
+
+        public AdultPerson(int pAge,int pId)
+        {
+            PAge = pAge;
+            PId = pId;
+        }
+
+        public void JudgeAdultPerson(AdultPerson p)
+        {
+            if(p!=null && p.PAge>18)
+            {
+                OnJudgeAdult(p.PAge);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("PAge:{0},PId:{1}", PAge, PId);
+        }
+
+
+        public virtual void OnJudgeAdult(int page )
+        {
+            if(JudgeEvent!=null)
+            {
+                JudgeEvent(this, new PersonEventArgs(page,DateTime.Now));
+            }
+
+        }
+
+    }
+    public class PersonEventArgs:EventArgs
+    {
+        public int PAge { get; set; } 
+        public DateTime Dt { get; set; }
+
+        public PersonEventArgs(int pAge,DateTime dt)
+        {
+            PAge = pAge;
+            Dt = dt;
+        }
+         
+    }
+}
