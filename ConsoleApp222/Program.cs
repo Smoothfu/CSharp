4b4ebd32f807a1094804857e078b9caa23db97c0
Fred1987 <2864326836@qq.com>
2018-06-12 15:48:17 +0800

    [ComVisible(true)] public delegate void ThreadStart(); Represents the method that executes on a System.Threading.Thread

--- ConsoleApp222/Program.cs
+++ ConsoleApp222/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Threading;
 
 
 namespace ConsoleApp222
@@ -14,12 +15,22 @@ namespace ConsoleApp222
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("The main ThreadManagerThreadId:{0}", Thread.CurrentThread.ManagedThreadId);
+            ThreadStart ts = new ThreadStart(GetDataBySP);
+            Thread thread = new Thread(ts);
+            thread.Start();
+
+            Console.ReadLine();
+        }
+
+        static void GetDataBySP()
+        {
+            Console.WriteLine("The thread Id in GetDataBySP() :{0}\n", Thread.CurrentThread.ManagedThreadId);
             string sqlString = ConfigurationManager.AppSettings["connString"].ToString();
             SqlConnection conn = new SqlConnection(sqlString);
             conn.Open();
-            
 
-            if(conn.State==ConnectionState.Open)
+            if (conn.State == ConnectionState.Open)
             {
                 using (SqlCommand cmd = new SqlCommand())
                 {
@@ -32,20 +43,18 @@ namespace ConsoleApp222
                     DataSet ds = new DataSet();
                     sda.Fill(ds);
 
-                    if(ds.Tables[0].Rows.Count>0)
+                    if (ds.Tables[0].Rows.Count > 0)
                     {
-                        for(int i=0;i<ds.Tables[0].Rows.Count;i++)
+                        for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
                         {
-                            for(int j=0;j<ds.Tables[0].Columns.Count;j++)
+                            for (int j = 0; j < ds.Tables[0].Columns.Count; j++)
                             {
-                                Console.Write("{0,-2} :{1}\t",ds.Tables[0].Columns[j].ColumnName,ds.Tables[0].Rows[i][j].ToString());
+                                Console.Write("{0,-2} :{1}\t", ds.Tables[0].Columns[j].ColumnName, ds.Tables[0].Rows[i][j].ToString());
                             }
                         }
                     }
                 }
             }
-
-            Console.ReadLine();
         }
     }
 }
