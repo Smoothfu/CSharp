cb6f3d4e90f60a4e450b825e674fa62a0139ad62
Fred1987 <2864326836@qq.com>
2018-06-12 21:39:04 +0800

    [OperationContract] add ServiceReference http://localhost:3146/Service1.svc ServiceReference1.Client client=new ServiceReference1()

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ClassLibrary2;
+ 
 
 namespace ConsoleApp1
 {
@@ -12,11 +12,13 @@ namespace ConsoleApp1
         static int x = 10000, y = 10000;
         static void Main(string[] args)
         {
-            MathClass mc = new MathClass();
-            mc.AddMethod(x, y);
-            mc.SubtractMethod(x, y);
-            mc.MultiplyMethod(x, y);
-            mc.DivideMethod(x, y);
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            string addResult = client.AddMethod(x, y);
+            Console.WriteLine(addResult);
+            Console.WriteLine();
+
+            string multiplyResult = client.MultiplyMethod(x, y);
+            Console.WriteLine(multiplyResult);
             Console.ReadLine();
         }
     }
