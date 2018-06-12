18bce2cea80d4d09d1f376def65d90ea7921f648
Fred1987 <2864326836@qq.com>
2018-06-12 21:20:15 +0800

    ClassLibrary,Add Reference,

---
+++ ClassLibrary2/MathClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class MathClass
+    {
+        public void AddMethod(int x,int y)
+        {
+            Console.WriteLine("Add:{0}+{1}={2}\n", x, y, x + y);
+        }
+
+        public void SubtractMethod(int x,int y)
+        {
+            Console.WriteLine("Subtract:{0}-{1}={2}\n", x, y, x - y);
+        }
+
+        public void MultiplyMethod(int x,int y)
+        {
+            Console.WriteLine("Multiply:{0}*{1}={2}\n", x, y, x*y);
+        }
+
+        public void DivideMethod(int x,int y)
+        {
+            Console.WriteLine("Divide: {0}/{1}={2}", x, y, x/y);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int x = 10000, y = 10000;
+        static void Main(string[] args)
+        {
+            MathClass mc = new MathClass();
+            mc.AddMethod(x, y);
+            mc.SubtractMethod(x, y);
+            mc.MultiplyMethod(x, y);
+            mc.DivideMethod(x, y);
+            Console.ReadLine();
+        }
+    }
+}
