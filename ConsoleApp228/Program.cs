ed9044a60f1a1cdd4a306e54849688c73ce9e781
Fred1987 <2864326836@qq.com>
2018-06-19 22:47:31 +0800

    public delegate void DelNameHandler(object sender,EventNameArgs e); protected virtual void OnEventName()

--- ConsoleApp228/Program.cs
+++ ConsoleApp228/Program.cs
@@ -7,14 +7,20 @@ using System.Threading.Tasks;
 namespace ConsoleApp228
 {
     public delegate void JudgeAdultHandler(object sender,PersonEventArgs e);
+    public delegate void JudgeRichHandler(object sender, RichPersonEventArgs e);
     class Program
     {
         static void Main(string[] args)
         {
-            Person p = new Person(31, "Floomberg");
-            p.JudgeAdultEvent += P_JudgeAdultEvent;
-            p.JudgeAdult(p);
+            Person p = new Person(31, 1323452562465);
+            p.JudgeRichEvent += P_JudgeRichEvent;
+            p.JudgeRich(p);
             Console.ReadLine();
+        }
+
+        private static void P_JudgeRichEvent(object sender, RichPersonEventArgs e)
+        {
+            Console.WriteLine("This man is tycoon,and its net worth is " + e.RMoney);
         }
 
         private static void P_JudgeAdultEvent(object sender, PersonEventArgs e)
@@ -26,14 +32,22 @@ namespace ConsoleApp228
     public class Person
     {
         public event JudgeAdultHandler JudgeAdultEvent;
+        public event JudgeRichHandler JudgeRichEvent;
         public int Age { get; set; }
         public string Name { get; set; }
+
+        public decimal RMoney { get; set; }
         public Person(int age,string name)
         {
             Age = age;
             Name = name;
         }
 
+        public Person(int age,decimal rMoney)
+        {
+            Age = age;
+            RMoney = rMoney;
+        }
         protected virtual void OnJudgeAdultEvent()
         {
             if(JudgeAdultEvent!=null)
@@ -41,6 +55,16 @@ namespace ConsoleApp228
                 JudgeAdultEvent(this, new PersonEventArgs(Age));
             }
         }
+
+        protected virtual void OnJudgeRichPerson()
+        {
+            if (JudgeRichEvent != null)
+            {
+                JudgeRichEvent(this, new RichPersonEventArgs(RMoney));
+            }
+        }
+
+
         public override string ToString()
         {
             return string.Format("Age:{0},Name:{1}\n", Age, Name);
@@ -53,6 +77,14 @@ namespace ConsoleApp228
                 OnJudgeAdultEvent();
             }
         }
+
+        public void JudgeRich(Person p)
+        {
+            if(p.RMoney>100000000m)
+            {
+                OnJudgeRichPerson();
+            }
+        }
     }
 
     public class PersonEventArgs:EventArgs
@@ -61,6 +93,15 @@ namespace ConsoleApp228
         public PersonEventArgs(int eAge)
         {
             EAge = eAge;
+        }
+    }
+
+    public class RichPersonEventArgs:EventArgs
+    {
+        public decimal RMoney { get; set; }
+        public RichPersonEventArgs(decimal rMoney)
+        {
+            RMoney = rMoney; 
         }
     }
 }
