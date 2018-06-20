b8eebef921b6d1217ee785d53fc62fc0563a77fb
Fred1987 <2864326836@qq.com>
2018-06-20 19:33:26 +0800

    [ClassInterface(ClassInterfaceType.None)] [ComDefaultInterface(typeof(_Thread))][ComVisible(true)] public sealed class Thread:CriticalFinalizerObject,_Thread

---
+++ ConsoleApp229/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp229
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The ManagedThreadId in Main method is {0}\n", Thread.CurrentThread.ManagedThreadId);
+            Thread thread = new Thread(() =>
+              {
+                  Add(100, 200);
+              });
+            thread.Start();
+            Console.ReadLine();
+        }
+
+        static void Add(int x,int y)
+        {
+            Console.WriteLine("The ManagerThreadId in Add method is {0}\n", Thread.CurrentThread.ManagedThreadId);
+
+            Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+        }
+    }
+}
