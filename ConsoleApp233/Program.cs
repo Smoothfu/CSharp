e92208b872ddee33426e17da2843d70de1dcedf2
Fred1987 <2864326836@qq.com>
2018-06-25 22:37:42 +0800

    [SecuritySafeCritical]  public static string[] GetLogicalDrives();  Retrieves the names of the logical drives on this computer in the form

---
+++ ConsoleApp233/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp233
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryGetLogicalDrives();
+            Console.ReadLine();
+        }
+
+        static void DirectoryGetLogicalDrives()
+        {
+            string[] allDrivess=Directory.GetLogicalDrives();
+            if(allDrivess!=null && allDrivess.Any())
+            {
+                Console.WriteLine("There are {0} dirves in the {1}\n", allDrivess.Count(), Environment.MachineName);
+
+                Parallel.ForEach(allDrivess, x =>
+                {
+                    Console.WriteLine(x);
+                });
+            } 
+        }
+    }
+}
