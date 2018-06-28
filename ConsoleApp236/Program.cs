7e8d0ca5c0a06ac1f66478f95978ab70e67e0f1c
Fred1987 <2864326836@qq.com>
2018-06-28 22:51:18 +0800

    public delegate void JudgeAdultHandler(object sender,PersonEventArgs e);

---
+++ ConsoleApp236/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp236
+{
+    public delegate void JudgeAdultHandler(object sender, PersonEventArgs e);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person(31, "Fred");
+            p.JudgeAdultEvent += P_JudgeAdultEvent;
+            p.JudgePersonAdult(p);
+            Console.ReadLine();
+        }
+
+        private static void P_JudgeAdultEvent(object sender, PersonEventArgs e)
+        {
+            Console.WriteLine("It's age is {0},and time is {1}\n", e.Age, e.Dt);
+        }
+    }
+
+    public class Person
+    {
+        public event JudgeAdultHandler JudgeAdultEvent;
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public Person(int age,string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public void JudgePersonAdult(Person p)
+        {
+            if(p.Age>18)
+            {
+                OnJudgeAdultEvent();
+            }
+        }
+
+        protected void OnJudgeAdultEvent()
+        {
+            if(JudgeAdultEvent!=null)
+            {
+                JudgeAdultEvent(this, new PersonEventArgs(Age, DateTime.Now));
+            }
+        }
+    }
+
+    public class PersonEventArgs:EventArgs
+    {
+        public int Age { get; set; }
+        public DateTime Dt { get; set; }
+
+        public PersonEventArgs(int age,DateTime dt)
+        {
+            Age = age;
+            Dt = dt;
+        }
+    }
+}
