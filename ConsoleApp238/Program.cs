1f0373564151063c2c6a5de44448fd032d014ebf
Fred1987 <2864326836@qq.com>
2018-07-02 21:22:42 +0800

    [ComVisible(false)] public delegate void ParameterizedThreadStart(object obj);  Represents the method that executes on a System.Threading.Thread

---
+++ ConsoleApp238/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp238
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person(31, "Fred");
+            ParameterizedThreadStart pts = new ParameterizedThreadStart(DescPerson);
+            Thread thread = new Thread(pts);
+            thread.Start(p);
+            Console.ReadLine();
+        }
+
+        static void DescPerson(object obj)
+        {
+            var person = obj as Person;
+            Console.WriteLine(person.ToString());
+
+        }
+
+    }
+
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(int age,string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name:{0},Age:{1}\n", Name, Age);
+        }
+    }
+}
