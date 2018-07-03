876e00c22d5edb21e44a0a85a04503e6d62252f8
Fred1987 <2864326836@qq.com>
2018-07-04 00:27:39 +0800

    ParameterizedThreadStart contains an object parameter

---
+++ ConsoleApp239/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp239
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            ParameterizedThreadStart pts = new ParameterizedThreadStart(GetObjectHashCode);
+            Thread thread = new Thread(pts);
+            thread.Start(obj);
+            Console.ReadLine();
+        }
+
+        static void GetObjectHashCode(object obj)
+        {
+            Console.WriteLine(obj.GetHashCode());
+        }
+    }
+}
