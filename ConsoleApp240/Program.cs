446d7f68d2ca23bcc38533f352d6212df9f5170b
Fred1987 <2864326836@qq.com>
2018-07-05 21:57:40 +0800

    [AttributeUsage] public sealed class DataContractAttribute:Attribute, Specifies that the type defines or implements a data contract and is serializable by a serializer,suc h as the System.Runtime.Serialization.DataContractSerializer. To make their type serializable,type authors must define a data contract for their type

---
+++ ConsoleApp240/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace ConsoleApp240
+{
+    class Program
+    {
+        static TokenKey tokenKeyObj = new TokenKey();
+        static void Main(string[] args)
+        {
+            GetTokenAndKey();
+            Console.ReadLine();
+        }
+
+        static void GetTokenAndKey()
+        {
+            string loginUrl = "http://crmtest.zjlhhs.com.cn/zex-crm-rest/auth?userName=104&password=sj123@";
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(loginUrl);
+            req.Method = "GET";
+            req.ContentType = "application/json;charset=UTF-8";
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            using (Stream stream = res.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    string[] arr = result.Split(new string[] { ":",",",";","{","}" },StringSplitOptions.RemoveEmptyEntries);
+                    tokenKeyObj.ServerToken = arr[1];
+                    tokenKeyObj.ServerKey = arr[3];
+                    Console.WriteLine(result);
+                    Console.WriteLine(tokenKeyObj.ServerToken);
+                    Console.WriteLine(tokenKeyObj.ServerKey);
+                }
+            }
+        }
+    }
+
+    [DataContract]
+    public class TokenKey
+    {
+        [DataMember]
+        public string ServerToken { get; set; }
+        
+        [DataMember]
+        public string ServerKey { get; set; }
+    }
+}
