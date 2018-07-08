41f6a06a156651615d4225f225e6acafaf6261f9
Fred1987 <2864326836@qq.com>
2018-07-08 18:17:22 +0800

    The SoapFormatter type persists an objects state as a SOAP message the standard XML format for passing messages to/from a SOAP-based web service.This type is defined within the System.Runtime.Serialization.Formatters.Soap namespace,which is defined in a separate assembly.Thus,to format your object graph iinto a SOAP message.you must first set a reference to System.Runtime.Serizalization.Formatters.Soap.dll using the Visual Studio Add reference dialog box and then specify the following C# using directive. // Must reference System.Runtime.Serialization.Formatters.Soap.dll. using System.Runtime.Serialization.Formatters.Soap;

--- ConsoleApp242/Program.cs
+++ ConsoleApp242/Program.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
 using System.IO;
 
 namespace ConsoleApp242
