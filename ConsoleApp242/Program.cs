62d491eba702fb1b1df5395a2970c4d0bfc62a17
Fred1987 <2864326836@qq.com>
2018-07-08 17:32:23 +0800

    Thread thread=new Thread(()=>{});thread.Start();

---
+++ ConsoleApp242/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp242
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The managedThreadIs is :{0}\n", Thread.CurrentThread.ManagedThreadId);
+            Thread thread = new Thread(() =>
+              {
+                  AddMethod(235434, 456345645);
+              });
+            thread.Start();
+            Console.ReadLine();
+        }
+
+        static void AddMethod(int x,int y)
+        {
+            Console.WriteLine("The managedthreadIs in AddMethod is :{0}\n", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("{0}+{1}={2}", x, y, x + y);
+        }
+    }
+}
