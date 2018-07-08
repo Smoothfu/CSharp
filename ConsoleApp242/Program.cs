3c4c368575695a996b680e457ae8861523408624
Fred1987 <2864326836@qq.com>
2018-07-08 18:09:07 +0800

    The BinaryFormatter type serializes your objects state to a stream using a compact binary format.This type is defined within the System.Runtime.Serializaion.Formatters.Binary namespace that is part of mscorlib.dll.If you want to gain access to this type,you can specify the following C# using directive: using System.Runtime.Serialization.Formatters.Binary;

--- ConsoleApp242/Program.cs
+++ ConsoleApp242/Program.cs
@@ -44,4 +44,53 @@ namespace ConsoleApp242
         public string WindowColor;
         public int FontSize;
     }
+
+    [Serializable]
+    public class Radio
+    {
+        public bool hasTweeters;
+        public bool hasSubWoofers;
+        public double[] stationPresets;
+
+        [NonSerialized]
+        public string radioID = "XF-552RR6";
+    }
+
+    [Serializable]
+    public class Car
+    {
+        public Radio theRadio = new Radio();
+        public bool isHatchBack;
+    }
+
+    [Serializable]
+    public class JamesBondCar : Car
+    {
+        public bool canFly;
+        public bool canSubmerge;
+    }
+
+    [Serializable]
+    public class Person
+    {
+        //A public field
+        public bool isAlive = true;
+
+        //A private field
+        private int personAge = 21;
+
+        //public property/private data.
+        private string fName = string.Empty;
+        public string FirstName
+        {
+            get
+            {
+                return fName;
+            }
+            set
+            {
+                fName = value;
+            }
+        }
+    }
 }
