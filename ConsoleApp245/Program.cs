22e94ba07c7fd0071d4a1615fff9b658ec427226
Fred1987 <2864326836@qq.com>
2018-07-17 20:58:52 +0800

    [ComVisible(true)] public sealed class FileInfo:FileSystemInfo  Provides properties and instance methods for the creation,copying,deletion,moving,and opening of fies,and aids in the creation of System.IO.FileStream objects. [SecuritySafeCritical] public FileInfo(string fileName); Initialize a new instance of the System.IO.FileInfo class,which acts as a wrapper for a file path

---
+++ ConsoleApp245/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+
+
+namespace ConsoleApp245
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = @"You can use the BinaryFormatter type to illustrate how easy it is to persist an instance of the JamesBondCar
+to a physical file. Again, the two key methods of the BinaryFormatter type to be aware of are Serialize()
+and Deserialize().• Serialize(): Persists an object graph to a specified stream as a sequence of bytes
+• Deserialize(): Converts a persisted sequence of bytes to an object graph";
+            FileInfo fi = new FileInfo(".\\mydat3.dat");
+          
+                using (StreamWriter writer = new StreamWriter(fi.FullName))
+                {
+                    writer.Write(str);
+                }
+                using (StreamReader streamReader = new StreamReader(fi.FullName))
+                {
+                    string result = streamReader.ReadToEnd();
+                    Console.WriteLine(result);
+                }
+            
+            Console.ReadLine();
+        }
+
+       
+        static void SaveAsBinaryFormat(object obj,string fileName)
+        {
+            //Save object to a file named mybin.dat in binary;
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using (Stream fStream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
+            {
+                binFormat.Serialize(fStream, obj);
+            }
+           
+           
+           Console.WriteLine("=>Saved in binary format!");
+        }
+    }
+}
