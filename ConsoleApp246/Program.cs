eda2e864ae76be6b91616a98d6e7bfbe59f1b56e
Fred1987 <2864326836@qq.com>
2018-07-18 20:16:49 +0800

    [EditorBrowsable(EditorBrowasableState.Never)] Represents a reader that provides fast,non-cached,forward-only access to XML data.Starting with the .NET framework 2.0 ,we recommend that you use System.Xml.XmlReader class instead

---
+++ ConsoleApp246/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace ConsoleApp246
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an instance of XmlTextReader and call Read method to read the file.
+            string fullXmlPath = @" D:\C\ConsoleApp246\ConsoleApp246\XMLFile1.xml";
+            XmlTextReader xmlReader = new XmlTextReader(fullXmlPath);
+            xmlReader.Read();
+
+            //If the node has value.
+            while(xmlReader.Read())
+            {
+                //Move to first element.
+                xmlReader.MoveToElement();
+                Console.WriteLine("XmlTextReader properties test:\n");
+                Console.WriteLine("=====================");
+                Console.WriteLine(xmlReader.Name);
+                Console.WriteLine(xmlReader.BaseURI);
+                Console.WriteLine(xmlReader.LocalName);
+                Console.WriteLine(xmlReader.AttributeCount.ToString());
+                Console.WriteLine(xmlReader.Depth.ToString());
+                Console.WriteLine(xmlReader.LineNumber.ToString());
+                Console.WriteLine(xmlReader.NodeType.ToString());
+                Console.WriteLine(xmlReader.Value.ToString());
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
