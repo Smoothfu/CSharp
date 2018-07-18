05c8b4c0fa67ce90d295ca6b3dbd5b564f76a28c
Fred1987 <2864326836@qq.com>
2018-07-18 22:09:27 +0800

    PresentationFramework. using System.Windows.Shapes;

--- ConsoleApp247/Program.cs
+++ ConsoleApp247/Program.cs
@@ -5,7 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
-
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Media;
 
 namespace ConsoleApp247
 {
@@ -26,4 +29,46 @@ namespace ConsoleApp247
             Console.WriteLine("Your clone is a:{0}\n", theClone.GetType().Name);
         }
     }
+
+    //New Shape derived class named Triangle
+    class Triangle : Shape, IPoint
+    {
+        public Triangle()
+        {
+
+        }
+        public Triangle(string name)
+        {
+
+        }
+
+       
+
+        protected override Geometry DefiningGeometry => throw new NotImplementedException();
+
+        public byte GetNumberOfPoints()
+        {
+            return 3;
+        }
+    }
+
+    class Hexagon : Shape, IPoint
+    {
+        public Hexagon()
+        {
+
+        }
+
+        public Hexagon(string name)
+        {
+
+        }
+        protected override Geometry DefiningGeometry => throw new NotImplementedException();
+
+        public byte GetNumberOfPoints()
+        {
+            return 6;
+        }
+    }
+
 }
