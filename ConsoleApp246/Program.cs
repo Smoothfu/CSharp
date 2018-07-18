81baf2894f1df086e8182a2e02c8f50084d2ad9f
Fred1987 <2864326836@qq.com>
2018-07-18 20:46:05 +0800

    [SecuritySafeCritical] public FileStream(string path,FileMode mode,FileAccess access,FileShare share); Initializes a new instance of the System.IO.FileStream class with the specified path,creation mode,read/write permission,and sharing permission.

--- ConsoleApp246/Program.cs
+++ ConsoleApp246/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Xml;
 using System.Xml.Schema;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ConsoleApp246
 {
@@ -13,31 +14,29 @@ namespace ConsoleApp246
     {
         static void Main(string[] args)
         {
-            //Create an instance of XmlTextReader and call Read method to read the file.
-            string fullXmlPath = @" D:\C\ConsoleApp246\ConsoleApp246\XMLFile1.xml";
-            XmlTextReader xmlReader = new XmlTextReader(fullXmlPath);
-            xmlReader.Read();
+            Console.WriteLine("*****Fun with Object Serialization*****\n");
+            string str = @"Assume you have created an instance of JamesBondCar, modified some state data, and want to
+persist your spy mobile into a *.dat file. Begin by creating the *.dat file itself. You can achieve this by
+creating an instance of the System.IO.FileStream type. At this point, you can create an instance of the
+BinaryFormatter and pass in the FileStream and object graph to persist. Consider the following
+Main() method:";
 
-            //If the node has value.
-            while(xmlReader.Read())
-            {
-                //Move to first element.
-                xmlReader.MoveToElement();
-                Console.WriteLine("XmlTextReader properties test:\n");
-                Console.WriteLine("=====================");
-                Console.WriteLine(xmlReader.Name);
-                Console.WriteLine(xmlReader.BaseURI);
-                Console.WriteLine(xmlReader.LocalName);
-                Console.WriteLine(xmlReader.AttributeCount.ToString());
-                Console.WriteLine(xmlReader.Depth.ToString());
-                Console.WriteLine(xmlReader.LineNumber.ToString());
-                Console.WriteLine(xmlReader.NodeType.ToString());
-                Console.WriteLine(xmlReader.Value.ToString());
-
-            }
+            //Now save the car to a specfic file in a binary format.
+            SaveAsBinaryFormat(str, ".\\CarData.dat");
 
             Console.ReadLine();
 
+        }
+
+        static void SaveAsBinaryFormat(object objGraph,string fileName)
+        {
+            //Save object to a file named CarData.data in binary.
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using (Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                binFormat.Serialize(fStream, objGraph);
+            }
+            Console.WriteLine("=>Saved in binary format!\n");
         }
     }
 }
