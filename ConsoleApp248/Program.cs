2667e95f13d672be5f063d6945ae7661c3953b24
Fred1987 <2864326836@qq.com>
2018-07-20 21:56:43 +0800

    public interface IAddMethod { void AddMethod(int x,int y,int z);}  Implementing an interface.When a class chooses to extend its functionality by supporting interfaces,it does not using a comma-delimited list in the type definition.Be aware that the direct base class must be the first item listed after the colon operator.

--- ConsoleApp248/Program.cs
+++ ConsoleApp248/Program.cs
@@ -7,14 +7,20 @@ using System.Threading;
 
 namespace ConsoleApp248
 {
-    class Program
+    interface IAdd
+    {
+       void AddMethod(int x, int y, int z);
+    }
+    class Program:IAdd
     {
         static void Main(string[] args)
         {
             Console.WriteLine("The main ManagedThreadId is :{0}\n", Thread.CurrentThread.ManagedThreadId);
+            Program obj = new Program();
             Thread thread = new Thread(() =>
               {
                   AddMethod(1334, 4764567);
+                  obj.AddMethod(452346245, 3445346, 456345645);
               });
             thread.Start();
             Console.ReadLine();
@@ -24,6 +30,11 @@ namespace ConsoleApp248
         {
             Console.WriteLine("The AddMethod ManagedThreadId is :{0}\n", Thread.CurrentThread.ManagedThreadId);
             Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+        }
+
+        public void AddMethod(int x, int y, int z)
+        {
+            Console.WriteLine("The overload AddMethod {0}+{1}+{2}={3}\n", x, y, z, x + y + z);
         }
     }
 }
