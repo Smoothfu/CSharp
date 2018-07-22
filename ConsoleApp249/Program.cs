f9a2aa036c6de51638414f154ca14a35827d8ce8
Fred1987 <2864326836@qq.com>
2018-07-22 16:55:24 +0800

    When you implement several interfaces that have identical members,you can resolve this sort of name clash using explicit interface implementation syntax

---
+++ ConsoleApp249/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp249
+{
+    //Draw image to a form
+    public interface IDrawToForm
+    {
+        void Draw();
+    }
+
+    //Draw to buffer in memory
+    public interface IDrawToMemory
+    {
+        void Draw();
+    }
+
+    //Render to the printer
+    public interface IDrawToPrinter
+    {
+        void Draw();
+    }
+
+    class OCtagon : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        void IDrawToForm.Draw()
+        {
+            Console.WriteLine("Draw To Form");
+        }
+
+        void IDrawToMemory.Draw()
+        {
+            Console.WriteLine("Draw to Memory");
+        }
+
+        void IDrawToPrinter.Draw()
+        {
+            Console.WriteLine("Draw to Printer");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Fun with Interface Name clashes*****\n");
+            //All of these invocations call the same Draw() method!
+            OCtagon octObj = new OCtagon();
+            IDrawToForm itfForm = octObj as IDrawToForm;
+            if (itfForm != null)
+            {
+                itfForm.Draw();
+            }
+
+            IDrawToPrinter iftPrinter = octObj as IDrawToPrinter;
+            if (iftPrinter != null)
+            {
+                iftPrinter.Draw();
+            }
+
+            IDrawToMemory itfMemory = octObj as IDrawToMemory;
+            if (itfMemory != null)
+            {
+                itfMemory.Draw();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
