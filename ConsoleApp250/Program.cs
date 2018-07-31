90e0142e7ec8d19cd7888fca8de2409114829d48
Fred1987 <2864326836@qq.com>
2018-07-31 21:41:58 +0800

    public interface IEnumerable IEnumerator GetEnumerator(); Returns an enumerator that iterates through a collection.Returns an System.Collections.IEnumerator object that can be used to iterate through the collection.Exposes an enumerator,which supports a simple iteration over a non-generic collection.To browse the .NET Framework source code for this type

--- ConsoleApp250/Program.cs
+++ ConsoleApp250/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Collections;
 
 namespace ConsoleApp250
 {
@@ -11,10 +12,44 @@ namespace ConsoleApp250
     {
         static void Main(string[] args)
         {
-            Process proc = Process.GetProcessById(5208);
-            Console.WriteLine("ProcessName:{0},Id:{1}\n", proc.ProcessName, proc.Id);
+            //Manually work with IEnumerator.
+            Garage carLot = new Garage();
+            IEnumerator enumerator = carLot.GetEnumerator();
+            enumerator.MoveNext();
+            Car myCar = (Car)enumerator.Current;
+            Console.WriteLine("Name:{0},Id:{1}\n", myCar.Name, myCar.Id);
            
             Console.ReadLine();
+        }
+    }
+
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public Car(string carName,int carId)
+        {
+            Name = carName;
+            Id = carId; 
+        }
+    }
+
+    public class Garage : IEnumerable
+    {
+        //System.Array already implements IEnumerator!
+        private Car[] carArray = new Car[5];
+        public Garage()
+        {
+            carArray[0] = new Car("G650", 1);
+            carArray[1] = new Car("GL550", 2);
+            carArray[2] = new Car("Crusider", 3);
+            carArray[3] = new Car("X6", 4);
+            carArray[4] = new Car("S600", 5);
+        }
+        public IEnumerator GetEnumerator()
+        {
+            //return the array objects IEnumerator
+            return carArray.GetEnumerator();
         }
     }
 }
