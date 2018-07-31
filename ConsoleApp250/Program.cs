c922f8a13566026f2361c54075615014f62daf33
Fred1987 <2864326836@qq.com>
2018-07-31 21:21:48 +0800

    public static Process[] GetProcesses(); Creates a new System.Diagnostics.Process component for each process resource on the local computer.An array of type system.Diagnostics.Process that represents all the process resources running on the local computer

---
+++ ConsoleApp250/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp250
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] allProcesses= Process.GetProcesses();
+            if(allProcesses!=null && allProcesses.Any())
+            {
+              var sortedProcesses=   allProcesses.OrderBy(x => x.ProcessName);
+                sortedProcesses.All(x =>
+                {
+                    Console.WriteLine("ProcessName:{0},Id:{1}\n",x.ProcessName, x.Id);
+                    return true;
+                });
+            }
+            Console.ReadLine();
+        }
+    }
+}
