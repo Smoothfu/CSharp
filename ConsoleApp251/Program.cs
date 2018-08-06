c9c419aa5b351d57d3c7b95eb22a5d8836daf54d
Fred1987 <2864326836@qq.com>
2018-08-06 20:52:19 +0800

    Designing interface hierarchies.Interfaces can be arranged in an interface hierarchy.Like a class hierarchy,when an interface extends an existing interface,it inherits the abstract members defined by the parent

---
+++ ConsoleApp251/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp251
+{
+    public interface IDrawable
+    {
+        void Draw();
+    }
+
+    public interface IAdvancedDraw:IDrawable
+    {
+        void DrawInBoundingBox(int top, int left, int bottom, int right);
+        void DrawUpsideDown();
+    }
+
+    public class BitmapImage : IAdvancedDraw
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing....");
+        }
+
+        public void DrawInBoundingBox(int top, int left, int bottom, int right)
+        {
+            Console.WriteLine("Drawing in a box...");
+        }
+
+        public void DrawUpsideDown()
+        {
+            Console.WriteLine("Drawing upside down");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Simple Interface Hierarchy*****");
+
+            //call from object level
+            BitmapImage bm = new BitmapImage();
+            bm.Draw();
+            bm.DrawInBoundingBox(10, 10, 100, 150);
+            bm.DrawUpsideDown();
+
+
+            //Get IAdvancedDraw explicitly
+            IAdvancedDraw iAdvDraw = bm as IAdvancedDraw;
+            if(iAdvDraw!=null)
+            {
+                iAdvDraw.DrawUpsideDown();
+            }
+            Console.ReadLine();
+        }
+    }
+}
