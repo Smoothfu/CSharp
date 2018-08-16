9c67ab1e0ff67b89dee41c06c45f08efa5b81e4f
Fred1987 <2864326836@qq.com>
2018-08-16 21:04:26 +0800

    Connect Timeout,while server is not local,must configure connect timeout,while when the server is local,it is redudant

---
+++ ConsoleApp253/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace ConsoleApp253
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = ConfigurationManager.AppSettings["conString"].ToString();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    MessageBox.Show("Connected");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
