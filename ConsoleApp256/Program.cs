c1d55e272bb5501ab0f290e6aed5a423362b4737
Fred1987 <2864326836@qq.com>
2018-08-27 19:48:17 +0800

    [SecurityCritical] public abstract class SafeHandle:CriticalFinalizerObject,IDisposable. Represents a wrapper class for operating system handles.this class must be inherited

---
+++ ConsoleApp256/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp256
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass unmanagedResource = new BaseClass();
+            unmanagedResource.Dispose();
+            Console.ReadLine();
+        }
+    }
+
+    class BaseClass : IDisposable
+    {
+        //Flag:Has Dispose alreay been called?
+        bool disposed = false;
+
+        //Instantiate a SafeHandle instance.
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        //Public implementation if Dispose pattern callable by consumers.
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        //protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposed)
+            {
+                return;
+            }
+
+            if(disposing)
+            {
+                handle.Dispose();
+                //Free any other managed objects here...
+            }
+
+            disposed = true;
+        }
+    }
+}
