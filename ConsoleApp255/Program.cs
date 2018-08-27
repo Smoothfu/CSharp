c4013d628735620ecbdb521d29f5f466a2afbcd7
Fred1987 <2864326836@qq.com>
2018-08-27 14:52:53 +0800

    In addition to leveraging the Radio class via containment/delegation,the car class is defined in such a way that if the user acclerate a car object beyond a predefined maximum speed.its engine exolodes,rendering the car unusable

---
+++ ConsoleApp255/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp255
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Simple Exception Example*****");
+            Console.WriteLine("=>Creating a car and stepping on it!");
+            Car mc = new Car("PRADO", 20);
+            mc.CrankTunes(true);
+            
+            for(int i=0;i<10;i++)
+            {
+                mc.Acclerate(10);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Radio
+    {
+        public void TurnOn(bool on)
+        {
+            if(on)
+            {
+                Console.WriteLine("Jamming");
+            }
+            else
+            {
+                Console.WriteLine("Quite time...");
+            }
+        }
+    }
+
+    class Car
+    {
+        //Constant for maxium speed.
+        public const int MaxSpeed = 100;
+
+        //Car properties
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        //Is the car still operational?
+        private bool carIsDead;
+
+        //A car has-a radio.
+        private Radio theMusicBox = new Radio();
+
+        //Constructors.
+        public Car()
+        {
+
+        }
+
+        public Car(string name,int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            //Delegate request to inner object.
+            theMusicBox.TurnOn(state);
+        }
+
+        //See if Car has overheated.
+        public void Acclerate(int delta)
+        {
+            if(carIsDead)
+            {
+                Console.WriteLine("{0} is out of order...", PetName);
+            }
+            else
+            {
+                CurrentSpeed += delta;
+
+                if(CurrentSpeed>MaxSpeed)
+                {
+                    Console.WriteLine("{0} has overheated!", PetName);
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("=>CurrentSpeed={0}\n", CurrentSpeed);
+                }
+            }
+        }
+    }
+}
