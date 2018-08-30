e8a5f5ea7f03a4e86868dd75216ce528cc569da3
Fred1987 <2864326836@qq.com>
2018-08-31 00:11:06 +0800

    [SecuritySafeCritical] protected object MemberwiseClone(); Creates a shallow copy of the current System.Object. Returns a shallow copy of the current System.Object

--- ConsoleApp257/Program.cs
+++ ConsoleApp257/Program.cs
@@ -32,19 +32,71 @@ namespace ConsoleApp257
     {
         static void Main(string[] args)
         {
-            AddClass obj = new AddClass();
-            IA objA = obj as IA;
-            if(objA!=null)
-            {
-                objA.AddMethod(1000, 1000000);
-            }
+            //Create an instane of Person and assign values to its fields.
+            Person p1 = new Person();
+            p1.Age = 31;
+            p1.Name = "Fred";
+            p1.IdInfo = new IdInfo(1000000);
 
-            IB objB = obj as IB;
-            if(objB!=null)
-            {
-                objB.AddMethod(100000, 24354000);
-            }
+
+            //Perform a shallow copy of p1 and assign it to p2.
+            Person p2 = p1.ShallowCopy();
+
+            //Display values of p1,p2
+            Console.WriteLine("Original values of p1 and p2:\n");
+            Console.WriteLine(" p1 instance values:\n");
+            DisplayValues(p1);
+            Console.WriteLine("p2 instance values:\n");
+            DisplayValues(p2);
+
+            //Change the value of p1 properties and display the values of p1 and p2.
+            p1.Age = 27;
+            p1.Name = "LZY";
+            p1.IdInfo.IdNumber = 787878787;
+            Console.WriteLine("\nValues of p1 and p2 after changes to p1:\n");
+            Console.WriteLine("p1 instance values:\n");
+            DisplayValues(p1);
+            Console.WriteLine("p2 instance values:\n");
+            DisplayValues(p2);
+           
+           
             Console.ReadLine();
+        }
+
+        static void DisplayValues(Person p)
+        {
+            Console.WriteLine("Name:{0:s},Age:{1:d}", p.Name, p.Age);
+            Console.WriteLine("Value:{0:d}", p.IdInfo.IdNumber);
+        }
+    }
+
+    public class IdInfo
+    {
+        public int IdNumber;
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+
+    public class Person
+    {
+        public int Age;
+        public string Name;
+        public IdInfo IdInfo;
+
+
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person other = (Person)this.MemberwiseClone();
+            other.IdInfo = new IdInfo(IdInfo.IdNumber);
+            other.Name = string.Copy(Name);
+            return other;
         }
     }
 }
