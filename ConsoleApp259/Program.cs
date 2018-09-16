23b53b6d1cf7c4e7cf3fb5bb141bb659681f135e
Fred1987 <2864326836@qq.com>
2018-09-16 20:57:46 +0800

    Object.GetHashCode method.Serve as the default hash function.  Returns int 32,A hash code for the current object.

---
+++ ConsoleApp259/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp259
+{
+    public struct Number
+    {
+        private int n;
+        public Number(int value)
+        {
+            n = value;
+        }
+
+        public int Value
+        {
+            get
+            {
+                return n;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Number))
+            {
+                return false;
+            }
+            else
+            {
+                return n == ((Number)obj).n;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return n;
+        }
+
+        public override string ToString()
+        {
+            return n.ToString();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for(int i=0;i<=9;i++)
+            {
+                int randomNum = rnd.Next(Int32.MinValue, Int32.MaxValue);
+                Number n = new Number(randomNum);
+                Console.WriteLine("n={0,12},hash code={1,12}", n, n.GetHashCode());
+            }
+            Console.ReadLine();
+        }
+    }
+}
