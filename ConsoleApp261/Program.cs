319e4f5140cc1a38a4cb9a46219734acb17b32a0
Fred1987 <2864326836@qq.com>
2018-09-21 15:31:46 +0800

    override,it will generate another new method in derive class

---
+++ ConsoleApp261/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp261
+{
+
+    public abstract class BaseClass
+    {
+        public virtual void Subtract(int x,int y)
+        {
+            Console.WriteLine("{0}-{1}={2}\n", x, y, x - y);
+        }
+        public void Add(int x,int y)
+        {
+            Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+        }
+    }
+
+    public class DeriveA : BaseClass
+    {
+        public override void Subtract(int x, int y)
+        {
+            Console.WriteLine("In DeriveA,{0}-{1}={2}\n", x, y, x - y);
+        }
+    }
+
+    public class DeriveB : BaseClass
+    {
+        public new void Subtract(int x,int y)
+        {
+            Console.WriteLine("In DeriveB:{0}+{1}={2}\n", x, y, x + y);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10000, y = 1000;
+            DeriveA objA = new DeriveA();
+            objA.Subtract(x, y);
+            DeriveB objB = new DeriveB();
+            objB.Subtract(x, y);
+            Console.ReadLine();
+        }
+    }
+}
