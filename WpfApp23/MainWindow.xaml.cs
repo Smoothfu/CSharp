1e6b09bc2798c7de0f75aea634a370dc01436b34
Fred1987 <2864326836@qq.com>
2018-09-22 10:25:35 +0800

    However,you can easily traverse both thel gical and visual trees using the somewhat symmetrical System.Windows.LogicalTreeHelper and System.Windows.Media.VisualTreeHelper classes

---
+++ WpfApp23/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+
+namespace WpfApp23
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrintLogicalTree(0, this);
+          
+        }
+
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+            PrintVisualTree(0, this);
+        }
+
+        void PrintLogicalTree(int depth,object obj)
+        {
+            //Print the object with preceding spaces that represent its depth.
+            Debug.WriteLine(new string(' ', depth) + obj);
+
+            //Sometimes leaf nodes are not DependencyObjects 
+            if(!(obj is DependencyObject))
+            {
+                return;
+            }
+
+            //Recursive call for each logical child
+            foreach(object child in LogicalTreeHelper.GetChildren(obj as DependencyObject))
+            {
+                PrintLogicalTree(depth + 1, child);
+            }
+        }
+
+        void PrintVisualTree(int depth,DependencyObject obj)
+        {
+            //Print the object with preceding spaces that represent its depth.
+            Debug.WriteLine(new string(' ', depth) + obj);
+
+            //Recursive call for each visual child
+            for(int i=0;i<VisualTreeHelper.GetChildrenCount(obj);i++)
+            {
+                PrintVisualTree(depth + 1, VisualTreeHelper.GetChild(obj, i));
+            }
+        }
+    }
+}
