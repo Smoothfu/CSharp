5e24a9c14ae8093e4ce6275bb5e58356dc42d84f
Fred1987 <2864326836@qq.com>
2018-09-22 12:50:10 +0800

    Metadata helps components to interact seamlessly. Binary infomation,CLR,PE, the runtime loads metadata into memory and references it to discover informatin about your code classes,members,inheritance.Metadata describes every type and member defined in your code in language-neutral manner.Metadata stores the following information.Description of the assembly,identitfy,name,version,culture,public key.the types that are exported,other assemblies that this assembly depends on.Security permissions needed to run.Descriptions of types,Name,visibility,base class,and interface implemented.members methods,fields,properties,events,nested types.attributes, additional descriptive elements that modify types and members.Metadata elimates the need for interface definition language,header files,or any external method of component reference,Metadata enables .net Framework languages to describe themselves automatically in a language-neutral manner,self-describing files. Language interoperability and easier component-based design. attributes,

---
+++ ConsoleApp262/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp262
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 91935, y = 35647;
+            AddMethod(x, y);
+            Console.ReadLine();
+        }
+
+        static void AddMethod(int x,int y)
+        {
+            Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+        }
+    }
+}
