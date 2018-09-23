3bd27829598a5bd58f839b81811886de099cbd71
Fred1987 <2864326836@qq.com>
2018-09-23 15:26:41 +0800

    WPF Fundamentals,reveals that the default visual tree for datagrid

---
+++ WpfApp27/PersonVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp27
+{
+    class PersonVM
+    {
+        private List<Person> mPersonList=new List<Person>();
+        public List<Person> PersonList
+        {
+            get
+            {
+                return mPersonList;
+            }
+            set
+            {
+                mPersonList = value;
+            }
+        }
+        public PersonVM()
+        {
+            for(int i=0;i<100000;i++)
+            {
+                mPersonList.Add(new Person()
+                {
+                    Id = i,
+                    Name = "Fred" + i,
+                    Age = 30 + i,
+                    Job = "CS" + i,
+                    Hobby = "Business" + i
+                });
+            }
+        }
+    }
+}
