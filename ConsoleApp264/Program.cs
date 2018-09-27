d3bfab5e90292a3c850f970ec01f7a292deb19b0
Fred1987 <2864326836@qq.com>
2018-09-27 19:44:15 +0800

    Generics provide greater type safety and performance benefits

---
+++ ConsoleApp264/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp264
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Make an array of string data.
+            string[] strArray = { "First", "Second", "Third" };
+
+            //Show number of items in array using Length property.
+            Console.WriteLine("This array has {0} items.", strArray.Length);
+            Console.WriteLine();
+
+
+            //Display contents using enumerator.
+            foreach(string str in strArray)
+            {
+                Console.WriteLine("Array Entry: {0}",str);
+            }
+
+            Console.WriteLine();
+
+            //Reverse the array and print again.
+            Array.Reverse(strArray);
+            foreach(string str in strArray)
+            {
+                Console.WriteLine("Array Entry:{0}.", str);
+            }
+            Console.ReadLine();
+        }
+    }
+}
