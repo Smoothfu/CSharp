c0e0a384b55ee9add1de1ae0cf5777309c21915d
Fred1987 <2864326836@qq.com>
2018-09-30 20:47:58 +0800

    Generics allow you to delay the specification of the data type of programming elements in a class or a method,until it is usually used in the program.In other words,generics allow you to write a class or method that can work with any data type

---
+++ ConsoleApp265/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp265
+{
+    public class MyGenericArray<T>
+    {
+        private T[] array;
+        public MyGenericArray(int size)
+        {
+            array = new T[size + 1];
+        }
+
+        public T GetItem(int index)
+        {
+            return array[index];
+        }
+
+        public void SetItem(int index,T value)
+        {
+            array[index] = value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring an int array.
+            MyGenericArray<int> intArray = new MyGenericArray<int>(5);
+
+            //Setting values.
+            for(int i=0;i<5;i++)
+            {
+                intArray.SetItem(i, i * 5);
+            }
+
+            //retrieving the values.
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine(intArray.GetItem(i));
+            }
+
+            Console.WriteLine();
+
+            //Declaring a character array.
+            MyGenericArray<char> charArray = new MyGenericArray<char>(5);
+
+            //setting values
+            for(int i=0;i<5;i++)
+            {
+                charArray.SetItem(i, (char)(i + 97));
+            }
+
+            //retrieving the values.
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine(charArray.GetItem(i));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
