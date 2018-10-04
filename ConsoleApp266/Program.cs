d67360c651f55bb095ef227c898c6d50c0c1b029
Fred1987 <2864326836@qq.com>
2018-10-04 19:23:21 +0800

    C# arrays allow you to define a set of identically typed items including an array of System.Objects which essentially represents an array of any type of data of a fixed upper limit

--- ConsoleApp266/Program.cs
+++ ConsoleApp266/Program.cs
@@ -6,102 +6,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp266
-{
-    class MyGenericClass<T> where T:class
-    {
-        private T genericMemberVariable;
-        public MyGenericClass(T value)
-        {
-            genericMemberVariable = value;
-        }
-
-        public T generateMethod(T genericParameter)
-        {
-            Console.WriteLine("Parameter type:{0},value:{1}\n", typeof(T).ToString(), genericParameter);
-            Console.WriteLine("Return type:{0},value:{1}\n", typeof(T).ToString(), genericMemberVariable);
-            return genericMemberVariable;
-        }
-
-        public T genericMethod<U>(T genericParameter,U anotherGenericType) where U:struct
-        {
-            Console.WriteLine("Generic Parameter of type {0},value {1}\n", typeof(T).ToString(), genericParameter);
-            Console.WriteLine("Return value of type {0},value {1}\n", typeof(T).ToString(), genericMemberVariable);
-            return genericMemberVariable;
-        }
-
-        public T genericProperty { get; set; }
-    }
+{ 
     class Program
     {
-        public delegate T Add<T>(T param1, T param2);
         static void Main(string[] args)
         {
-            //Declaring an int array.
-            MyGenericArray<int> intArray = new MyGenericArray<int>(5);
+            //Make an array of string data.
+            string[] stringArray = { "First", "Second", "Third" };
 
-            //setting values
-            for(int i=0;i<5;i++)
-            {
-                intArray.SetItem(i, i * i);
-            }
-
-            //Retrieving the values/
-            for(int i=0;i<5;i++)
-            {
-                Console.WriteLine(intArray.GetItem(i));
-            }
-
+            //Show number if items in array using length property.
+            Console.WriteLine("This array has {0} items.\n", stringArray.Length);
             Console.WriteLine();
 
-            //Declaring a character array.
-            MyGenericArray<char> charArray = new MyGenericArray<char>(10);
-
-            //setting values/
-            for(int i=0;i<10;i++)
+            //Display contents using enumerator.
+            foreach(string str in stringArray)
             {
-                charArray.SetItem(i, (char)(i + 97));
+                Console.WriteLine(str);
+            }
+            Console.WriteLine();
+
+            //Reverse the array and print again.
+            Array.Reverse(stringArray);
+            foreach(string str in stringArray)
+            {
+                Console.WriteLine("Array Entry:{0}\n", str);
             }
 
-            //retrieving the values/
-            for(int i=0;i<10;i++)
-            {
-                Console.WriteLine(charArray.GetItem(i));
-            }
-            Console.ReadLine();             
+            Console.ReadLine();
         }
-
-        public static int AddNumber(int val1,int val2)
-        {
-            return val1 + val2;
-        }
-
-        public static string Concate(string firstStr,string secondStr)
-        {
-            return firstStr + secondStr;
-        }
-
-        static bool IsPositiveInt(int i)
-        {
-            return i > 0;
-        }
-    }
-
-    public class MyGenericArray<T>
-    {
-        private T[] array;
-        public MyGenericArray(int size)
-        {
-            array = new T[size + 1];
-        }
-
-        public T GetItem(int index)
-        {
-            return array[index];
-        }
-
-        public void SetItem(int index,T value)
-        {
-            array[index] = value;
-        }
-    }
+    } 
 }
