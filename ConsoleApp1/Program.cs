ebfcc2c9d368839eadf2033a8c6c436811124123
Fred1987 <2864326836@qq.com>
2018-10-05 21:06:50 +0800

    one benefit of selecting the WCF service library project template is that it also supplies you with an App.config file,which might seem strange because you are building a .NET *.dll,not a .NET *.exe.However,this file is useful because when you debug or run your WCF service library project,the visual studio IDE will automatically launch the WCF Test Client application.This program WcfTestClient.exe will look up the settings in the App.config file,so it can host your service for testing purpose

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WcfServiceLibrary4;
+ 
 
 namespace ConsoleApp1
 {
@@ -11,9 +12,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            WcfServiceLibrary4.Service1 client = new Service1();
-            int result = client.AddMethod(10, 100000000);
-            Console.WriteLine(result);
+            Task task = Task.Run(() =>
+            {
+                AddService.Service1Client client = new AddService.Service1Client();
+                int result = client.AddMethod(100, 1000000);
+                Console.WriteLine(result);
+            });
+           
             Console.ReadLine();
         }
     }
