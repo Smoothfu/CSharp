9c0511b48ebeaac9be5aabe47e2c5aaf80d85512
Fred1987 <2864326836@qq.com>
2018-10-05 20:58:05 +0800

    [AttributeUsage(AttributeTargets.Method)] public sealed class OperationContractAttribute:Attribute. Indicates that a method defines an operation that is part of a service contract in a Window Communication Foundation application

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary4;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WcfServiceLibrary4.Service1 client = new Service1();
+            int result = client.AddMethod(10, 100000000);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
