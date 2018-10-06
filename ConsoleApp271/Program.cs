002625c8b6395d71f4c8c4c6378484871819c505
Fred1987 <2864326836@qq.com>
2018-10-06 12:22:43 +0800

    Add service reference , Fred@Fred MINGW64 / $
    http://localhost:8733/Design_Time_Addresses/WcfServiceLibrary5/Service1/mex

---
+++ ConsoleApp271/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp271.MathServiceReference;
+
+namespace ConsoleApp271
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10000, y = 242236;
+            MathServiceReference.MathClient math = new ConsoleApp271.MathServiceReference.MathClient();
+            int sum = math.Sum(x, y);
+            Console.WriteLine("{0}+{1}={2}\n", x, y, sum);
+            int subtract = math.Subtract(x, y);
+            Console.WriteLine("{0}-{1}={2}\n", x, y, subtract);
+            int multiply = math.Multiply(x, y);
+            Console.WriteLine("{0}*{1}={2}\n", x, y, multiply);
+            int divide = math.Divide(x, y);
+            Console.WriteLine("{0}/{1}={2}\n", x, y, divide);
+            Console.ReadLine();
+
+        }
+    }
+}
