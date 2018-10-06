9a6f997cb93bb967e5c4314855999e99b8c9b794
Fred1987 <2864326836@qq.com>
2018-10-06 20:07:53 +0800

    <configuration><system.serviceModel><services><service name="" behaviorConfiguration="mxbehavior"> <endpoint address="" binding="netTcpBinding" contract=""> <identity><dns value="localhost"/></identity> </endpoint><endpoint address="mex" binding="mexHttpBinding" contract="IMetadataExchange"/>

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,23 +12,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Uri uri = new Uri("http://localhost:8733/Design_Time_Addresses/WcfServiceLibrary10/Service1/mex");
-            using (ServiceHost host = new ServiceHost(typeof(MathService), uri))
-            {
-                try
-                {
-                    if(host.State!=CommunicationState.Opened)
-                    {
-                        host.Open();
-                    }                    
-                    Console.WriteLine("The service has been started!");
-                    Console.ReadLine();
-                }
-                catch(Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
+            int x = 789034534, y = 3452345;
+            WcfServiceLibrary10.MathService mathService = new MathService();
+            int result = mathService.AddMethod(x, y);
+            Console.WriteLine("{0}+{1}={2}\n", x, y, result);
+
+
+            //Uri uri = new Uri("http://localhost:8733/Design_Time_Addresses/WcfServiceLibrary10/Service1/mex");
+            //using (ServiceHost host = new ServiceHost(typeof(MathService), uri))
+            //{
+            //    try
+            //    {
+            //        if(host.State!=CommunicationState.Opened)
+            //        {
+            //            host.Open();
+            //        }                    
+            //        Console.WriteLine("The service has been started!");
+            //        Console.ReadLine();
+            //    }
+            //    catch(Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+            //}
             Console.ReadLine();
 
         }
