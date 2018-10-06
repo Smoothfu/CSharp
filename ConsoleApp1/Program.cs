d4e0910f4347a7397d63388490dab783bd18dee4
Fred1987 <2864326836@qq.com>
2018-10-06 21:37:33 +0800

    <configuration><system.serviceModel> <client> <endpoint name="net.tcp" binding="netTcpBinding" bindingConfiguration="" contract="IMetadataExchange"/> ‘ git remote

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary11;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IMathService> factory = new ChannelFactory<IMathService>(new NetTcpBinding(), "net.tcp://localhost:8733/Design_Time_Addresses/WcfServiceLibrary11/Service1/mex");
+            var channel = factory.CreateChannel();
+            int result = channel.AddMethod(1000000, 23000000);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
