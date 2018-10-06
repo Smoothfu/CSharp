629456428e5f0ffec78e0ef9b1dff05fc6c9da95
Fred1987 <2864326836@qq.com>
2018-10-06 16:18:59 +0800

    public ServiceHost(Type serviceType,params Uri[] baseAddresses);  Initializes a new instance of the System.ServiceModel.ServiceHost class with the type of service and its base addresses specified.public class ServiceHost:ServiceHostBase.Provides a host for services

---
+++ ConsoleApp274/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary9;
+using System.ServiceModel;
+
+namespace ConsoleApp274
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 56789, y = 6578900;
+            Uri baseAddress = new Uri("http://localhost:8733/Design_Time_Addresses/WcfServiceLibrary9/Service1/mex");
+            using (ServiceHost host = new ServiceHost(typeof(WcfServiceLibrary9.MathService), baseAddress))
+            {
+                host.Open();
+                
+                Console.WriteLine("The WCF service has started!\n");
+
+                WcfServiceLibrary9.MathService service = new MathService();
+                int result = service.Add(x, y);
+                Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+            }
+            Console.ReadLine();
+        }
+    }
+}
