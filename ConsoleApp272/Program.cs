c88ec0754af43304a44107dfef314bd2c3e67f24
Fred1987 <2864326836@qq.com>
2018-10-06 13:37:43 +0800

    Reference.cs public interface IMath { [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IMath/AddMethod", ReplyAction="http://tempuri.org/IMath/AddMethodResponse")] int AddMethod(int x,int y); [System.ServiceModel.OperationContractAttribute(Action="http://tempuri/org/IMath/AddMethod",ReplyAction="http://tempuri.org/IMath/AddMethodResponse")] System.Threading.Tasks.Task<int> AddMethodAsync(int x,int y);

---
+++ ConsoleApp272/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp272.MathService;
+
+namespace ConsoleApp272
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3564556, y = 256346745;
+            MathService.MathClient client = new MathClient();
+            ServiceReference1.MathClient mathClient = new ServiceReference1.MathClient();
+            int sum = client.AddMethod(x, y);
+            Console.WriteLine(sum);
+
+            int add = mathClient.AddMethod(x, y);
+            Console.WriteLine(add);
+            Console.ReadLine();
+        }
+    }
+}
