1c759a333917b9eaf36f060fa7ac9c708e9c708c
Fred1987 <2864326836@qq.com>
2018-10-06 14:10:58 +0800

    public class ServiceHost:ServiceHostBase Provides a host for services. public ServiceHost(Type serviceType,params Uri[] baseAddresses);  Initializes a new instance of the System.ServiceModel.ServiceHost class with the type of service and its base addresses specififed.serviceType:The type of hosted service.baseAddresses:An array of type System.Uri that contains the base addresses for the hosted service

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WcfServiceLibrary7;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 
 namespace ConsoleApp1
 {
@@ -11,10 +13,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int x = 678907678, y = 45678986, z = 567890567;
-            WcfServiceLibrary7.MathService mathService = new MathService();
-            int sum = mathService.Add(x, y,z);
-            Console.WriteLine("{0}+{1}+{2}={3}\n",x,y,z,sum);
+            //Create a URI to serve as the base address
+            Uri httpUrl = new Uri("http://localhost:8733/Design_Time_Addresses/WcfServiceLibrary7/Service1/mex");
+            
+            //Create service host.
+            ServiceHost host=new ServiceHost(typeof(WcfServiceLibrary7.MathService),httpUrl);
+            
+            //Add a service endpoint.
+            host.AddServiceEndpoint(typeof(WcfServiceLibrary7.IMath),new WSHttpBinding(),"");
+
+            //Enable metadata exchange.
+            ServiceMetadataBehavior serviceMetadataBehavior = new ServiceMetadataBehavior();
+            serviceMetadataBehavior.HttpGetEnabled = true;
+            host.Description.Behaviors.Add(serviceMetadataBehavior);
+
+            //start the service.
+            host.Open();
+            Console.WriteLine("Service is host at: " + DateTime.Now.ToString());
+            Console.WriteLine("Host is running... Press key to stop!");
             Console.ReadLine();
         }
     }
