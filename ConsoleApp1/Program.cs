5e62714fceec4c240fb85cb8c7b78e267cbb5f85
Fred1987 <2864326836@qq.com>
2018-10-06 21:05:53 +0800

    App.config.  <configuration><appSettings> <add key="baseUrl" value="http://localhost:8735/MathService");

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using WcfServiceLibrary10;
 using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Configuration;
 
 namespace ConsoleApp1
 {
@@ -12,31 +14,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int x = 789034534, y = 3452345;
-            WcfServiceLibrary10.MathService mathService = new MathService();
-            int result = mathService.AddMethod(x, y);
-            Console.WriteLine("{0}+{1}={2}\n", x, y, result);
+            ServiceHost host = null;
+            try
+            {
+                Uri uri = new Uri("http://localhost:8735/Design_Time_Addresses/WcfServiceLibrary10/Service1/mex");
+                host = new ServiceHost(typeof(MathService), new Uri(ConfigurationManager.AppSettings["baseUrl"]));
+                host.Faulted += Host_Faulted;
+                ServiceEndpoint serviceEndpoint = host.AddServiceEndpoint(typeof(IMathService), new NetTcpBinding(), ConfigurationManager.AppSettings["endpointUrl"]);
 
+                //公布元数据
+                host.Description.Behaviors.Add(new ServiceMetadataBehavior() { HttpGetEnabled = true });
+                host.AddServiceEndpoint(typeof(IMathService), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
+                host.Open();
+                Console.WriteLine("The service has been started!\n");
+                Console.WriteLine("{0},{1}", serviceEndpoint.Address.ToString(), serviceEndpoint.Binding.Name);
 
-            //Uri uri = new Uri("http://localhost:8733/Design_Time_Addresses/WcfServiceLibrary10/Service1/mex");
-            //using (ServiceHost host = new ServiceHost(typeof(MathService), uri))
-            //{
-            //    try
-            //    {
-            //        if(host.State!=CommunicationState.Opened)
-            //        {
-            //            host.Open();
-            //        }                    
-            //        Console.WriteLine("The service has been started!");
-            //        Console.ReadLine();
-            //    }
-            //    catch(Exception ex)
-            //    {
-            //        Console.WriteLine(ex.Message);
-            //    }
-            //}
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+            finally
+            {
+                if(host.State==CommunicationState.Faulted)
+                {
+                    host.Abort();
+                }
+                else
+                {
+                    host.Close();
+                }
+            }
 
+           
+
+        }
+
+        private static void Host_Faulted(object sender, EventArgs e)
+        {
+            Console.WriteLine("The MathService host has faulted.\n");
         }
     }
 }
