723947c41e3c7c0152e508379dc58286f61a4e6d
Fred1987 <2864326836@qq.com>
2018-10-06 13:57:46 +0800

    Add service reference. ServiceContract,OperationContract

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary7;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 678907678, y = 45678986, z = 567890567;
+            WcfServiceLibrary7.MathService mathService = new MathService();
+            int sum = mathService.Add(x, y,z);
+            Console.WriteLine("{0}+{1}+{2}={3}\n",x,y,z,sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WcfServiceLibrary7/Service1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary7
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class MathService : IMath
+    {
+        public int Add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+    }
+}
