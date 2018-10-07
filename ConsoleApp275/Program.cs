84ce0444cc9b4a96df4e05ca7d6fa62f0a261035
Fred1987 <2864326836@qq.com>
2018-10-07 20:47:20 +0800

    Add service reference.http://localhost:8733/Design_Time_Addresses/WcfServiceLibrary12/Service1/mex,ServiceReference

---
+++ ConsoleApp275/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary12;
+using ConsoleApp275.MathServiceReference;
+
+
+namespace ConsoleApp275
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathServiceClient mathClient = new MathServiceClient();
+            List<DBTables> resultList = mathClient.GetDBColumnString().ToList();
+            if(resultList!=null && resultList.Any())
+            {
+                resultList.ForEach(x =>
+                {
+                    Console.WriteLine("STABLECATALOG:{0,-20} STABLESCHEMA:{1,-20} STABLENAME:{2,-50} STABLETYPE:{3,-30}\n", x.STABLECATALOG, x.STABLESCHEMA, x.STABLENAME, x.STABLETYPE);
+                });
+            }
+            Console.ReadLine();                
+        }
+    }
+
+    class CTableString
+    {
+        public string CTableCatalog { get; set; }
+        public string CTableScheme { get; set; }
+        public string CTableName { get; set; }
+        public string CTableType { get; set; }
+    }
+}
