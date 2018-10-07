614a66fd2f0c3ba44d32516c4e08842e684013a3
Fred1987 <2864326836@qq.com>
2018-10-07 20:57:54 +0800

    Add Service Reference,Address,try,using catch

--- ConsoleApp275/Program.cs
+++ ConsoleApp275/Program.cs
@@ -13,15 +13,27 @@ namespace ConsoleApp275
     {
         static void Main(string[] args)
         {
-            MathServiceClient mathClient = new MathServiceClient();
-            List<DBTables> resultList = mathClient.GetDBColumnString().ToList();
-            if(resultList!=null && resultList.Any())
+            try
             {
-                resultList.ForEach(x =>
+                using (MathServiceClient mathClient = new MathServiceClient())
                 {
-                    Console.WriteLine("STABLECATALOG:{0,-20} STABLESCHEMA:{1,-20} STABLENAME:{2,-50} STABLETYPE:{3,-30}\n", x.STABLECATALOG, x.STABLESCHEMA, x.STABLENAME, x.STABLETYPE);
-                });
+                    List<DBTables> resultList = mathClient.GetDBColumnString().ToList();
+                    if (resultList != null && resultList.Any())
+                    {
+                        resultList.ForEach(x =>
+                        {
+                            Console.WriteLine("STABLECATALOG:{0,-20} STABLESCHEMA:{1,-20} STABLENAME:{2,-50} STABLETYPE:{3,-30}\n", x.STABLECATALOG, x.STABLESCHEMA, x.STABLENAME, x.STABLETYPE);
+                        });
+                    }
+                }
             }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+               
             Console.ReadLine();                
         }
     }
