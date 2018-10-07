df9c00e46affdcce2c862bc87fdcbea2be08c860
Fred1987 <2864326836@qq.com>
2018-10-07 20:09:43 +0800

    public sealed class ServiceAttribute:Attribute,Indicates that an interface or a class defines a service contract in a Windows Communication Foundation application.public sealed class OperationContractAttribute:Attribute,Indicates that a method defines an operation that is part of a service contract in a WCF application

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary12;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3453145, y = 14542645;
+            using (MathService service = new MathService())
+            {
+                string stringResult = service.AddMethod(x,y);
+                Console.WriteLine(stringResult);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WcfServiceLibrary12/Service1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary12
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class MathService : IMathService
+    {
+        public string AddMethod(int x, int y)
+        {
+            return string.Format("{0}+{1}={2}\n", x, y, x + y);
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
