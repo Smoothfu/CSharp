a9a8aaa77b41d31af4e9f45c4cc9f087bbff6dab
Fred1987 <2864326836@qq.com>
2018-10-09 11:59:12 +0800

    global config --user.email,global config --user.username

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 34546646, y = 456735676;
+            MathServiceReference.MathServciceClient client = new MathServiceReference.MathServciceClient();
+            int result = client.AddMethod(x,y);
+            Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
+            Console.ReadLine();
+        }
+    }
+}
