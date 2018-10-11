b1c226a67804bb3670b18f94a164afa6a7b97c66
Fred1987 <2864326836@qq.com>
2018-10-11 21:14:25 +0800

    dire.GetFiles("*",SearchOption.AllDirectories);

---
+++ ConsoleApp277/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace ConsoleApp277
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string path=Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            DirectoryInfo dir = new DirectoryInfo(path).Parent.Parent.Parent.Parent;
+            FileInfo[] allFiles = dir.GetFiles("*",SearchOption.AllDirectories);
+            if(allFiles!=null && allFiles.Any())
+            {
+                allFiles.ToList().ForEach(x =>
+                {
+                    Console.WriteLine(x.FullName);
+                });
+
+                Console.WriteLine("\n\n\nThere are totally {0} files in {1}", allFiles.Count(), dir.FullName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
