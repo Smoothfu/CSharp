90ec02b8a8da503791589bf490a15f5374a72b3e
Fred1987 <2864326836@qq.com>
2018-10-13 15:27:34 +0800

    Image<Bgr,byte> img=new Image<Bgr,byte>(ofd.FileName);  public Image(string fileName); Read image from a file

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using Emgu.Util;
+using System.Windows.Forms;
+using Emgu.CV.Structure;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            InitImageViewer();
+            Console.ReadLine();
+        }
+
+        static void InitImageViewer()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog()==DialogResult.OK)
+            {
+                Image<Bgr, byte> img = new Image<Bgr, byte>(ofd.FileName);
+                ImageViewer imageViewer = new ImageViewer(img, "ImageViewer in Console");
+                imageViewer.ShowDialog();
+            }
+
+        }
+
+    }
+
+
+     
+}
