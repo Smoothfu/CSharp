b17d4fdcd59690379716d5c13eb57bd66e86a2cf
Fred1987 <2864326836@qq.com>
2018-10-21 16:41:48 +0800

    [ComVisible(false)] public delegate void ParameterizedThreadStart(object obj);

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pn = new Person(1, "Floomberg");
+            ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart(DescPerson);
+            
+            Thread thread = new Thread(parameterizedThreadStart);
+            thread.Start(pn);
+            Console.ReadLine();
+        }
+
+        static void GetCurrentTime()
+        {
+            Console.WriteLine("Now is {0}\n", DateTime.Now.ToString("yyyyMMddHHmmssfff"));
+        }
+
+
+        static void DescPerson(object obj)
+        {
+            var personObj = obj as Person;
+            if(personObj!=null)
+            {
+                Console.WriteLine(personObj.ToString());
+            }
+        }
+    }
+
+    public class Person
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+
+        public Person(int pId,string pName)
+        {
+            PId = pId;
+            PName = pName;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Id:{0},Name:{1}\n", PId, PName);
+        } 
+    }
+}
