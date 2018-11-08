0c40578fbf05e16119804bc54bace917426fcf75
Fred1987 <2864326836@qq.com>
2018-11-08 14:18:03 +0800

    Associate the delegate with an anonymous method.By using annoymous methods,you reduce the coding overhead in instantiating delegates because you do not have to create a separate method

--- ConsoleApp279/Program.cs
+++ ConsoleApp279/Program.cs
@@ -9,17 +9,17 @@ namespace ConsoleApp279
     class Program
     {
         delegate void MathDel(int x, int y);
+        delegate void StringDel(string str);
         static void Main(string[] args)
         {
             int x = 10, y = 20;
-        
-            //Instantiate the delegate using an annoymous method.
-            MathDel addDel = delegate (int w, int z)
-            {
-                Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
-            };
 
-            addDel(x, y);
+            //Instantiate the delegate using an annoymous method.
+            StringDel strDel = delegate (string str)
+              {
+                  Console.WriteLine("This string is {0}\n", str);
+              };
+            strDel("Annoymous methods"); 
             Console.ReadLine();
 
         }
