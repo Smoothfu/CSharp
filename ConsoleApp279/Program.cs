a4bb06237afa9f67d36834b92860961e3784b592
Fred1987 <2864326836@qq.com>
2018-11-09 13:58:17 +0800

    sqlDataAdapterSelectCommand=cmd

--- ConsoleApp279/Program.cs
+++ ConsoleApp279/Program.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Linq.Expressions;
 using System.Threading;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ConsoleApp279
 {
@@ -14,17 +17,48 @@ namespace ConsoleApp279
         delegate void StringDel(string str);
         static void Main(string[] args)
         {
-            Thread thread = new Thread(() =>
-              {
-                  AddDel del = delegate (int x, int y)
-                  {
-                      Console.WriteLine("{0}+{1}={2}\n", x, y, x + y);
-                  };
-                  del(100, 100000000);
-              });
-            thread.Start();
-           
-            Console.ReadLine();
+            string connString = ConfigurationManager.AppSettings["conString"].ToString();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if(conn.State!=ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+
+                string select = "select top(20) * from INFORMATION_SCHEMA.tables " +
+                    "where TABLE_NAME not in(select top(10) TABLE_NAME from INFORMATION_SCHEMA.TABLES " +
+                    "order by TABLE_NAME) order by TABLE_NAME";
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = select;
+
+                    cmd.ExecuteNonQuery();
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+                    sqlDataAdapter.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    sqlDataAdapter.Fill(ds);
+
+                    if(ds.Tables[0]!=null && ds.Tables[0].Rows.Count>0)
+                    {
+                        for(int i=0;i<ds.Tables[0].Rows.Count;i++)
+                        {
+                            for(int j=0;j<ds.Tables[0].Columns.Count;j++)
+                            {
+                                Console.Write(ds.Tables[0].Rows[i][j]+"\t");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+
+                }
+
+            }
+
+
+                Console.ReadLine();
         }
 
         static void AddMethod(int x,int y)
