4abd348c18a6954943040e1ce66b6a881f6321bc
Fred1987 <2864326836@qq.com>
2018-11-09 13:28:48 +0800

    select top(20) * from INFORMATION_SCHEMA.tables where TABLE_NAME not in( select top(10) TABLE_NAME from INFORMATION_SCHEMA.TABLES order by TABLE_NAME) order by TABLE_NAME

---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+use AdventureWorks2017
+select * from INFORMATION_SCHEMA.TABLES
+
+
+
+select top(20) * from INFORMATION_SCHEMA.tables where TABLE_NAME not in(
+select top(10) TABLE_NAME from INFORMATION_SCHEMA.TABLES order by TABLE_NAME) order by TABLE_NAME
+  
