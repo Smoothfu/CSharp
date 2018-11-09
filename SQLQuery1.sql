4af98f93ee01b1d8f84a00277c6809823ba906dc
Fred1987 <2864326836@qq.com>
2018-11-09 11:33:28 +0800

    db,join,inner join,left join,right join,full join

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,17 +3,73 @@ from person.Person pp
 inner join sales.Customer sc
 on pp.BusinessEntityID=sc.PersonID
 
-select CAST(LastName+','+FirstName as varchar(55)) as FullName,AccountNumber
-from Person.Person pp
-inner join sales.Customer sc
-on pp.BusinessEntityID=sc.PersonID
+select sso.SpecialOfferID,Description,DiscountPct,ProductID
+from sales.SpecialOffer sso
+join sales.SpecialOfferProduct ssop
+on sso.SpecialOfferID=ssop.SpecialOfferID
+where sso.SpecialOfferID!=1
 
-select CAST(LastName+','+FirstName as varchar(55)) as FullName,AccountNumber
-from Person.Person pp
-left join sales.Customer sc
-on pp.BusinessEntityID=sc.PersonID
+select sso.SpecialOfferID,Description,DiscountPct,ProductID
+from sales.SpecialOffer sso
+left join sales.SpecialOfferProduct ssop
+on sso.SpecialOfferID=ssop.SpecialOfferID
+where sso.SpecialOfferID!=1
 
-select CAST(LastName+','+FirstName as varchar(55)) as FullName,AccountNumber
-from Person.Person pp
-right join sales.Customer sc
-on pp.BusinessEntityID=sc.PersonID
+
+select sso.SpecialOfferID,Description,DiscountPct,ProductID
+from sales.SpecialOfferProduct ssop
+right join sales.SpecialOffer sso
+on ssop.SpecialOfferID=sso.SpecialOfferID
+where sso.SpecialOfferID!=1
+
+select sso.SpecialOfferID,Description,DiscountPct,ProductID
+from sales.SpecialOffer sso
+right join sales.SpecialOfferProduct ssop
+on sso.SpecialOfferID=ssop.SpecialOfferID
+where sso.SpecialOfferID!=1
+
+select Description
+from sales.SpecialOfferProduct ssop
+right outer join sales.SpecialOffer sso
+on ssop.SpecialOfferID=sso.SpecialOfferID
+where sso.SpecialOfferID!=1
+and ssop.SpecialOfferID is null
+
+if(null=null)
+print 'it does'
+else
+print 'it does not'
+
+select pbe.BusinessEntityID,hre.JobTitle,pp.FirstName,pp.LastName
+from Person.BusinessEntity pbe
+inner join HumanResources.Employee hre
+on pbe.BusinessEntityID=hre.BusinessEntityID
+inner join Person.Person pp
+on pbe.BusinessEntityID =pp.BusinessEntityID
+where hre.BusinessEntityID<4
+
+select count(*) from Person.BusinessEntity
+
+select pp.BusinessEntityID,pp.FirstName,pp.LastName
+from person.Person pp
+left join HumanResources.Employee hre
+on pp.BusinessEntityID=hre.BusinessEntityID
+where hre.BusinessEntityID is null
+
+select pp.BusinessEntityID,pp.FirstName,pp.LastName
+from HumanResources.Employee hre
+right join Person.Person pp
+on pp.BusinessEntityID=hre.BusinessEntityID
+where hre.BusinessEntityID is null
+
+
+select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME like 'v%' 
+select * from Purchasing.Vendor
+
+select v.AccountNumber
+from Purchasing.Vendor v
+
+select distinct v.Name
+from Purchasing.Vendor v
+left join Purchasing.ProductVendor pv
+on v.BusinessEntityID=pv.BusinessEntityID
