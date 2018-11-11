a854f458547835f5025b5eec010dbb3fd98f9b19
Fred1987 <2864326836@qq.com>
2018-11-11 18:46:02 +0800

    SqlDataAdapapter sqlDataAdapter=new SqlDataAdapter(cmd); sqlDataAdapter.SelectCommand=cmd

---
+++ ConsoleApp280/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ConsoleApp280
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = ConfigurationManager.AppSettings["conString"].ToString();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if(conn.State!=ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+                string selectSQL = "select distinct sod.ProductID from " +
+                    "sales.SalesOrderHeader soh join sales.SalesOrderDetail " +
+                    "sod on soh.SalesOrderID = sod.SalesOrderID where " +
+                    "OrderDate = (select min(OrderDate) from sales.SalesOrderHeader)";
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn; 
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = selectSQL;
+                    cmd.ExecuteNonQuery();
+
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+                    sqlDataAdapter.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    sqlDataAdapter.Fill(ds);
+                    if(ds.Tables[0]!=null && ds.Tables[0].Rows.Count>0)
+                    {
+                        for(int i=0;i<ds.Tables[0].Rows.Count;i++)
+                        {
+                            for(int j=0;j<ds.Tables[0].Columns.Count;j++)
+                            {
+                                Console.Write(ds.Tables[0].Rows[i][j].ToString());
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
