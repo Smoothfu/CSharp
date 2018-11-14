595bc8eb3fab162f17b1ce160aebe35f327c2d13
Fred1987 <2864326836@qq.com>
2018-11-15 07:01:39 +0800

    public interface IComparable Defines a generalized type-specific comparison method that a value type or class implements to order or sort its instances.int CompareTo(object obj); Compares the current instance with another object for the same type and returns an integer that indicates whether the current instance precedes,follows,or occurs in the same position in the sort order as the other object

---
+++ ConsoleApp283/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp283
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person(1, "Fred1");
+            Person p2 = new Person(2, "Fred2");
+            int result = p1.CompareTo(p2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+
+    public class Person:IComparable,IEnumerable,IEquatable,IComparer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Person(int id,string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(obj==null)
+            {
+                return 1;
+            }
+            var person = obj as Person;
+            if(person!=null)
+            {
+                return this.Id.CompareTo(person.Id);
+            }
+
+            else
+            {
+                throw new ArgumentException("Object is not a person");
+            }
+            
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Compare(object x, object y)
+        {
+            var personX = x as Person;
+            var personY = y as Person;
+
+            if(personX!=null && personY!=null)
+            {
+                if(personX.Id==personY.Id && personX.Name==personY.Name)
+                {
+                    return 0;
+                }
+
+                if(personX.Id>personY.Id)
+                {
+                    return 1;
+                }
+
+                if(personX.Id<personY.Id)
+                {
+                    return -1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
