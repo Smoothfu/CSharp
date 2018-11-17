ab8950f80bf6bf5cad8173f4990eee953da95b26
Fred1987 <2864326836@qq.com>
2018-11-17 09:34:03 +0800

    SqlCommand cmd=new SqlCommand(); cmd.Parameters.Add("@param","value");

---
+++ ConsoleApp286/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ConsoleApp286
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = ConfigurationManager.AppSettings["conString"].ToString();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if(conn.State!=ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "spGetSalesTables";
+                    cmd.Parameters.Add("@tableSchema", "Sales");
+                    cmd.ExecuteNonQuery();
+
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                    dataAdapter.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    dataAdapter.Fill(ds);
+
+                    if(ds.Tables[0].Rows.Count>0)
+                    {
+                        for(int i=0;i<ds.Tables[0].Rows.Count;i++)
+                        {
+                            for(int j=0;j<ds.Tables[0].Columns.Count;j++)
+                            {
+                                Console.Write(ds.Tables[0].Rows[i][j].ToString()+"\t");
+                            }
+                            Console.WriteLine("\n");
+                        }
+                    }
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
