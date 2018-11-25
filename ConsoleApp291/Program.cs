d186ff78e0781b9607da94a717dd097e311039c8
Fred1987 <2864326836@qq.com>
2018-11-25 08:34:16 +0800

    find the primes in a limited scope by iterations method

---
+++ ConsoleApp291/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp291
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindPrimes(100);
+            Console.ReadLine();
+        }
+
+        static void FindPrimes(int num)
+        {
+            if(num<=1)
+            {
+                return;
+            }
+
+           
+            List<int> primeList = new List<int>();
+            
+            for (int i=2;i<=num;i++)
+            {
+                bool isPrime = true;
+                for (int j=2;j<=Math.Sqrt(i);j++)
+                {
+                    if(i%j==0)
+                    {
+                        isPrime = false;
+                        break;
+                    }                                               
+                }
+
+                if (isPrime)
+                {
+                    primeList.Add(i);
+                }
+            }
+
+            foreach(int i in primeList)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
