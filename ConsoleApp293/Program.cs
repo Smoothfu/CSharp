c7e963551fd0870e476649a50f8225c55fe896f1
Fred1987 <2864326836@qq.com>
2018-11-29 14:34:15 +0800

    Query expression is nothing but a LINQ query,expressed in a form similar to that of SQL with query operators like select,where and order by.Query expressions usually start with the keyword from

--- ConsoleApp293/Program.cs
+++ ConsoleApp293/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp293
     {
         static void Main(string[] args)
         {
+            //Specify the data source.
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+
+            //Define the query expression.
+            IEnumerable<int> scoreQuery = from s in scores where s > 80 select s;
+
+            //Execute the query.
+            foreach(int i in scoreQuery)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void LINQWords()
+        {
             string[] words = { "hello", "wonderful", "LINQ", "beautifulgril", "World" };
 
             ////get only short words
@@ -19,19 +37,17 @@ namespace ConsoleApp293
             var shortWords = words.Where(x => x.Length <= 5);
 
             //print each word out.
-            foreach(var word in shortWords)
+            foreach (var word in shortWords)
             {
                 Console.WriteLine(word);
             }
 
             Console.WriteLine("The following are longer words:\n");
             var longWords = from w in words where w.Length >= 10 select w;
-            foreach(var word in longWords)
+            foreach (var word in longWords)
             {
                 Console.WriteLine(word);
             }
-
-            Console.ReadLine();
         }
     }
 }
