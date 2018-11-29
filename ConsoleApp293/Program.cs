ef89651ab83f36f12fa99f2a82bfdd292620e5f3
Fred1987 <2864326836@qq.com>
2018-11-29 14:17:02 +0800

    from word in words where word.length<=5 select word

---
+++ ConsoleApp293/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp293
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "hello", "wonderful", "LINQ", "beautiful", "World" };
+
+            //get only short words
+            var shortWords = from word in words
+                             where word.Length <= 5
+                             select word;
+
+            //print each word out.
+            foreach(var word in shortWords)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
