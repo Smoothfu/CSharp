ef9984d30beb61a5f858e676250c1985898f9cb6
Fred1987 <2864326836@qq.com>
2018-11-29 16:00:37 +0800

    To retrieve data using a DataReader,create an instance of the Command object,and then create a DataReader by calling Command.ExecuteReader to retrieve rows from a data source.The DataReader provides an unbuffered stream of data that allows procedural logical to efficiently process results from a data source sequentially. The DataReader is a good choide when you are retrieving large amounts of data because the data is not cached in memory

--- ConsoleApp293/Program.cs
+++ ConsoleApp293/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ConsoleApp293
 {
@@ -10,17 +13,56 @@ namespace ConsoleApp293
     {
         static void Main(string[] args)
         {
-            //Specify the data source.
-            int[] scores = new int[] { 97, 92, 81, 60 };
+            string conString = ConfigurationManager.AppSettings["conString"].ToString();
 
-
-            //Define the query expression.
-            IEnumerable<int> scoreQuery = from s in scores where s > 80 select s;
-
-            //Execute the query.
-            foreach(int i in scoreQuery)
+            using (SqlConnection conn = new SqlConnection(conString))
             {
-                Console.WriteLine(i);
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+                string selectSQL = "select ProductID,name,ProductNumber," +
+                    "MakeFlag,FinishedGoodsFlag,SafetyStockLevel,ReorderPoint " +
+                    "from Production.Product where SafetyStockLevel is not null";
+                using (SqlCommand cmd = new SqlCommand(selectSQL, conn))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();                   
+                   
+                    if(reader.HasRows)
+                    {
+                        string productIdCN = reader.GetName(0);
+                        string nameCN = reader.GetName(1);
+                        string productNumberCN = reader.GetName(2);
+                        string makeFlagCN = reader.GetName(3);
+                        string finishedGoodsFlagCN = reader.GetName(4);
+                        string safetyStockLevelCN = reader.GetName(5);
+                        string reorderPointCN = reader.GetName(6);
+
+                        string formatHeaderName = string.Format("{0,-30}  {1,-40}  {2,-20}  {3,-20}  {4,-20}  {5,-20}  {6,-20}\n\n",
+                            productIdCN, nameCN, productNumberCN, makeFlagCN, finishedGoodsFlagCN, safetyStockLevelCN, reorderPointCN);
+                        Console.WriteLine(formatHeaderName);
+                        int rowCount = 0;
+                        while (reader.Read())
+                        {
+                            int productId = reader.GetInt32(0);
+                            string name = reader.GetString(1);
+                            string productNumber = reader.GetString(2);
+                            bool makeFlag = reader.GetBoolean(3);
+                            bool finishedGoodsFlag = reader.GetBoolean(4);
+                            Int16 safetyStockLevel = reader.GetInt16(5);
+                            Int16 reorderPoint = reader.GetInt16(6);
+
+                            string msg = string.Format("{0,-30} {1,-40} {2,-20} {3,-20} {4,-20} {5,-20} {6,-20}",
+                                productId, name, productNumber, makeFlag, finishedGoodsFlag, safetyStockLevel, reorderPoint);
+                            Console.WriteLine(msg + "\n");
+                            ++rowCount;
+                            Console.WriteLine();
+                        }
+
+                        Console.WriteLine("There are totally {0} rows data!\n", rowCount);
+                    }
+                   
+                }
             }
 
             Console.ReadLine();
@@ -47,6 +89,20 @@ namespace ConsoleApp293
             foreach (var word in longWords)
             {
                 Console.WriteLine(word);
+            }
+
+
+            //Specify the data source.
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+
+            //Define the query expression.
+            IEnumerable<int> scoreQuery = from s in scores where s > 80 select s;
+
+            //Execute the query.
+            foreach (int i in scoreQuery)
+            {
+                Console.WriteLine(i);
             }
         }
     }
