f33442b158269c6f16ca61f731fd1fe0d4852557
Fred1987 <2864326836@qq.com>
2018-11-30 08:36:09 +0800

    LINQ Aggregate,Sum,Max,Min

---
+++ ConsoleApp294/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp294
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LINQSum();
+            Console.ReadLine();
+        }
+
+        static void LINQSum()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int sum = arr.Sum(x => x);
+            Console.WriteLine("Sum={0}\n", sum);
+            int max = arr.Max();
+            Console.WriteLine("Max={0}\n", max);
+            int min = arr.Min();
+            Console.WriteLine(min);
+        }
+    }
+}
