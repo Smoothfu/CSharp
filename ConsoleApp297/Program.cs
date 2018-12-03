f757cf3c9310a7d71ff28064dc7cbb4556e6edf1
Fred1987 <2864326836@qq.com>
2018-12-03 15:32:21 +0800

    Variable scope in Lambda Expression.There are some rules while using variable scope in a lambda expression like variables that are initiated within a lambda expression are not meant to be visible in an outer method

--- ConsoleApp297/Program.cs
+++ ConsoleApp297/Program.cs
@@ -9,12 +9,50 @@ namespace ConsoleApp297
     class Program
     {
         delegate int Del(int i);
+        delegate bool D();
+        delegate bool D2(int i);
+
+        class Test
+        {
+           public D del;
+           public  D2 del2;
+
+            public void TestMethod(int input)
+            {
+                int j = 0;
+
+                //Initialize the delegates with lambda expressions.
+                del = () => { j = 10; return j > input; };
+
+                //del2 will be invoked after TestMethod goes out of scope.
+                del2 = (x) => { return x == j; };
+
+                //Demonstrate value of j：
+                //The delegate has not been invoked yet.
+                Console.WriteLine("j={0}", j);
+                bool boolResult = del();
+
+                Console.WriteLine("j={0},b={1}\n", j, boolResult);
+            }
+        }
         static void Main(string[] args)
+        {
+
+            Test test = new Test();
+            test.TestMethod(5);
+
+            //Prove that del2 still has a copy of local variable j from TestMethod.
+            bool result = test.del2(10);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+        static void LINQAVG()
         {
             int[] fibNum = { 1, 1, 2, 3, 5, 8, 13, 21, 34 };
             double avgValue = fibNum.Average();
             Console.WriteLine(avgValue);
-            Console.ReadLine();
         }
 
         static void LINQLambdaExpression()
