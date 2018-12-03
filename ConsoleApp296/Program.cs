9231e4fc4edb424747b1a342805f5e8c63e87625
Fred1987 <2864326836@qq.com>
2018-12-03 10:57:27 +0800

    public static XDocument Parse(string text);  Creates a new System.Xml.Linq.XDocument from a string

--- ConsoleApp296/Program.cs
+++ ConsoleApp296/Program.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Xml.Linq;
 
 namespace ConsoleApp296
 {
@@ -14,8 +15,30 @@ namespace ConsoleApp296
     {
         static void Main(string[] args)
         {
+            string myXML = @"<Departments>
+                        <Department>Sales</Department>
+                        <Department>IT</Department>
+                        <Department>Finance</Department>
+                        </Departments>";
+
+            XDocument xdoc = new XDocument();
+            xdoc = XDocument.Parse(myXML);
+
+            var result = xdoc.Element("Departments").Descendants();
+
+            foreach(XElement item in result)
+            {
+                Console.WriteLine("Department Name:- " + item.Value);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void LINQDATASET()
+        {
             string connString = ConfigurationManager.ConnectionStrings["AdventureWorks2012Entities"].ToString();
-            string selectSQL = "select * from HumanResources.Department;" + "select* from HumanResources.Employee;";
+            string selectSQL = "select * from HumanResources.Department;" + "select* from HumanResources.Employee;" +
+                " select * from HumanResources.EmployeeDepartmentHistory";
 
             //Create the data adpater to retrieve data from the database.
             SqlDataAdapter dataAdapter = new SqlDataAdapter(selectSQL, connString);
@@ -27,7 +50,7 @@ namespace ConsoleApp296
             //Create and fill the dataset
             DataSet ds = new DataSet();
             dataAdapter.Fill(ds);
-            
+
             DataTable department = ds.Tables["Department"];
             DataTable employee = ds.Tables["Employee"];
 
@@ -43,12 +66,10 @@ namespace ConsoleApp296
                             DepartmentName = d.Field<string>("Name")
                         };
 
-            foreach(var q in query)
+            foreach (var q in query)
             {
                 Console.WriteLine("BusinessEntityID={0},Name={1},DepartmentId={2},DepartmentName={3}", q.BusinessEntityID, q.Name, q.DepartmentId, q.DepartmentName);
             }
-
-            Console.ReadLine();
         }
     }
 }
