bd0a7a22e4d2b5d28af7effdf5a6032291ac00c3
Fred1987 <2864326836@qq.com>
2018-12-03 15:02:36 +0800

    The term Lambda expression has derived its name from lambda calculus which in turn is a mathematical applied for defining functions.Lambda expressions as a LINQ equations executable part translate logic in a way at run time so it can pass on to the data source conventiently.

--- ConsoleApp297/Program.cs
+++ ConsoleApp297/Program.cs
@@ -8,16 +8,25 @@ namespace ConsoleApp297
 {
     class Program
     {
+        delegate int Del(int i);
         static void Main(string[] args)
+        {
+            Del myDel = y => y * y * y;
+            int j = myDel(5);
+            Console.WriteLine("The cube of {0} is {1}", 5, j);
+            Console.ReadLine();
+        }
+
+        static void LinqEntity()
         {
             using (AdventureWorks2012Entities context = new AdventureWorks2012Entities())
             {
                 //Get the list of department from database.
                 var departList = from d in context.Departments select d;
 
-                foreach(var de in departList)
+                foreach (var de in departList)
                 {
-                    Console.WriteLine("Department Id={0},Department Name={1}\n",de.DepartmentID,de.Name);
+                    Console.WriteLine("Department Id={0},Department Name={1}\n", de.DepartmentID, de.Name);
                 }
 
                 ////Add new Department.
@@ -28,11 +37,11 @@ namespace ConsoleApp297
                 //context.Departments.Add(dept);
                 //context.SaveChanges();
 
-               
+
 
                 //Update existing Department.
                 Department updateDept = context.Departments.FirstOrDefault(x => x.Name == "SupportTeam2");
-                
+
                 context.Departments.Remove(updateDept);
                 context.SaveChanges();
 
@@ -42,8 +51,10 @@ namespace ConsoleApp297
                 {
                     Console.WriteLine(d.Name);
                 }
-                Console.ReadLine();
             }
         }
     }
+
+   
 }
+
