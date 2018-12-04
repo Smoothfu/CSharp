2c4f5a17f341985f5b015e0de9f93254f823624c
Fred1987 <2864326836@qq.com>
2018-12-04 14:22:43 +0800

    public abstract class MarshalByRefObject. Enables access to objects across application domain boundaries in applications that support remoting

---
+++ ConsoleApp299/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp299
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer(x=>
+            {
+                OutputTime();
+            },null,0,1000);
+            Console.ReadLine();
+        }
+
+        static void OutputTime()
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfff"));
+        }
+    }
+}
