8d12c476df73d7f386443f5ca111ba261aa3357c
Fred1987 <2864326836@qq.com>
2018-12-04 15:39:53 +0800

    Stack<int> intStack,first in last out

--- ConsoleApp299/Program.cs
+++ ConsoleApp299/Program.cs
@@ -11,11 +11,39 @@ namespace ConsoleApp299
     {
         static void Main(string[] args)
         {
-            Singleton instance = Singleton.GetSingletonInstance();
-            Console.WriteLine(instance.GetType().Name);
+            Stack<int> intStack = new Stack<int>();
+            Random rnd = new Random();
+            Console.WriteLine("The push in order:\n");
+            for(int i=0;i<10;i++)
+            {
+                int tempValue = rnd.Next(0, int.MaxValue);
+                intStack.Push(tempValue);
+                Console.Write(tempValue + "\t");
+            }
+
+            Console.WriteLine("\nThe pop up order:\n");
+            while(intStack.Count>0)
+            {
+                Console.Write(intStack.Pop() + "\t");
+            }
             Console.ReadLine();
         }
 
+        static void ReadNumber()
+        {
+            int num;
+            string sNum;
+            Console.Write("Enter a number: ");
+            sNum = Console.ReadLine();
+            num = Int32.Parse(sNum);
+            Console.WriteLine();
+            Console.WriteLine(num);
+        }
+        static void GetSingleton()
+        {
+            Singleton instance = Singleton.GetSingletonInstance();
+            Console.WriteLine(instance.GetType().Name);
+        }
         static void OutputTime()
         {
             Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfff"));
