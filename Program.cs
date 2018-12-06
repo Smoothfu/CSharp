7f2594c6e4323fa0793558304f474152a100f971
Fred1987 <2864326836@qq.com>
2018-12-06 15:23:41 +0800

    Bubble sort,the sort gets its name because values float like a bubble from one end of the list to another.Assuming you are sorting a list of numbers in ascending order,higher values float to the right whereas lower values float to the left. This behavior is caused by moving through the list mant times,comparing adjacent values and swapping them if the value to the left is greater than the value to the right

--- Program.cs
+++ Program.cs
@@ -11,18 +11,23 @@ namespace ConsoleApp301
     {
         static void Main(string[] args)
         {
-            CArray arr = new CArray(10);
-            Random rnd = new Random();
-            for(int i=0;i<10;i++)
-            {
-                arr.Insert((int)(rnd.NextDouble() * 100));
-            }
-            arr.DisplayElements();
+            BubbleSortAsc();
             //86	99	67	86	60	62	88	47	98	25
             //92	62	72	70	98	49	17	56	14	77
             //48	92	4	73	44	19	75	98	48	59
             //在擂台下面，台上的生死更你没关，你只不过是看个乐子；可是站在这里头，你只能赢，不能输！
             Console.ReadLine();
+        }
+
+        static void RandomPseduo()
+        {
+            CArray arr = new CArray(10);
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                arr.Insert((int)(rnd.NextDouble() * 100));
+            }
+            arr.DisplayElements();
         }
 
         static void ArrayListDynamicalyIncreaseSize()
@@ -47,8 +52,8 @@ namespace ConsoleApp301
         static void BubbleSortAsc()
         {
             Random rnd = new Random();
-            int[] arr = new int[100];
-            for (int i = 0; i < 100; i++)
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++)
             {
                 int tempValue = rnd.Next(1, 1000000000);
                 arr[i] = tempValue;
