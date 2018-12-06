c78c24a2bcd030107cad2d922e51fe55a0abbc30
Fred1987 <2864326836@qq.com>
2018-12-06 09:41:25 +0800

    public class ArrayList:IList,ICollection,IEnumerable,ICloneable.  Implements the System.Collections.IList interface using an array whose size is dynamically increased as required

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            al.Add(1);
+            al.Add(true);
+            al.Add(1.1);
+            al.Add('c');
+            al.Add("Seeking truth");
+
+
+            if (al!=null && al.Count>0)
+            {
+                foreach(var a in al)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
