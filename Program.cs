29b207742542ea2d125b3e47bd96d8cc56316276
Fred1987 <2864326836@qq.com>
2018-12-06 10:08:27 +0800

    Bubble sort

--- Program.cs
+++ Program.cs
@@ -11,6 +11,21 @@ namespace ConsoleApp301
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int[] arr = new int[100];
+            for(int i=0;i<100;i++)
+            {
+                int tempValue = rnd.Next(1, 1000000000);
+                arr[i] = tempValue;
+            }
+
+            BubbleSortAsc(arr);
+
+            Console.ReadLine();
+        }
+
+        static void ArrayListDynamicalyIncreaseSize()
+        {
             ArrayList al = new ArrayList();
             al.Add(1);
             al.Add(true);
@@ -19,14 +34,45 @@ namespace ConsoleApp301
             al.Add("Seeking truth");
 
 
-            if (al!=null && al.Count>0)
+            if (al != null && al.Count > 0)
             {
-                foreach(var a in al)
+                foreach (var a in al)
                 {
                     Console.WriteLine(a);
                 }
             }
-            Console.ReadLine();
+        }
+
+        static void BubbleSortAsc(int[] arr)
+        {
+            if(arr==null || !arr.Any())
+            {
+                return;
+            }
+
+            Console.WriteLine("Before sorting,the original order:");
+            foreach(int a in arr)
+            {
+                Console.Write(a + "\t");
+            }
+            for (int i=0;i<arr.Length;i++)
+            {
+                for(int j=i+1;j<arr.Length;j++)
+                {
+                    if(arr[i]>arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("\n\nAfter sorting in order ascending:");
+            foreach(int a in arr)
+            {
+                Console.Write(a+"\t");
+            }            
         }
     }
 }
