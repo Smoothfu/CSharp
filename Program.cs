af2c5b590e8ffe02b2cd5d1456bd5028e60f8791
Fred1987 <2864326836@qq.com>
2018-12-07 16:07:27 +0800

    SelectionSort the most effencient sort

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random rnd = new Random();
+            for(int i=0;i<10;i++)
+            {
+                arr[i] = rnd.Next(10, 1000);
+            }
+
+            SelectionSort(arr);
+            Console.ReadLine();
+        }
+
+
+        static void SelectionSort(int[] arr)
+        {
+            if(arr==null || !arr.Any())
+            {
+                return;
+            }
+
+            Console.Write("Initial array: ");
+            Console.WriteLine(string.Join("\t", arr));
+            
+            for(int i=0;i<arr.Length;i++)
+            {
+                int minIndex = i;
+                for(int j=i+1;j<arr.Length;j++)
+                {
+                    if(arr[minIndex]>arr[j])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if(i!=minIndex)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                }
+            }
+
+            Console.Write("\nAfter sort: ");
+            Console.WriteLine(string.Join("\t", arr));
+        }
+    }
+}
