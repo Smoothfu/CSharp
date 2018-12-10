7fea03e0c7088463b6f7cf8324c8559f49b542ae
Fred1987 <2864326836@qq.com>
2018-12-10 16:21:44 +0800

    public class Queue:ICollection,IEnumerable,ICloneable  Represents a first-in,first-out collection of objects

--- Program.cs
+++ Program.cs
@@ -1,22 +1,169 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
 
 namespace ConsoleApp305
 {
     class Program
-    {
+    {    
         static void Main(string[] args)
         {
-            Queue myQueue = new Queue(32, 3);
-            for(int i=0;i<33;i++)
+            Queue[] numQueue = new Queue[10];
+            int[] nums = new int[] { 91, 46, 85, 15, 92, 35, 31, 22 };
+            int[] random = new int[99];
+            //display original list
+            for(int i=0;i<10;i++)
             {
-                myQueue.Enqueue(i);
+                numQueue[i] = new Queue();
             }
 
-            Console.WriteLine(myQueue.Count);
-            Console.WriteLine();
+            RSort(numQueue, nums, DigitType.ones);
+
+            //numQueue,nums,1
+            BuildArray(numQueue, nums);
+            Console.WriteLine("\nFirst pass results:");
+            DisplayArray(nums);
+
+            //Second pass sort.
+            RSort(numQueue, nums, DigitType.tens);
+            BuildArray(numQueue, nums);
+            Console.WriteLine("\nSecond pass results:");
+            //display final results
+            DisplayArray(nums);
             Console.ReadLine();
         }
+
+        enum DigitType { ones = 1, tens = 10 }
+        static void DisplayArray(int[] arr)
+        {
+            for (int x = 0; x < arr.GetUpperBound(0); x++)
+            {
+                Console.Write(arr[x] + " ");
+            }
+        }
+
+        static void RSort(Queue[] que,int[] arr,DigitType digit)
+        {
+            int snum;
+            for(int x=0;x<=arr.GetUpperBound(0);x++)
+            {
+                if(digit==DigitType.ones)
+                {
+                    snum = arr[x] % 10;
+                }
+                else
+                {
+                    snum = arr[x] / 10;
+                }
+
+                que[snum].Enqueue(arr[x]);
+            }
+        }
+
+        static void BuildArray(Queue[] queue,int [] arr)
+        {
+            int j = 0;
+            for(int i=0;i<=9;i++)
+            {
+                while(queue[i].Count>0)
+                {
+                    arr[j] = Int32.Parse(queue[i].Dequeue().ToString());
+                    j++;
+                }
+            }
+        }
+        static void newDancers(Queue male, Queue female)
+        {
+            Dancer m, w;
+            m = new Dancer();
+            w = new Dancer();
+            if (male.Count > 0 && female.Count > 0)
+            {
+                m.GetName(male.Dequeue().ToString());
+                w.GetName(female.Dequeue().ToString());
+            }
+
+            else if ((male.Count > 0) && (female.Count == 0))
+            {
+                Console.WriteLine("Waitting on a female dancer.");
+            }
+            else if ((female.Count > 0) && (male.Count == 0))
+            {
+                Console.WriteLine("Waiting on a male dancer.");
+            }
+        }
+
+        static void HeadOfLine(Queue male, Queue female)
+        {
+            Dancer w, m;
+            m = new Dancer();
+            w = new Dancer();
+            if (male.Count > 0)
+            {
+                m.GetName(male.Peek().ToString());
+            }
+
+            if (female.Count > 0)
+            {
+                w.GetName(female.Peek().ToString());
+            }
+
+            if (m.name != " " && w.name != " ")
+            {
+                Console.WriteLine("Next in the line are: " + m.name + "\t" + w.name);
+            }
+            else
+            {
+                if (m.name != " ")
+                {
+                    Console.WriteLine("Next in line is :" + m.name);
+                }
+
+                else
+                {
+                    Console.WriteLine("Next in line is: " + w.name);
+                }
+            }
+        }
+
+        static void StartDancing(Queue male, Queue female)
+        {
+            Dancer m, w;
+            m = new Dancer();
+            w = new Dancer();
+            Console.WriteLine("Dance partners are: ");
+            Console.WriteLine();
+
+            for (int count = 0; count <= 3; count++)
+            {
+                m.GetName(male.Dequeue().ToString());
+                w.GetName(female.Dequeue().ToString());
+                Console.WriteLine(w.name + "\t" + m.name);
+            }
+        }
+
+        static void FormLines(Queue male, Queue female)
+        {
+            Dancer d = new Dancer();
+            StreamReader inFile;
+            inFile = File.OpenText(".\\bat.dat");
+            string line;
+            while (inFile.Peek() != -1)
+            {
+                line = inFile.ReadLine();
+                d.sex = line.Substring(0, 1);
+                d.name = line.Substring(2, line.Length - 2);
+                if (d.sex == "M")
+                {
+                    male.Enqueue(d);
+                }
+                else
+                {
+                    female.Enqueue(d);
+                }
+            }
+        }
+
     }
 
     public class CQueue
@@ -51,5 +198,22 @@ namespace ConsoleApp305
         {
             return pqueue.Count;
         }
+    }
+
+    public struct Dancer
+    {
+        public string name;
+        public string sex;
+        public void GetName(string n)
+        {
+            name = n;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+       
     }
 }
