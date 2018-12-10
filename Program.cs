65ed9c48f453c51c5b426c511787893f5e77ab01
Fred1987 <2864326836@qq.com>
2018-12-10 15:31:17 +0800

    A queue is data structure where data enters at the rear of a list and is removed from the front of the list. Queues are used to store items in the order in which they occur. Queues are an example of a first-in,first-out data structure. Queues are used to order processes submitted to an operation system or a print spooler,and simulation applications use queues to model customers waiting in a line

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp305
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CQueue queue = new CQueue();
+            queue.EnQueue(10);
+            queue.EnQueue(10);
+            queue.EnQueue(10);
+            queue.EnQueue(10);
+            queue.EnQueue(10);
+            queue.EnQueue(10);
+            queue.EnQueue(10);
+            queue.EnQueue(10);
+
+            var peekResult = queue.Peek();
+            Console.WriteLine(peekResult);
+            queue.DeQueue();
+
+            int queueEleCount2 = queue.Count();
+            Console.WriteLine(queueEleCount2);
+            queue.ClearQueue();
+            int queueEleCount = queue.Count();
+            Console.WriteLine(queueEleCount);
+            Console.ReadLine();
+        }
+    }
+
+    public class CQueue
+    {
+        private ArrayList pqueue;
+        public CQueue()
+        {
+            pqueue = new ArrayList();
+        }
+
+        public void EnQueue(object item)
+        {
+            pqueue.Add(item);
+        }
+        
+        public void DeQueue()
+        {
+            pqueue.RemoveAt(0);
+        }
+
+        public object Peek()
+        {
+            return pqueue[0];
+        }
+
+        public void ClearQueue()
+        {
+            pqueue.Clear();
+        }
+
+        public int Count()
+        {
+            return pqueue.Count;
+        }
+    }
+}
