0c09c7a1b00d7c8091908d61b2eeb4c082e3b6d3
Fred1987 <2864326836@qq.com>
2018-12-10 10:19:10 +0800

    binary search is used when the items are sorted in the list

--- Program.cs
+++ Program.cs
@@ -10,14 +10,16 @@ namespace ConsoleApp304
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[100];
-            Random rnd = new Random();
-            for(int i=0;i<100;i++)
-            {
-                arr[i] = rnd.Next(1,10000000);
-            }
+            //int[] arr = new int[100];
+            //Random rnd = new Random();
+            //for(int i=0;i<100;i++)
+            //{
+            //    arr[i] = rnd.Next(1,10000000);
+            //}
 
-            SelectionSort(arr);
+            int[] arr={ 10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170};
+            int resultIndex = BinarySearch(arr, 130);
+            Console.WriteLine(resultIndex);
             Console.ReadLine();
         }
 
@@ -50,6 +52,37 @@ namespace ConsoleApp304
 
             Console.WriteLine("\nAfter selection sort:\n " + string.Join("\t", arr));
 
+        }
+
+        static int BinarySearch(int[] arr,int target)
+        {
+            if(arr==null || !arr.Any())
+            {
+                return -1;
+            }
+
+            Array.Sort(arr);
+            int low = 0, high = arr.Length - 1;
+            while(low<=high)
+            {
+                int mid = (low + high) / 2;
+                if(arr[mid]==target)
+                {
+                    return mid + 1;
+                }
+
+                if(arr[mid]>target)
+                {
+                    high = mid - 1;
+                }
+
+                if(arr[mid]<target)
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return -1;
         }
     }
 }
