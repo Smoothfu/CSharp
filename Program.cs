070da9fe6bb354e4a72c5f6505d2207e8762a46e
Fred1987 <2864326836@qq.com>
2018-12-12 10:38:07 +0800

    public static string Join(string separator,params string[] value);  Concatenates all the elements of a string array,using the specified separator between each element. An array that contains the elements to concatenate. returns a string that consists of the elements in value delimited by the separator string.If value is an empty array,the method returns System.string.Empty

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace ConsoleApp307
 {
@@ -6,15 +7,88 @@ namespace ConsoleApp307
     {
         static void Main(string[] args)
         {
-            string str = "Hello, world!";
-            int len = str.Length;
-            int pos = str.IndexOf(" ");
-            string firstWord, secondWord;
-            firstWord = str.Substring(0, pos);
-            secondWord = str.Substring(pos + 1, (len - 1) - (pos + 1));
-            Console.WriteLine("First word: " + firstWord);
-            Console.WriteLine("Second Word: " + secondWord);
+            StringSplitSeparator();
             Console.ReadLine();
+        }
+
+        static void SplitString()
+        {
+            string str = "Now is the time";
+            int pos;
+            string word;
+            ArrayList words = new ArrayList();
+            pos = str.IndexOf(" ");
+            while (pos > 0)
+            {
+                word = str.Substring(0, pos);
+                words.Add(word);
+                str = str.Substring(pos + 1, str.Length - (pos + 1));
+                pos = str.IndexOf(" ");
+                if (pos == -1)
+                {
+                    word = str.Substring(0, str.Length);
+                    words.Add(word);
+                }
+            }
+
+            if (words.Count > 0)
+            {
+                foreach (string w in words)
+                {
+                    Console.Write(w + "\t");
+                }
+            }
+        }
+
+        static ArrayList SplitWords(string str)
+        {
+            string[] ws = new string[str.Length - 1];
+            ArrayList words = new ArrayList();
+            int pos;
+            string word;
+             
+            while(str.IndexOf(" ") > 0)
+            {
+                pos = str.IndexOf(" ");
+                word = str.Substring(0, pos);
+                words.Add(word);
+                str = str.Substring(pos + 1, str.Length - (pos + 1));
+                if(pos==-1)
+                {
+                    word = str.Substring(0, str.Length);
+                    words.Add(word);
+                }
+            }
+            return words;
+        }
+
+        static void StringSplit()
+        {
+            string data = "Mike,McMillan,3000 W. Scenic,North Little Rock,AR,72118";
+            string[] sdata;
+            char[] delimiter = new char[] { ',' };
+            sdata = data.Split(delimiter, data.Length);
+            foreach (string word in sdata)
+            {
+                Console.Write(word + " ");
+            }
+                
+        }
+
+        static void StringSplitSeparator()
+        {
+            string data = "Mike,McMillan,3000 W. Scenic,North Little Rock, AR,72118";
+            string[] strData;
+            char[] delimiters = new char[] { ',' };
+            strData = data.Split(delimiters, data.Length);
+            foreach(string str in strData)
+            {
+                Console.Write(str + "\t");
+            }
+
+            string strJoin;
+            strJoin = string.Join(",", strData);
+            Console.WriteLine("\n" + strJoin);
         }
     }
 }
