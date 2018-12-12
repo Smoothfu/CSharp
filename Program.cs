ced39d10639ae65ff1fe19fecda2faced5570673
Fred1987 <2864326836@qq.com>
2018-12-12 09:29:27 +0800

    public String[] Split(string operator,StringOptions options=StringSplitOptions.None);

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "This is a string which consist of any character that is part of the Unicode character set";
+            string[] arr = str.Split(" ", StringSplitOptions.None);
+            if(arr!=null && arr.Length>0)
+            {
+                foreach(string a in arr)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
