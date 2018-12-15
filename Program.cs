d8277682caca13d8435d25505280014bea1fd1f8
Fred1987 <2864326836@qq.com>
2018-12-15 15:56:13 +0800

    Install-Package EntityFramework -version 6.2.0

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp311
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AdventureWorks2017Entities db = new AdventureWorks2017Entities())
+            {
+                var stores = db.Stores;
+                if(stores!=null && stores.Count() > 0)
+                {
+                    foreach (Store s in stores)
+                    {
+                        Console.WriteLine(s.BusinessEntityID);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
