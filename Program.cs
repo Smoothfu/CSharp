535098382e5858478affcca93f8924749117762d
Fred1987 <2864326836@qq.com>
2018-12-17 22:46:27 +0800

    yield keyword indicate that the method,or get accessor in which it appears is an iterator

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,19 @@ namespace ConsoleApp312
     class Program
     {
         static void Main(string[] args)
+        {
+            List<int> intList = Power(2, 10).ToList();
+            if(intList!=null && intList.Count>0)
+            {
+                foreach(int i in intList)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static void EnumerableToArray()
         {
             using (AdventureWorks2017Entities db = new AdventureWorks2017Entities())
             {
@@ -33,16 +47,60 @@ namespace ConsoleApp312
 
 
                 List<int> intList = new List<int>();
-                if (result != null && result.Count()> 0)
+                if (result != null && result.Count() > 0)
                 {
-                    Console.WriteLine("Count:"+result.Count());
+                    Console.WriteLine("Count:" + result.Count());
                     foreach (var r in result)
                     {
-                        Console.WriteLine(r.SID + ","+r.CRC+"\n");
+                        Console.WriteLine(r.SID + "," + r.CRC + "\n");
                     }
                 }
             }
-            Console.ReadLine();
+            
+        }
+
+        static IEnumerable<int> Power(int num,int exponent)
+        {
+            int result = 1;
+            for(int i=0;i<exponent;i++)
+            {
+                result = result * num;
+                yield return result;
+            }
+        }
+    }
+
+    public static class GalaxyClass
+    {
+        public static void ShowGalaxies()
+        {
+            var theGalaxies = new Galaxies();
+            foreach (Galaxy theGalaxy in theGalaxies.NextGalaxy)
+            {
+                Console.WriteLine(theGalaxy.Name + " " + theGalaxy.MegaLightYears.ToString());
+            }
+        }
+
+        public class Galaxies
+        {
+
+            public System.Collections.Generic.IEnumerable<Galaxy> NextGalaxy
+            {
+                get
+                {
+                    yield return new Galaxy { Name = "Tadpole", MegaLightYears = 400 };
+                    yield return new Galaxy { Name = "Pinwheel", MegaLightYears = 25 };
+                    yield return new Galaxy { Name = "Milky Way", MegaLightYears = 0 };
+                    yield return new Galaxy { Name = "Andromeda", MegaLightYears = 3 };
+                }
+            }
+
+        }
+
+        public class Galaxy
+        {
+            public String Name { get; set; }
+            public int MegaLightYears { get; set; }
         }
     }
 }
