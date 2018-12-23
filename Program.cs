553f71264e0fff96fabc4f0846123d103c7cc982
Fred1987 <2864326836@qq.com>
2018-12-23 15:03:31 +0800

    public event EventHandler EventName.

--- Program.cs
+++ Program.cs
@@ -7,20 +7,35 @@ using System.Threading.Tasks;
 namespace ConsoleApp313
 {
     delegate void MathDel(int x, int y);
+    delegate void CustomeEventHandler(object sender, CustomEventArgs e);
+    
+
     class Program
     {
+        public delegate void CallHandler(string msg);
+        public event CallHandler CallEvent;
         static void Main(string[] args)
         {
-            int x = 34543, y = 4256245;
-            MathDel del = new MathDel(Add);
-            del(x, y);
-            del = new MathDel(Subtract);
-            del(x, y);
-            del = new MathDel(Multiply);
-            del(x, y);
+            Friend fred = new Friend("Fred");
+            Program p = new Program();
+            p.CallEvent += new CallHandler(fred.SendMsg);
+            p.OnCall("Any one received message!");
+
+            Console.WriteLine("\n\n\n\n\n");
+            p.CallEvent -= new CallHandler(fred.SendMsg);
+            p.OnCall("Cancel message");
+            
+            
             Console.ReadLine();
         }
 
+        public void OnCall(string msg)
+        {
+            if (CallEvent != null)
+            {
+                CallEvent(msg);
+            }
+        }
         static void Add(int x,int y)
         {
             Console.WriteLine($"{x}+{y}={x + y}!");
@@ -34,6 +49,29 @@ namespace ConsoleApp313
         static void Multiply(int x,int y)
         {
             Console.WriteLine($"{x}*{y}={x * y}");
+        }
+    }
+
+    public class CustomEventArgs:EventArgs
+    {
+        private string Msg { get; }
+        public CustomEventArgs(string str)
+        {
+            Msg = str;
+        }
+    }
+
+    public class Friend
+    {
+        public string Name { get; set; }
+        public Friend(string name)
+        {
+            Name = name;
+        }
+
+        public void SendMsg(string msg)
+        {
+            Console.WriteLine(this.Name + " receive the message: " + msg);
         }
     }
 }
