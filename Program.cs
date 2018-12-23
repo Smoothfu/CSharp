86b7b217710634cfd6b13fe9682dd5cfaf06dc26
Fred1987 <2864326836@qq.com>
2018-12-23 14:19:45 +0800

    Signature of the method,method as the parameter

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp313
+{
+    delegate void MathDel(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 34543, y = 4256245;
+            MathDel del = new MathDel(Add);
+            del(x, y);
+            del = new MathDel(Subtract);
+            del(x, y);
+            del = new MathDel(Multiply);
+            del(x, y);
+            Console.ReadLine();
+        }
+
+        static void Add(int x,int y)
+        {
+            Console.WriteLine($"{x}+{y}={x + y}!");
+        }
+
+        static void Subtract(int x,int y)
+        {
+            Console.WriteLine($"{x}-{y}={x - y}");
+        }
+
+        static void Multiply(int x,int y)
+        {
+            Console.WriteLine($"{x}*{y}={x * y}");
+        }
+    }
+}
