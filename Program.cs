b0f5d241b00eb0587bdc9adf2162232050702840
Fred1987 <2864326836@qq.com>
2018-12-30 20:46:03 +0800

    public class DbSet<TEntity>:DbQuery<TEntity>,IDbSet<TEntity>,IQueryable<TEntity>,IEnumerable<TEntity>,IQueryable,IEnumerable,IInternalSetAdapter where TEntity:class Note that DbSet does not support MEST meaning that there is always a one-to-one correlation between a type and a set

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AdventureWorks2017Entities db = new AdventureWorks2017Entities())
+            {
+                var stores = db.Stores;
+                if(stores!=null && stores.Any())
+                {
+                    foreach(var s in stores)
+                    {
+                        Console.WriteLine($"{s.BusinessEntityID},{s.ModifiedDate},{s.Name},{s.rowguid}");
+                    }
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
