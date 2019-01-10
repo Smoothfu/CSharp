d7bdfae3c6ae06e888f7ca3bfc7c3187427e0fc2
Fred1987 <2864326836@qq.com>
2019-01-10 22:56:58 +0800

    public object GetValue(object obj); Returns the property value of a specified object.the property value of the specified object

---
+++ ExportClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+
+namespace ConsoleApp315
+{
+    class ExportClass
+    {
+        //static void ExportByNPOI()
+        //{
+        //    watch.Start();
+        //    using (AdventureWorks2017Entities db = new AdventureWorks2017Entities())
+        //    {
+        //        List<SalesOrderDetail> salesDetailList = db.SalesOrderDetails.ToList();
+
+        //        Thread exportThread = new Thread(() =>
+        //        {
+        //            string threadMsg = string.Format($"ThreadId:{Thread.CurrentThread.ManagedThreadId},Time is {DateTime.Now.ToString("yyyyMMddHHmmssffffff")} \n\n\n");
+        //            File.AppendAllText(logPath, threadMsg, Encoding.UTF8);
+        //            InvokeExportByNPOI<SalesOrderDetail>(salesDetailList.ToArray());
+        //        });
+        //        exportThread.SetApartmentState(ApartmentState.STA);
+        //        exportThread.Start();
+        //        exportThread.Join();
+        //    }
+        //}
+        //static void InvokeExportByNPOI<T>(T[] arr)
+        //{
+        //    if (arr != null && !arr.Any())
+        //    {
+        //        return;
+        //    }
+
+        //    string excelName = DateTime.Now.ToString("yyyyMMddHHmmssffffff") + Guid.NewGuid().ToString() + ".xlsx";
+        //    XSSFWorkbook book = new XSSFWorkbook();
+        //    var sheet = book.CreateSheet("Sheet1");
+        //    var firstRow = sheet.CreateRow(0);
+        //    var propertiesArr = typeof(T).GetProperties().Where(x => !x.GetMethod.IsVirtual).ToArray();
+        //    for (int i = 0; i < propertiesArr.Length; i++)
+        //    {
+        //        var column = firstRow.CreateCell(i);
+        //        column.SetCellValue(propertiesArr[i].Name);
+        //    }
+
+
+        //    using (FileStream fs = new FileStream(excelName, FileMode.Append))
+        //    {
+        //        for (int time = 0; time < 10; time++)
+        //        {
+        //            watch.Start();
+        //            exportNum += 10;
+        //            for (int i = time * 10 + 1; i <= time * 10 + 10; i++)
+        //            {
+        //                var indexRow = sheet.CreateRow(i);
+        //                for (int j = 0; j < propertiesArr.Length; j++)
+        //                {
+        //                    var indexColumn = indexRow.CreateCell(j);
+        //                    var indexColumnName = arr[i % 10].GetType().GetProperties().Where(x => !x.GetMethod.IsVirtual).ToArray()[j];
+
+        //                    if (indexColumnName.GetValue(arr[i % 10]) != null)
+        //                    {
+        //                        var columnValue = indexColumnName.GetValue(arr[i % 10]).ToString();
+        //                        indexColumn.SetCellValue(columnValue);
+        //                    }
+        //                }
+        //            }
+        //            book.Write(fs);
+        //            watch.Stop();
+        //            string exportMsg = string.Format($"Saved in {excelName}.\nThere are totally {exportNum} salesorderdetails and cost {watch.ElapsedMilliseconds} millseconds and now is {DateTime.Now.ToString("yyyyMMddHHmmssfff")} \n\n");
+        //            File.AppendAllText(logPath, exportMsg, Encoding.UTF8);
+        //        }
+        //        fs.Close();
+        //    }
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.XSSF.UserModel;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConsoleApp315
+{
+    class Program
+    {
+        static Stopwatch watch = new Stopwatch();
+        static int exportNum = 0;
+        static string logPath = Directory.GetCurrentDirectory() + "\\ExportLog.txt";
+        static string exportMsg = "";
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Thread exportThread = new Thread(() =>
+              {
+                  using (AdventureWorks2017Entities db = new AdventureWorks2017Entities())
+                  {
+                      List<SalesOrderDetail> orderList = db.SalesOrderDetails.ToList();
+                      orderList.AddRange(orderList);
+                      orderList.AddRange(orderList);                      
+                      ExportTEntity<SalesOrderDetail>(orderList.ToArray());
+                  }
+              });
+
+            exportThread.SetApartmentState(ApartmentState.STA);
+            exportThread.Start();
+            exportThread.Join();
+           
+            System.Windows.Forms.MessageBox.Show(exportMsg);
+            Console.ReadLine();
+        }    
+       
+        [STAThread]
+        static void ExportTEntity<T>(T[] arr)
+        {            
+            if (arr != null && !arr.Any())
+            {
+                return;
+            }
+            exportNum = arr.Length;
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                watch.Start();
+                XSSFWorkbook book;
+                sfd.Filter = "Excel Files(.xls)|*.xls|Excel Files(.xlsx)| *.xlsx | All Files | *.*";
+                sfd.FileName = DateTime.Now.ToString("yyyyMMddHHmmssffffff") + Guid.NewGuid().ToString() + ".xlsx";
+                sfd.InitialDirectory = Directory.GetCurrentDirectory();
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    var firstRowData = arr.FirstOrDefault();
+                    book = new XSSFWorkbook();
+                    var sheet = book.CreateSheet("Sheet1");
+                    var firstRow = sheet.CreateRow(0);
+                    var propertiesArr = firstRowData.GetType().GetProperties().Where(x => !x.GetMethod.IsVirtual).ToArray();
+                    for (int i = 0; i < propertiesArr.Length; i++)
+                    {
+                        var column = firstRow.CreateCell(i);
+                        column.SetCellValue(propertiesArr[i].Name);
+                    }
+                    for (int i = 1; i <= arr.Length; i++)
+                    {
+                        var indexRow = sheet.CreateRow(i);                        
+                        for (int j = 0; j < propertiesArr.Length; j++)
+                        {
+                            var indexColumn = indexRow.CreateCell(j);
+                            var indexColumnName = propertiesArr[j];
+                            var columnValue = indexColumnName.GetValue(arr[i - 1]);
+                            if (columnValue != null)
+                            {                                 
+                                indexColumn.SetCellValue(columnValue.ToString());
+                            }
+                        }
+                    }                     
+                    using (FileStream stream = File.OpenWrite(sfd.FileName))
+                    {
+                        book.Write(stream);
+                        stream.Close();
+                    }
+                    watch.Stop();
+                    exportMsg = string.Format($"Saved in {sfd.FileName}.\nThere are totally {exportNum} salesorderdetails and cost {watch.ElapsedMilliseconds} millseconds and now is {DateTime.Now.ToString("yyyyMMddHHmmssfff")} \n\n");
+                    File.AppendAllText(logPath, exportMsg);
+                }
+                arr = null;
+            }
+        }
+    }     
+}
