78ae78f20f88b6c6f4776d11d3142ae09835ea5c
Fred1987 <2864326836@qq.com>
2019-01-21 20:02:49 +0800

    [ComVisible(true)] public delegate void TimerCallback(object state); Represents the method that handles calls from a System.Threading.Timer

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp319
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimerCallback tcb = new TimerCallback(PrintTime);
+            Timer timer = new Timer(tcb, null, 0, 1000);
+            Console.ReadLine();
+        }
+        static void Divide(int x,int y)
+        {
+            try
+            {
+                Console.WriteLine($"{x}/{y}={x / y}");
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message+"In DivideByZeroException");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message + "In Exception");
+            }
+
+            finally
+            {
+                Console.WriteLine("This is the finally block!");
+            }          
+
+        }
+
+        static void PrintTime(object obj)
+        {
+            Console.WriteLine($"Now is {DateTime.Now.ToString("yyyyMMddHHmmss")}");
+        }
+    }
+}
