662183e9a683365add3d7e269f76689d70e723b1
Fred1987 <2864326836@qq.com>
2019-02-11 23:08:17 +0800

    SqlDataReader dataReader=cmd.ExecuteReader();Sends the System.Data.SqlClient.SqlCommand.CommandText to the System.Data.SqlClient.SqlCommand and builds a System.Data.SqlClient.SqlDataReader. returns A System.Data.SqlClient.SqlDataReader object

--- ConsoleApp323/Program.cs
+++ ConsoleApp323/Program.cs
@@ -6,23 +6,29 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Diagnostics;
+using System.IO;
 
 namespace ConsoleApp323
 {
     class Program
     {
+        static string connString = ConfigurationManager.ConnectionStrings["connString"].ToString();
+        static string selectSQL = @"select SalesOrderID,SalesOrderDetailID,CarrierTrackingNumber,OrderQty,
+            ProductID,SpecialOfferID,UnitPrice,UnitPriceDiscount,LineTotal,rowguid,ModifiedDate from sales.SalesOrderDetail";
+        static Stopwatch stopwatch = new Stopwatch();
+        static string exportMsg = "";
+        static int exportNum = 0;
+        static string logFullName = Directory.GetCurrentDirectory() + "\\" + DateTime.Now.ToString("yyyyMMdd") + "log.txt";
         static void Main(string[] args)
         {
-            SqlDataAdapterDB();
+            SqlDataReaderDB();
             Console.ReadLine();
         }
 
         static void SqlDataAdapterDB()
         {
-            string connString = ConfigurationManager.ConnectionStrings["connString"].ToString();
-            string selectSQL= @"select SalesOrderID,SalesOrderDetailID,CarrierTrackingNumber,OrderQty,
-            ProductID,SpecialOfferID,UnitPrice,UnitPriceDiscount,LineTotal,rowguid,ModifiedDate 
-            from sales.SalesOrderDetail";
+            stopwatch.Restart();
             using (SqlConnection conn = new SqlConnection(connString))
             {
                 if(conn.State!=ConnectionState.Open)
@@ -39,6 +45,7 @@ namespace ConsoleApp323
                     {
                         for(int i=0;i<ds.Tables[0].Rows.Count;i++)
                         {
+                            exportNum++;
                             string rowMsg = $"{ds.Tables[0].Rows[i][0],-10}{ds.Tables[0].Rows[i][1],-10}" +
                                  $"{ds.Tables[0].Rows[i][2],-15}{ds.Tables[0].Rows[i][3],-5}" +
                                  $"{ds.Tables[0].Rows[i][4],-5}{ds.Tables[0].Rows[i][5],-5}" +
@@ -51,6 +58,48 @@ namespace ConsoleApp323
                 }
             }
 
+            stopwatch.Stop();
+            exportMsg = $"SqlDataAdapter cost:{stopwatch.ElapsedMilliseconds} milliseconds,export number:{exportNum}," +
+                $"now:{DateTime.Now.ToString("yyyyMMddHHmmssfff")},memory:{Process.GetCurrentProcess().PrivateMemorySize64}";
+            LogMessage(exportMsg);
+        }
+
+        static void SqlDataReaderDB()
+        {
+            exportNum = 0;
+            stopwatch.Restart();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if(conn.State!=ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+                using (SqlCommand cmd = new SqlCommand(selectSQL, conn))
+                {
+                    SqlDataReader dataReader = cmd.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        exportNum++;
+                        string dataReaderMsg = $"{dataReader[0],-10}{dataReader[1],-10}{dataReader[0],-10}" +
+                            $"{dataReader[2],-15}{dataReader[3],-5}{dataReader[4],-5}{dataReader[5],-5}{dataReader[6],-10}" +
+                            $"{dataReader[7],-10}{dataReader[8],-15}{dataReader[9],-45}{dataReader[0],-30}";
+                        Console.WriteLine(dataReaderMsg);
+                    }
+                  }
+            }
+            stopwatch.Stop();
+            exportMsg = $"SqlDataReader cost:{stopwatch.ElapsedMilliseconds} milliseconds,export number:{exportNum}" +
+                $"now:{DateTime.Now.ToString("yyyyMMddHHmmssfff")},memory:{Process.GetCurrentProcess().PrivateMemorySize64}";
+            LogMessage(exportMsg);
+            
+        }
+
+        static void LogMessage(string logMsg)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(logFullName, true))
+            {
+                streamWriter.WriteLine(logMsg);
+            }
         }
     }
 }
