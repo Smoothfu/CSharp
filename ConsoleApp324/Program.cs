7b3de027e5e2467d7cc666ca871cce0b33d9ff55
Fred1987 <2864326836@qq.com>
2019-02-13 19:28:17 +0800

    public delegate void Action<in T>(T obj); Encapsulates a method that has a single parameter and does not return a value;

---
+++ ConsoleApp324/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp324
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActionTSample();
+            Console.ReadLine();
+        }
+
+        static void ActionTSample()
+        {
+            Action<int> intAction = x => { Console.WriteLine($"The cubic of {x} is {x * x * x}"); };
+            intAction(1000);
+
+            Action<string> stringAction = str =>
+            {
+                Console.WriteLine($"Now is {DateTime.Now.ToString("yyyyMMddHHmmssffff")}, str is {str}");
+            };
+
+            stringAction("Work hard!");
+        }
+        static void ThreadStartSample()
+        {
+            Console.WriteLine("Main thread:Start a second thread.");
+            Thread thread = new Thread(new ThreadStart(ThreadProc));
+            thread.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Main thread:Do some work.");
+                Thread.Sleep(0);
+            }
+
+            Console.WriteLine("Main thread:Call Join(),to wait until ThreadProc ends.");
+            thread.Join();
+            Console.WriteLine("Main thread:ThreadProc.Join has returned.");
+        }
+        public static void ThreadProc()
+        {
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine($"ThreadProc:{i}");
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
