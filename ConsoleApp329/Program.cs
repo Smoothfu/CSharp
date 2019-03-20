a8766f58c76cf7e99f80990b202766b3d875db64
Fred1987 <2864326836@qq.com>
2019-03-20 23:33:46 +0800

    from i in intlist orderby i ascending select i

--- ConsoleApp329/Program.cs
+++ ConsoleApp329/Program.cs
@@ -10,19 +10,7 @@ namespace ConsoleApp329
     {
         static void Main(string[] args)
         {
-            string[] names = new string[99];
-            string name;
-            string[] someNames = new string[] {"David","Jennifer","Donnie","Mayo","Raymond",
-            "Bernica","Mike","Clayton","Beata","Michael"};
-
-            int hashVal;
-            for(int i=0;i<10;i++)
-            {
-                name = someNames[i];
-                hashVal = SimpleHash(name, names);
-                names[hashVal] = name;
-            }
-            ShowDistrib(names);
+            BubbleSort();
             Console.ReadLine();
         }
 
@@ -45,6 +33,41 @@ namespace ConsoleApp329
                 {
                     Console.WriteLine(i + " " + arr[i]);
                 }
+            }
+        }
+
+        static int BetterHash(string s,string[] arr)
+        {
+            int tot = 0;
+            char[] cname;
+            cname = s.ToCharArray();
+            for(int i=0; i<=cname.GetUpperBound(0);i++)
+            {
+                tot += 37 * tot + (int)cname[i];
+            }
+            tot = tot % arr.GetUpperBound(0);
+            if(tot<0)
+            {
+                tot += arr.GetUpperBound(0);
+            }
+            return tot;
+        }
+
+        static void BubbleSort()
+        {
+            Random rnd = new Random();
+            List<int> intList = new List<int>();
+            for(int i=0;i<100000;i++)
+            {
+                intList.Add(rnd.Next(1, 100000000));
+            }
+
+            var sortedList = from i in intList
+                             orderby i ascending
+                             select i;
+            foreach(var i in sortedList)
+            {
+                Console.WriteLine(i);
             }
         }
     }
