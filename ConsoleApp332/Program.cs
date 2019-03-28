77f64c846a84dae60a8d0154b176eb7bcf3792bf
Fred1987 <2864326836@qq.com>
2019-03-28 19:18:52 +0800

    public static Task Run(Action action,CancellationToken cancellationToken);Queues the specified work to run on the thread pool and returns a System.Threading.Tasks.Task object that represents that work.A cancellation token allows the work to be cancelled.action,the work to execute asynchronously.A cancellation token that can be use to can cel the work

--- ConsoleApp332/Program.cs
+++ ConsoleApp332/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.IO;
 
 namespace ConsoleApp332
 {
@@ -11,25 +12,56 @@ namespace ConsoleApp332
     {
         static int i = 0;
         static CancellationTokenSource cts = new CancellationTokenSource();
+
         static void Main(string[] args)
         {
-            Task.Run((()=>
+            Task[] allTasks = new Task[4];
+            Parallel.ForEach(allTasks, x =>
             {
-                Add(i);
-            }),cts.Token);
+                Task.Run(() => { Add(i); }, cts.Token);
+            });
            
             Console.ReadLine();
         }
         static void Add(object obj)
         {
-            while (i < 100)
+            try
             {
-                while(i==90)
+                while (i < 100)
                 {
-                    cts.Cancel();
+                    while (i == 90)
+                    {
+                        cts.Cancel();
+                    }
+                    string msg = $"i {i},Guid:{Guid.NewGuid()}";
+                    Console.WriteLine(msg);
+                    Logger.WriteLog(msg);
+                    i++;
                 }
-                Console.WriteLine($"Guid:{Guid.NewGuid()}, i {i}");
-                i++;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+                  
+        }
+    }
+
+    public static class Logger
+    {
+        private static string lockString = "lockString";
+        private static string logFullPath = Directory.GetCurrentDirectory() + "\\"
+            + DateTime.Now.ToString("yyyyMMdd");
+        
+        public static void WriteLog(string logMessage)
+        {   
+            lock(lockString)
+            {
+                using (StreamWriter logWriter = new StreamWriter(logFullPath, true, Encoding.UTF8))
+                {
+
+                    logWriter.WriteLine(logMessage);
+                }
             }            
         }
     }
