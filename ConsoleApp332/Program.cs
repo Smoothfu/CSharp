663f03c6ca70560b7a2280f7fb35260fdec3dc69
Fred1987 <2864326836@qq.com>
2019-03-28 22:21:51 +0800

    public const Double MaxValue = 1.7976931348623157E+308;

--- ConsoleApp332/Program.cs
+++ ConsoleApp332/Program.cs
@@ -14,11 +14,14 @@ namespace ConsoleApp332
         static CancellationTokenSource cts = new CancellationTokenSource();
 
         static void Main(string[] args)
-        {
-            Task<string> stringTask = Task<string>.Run<string>(() => RetrieveString(),cts.Token);
-
-            string result = stringTask.Result;
-            Console.WriteLine(result);
+        {           
+            Console.WriteLine($"sizeof(int):{sizeof(int)},Int32.MaxValue:{Int32.MaxValue}");
+            Console.WriteLine($"sizeof(long):{sizeof(long)},long.MaxValue:{long.MaxValue}");
+            Console.WriteLine($"sizeof(decimal):{sizeof(decimal)},decimal.MaxValue:{decimal.MaxValue}");
+            Console.WriteLine($"sizeof(double):{sizeof(double)},double.MaxValue:{double.MaxValue}");             
+            Console.WriteLine($"sizeof(char):{sizeof(char)}");
+            Console.WriteLine($"sizeof(bool):{sizeof(bool)}");
+           
             Console.ReadLine();
         }
         static void Add(object obj)
@@ -49,6 +52,23 @@ namespace ConsoleApp332
             string msg = $"i {i},Guid:{Guid.NewGuid()}";
             cts.Cancel();
             return msg;
+        }
+
+        static long ReturnInt()
+        {
+            cts.Cancel();
+            return Int64.MaxValue;
+        }
+
+        static decimal ReturnDecimal()
+        {
+            return Decimal.MaxValue;
+        }
+
+        static double ReturnDouble()
+        {
+             
+            return double.MaxValue;
         }
     }
 
