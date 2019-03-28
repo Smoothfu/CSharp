b4c95bb195eca735102bfdefd829375a38fafc8f
Fred1987 <2864326836@qq.com>
2019-03-28 18:52:16 +0800

    public static Task Run(Action action,CancellationToken cancellationToken); Queues the specified work to run on the thread pool and returns a System.Threading.Tasks.Task object that represents that work.A cancellation token allows the work to be cancelled

--- ConsoleApp332/Program.cs
+++ ConsoleApp332/Program.cs
@@ -9,14 +9,28 @@ namespace ConsoleApp332
 {
     class Program
     {
+        static int i = 0;
+        static CancellationTokenSource cts = new CancellationTokenSource();
         static void Main(string[] args)
         {
-            ThreadPool.QueueUserWorkItem(Add, null);
+            Task.Run((()=>
+            {
+                Add(i);
+            }),cts.Token);
+           
             Console.ReadLine();
         }
         static void Add(object obj)
         {
-            Console.WriteLine($"Guid:{Guid.NewGuid()}");
+            while (i < 100)
+            {
+                while(i==90)
+                {
+                    cts.Cancel();
+                }
+                Console.WriteLine($"Guid:{Guid.NewGuid()}, i {i}");
+                i++;
+            }            
         }
     }
 }
