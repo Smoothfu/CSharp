7ddb971cf141bf5390a4a623ced84926be957118
Fred1987 <2864326836@qq.com>
2019-03-28 18:30:49 +0800

    [SecuritySafeCritical] public static bool QueueUserWorkItem(WaitCallback callBack,object state);  Queueus a method for execution,and specifies an object containing data to be used by the method.The method executes when a thread pool thread becomes available

---
+++ ConsoleApp332/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp332
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(Add, null);
+            Console.ReadLine();
+        }
+        static void Add(object obj)
+        {
+            Console.WriteLine($"Guid:{Guid.NewGuid()}");
+        }
+    }
+}
