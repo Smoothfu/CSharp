0eac956b317612a35dc71748ea32dc57946dc6ee
Fred1987 <2864326836@qq.com>
2019-04-09 22:53:39 +0800

    [ReliabilityContract(Consistency.WillNotCorruptState,Cer.MayFail)] [SecurityCritical] public Mutex(bool initiallyOwned,string name,out bool createdNew); Initializes a new instance of the System.Threading.Mutex class with a Boolean value that indicates whether the calling thread should have initial ownership of the mutex,a string that is the name of the mutex,and a Boolean value that,when the method returns,indicates whether the calling thread was granted initial ownership of the mutex

---
+++ ConsoleApp336/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ConsoleApp336
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dtNow = $"Now is {DateTime.Now.ToString("yyyyMMddHHmmssffff")}";
+            Console.WriteLine(dtNow);
+            bool isCreatedNew;
+            Mutex mutex = new Mutex(true, "ConsoleApp335", out isCreatedNew);
+            if (!isCreatedNew)
+            {
+                MessageBox.Show("ConsoleApp335.exe is always running!");
+                return;
+            }
+            Console.ReadLine();
+        }
+    }
+}
