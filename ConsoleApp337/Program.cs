0e5e59f167d1795cd4532606c332cf5d7d7e77e2
Fred1987 <2864326836@qq.com>
2019-04-10 18:50:18 +0800

    overload optimize

---
+++ ConsoleApp337/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp337
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add(0);
+            Add(1, 2);
+            Add(1, 2, 3);
+            Add(1, 2, 3, 4); 
+            Console.ReadLine();
+        }
+
+        static void Add(params int[]arr)
+        {
+            Console.WriteLine("params int[]arr");
+        }
+        static void Add(int x,int y)
+        {
+            Console.WriteLine("X&Y");
+        }
+    }
+}
