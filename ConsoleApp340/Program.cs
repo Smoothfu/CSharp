3eac94d647ff7871ff38466253f869adf59b25d5
Fred1987 <2864326836@qq.com>
2019-04-16 23:37:56 +0800

    compare the performance between Task.Factory.StartNew(()=>{}) and Task.Run(()=>{});

--- ConsoleApp340/Program.cs
+++ ConsoleApp340/Program.cs
@@ -3,90 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace ConsoleApp340
 {
     class Program
     {
+        static string logFullName = Directory.GetCurrentDirectory() + "//" + DateTime.Now.ToString("yyyyMMddHH") + ".txt";
+        static int runTimes;
+        static string runMsg;
+        static int secondsSpan;
         static void Main(string[] args)
         {
-            ClassTest();
-            StructTest();
-            Console.ReadLine();
-        } 
-
-        static void ClassTest()
-        {
-            Console.WriteLine("Class Test:");
-            Student stuA = new Student(1,"Fred1");
-            Student stuB = stuA;
-            Console.WriteLine($"stuA:{stuA.GetHashCode()},stuA.Id:{stuA.Id},stuA.Name:{stuA.Name}");
-            Console.WriteLine($"stuB:{stuB.GetHashCode()},stuB.Id:{stuB.Id},stuB.Name:{stuB.Name}");
-            stuA.Id = 3;
-            stuA.Name = "Fred2";
-            Console.WriteLine($"stuA:{stuA.GetHashCode()},stuA.Id:{stuA.Id},stuA.Name:{stuA.Name}");
-            Console.WriteLine($"stuB:{stuB.GetHashCode()},stuB.Id:{stuB.Id},stuB.Name:{stuB.Name}");
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
+            secondsSpan = 100;
+            //TaskFactoryStartNew();
+            TaskRun();
         }
 
-        static void StructTest()
+        static void TaskRun()
         {
-            Console.WriteLine("Struct Test:");
-            StudentStruct stuA = new StudentStruct(1, "Fred1");
-            StudentStruct stuB = stuA;
-            Console.WriteLine($"stuA:{stuA.GetHashCode()},stuA.Id:{stuA.Id},stuA.Name:{stuA.Name}");
-            Console.WriteLine($"stuB:{stuB.GetHashCode()},stuB.Id:{stuB.Id},stuB.Name:{stuB.Name}");
-            stuA.Id = 2;
-            stuA.Name = "Fred2";
-            Console.WriteLine($"stuA:{stuA.GetHashCode()},stuA.Id:{stuA.Id},stuA.Name:{stuA.Name}");
-            Console.WriteLine($"stuB:{stuB.GetHashCode()},stuB.Id:{stuB.Id},stuB.Name:{stuB.Name}");
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-        }
-
-        static void StringPoolStay()
-        {
-            string strA = "这是美丽的春天，希望的春天，生机勃勃的春天；这是美丽的春天，希望的春天，生机勃勃的春天";
-            string strB = strA;
-            int aHashcode = strA.GetHashCode();
-            Console.WriteLine($"strA:{strA},aHashcode:{aHashcode}");
-            int bHashCode = strB.GetHashCode();
-            Console.WriteLine($"strB:{strB},bHashCode:{bHashCode}");
-            if (aHashcode == bHashCode)
+            runTimes = 0;
+            DateTime dtNow = DateTime.Now;
+            DateTime dtEnd = dtNow.AddSeconds(secondsSpan);
+            Task runTask = Task.Run(() =>
             {
-                Console.WriteLine("Equal");
+                while (DateTime.Now < dtEnd)
+                {
+                    Debug.WriteLine($"{DateTime.Now.ToString("yyyyMMddHHmmssffff")},guid is {Guid.NewGuid()}");
+                    runTimes++;
+                }
+            });
+            runTask.Wait();
+            runMsg = $"In {secondsSpan} seconds, TaskRun() it run {runTimes} times";
+            LogMessage(runMsg);
+        }
+
+        static void TaskFactoryStartNew()
+        {
+            runTimes = 0;
+            DateTime dtNow = DateTime.Now;
+            DateTime dtEnd = dtNow.AddSeconds(secondsSpan);
+
+            Task taskFactory = Task.Factory.StartNew(() =>
+            {
+                while (DateTime.Now < dtEnd)
+                {
+                    Debug.WriteLine($"{DateTime.Now.ToString("yyyyMMddHHmmssffff")},guid is {Guid.NewGuid()}");
+                    runTimes++;
+                }
+            });
+            taskFactory.Wait();
+            runMsg = $"In {secondsSpan} seconds,Task.Factory.StartNew() it run {runTimes} times";
+            LogMessage(runMsg);
+        }     
+
+        static void LogMessage(string msg)
+        {
+            using (StreamWriter logStreamWriter = new StreamWriter(logFullName, true))
+            {
+                logStreamWriter.WriteLine(msg);
             }
-            strA = "Hello world,Everything depends on myself";
-            aHashcode = strA.GetHashCode();
-            Console.WriteLine($"strA:{strA},aHashcode:{aHashcode}");
-            Console.WriteLine($"strB:{strB},bHashCode:{bHashCode}");
         }
-    }
-
-    class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public Student(int id,string name)
-        {
-            Id = id;
-            Name = name;
-        }
-        
-    }
-
-    struct StudentStruct
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-        public StudentStruct(int id,string name)
-        {
-            Id = id;
-            Name = name;
-        }
-    }
+    }   
 }
