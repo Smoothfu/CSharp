bc968838d9ae2bfe0ffd775054320bf1b0030967
Fred1987 <2864326836@qq.com>
2019-05-07 19:46:48 +0800

    A hash table data structure is designed around an array.The array consists of elements 0 through some predetermined size,though we can increase the size later if necessary. Each data item is stored in the array based on some piece of the data,called the key.To store an element in the hash table,the key is mapped into a number in the range of 0 to the hash table size using a function called a hash function

---
+++ ConsoleApp344/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp344
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[99];
+            string name;
+            string[] someNames = new string[]
+            {
+                "David","Jennifer","Donnie","Mayo","Raymond","Bernica","Mike","Clayton","Beata","Michael"
+            };
+            int hashVal;
+            for(int i=0;i<10;i++)
+            {
+                name = someNames[i];
+                hashVal = SimpleHash(name, names);
+                names[hashVal] = name;
+            }
+            ShowDistrib(names);
+            Console.ReadLine();
+        }
+
+        static int SimpleHash(string str,string[] arr)
+        {
+            int tot = 0;
+            char[] cName;
+            cName = str.ToCharArray();
+            for(int i=0;i<cName.Length;i++)
+            {
+                tot += (int)cName[i];
+            }
+            return tot % arr.Length;
+        }
+
+        static void ShowDistrib(string[] arr)
+        {
+            for(int i=0;i<arr.Length;i++)
+            {
+                if(null!=arr[i])
+                {
+                    Console.WriteLine(i + "\t" + arr[i]);
+                }
+            }
+        }
+    }
+}
