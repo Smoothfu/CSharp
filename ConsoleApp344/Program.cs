9c00f425b206ec6faf15410302450b04b2790748
Fred1987 <2864326836@qq.com>
2019-05-07 20:09:53 +0800

    object.GetHashCode();[TargetedPatchingOptOut("Performance critical to inline across NGEN image boundaries. public virtual int GetHashCode();Serves as the default hash function

--- ConsoleApp344/Program.cs
+++ ConsoleApp344/Program.cs
@@ -10,6 +10,27 @@ namespace ConsoleApp344
     {
         static void Main(string[] args)
         {
+            HashCodeDistinct();
+            Console.ReadLine();
+        }
+
+        static void HashCodeDistinct()
+        {
+            List<int> hashCodeList = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < 10000; i++)
+            {
+                int temp = rnd.Next(0, Int32.MaxValue);
+                hashCodeList.Add(temp.GetHashCode());
+            }
+
+            var distinct = (from str in hashCodeList select str).Distinct();
+
+            Console.WriteLine($"The hashCodeList's count is {hashCodeList.Count}," +
+                $"distinct list count is {distinct.Count()}");
+        }
+        static void HashTableExample()
+        {
             string[] names = new string[99];
             string name;
             string[] someNames = new string[]
@@ -17,16 +38,14 @@ namespace ConsoleApp344
                 "David","Jennifer","Donnie","Mayo","Raymond","Bernica","Mike","Clayton","Beata","Michael"
             };
             int hashVal;
-            for(int i=0;i<10;i++)
+            for (int i = 0; i < 10; i++)
             {
                 name = someNames[i];
                 hashVal = SimpleHash(name, names);
                 names[hashVal] = name;
             }
             ShowDistrib(names);
-            Console.ReadLine();
         }
-
         static int SimpleHash(string str,string[] arr)
         {
             int tot = 0;
@@ -38,7 +57,6 @@ namespace ConsoleApp344
             }
             return tot % arr.Length;
         }
-
         static void ShowDistrib(string[] arr)
         {
             for(int i=0;i<arr.Length;i++)
