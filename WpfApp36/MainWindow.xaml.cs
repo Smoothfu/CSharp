96d758326643ab1da0c02112313132dd3d626245
Fred1987 <2864326836@qq.com>
2019-05-08 19:36:04 +0800

    public class BackgroundWorker:Component. Executes an operation on a separate thread

--- WpfApp36/MainWindow.xaml.cs
+++ WpfApp36/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,15 +25,77 @@ namespace WpfApp36
         public MainWindow()
         {
             InitializeComponent();
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(1000);
-            timer.Tick += Timer_Tick;
-            timer.Start();
+            
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
+        
+
+        private void btnDoSync_Click(object sender, RoutedEventArgs e)
         {
-            lblTime.Content = DateTime.Now.ToString("HHmmssffff");
+            int max = 10000;
+            pbProgress.Value = 0;
+            lbResults.Items.Clear();
+
+            int results = 0;
+            for (int i = 0; i < max; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    lbResults.Items.Add(i);
+                    results++;
+                }
+                System.Threading.Thread.Sleep(1);
+                pbProgress.Value = Convert.ToInt32(((double)i / max) * 100);
+            }
+            MessageBox.Show("Numbers between 0 and 10000 divisible by 7: " + results);
+        }
+
+        private void btnDoAsync_Click(object sender, RoutedEventArgs e)
+        {
+            pbProgress.Value = 0;
+            lbResults.Items.Clear();
+            BackgroundWorker bgWorker = new BackgroundWorker();
+            bgWorker.WorkerReportsProgress = true;
+            bgWorker.DoWork += BgWorker_DoWork;
+            bgWorker.ProgressChanged += BgWorker_ProgressChanged;
+            bgWorker.RunWorkerCompleted += BgWorker_RunWorkerCompleted;
+            bgWorker.RunWorkerAsync(10000);
+        }
+
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Numbers between 0 and 10000 divisible by 7: " + e.Result);
+        }
+
+        private void BgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbProgress.Value = e.ProgressPercentage;
+            if(e.UserState!=null)
+            {
+                lbResults.Items.Add(e.UserState);
+            }
+        }
+
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int max = (int)e.Argument;
+            int result = 0;
+            for(int i=0;i<max;i++)
+            {
+                int progressPercentage = Convert.ToInt32(((double)i / max) * 100);
+                if(i%7==0)
+                {
+                    result++;
+                    (sender as BackgroundWorker).ReportProgress(progressPercentage, i);
+                }
+                else
+                {
+                    (sender as BackgroundWorker).ReportProgress(progressPercentage);
+
+                }
+                System.Threading.Thread.Sleep(1);
+            }
+            e.Result = result;
         }
     }
 }
