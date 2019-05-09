596fc5786fc091fc12a6fdfc9f79e359682b9133
Fred1987 <2864326836@qq.com>
2019-05-09 22:02:10 +0800

    [SecurityCritical] public int Run(Window window); Starts a Windows Presentation Foundation (WPF) application and opens the specified window

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows; 
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Window win = new Window();
+            win.Title = "Start from Console";
+            win.Content = "Dare to do terrible things";
+            Application app = new MainApp();
+            app.Run(win);
+            Console.ReadLine();
+        }
+    }
+
+    public class MainApp:Application
+    {
+        public MainApp():base()
+        {
+
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            Console.WriteLine("OnActivated");
+            base.OnActivated(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Console.WriteLine("OnExit");
+            Console.WriteLine($"ApplicationExitCode:{e.ApplicationExitCode}");
+            this.Shutdown(0);
+            
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Console.WriteLine("OnStartup");
+            base.OnStartup(e);
+        }
+
+        protected override void OnDeactivated(EventArgs e)
+        {
+            Console.WriteLine("OnDeactivated");
+            base.OnDeactivated(e);
+        }
+
+        protected override void OnSessionEnding(SessionEndingCancelEventArgs e)
+        {
+            Console.WriteLine("OnSessionEnding");
+            base.OnSessionEnding(e);
+        }
+    }
+}
---
+++ WpfApp38/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfApp38
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App():base()
+        {
+
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            Console.WriteLine("OnActivated");
+            base.OnActivated(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Console.WriteLine("OnExit");
+            this.Shutdown();
+        }
+
+        protected override void OnDeactivated(EventArgs e)
+        {
+            Console.WriteLine("OnDeactivated");
+            base.OnDeactivated(e);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Console.WriteLine("OnStartup");
+            base.OnStartup(e);
+        }
+
+        protected override void OnSessionEnding(SessionEndingCancelEventArgs e)
+        {
+            Console.WriteLine("OnSessionEnding");
+            base.OnSessionEnding(e);
+        }
+    }
+}
