e674730d0c4b34b43e66d434d8e3e82e565cb4da
Fred1987 <2864326836@qq.com>
2019-05-09 23:09:21 +0800

    [ComVisible(true)] public class ArithmeticException:SystemException. The exception that is thrown for errors in an arithmetic,casting,or conversion operation

---
+++ ConsoleApp345/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp345
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DivideNumber();
+            Console.ReadLine();
+        }
+
+        static void DivideNumber()
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    for (int i = 10; i < 100; i++)
+                    {
+                        for (int j = 10; j >= 0; j--)
+                        {
+                            Console.WriteLine($"i/j={i / j}");
+                        }
+                    }
+
+                }
+
+               
+
+                catch (ArithmeticException ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(ex.Message);
+                }
+
+                catch (DivideByZeroException ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(ex.Message);
+                }
+            });
+        }
+    }
+}
