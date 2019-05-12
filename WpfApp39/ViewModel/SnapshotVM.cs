5c0b94f28b340bb112709b9fa0bf61d2decd9c3d
Fred1987 <2864326836@qq.com>
2019-05-12 18:52:17 +0800

    public selaed class Grapgics:MarshalByRefObject,IDisposable,IDeviceContext Encapsulates a GDI+ drawing surface. public static Graphics FromImageImage image  Creates a new System.Drawing.Graphics from the specified System.Drawing.Image.  returns:This method returns a new System.Drawing.Graphics for the specified System.Drawing.Image

---
+++ WpfApp39/ViewModel/SnapshotVM.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.IO; 
+
+
+namespace WpfApp39.ViewModel
+{
+   public class SnapshotVM : INotifyPropertyChanged
+    {
+        public SnapshotVM()
+        {
+            SnapShotCmd = new DelegateCommand(SnapShotCmdExecuted, SnapShotCmdCanExecute);
+        }
+
+        private bool SnapShotCmdCanExecute()
+        {
+            return true;
+        }
+
+        private void SnapShotCmdExecuted()
+        { 
+            Rectangle bounds = Screen.GetBounds(System.Drawing.Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(System.Drawing.Point.Empty, System.Drawing.Point.Empty, bounds.Size);
+                }
+
+                using (SaveFileDialog sfd = new SaveFileDialog())
+                {
+                    sfd.Title = "Save Pictures";
+                    sfd.RestoreDirectory = true;
+                    sfd.Filter = "bmp files(*.bmp)|*.bmp|All Files(*.*)|*.*";
+                    sfd.FileName = DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".jpg";
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        string screenShotsFullName = Directory.GetCurrentDirectory() + "\\" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".jpg";                       
+                        bitmap.Save(sfd.FileName, ImageFormat.Jpeg);
+                    }
+                }                
+            }            
+        }
+       
+        public DelegateCommand SnapShotCmd
+        {
+            get;set;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            if(PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
