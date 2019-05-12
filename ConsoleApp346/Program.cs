06dd9901fb9c51f3aec418c643e0a5477497f2c7
Fred1987 <2864326836@qq.com>
2019-05-12 14:36:18 +0800

    public sealed class Grapgics:MarshalByRefObject,IDisposable,IDeviceContext, Encapsulates a GDI+ drawing surface.This class can not be inherited

---
+++ ConsoleApp346/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ConsoleApp346
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            ScreenCapture();
+
+        }
+
+        static void ScreenCapture()
+        {
+            Rectangle bounds = Screen.GetBounds(System.Drawing.Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(System.Drawing.Point.Empty, System.Drawing.Point.Empty, bounds.Size);
+                }
+                string fullName = Directory.GetCurrentDirectory() + "\\" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".jpg";
+                bitmap.Save(fullName, ImageFormat.Jpeg);
+            }
+        }
+    }
+}
