80ad725b59e812c7a28f1071e4ff41bdcb71a1ab
Fred1987 <2864326836@qq.com>
2019-05-19 23:08:27 +0800

    [SecuritySafeCritical] public void SetApartmentState(ApartmentState state); Sets the apartment state of a thread before it is started

---
+++ ConsoleApp347/DAO/ExportTByNPOI.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.XSSF;
+using System.Reflection;
+using NPOI.XSSF.UserModel;
+using System.IO;
+using NPOI.SS.UserModel;
+using System.Windows.Forms;
+
+namespace ConsoleApp347.DAO
+{
+    public class ExportTByNPOI
+    {
+        [STAThread]
+        public static void ExportTDataByNPOI<T>(List<T> dataList,string fileName)
+        {
+            if(dataList!=null && dataList.Any())
+            {
+                var firstData = dataList.FirstOrDefault();
+                var props = firstData.GetType().GetProperties().Where(x => !x.GetMethod.IsVirtual).ToList();
+                XSSFWorkbook workbook = new XSSFWorkbook();
+                ISheet firstSheet = workbook.CreateSheet("First Sheet");
+                IRow firstRow = firstSheet.CreateRow(0);
+                for(int i=0;i<props.Count;i++)
+                {
+                    ICell headerCell = firstRow.CreateCell(i);
+                    if(!string.IsNullOrEmpty(props[i].Name))
+                    {
+                        headerCell.SetCellValue(props[i].Name);
+                    }
+                }
+                for(int i=1;i<=dataList.Count;i++)
+                {
+                    IRow dataRow = firstSheet.CreateRow(i);
+                    for(int j=0;j<props.Count;j++)
+                    {
+                        ICell dataCell = dataRow.CreateCell(j);
+                        var dataCellValue = props[j].GetValue(dataList[i - 1]);
+                        if(dataCellValue!=null)
+                        {
+                            dataCell.SetCellValue(dataCellValue.ToString());
+                        }
+                    }
+                }
+
+                using (FileStream excelStream = File.Open(fileName,FileMode.OpenOrCreate))
+                {
+                    workbook.Write(excelStream);
+                }
+            }
+
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "Excel Files| *.xls; *.xlsx; *.xlsm|All files|*.*";
+                sfd.FileName = fileName;
+                sfd.OverwritePrompt = false;
+               
+                if (sfd.ShowDialog()==DialogResult.OK)
+                {
+                    sfd.FileName = fileName;
+                }
+            }
+
+        }
+    }
+}
--- ConsoleApp347/Program.cs
+++ ConsoleApp347/Program.cs
@@ -6,24 +6,29 @@ using System.Threading.Tasks;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using ConsoleApp347;
+using ConsoleApp347.DAO;
+using System.IO;
+using System.Threading;
 
 namespace ConsoleApp347
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
-        {          
-
-            Task task = new Task(() =>
+        {
+            string fileName = Directory.GetCurrentDirectory() + "\\" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".xlsx";
+            Thread exportThread = new Thread(() =>
               {
-                  ADO();
+                  using (AdventureWorks2017Entities db = new AdventureWorks2017Entities())
+                  {
+                      var dataList = db.SalesOrderDetails.ToList(); 
+                      ExportTByNPOI.ExportTDataByNPOI<SalesOrderDetail>(dataList, fileName);
+                  }
               });
-            Console.WriteLine($"task.Status:{task.Status}");
-            task.Start();
-            Console.WriteLine($"task.Status:{task.Status}");
-            task.Wait();
-            Console.WriteLine($"task.Status:{task.Status}");
-            Console.ReadLine();
+            exportThread.SetApartmentState(ApartmentState.STA);
+            exportThread.Start();
         }
 
         static void ADO()
