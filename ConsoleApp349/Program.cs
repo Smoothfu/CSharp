e8a731f06c67ff2a4a499332fc8b447bb2d3d8c0
Fred1987 <2864326836@qq.com>
2019-05-28 00:47:06 +0800

    one wcf project add multiple svc service files

---
+++ ConsoleApp349/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp349.MathServiceWCF;
+using ConsoleApp349.ToUpperServiceWCF;
+
+namespace ConsoleApp349
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 20;
+            MathServiceWCFClient mathClient = new MathServiceWCFClient();
+            int result = mathClient.Add(10, 20);
+            Console.WriteLine($"{x}+{y}={x + y}");
+
+            string str = "this is small case";
+            ToUpperServiceClient upperServiceWCFClient = new ToUpperServiceClient();
+            string upperCase = upperServiceWCFClient.StringToUpper(str);
+            Console.WriteLine($"small case:{str},upper case:{upperCase}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathService/MathServiceWCF.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text; 
+
+namespace MathService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MathServiceWCF" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MathServiceWCF.svc or MathServiceWCF.svc.cs at the Solution Explorer and start debugging.
+    public class MathServiceWCF : IMathServiceWCF
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
