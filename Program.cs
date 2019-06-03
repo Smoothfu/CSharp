e8122faf2bbd6f0e1394aff26b9cfb2dc6dee1bb
Fred1987 <2864326836@qq.com>
2019-06-03 18:09:04 +0800

    Delegate BeginInvoke,the delegate will be called asynchronously,callback and IAsyncResult result

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp351
+{
+    delegate void MyDel();
+
+    delegate void AddDel (int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100, y = 1000000;
+            AddDel addDel = new AddDel(AddMethod);
+            addDel.BeginInvoke(x, y, AddCallback, "This the begin invoke last parameter");
+            Console.ReadLine();
+        }
+
+        private static void AddCallback(IAsyncResult ar)
+        {
+            var iasynResult = ar.AsyncState;
+            Console.WriteLine(iasynResult.ToString());
+        }
+
+        static void AddMethod(int x,int y)
+        {
+            Console.WriteLine($"{x}+{y}={x + y}");
+        }
+        private static void DelCallback(IAsyncResult ar)
+        {
+            Console.WriteLine(ar.AsyncState);
+        }
+
+        static void MyMethod()
+        {
+            Console.WriteLine($"Now is {DateTime.Now.ToString("yyyyMMddHHMMssffff")};");
+        }
+    }
+}
