4b768d184cb0a46a76f86b410d6809c6affc2317
Fred1987 <2864326836@qq.com>
2019-06-04 13:26:56 +0800

    [ComVisible(true)] public interface IAsyncResult, represents the status of an asynchronous operation

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp352
+{
+    class Program
+    {
+        delegate void AddDel(int x, int y);
+        static void Main(string[] args)
+        {
+            int x = 345667456, y = 456567456;
+            AddDel addDel = new AddDel(AddMethod);
+            addDel.BeginInvoke(x, y, AddDelCallBack, "This is the AddDel BeginInvoke async state");
+            Console.ReadLine();
+        }
+
+        private static void AddDelCallBack(IAsyncResult ar)
+        {
+            var result = ar.ToString();
+            Console.WriteLine(result);
+            Console.WriteLine(ar.AsyncState);
+        }
+
+        static void AddMethod(int x,int y)
+        {
+            Console.WriteLine($"{x}+{y}={x + y}");
+        }
+    }
+}
