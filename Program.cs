4c78fc333a6f314f53881238ddd5a7cabf41a0d5
Fred1987 <2864326836@qq.com>
2019-06-08 21:01:01 +0800

    public void ThrowIfCancellationRequested();  Throws a System.OperationCanceledException if this token has had cancellation requested

---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp355
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> intList = Enumerable.Range(1,1000000000);
+            CancellationTokenSource cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(10000));
+            CancellationToken ctsToken = cts.Token;
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    foreach (int i in intList)
+                    {
+                        if (i % 10000000 == 0)
+                        {
+                            Console.WriteLine(i);
+                            if (i % 100000000 == 0)
+                            {
+                                cts.Cancel();
+                                ctsToken.ThrowIfCancellationRequested();
+                                break;
+                            }
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    cts.Dispose();
+                }                
+            }, ctsToken);
+            Console.ReadLine();
+        }
+
+        static void TaskCancellationTokenSource()
+        {
+            //Define the cancellation token.
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken ctsToken = cts.Token;
+
+            Random rnd = new Random();
+            object lockObj = new object();
+
+            List<Task<int[]>> tasks = new List<Task<int[]>>();
+            TaskFactory factory = new TaskFactory(ctsToken);
+
+            for (int taskCtr = 0; taskCtr <= 10; taskCtr++)
+            {
+                int iteration = taskCtr + 1;
+                tasks.Add(factory.StartNew(() =>
+                {
+                    int value;
+                    int[] values = new int[10];
+                    for (int ctr = 1; ctr <= 10; ctr++)
+                    {
+                        lock (lockObj)
+                        {
+                            value = rnd.Next(0, 101);
+                        }
+                        if (value == 0)
+                        {
+                            cts.Cancel();
+                            Console.WriteLine($"Cancelling at task{iteration}");
+                            break;
+                        }
+                        values[ctr - 1] = value;
+                    }
+                    return values;
+                }, ctsToken));
+            }
+
+            try
+            {
+                Task<double> fTask = factory.ContinueWhenAll(tasks.ToArray(),
+                    (results) =>
+                    {
+                        Console.WriteLine("Calculating overall mean...");
+                        long sum = 0;
+                        int n = 0;
+                        foreach (var t in results)
+                        {
+                            foreach (var r in t.Result)
+                            {
+                                sum += r;
+                                n++;
+                            }
+                        }
+                        return sum / (double)n;
+                    }, ctsToken);
+                Console.WriteLine($"The mean is {fTask.Result}");
+            }
+            catch (AggregateException ae)
+            {
+                foreach (Exception ex in ae.InnerExceptions)
+                {
+                    if (ex is TaskCanceledException)
+                    {
+                        Console.WriteLine($"Unable to compute mean:{((TaskCanceledException)ex).Message}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Exception :{ex.GetType().Name}");
+                    }
+                }
+            }
+
+            finally
+            {
+                cts.Dispose();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp355")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp355")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b58d7f2d-9c9d-433d-9ebe-619a26424648")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
