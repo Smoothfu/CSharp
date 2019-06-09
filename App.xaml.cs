3265ea6f707359ed13477a7766b845217cc360e8
Fred1987 <2864326836@qq.com>
2019-06-09 18:49:24 +0800

    ResizeMode="NoResize"

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp45
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string text = "Hello,default!";
+            if(e.Args.Length>0)
+            {
+                text = e.Args[0];
+            }
+            var win = new DetailsDialog();
+            win.Show();
+        }
+    }
+}
