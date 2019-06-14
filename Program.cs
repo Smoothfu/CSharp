812d7d5f66b7594dc83ad33bf545aee5cce6ab65
Fred1987 <2864326836@qq.com>
2019-06-14 20:36:39 +0800

    delegate instance BeginInvoke(x,y,callback,object state)

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp361
+{
+    class Program
+    {
+        delegate string MathDel(int x, int y);
+
+        static void Main(string[] args)
+        {
+            MathDel addDel = Add;
+            IAsyncResult asyncResult = addDel.BeginInvoke(10, 10, AddCallBack, "This parameter can be omitted");
+            string addResult = addDel.EndInvoke(asyncResult);
+            Console.WriteLine(addResult);
+            Console.ReadLine();
+        }
+
+        private static void AddCallBack(IAsyncResult ar)
+        {
+            Console.WriteLine(ar.AsyncState);
+        }
+
+        static string  Add(int x,int y)
+        {
+            return string.Format($"The sum of {x} and {y} is {x + y}");
+        }
+    }
+}
