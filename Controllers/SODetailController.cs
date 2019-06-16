1f85425ed12dabb62842837a4b626dbeac8957cb
Fred1987 <2864326836@qq.com>
2019-06-16 20:15:10 +0800

    http://localhost:10944/api/sodetail?rowguid=B207C96D-D9E6-402B-8470-2CC176C42283

--- Controllers/SODetailController.cs
+++ Controllers/SODetailController.cs
@@ -6,43 +6,71 @@ using System.Net.Http;
 using System.Web.Http;
 using WebApplication14.Models;
 
+
 namespace WebApplication14.Controllers
 {
     public class SODetailController : ApiController
     {
         private List<SalesOrderDetail> orderList = new List<SalesOrderDetail>();
         private List<SalesOrderDetail> topOrderList = new List<SalesOrderDetail>();
+        private List<SalesDetailClass> salesDetailList = new List<SalesDetailClass>();
         public SODetailController()
         {
             using (SODDB db = new SODDB())
             {
-                orderList = db.SalesOrderDetails.ToList();
-                topOrderList = orderList.Take(10).ToList();
+                orderList = db.SalesOrderDetails.ToList().Take(10000).ToList();
+                for(int i=0;i< orderList.Count; i++)
+                {
+                    salesDetailList.Add(new SalesDetailClass()
+                    {
+                        SalesOrderDetailID = orderList[i].SalesOrderDetailID
+                    });
+                }
             }
         }
 
-        public List<SalesOrderDetail> GetList()
+        //public List<SalesOrderDetail> GetList()
+        //{
+        //    return topOrderList;
+        //}
+
+        public List<SalesDetailClass> GetSalesDetailList()
         {
-            return topOrderList;
+            return salesDetailList;
         }
 
-        [HttpGet]
-        public SalesOrderDetail GetOrderBySODID(int soiID)
+        
+        public SalesDetailClass GetSalesDetailClass(int sodDetailId)
         {
             try
             {
-                var sod = orderList.First(x => x.SalesOrderDetailID == soiID);
-                if(sod!=null)
-                {
-                    return sod;
-                }               
+                var sodDetail = salesDetailList.First(x => x.SalesOrderDetailID == sodDetailId);
+                return sodDetail;
             }
             catch(Exception ex)
             {
                 System.Diagnostics.Debug.WriteLine(ex.Message);
             }
-
             return null;
         }
+
+        //[HttpGet]
+        //public SalesOrderDetail GetOrderBySODID(int soiID)
+        //{
+        //    try
+        //    {
+        //        var sod = orderList.First(x => x.SalesOrderDetailID == soiID);
+        //        if(sod!=null)
+        //        {
+        //            return sod;
+        //        }               
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        System.Diagnostics.Debug.WriteLine(ex.Message);
+        //    }
+
+        //    return null;
+        //}
     }
 }
