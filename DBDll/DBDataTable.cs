30cc9d22f56dd303df8dc777d17a2538d8eb721d
Fred1987 <2864326836@qq.com>
2019-08-29 19:36:31 +0800

    public static Configuration OpenExeConfiguration(string execPath); Opens the specified client configuration file as a System.Configuration.Configuration object

---
+++ DBDll/DBDataTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace DBDll
+{
+    public class DBDataTable
+    {
+        public DataTable GetDataTable()
+        {
+            Configuration myDllConfig =
+            ConfigurationManager.OpenExeConfiguration(this.GetType().Assembly.Location);
+            // Get the appSettings section
+            AppSettingsSection myDllConfigAppSettings =
+            (AppSettingsSection)myDllConfig.GetSection("appSettings");
+
+            string connString = myDllConfigAppSettings.Settings["SQLConnString"].Value;
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if(conn.State!=ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+                string selectSQL = "select * from dbo.sod2";
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter(selectSQL, conn))
+                {
+                    DataSet ds = new DataSet();
+                    dataAdapter.Fill(ds);
+                    if (ds.Tables.Count > 0)
+                    {
+                        dt = ds.Tables[0];
+                    }
+                }
+            }
+            return dt;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using DBDll;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp369
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = GetDT();
+            PrintDataTable(dt);
+            Console.ReadLine();
+        }
+
+        static DataTable GetDT()
+        {
+            DBDataTable obj = new DBDataTable();
+            DataTable dt = obj.GetDataTable();
+            return dt;
+        }
+
+        static void PrintDataTable(DataTable dt)
+        {
+            if(dt==null || dt.Rows.Count==0)
+            {
+                return;
+            }
+
+            for(int i=0;i<dt.Rows.Count;i++)
+            {
+                for(int j=0;j<dt.Columns.Count;j++)
+                {
+                    Console.Write($"{dt.Rows[i][j]?.ToString() + "\t"}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
