e162ea80b46d5fb43b9746488e3c981ab905c5e6
Fred1987 <2864326836@qq.com>
2019-08-30 13:32:48 +0800

    a

--- DBDll/Dll/SQLTransactionLevelDemo.cs
+++ DBDll/Dll/SQLTransactionLevelDemo.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Dapper;
-using Dapper.Contrib;
 
 namespace DBDll.Dll
 {
@@ -31,8 +25,7 @@ namespace DBDll.Dll
                     catch(Exception ex)
                     {
                         trans.Rollback();
-                    }
-                    
+                    }                    
                 }
             }
         }
