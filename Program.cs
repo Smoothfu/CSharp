15d92cc4988b9949f35b192100b52c7aed32781b
Fred1987 <2864326836@qq.com>
2019-09-01 19:30:03 +0800

    redis var conn=ConnectionMultiplexer("localhost")

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace ConsoleApp370
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisDemo();
+        }
+
+        static void RedisDemo()
+        {
+            using (var conn = ConnectionMultiplexer.Connect("localhost"))
+            {
+                IDatabase db = conn.GetDatabase(2);
+                for(int i=0;i<100000;i++)
+                { 
+                    string redisKey = Guid.NewGuid().ToString();                    
+                    db.StringSet(redisKey, redisKey);
+                }   
+            }
+        }
+    }
+}
