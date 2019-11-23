2e72588c279188d7275d27aea3b3126f9da1cf54
unknown <2864326836@qq.com>
2019-11-23 21:23:58 +0800

    WebClient DownloadProgressChanged and DownloadFileCompleted

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
 
 namespace ConsoleApp392
 {
@@ -11,10 +14,55 @@ namespace ConsoleApp392
     {
         static void Main(string[] args)
         {
+            WebClientDownloadDemo();
+            Console.ReadLine();
+        }
+
+        static void WebClientDownloadDemo()
+        {
+            string url = "https://go.microsoft.com/fwlink/?linkid=866662";
+            WebClient webClient = new WebClient();
+            webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
+            webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+            Console.WriteLine($"Started at {DateTime.Now.ToString("yyyyMMddHHmmssffff")}"); 
+            webClient.DownloadFileAsync(new Uri(url), "SSMS2019.exe");
+        }
+
+        private static void WebClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Completed!");
+        }
+
+        private static void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Debug.WriteLine($"{e.UserState?.ToString()}    downloaded {e.BytesReceived} of {e.TotalBytesToReceive} bytes. {e.ProgressPercentage} % complete...");       
+            Console.WriteLine($"{e.UserState?.ToString()}    downloaded {e.BytesReceived} of {e.TotalBytesToReceive} bytes. {e.ProgressPercentage} % complete...");
+        }
+
+        static void DownloadByWebRequest()
+        {
+            string url = "https://go.microsoft.com/fwlink/?linkid=2108895&amp;clcid=0x409";
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                using (Stream fileStream = File.OpenWrite("ssms.exe"))
+                {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead = responseStream.Read(buffer, 0, 4096);
+                    while (bytesRead > 0)
+                    {
+                        fileStream.Write(buffer, 0, bytesRead);
+                        bytesRead = responseStream.Read(buffer, 0, 4096);
+                    }
+                }
+            }
+        }
+        static void EventDemo()
+        {
             Adult adult = new Adult(18);
             adult.AdultEvent += Adult_AdultEvent;
             adult.Age = 20;
-            Console.ReadLine();
         }
 
         private static void Adult_AdultEvent(object sender, AdultArgs e)
@@ -31,48 +79,7 @@ namespace ConsoleApp392
             }
             Console.WriteLine($"The newly updated age is {newAge} and it's {msg} ");
         }
-    }     
-
-    public class DemoArgs
-    {
-        public DateTime DemoDT { get; set; }
-        public DemoArgs(DateTime dt)
-        {
-            DemoDT = dt;
-        }
-    }
-
-    public class EventDemo
-    {
-        public event EventHandler<DemoArgs> DemoEvent;
-
-        private DateTime dtValue;
-        public DateTime Dt
-        {
-            get
-            {
-                return dtValue;
-            }
-            set
-            {
-                if(value!=dtValue)
-                {
-                    dtValue = value;
-                    RaisePropertyChanged(value);
-                }
-            }
-        }
-
-        private void RaisePropertyChanged(DateTime value)
-        {
-            DemoEvent?.Invoke(this, new DemoArgs(value));
-        }
-
-        public EventDemo(DateTime eventDT)
-        {
-            Dt = eventDT;
-        }
-    }
+    }        
 
     public class AdultArgs
     {
