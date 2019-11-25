f846278136565afefd1d04a76c8dfc1cab71d221
unknown <2864326836@qq.com>
2019-11-25 13:23:51 +0800

    Socket public Socket(AddressFamily addressFamily,SocketType socketType,ProtocolType protocolType);  Initializes a new instance of the System.Net.Sockets.Socket class using the specified address family,socket type and protocol

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace ConsoleApp396
+{
+    class Program
+    {
+        private static Socket ConnectSocket(string server,int port)
+        {
+            Socket s = null;
+            IPHostEntry hostEntry = null;
+
+            //Get host related information
+            hostEntry = Dns.GetHostEntry(server);
+
+            foreach(IPAddress address in hostEntry.AddressList)
+            {
+                IPEndPoint ipe = new IPEndPoint(address, port);
+                Socket tempSocket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                tempSocket.Connect(ipe);
+
+                if (tempSocket.Connected)
+                {
+                    s = tempSocket;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return s;
+        }       
+        private static string SocketSendReceive(string server,int port)
+        {
+            string request = "GET / HTTP/1.1\r\nHost: " + server + "\r\nConnection:Close\r\n\r\n";
+            byte[] bytesSent = Encoding.ASCII.GetBytes(request);
+            byte[] bytesReceived = new byte[256];
+            string page = "";
+
+            using(Socket s = ConnectSocket(server, port))
+            {
+                if(s==null)
+                {
+                    return ("Connection failed!");
+                }
+
+                s.Send(bytesSent, bytesSent.Length, 0);
+                int bytes = 0;
+                page = "Default HTML page on " + server + ":\r\n";
+                do
+                {
+                    bytes = s.Receive(bytesReceived, bytesReceived.Length, 0);
+                    page = page + Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+                } while (bytes > 0);
+            }
+            return page;
+        }
+        public static void Main(string[] args)
+        {
+            string host;
+            int port = 80;
+            if(args.Length==0)
+            {
+                host = Dns.GetHostName();
+            }
+            else
+            {
+                host = args[0];
+            }
+
+            string result = SocketSendReceive(host, port);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp396")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp396")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6c9a00d4-b7a8-47ed-8705-36f96d891e9c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
