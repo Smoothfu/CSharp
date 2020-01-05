68f10cb756dd9c2babf9e979ecec2bccb0069ba8
unknown <2864326836@qq.com>
2020-01-05 17:16:59 +0800

    log4net log logs files and will generated subfiles automatically

--- Program.cs
+++ Program.cs
@@ -16,17 +16,31 @@ namespace ConsoleApp409
         private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
-            Log4netDemo();
-            Console.ReadLine();
+            for (int i = 0; i < 100000; i++)
+            {
+                Log4netDemo();
+            }
         }
 
         static void Log4netDemo()
         {
+            StringBuilder logBuilder = new StringBuilder();
+            logBuilder.AppendLine($"Info.{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
+            logBuilder.AppendLine($"Info.{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
+            logBuilder.AppendLine($"Debug.{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
+            logBuilder.AppendLine($"Error.{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
+            logBuilder.AppendLine($"Fatal.{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
+            logBuilder.AppendLine($"Warn.{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
             logger.Info($"Info.{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
             logger.Debug($"Debug.{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
             logger.Error($"Error.{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
             logger.Fatal($"Fatal.{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
             logger.Warn($"Warn.{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
+            Console.WriteLine("\n\n");
+            using(StreamWriter logWriter=new StreamWriter("Log4netWriter.txt",true,Encoding.UTF8))
+            {
+                logWriter.WriteLine(logBuilder.ToString());
+            }
         }
         static void ThreadDemo()
         {
@@ -41,19 +55,17 @@ namespace ConsoleApp409
             timerThread.Join();
             Console.WriteLine($"New thread finished.{Thread.CurrentThread.ManagedThreadId}");
         }
-
         private static void ThreadTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
             Console.WriteLine(e.SignalTime);
         }
-
         static void NewtonDemo()
         {
-            using(AdventureWorks2017Entities db=new AdventureWorks2017Entities())
+            using (AdventureWorks2017Entities db = new AdventureWorks2017Entities())
             {
                 var dbSales = db.SalesOrderDetails.ToList();
-                string newtonString = JsonConvert.SerializeObject(dbSales,Formatting.Indented);
-                using(StreamWriter stringWriter=new StreamWriter("JsonFile.Json",true,Encoding.UTF8))
+                string newtonString = JsonConvert.SerializeObject(dbSales, Formatting.Indented);
+                using (StreamWriter stringWriter = new StreamWriter("JsonFile.Json", true, Encoding.UTF8))
                 {
                     stringWriter.WriteLine(newtonString);
                 }
