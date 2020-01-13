8462391e64214fef5cf856a33b0111032dc630f9
unknown <2864326836@qq.com>
2020-01-13 21:39:07 +0800

    <file type="log4net.Util.PatternString" value="Logs\\Common\\%date{yyyy-MM-dd-HH-mm-ss-ffff}.log"/>

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using log4net;
+
+namespace ConsoleApp410
+{
+    class Program
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+         
+        static void Main(string[] args)
+        {
+            for(int i=0;i<1000000;i++)
+            {
+                Log4netDemo();
+            }
+            Console.ReadLine();
+        }
+
+        static void Log4netDemo()
+        {
+            logger.Info($"{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
+            logger.Error($"{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
+            logger.Debug($"{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
+            logger.Fatal($"{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
+            logger.Warn($"{DateTime.Now.ToString("yyyyMMddHHmmssffff")}");
+        }
+    }
+}
