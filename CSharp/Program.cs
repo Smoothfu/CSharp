6525ac669724b442a6eb866bdb4a4901835f097a
smoothfu <2864326836@qq.com>
2020-09-08 22:29:34 +0800

    restart

---
+++ CSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Delegates.Dels;
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelDemo();
+            Console.ReadLine();
+        }
+
+        static void DelDemo()
+        {
+            DelDemo demo = new DelDemo();
+            PrintMsgDel printDel = demo.PrintTime;
+            printDel(null);
+        }
+    }
+}
---
+++ Delegates/Dels/DelDemo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.Dels
+{
+    public delegate void PrintMsgDel(object obj);
+    public class DelDemo
+    {
+        public void PrintTime(object obj)
+        {
+            Console.WriteLine(DateTime.UtcNow.Ticks);
+        }
+    }
+}
